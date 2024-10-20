74cbb43ec2fba4813d8a250f7c72df984b941a86
TvoyBatyA1234 <maxdem06@gmail.com>
2024-10-20 19:13:20 +0300

    upgrade output

--- demehin.maxim/P2/output.cpp
+++ demehin.maxim/P2/output.cpp
@@ -1,17 +1,23 @@
 #include "taylor.h"
 #include <iostream>
 #include <stdexcept>
+#include <iomanip>
 
 void demehin::output(double x, const size_t k, const double error)
 {
+  const size_t column_width = 15;
+  size_t c = column_width;
+  std::cout << std::setw(c);
   std::cout << x << " ";
   try
   {
+    std::cout << std::setw(c);
     std::cout << demehin::f(x, k, error);
   }
   catch(const std::logic_error & e)
   {
     std::cout << "<MATH ERROR>";
   }
+  std::cout << std::setw(c);
   std::cout << " " << demehin::stdf(x) << "\n";
 }
--- demehin.maxim/P2/output.h
+++ demehin.maxim/P2/output.h
@@ -1,5 +1,1 @@
-#ifndef OUTPUT_H
-#define OUTPUT_H
-#include <cstddef>
-void output(double left, double right, size_t k);
-#endif
+
