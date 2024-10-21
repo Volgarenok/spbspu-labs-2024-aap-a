a3ed510a24617e613318ff038ffd0c6fd32c4200
ysffmn <andrewnikonov387@gmail.com>
2024-10-22 02:46:17 +0300

    ver 2.0 with added some input check

--- nikonov.andrew/P2/main.cpp
+++ nikonov.andrew/P2/main.cpp
@@ -5,7 +5,7 @@ int main()
   double left = 0.0, right = 0.0;
   size_t numberMax = 0;
   std::cin >> left >> right >> numberMax;
-  if (!std::cin || right > 1 || left < -1)
+  if (!std::cin || right > 1 || left < -1 || left > right)
   {
     std::cerr << "ERROR: incorrect input" << "\n";
     return 1;
