fb5f833856ccee861ec126a9aede8852b9ea2d62
ysffmn <andrewnikonov387@gmail.com>
2024-10-22 02:05:31 +0300

    ver 1.0 on test

---
+++ nikonov.andrew/P2/funcs.cpp
@@ -0,0 +1,53 @@
+#include "funcs.hpp"
+double nikonov::cos(double x, size_t numberMax, double absError)
+{
+    double next = (x * x) / 2;
+    double result = 1 - next;
+    for (size_t i = 1; i < numberMax; i++)
+    {
+        next *= x*x/((2*i + 1)*(2*i + 2));
+        if (i % 2 == 0)
+        {
+            result += next;
+        }
+        else
+        {
+            result -= next;
+        }
+    }
+    if (std::abs(next) > absError)
+    {
+        throw std::logic_error("<MATH ERROR>");
+    }
+    return result;
+}
+double nikonov::stdcos(double x)
+{
+    return std::cos(x);
+}
+void nikonov::stringOupout(double x, double val, double stdval)
+{
+    const char* errmsg = "<MATH ERROR>";
+    const size_t snd_col_width = std::strlen(errmsg);
+    const size_t oth_col_width = 10;
+
+    std::cout << std::setw(oth_col_width) << x;
+    std::cout << " ";
+    std::cout << std::setw(snd_col_width) << val;
+    std::cout << " ";
+    std::cout << std::setw(oth_col_width) << stdval;
+    std::cout << "\n";
+}
+void nikonov::stringOupout(double x, const std::logic_error& e, double stdval)
+{
+    const char* errmsg = "<MATH ERROR>";
+    const size_t snd_col_width = std::strlen(errmsg);
+    const size_t oth_col_width = 10;
+    
+    std::cout << std::setw(oth_col_width) << x;
+    std::cout << " ";
+    std::cout << std::setw(snd_col_width) << e.what();
+    std::cout << " ";
+    std::cout << std::setw(oth_col_width) << stdval;
+    std::cout << "\n";
+}
---
+++ nikonov.andrew/P2/funcs.hpp
@@ -0,0 +1,16 @@
+#ifndef FUNCS_HPP
+#define FUNCS_HPP
+#include <cstddef>
+#include <cmath>
+#include <iostream>
+#include <stdexcept>
+#include <iomanip>
+#include <cstring>
+namespace nikonov
+{
+    double cos(double x, size_t numberMax, double absError);
+    double stdcos(double x);
+    void stringOupout(double x, double val1, double stdval);
+    void stringOupout(double x, const std::logic_error& e, double stdval);
+}
+#endif
