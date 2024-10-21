d8eef6515ba8c49435bca5c15f82aa3e37589ced
evstyunichev.konstantin <korombodis.gmail.com>
2024-10-21 17:19:03 +0300

    again CG

--- evstyunichev.konstantin/P2/f.cpp
+++ evstyunichev.konstantin/P2/f.cpp
@@ -4,12 +4,12 @@ double kos::f(double x, size_t k, double error)
 {
 	const char *errmsg = "<MATH ERROR>";
 	double last = x, ans = x;
-	for(size_t i = 1; i < k; i++)
+	for (size_t i = 1; i < k; i++)
 	{
 		last = last * x * x * (i * 2 - 1) * (i * 2 - 1) / (i * 2 + 0.0) / (i * 2 + 1.0);
 		ans += last;
 	}
-	if(std::abs(last) > error)
+	if (std::abs(last) > error)
 	{
 		throw std::logic_error(errmsg);
 	}
@@ -25,7 +25,7 @@ void kos::output(double x, size_t k, double error)
 {
 	const size_t field_size = 12;
 	std::cout << std::setw(field_size) << x << "    ";
-	try 
+	try
 	{
 		std::cout << std::setw(field_size) << kos::f(x, k, error);
 	}
