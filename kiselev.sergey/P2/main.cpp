9ac1ee08a8f3539d9a22135c93b3f579b9a04de9
deonik3 <kis.sergey2006@gmail.com>
2024-10-21 18:52:36 +0300

    Checking the input parameters

--- kiselev.sergey/P2/main.cpp
+++ kiselev.sergey/P2/main.cpp
@@ -5,4 +5,10 @@ int main()
   double left = 0.0, right = 0.0;
   size_t k = 0;
   std::cin >> left >> right >> k;
+  if (std::cin.fail() || left < -0.5 || left >= 0.5 ||
+      right > 0.5 || right <= -0.5 || right < left || k > 10)
+  {
+    std::cerr << "Incorrect input\n";
+    return 1;
+  }
 }
