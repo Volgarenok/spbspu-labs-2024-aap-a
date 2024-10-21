4380ae7ee01b7fbb49151f5a7d54915d4f573d2c
evstyunichev.konstantin <korombodis.gmail.com>
2024-10-21 17:06:04 +0300

    input check with code exit added

--- evstyunichev.konstantin/P2/main.cpp
+++ evstyunichev.konstantin/P2/main.cpp
@@ -6,14 +6,20 @@ int main()
   double left = 0.0, right = 0.0;
   size_t k = 0;
   std::cin >> left >> right >> k;
-
-  const double step = 0.25;
+  if(!std::cin || (left <= -1) || (right >= 1) || (left > right))
+  {
+    std::cerr << "input error\n";
+    return 1;
+  }
+  const double step = 0.2;
   const double error = 0.001;
 
   for(double i = left; i < right; i += step)
   {
     kos::output(i, k, error);
   }
-  kos::output(right, k, error);
+  if(left != right) {
+    kos::output(right, k, error);
+  }
   return 0;
 }
