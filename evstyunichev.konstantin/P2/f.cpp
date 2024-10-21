e3a5dccc02ef815c5ac5509a434aa03621c5ca05
evstyunichev.konstantin <korombodis.gmail.com>
2024-10-21 17:23:19 +0300

    again CG.

--- evstyunichev.konstantin/P2/f.cpp
+++ evstyunichev.konstantin/P2/f.cpp
@@ -2,18 +2,18 @@
 
 double kos::f(double x, size_t k, double error)
 {
-	const char *errmsg = "<MATH ERROR>";
-	double last = x, ans = x;
-	for (size_t i = 1; i < k; i++)
-	{
-		last = last * x * x * (i * 2 - 1) * (i * 2 - 1) / (i * 2 + 0.0) / (i * 2 + 1.0);
-		ans += last;
-	}
-	if (std::abs(last) > error)
-	{
-		throw std::logic_error(errmsg);
-	}
-	return ans;
+  const char *errmsg = "<MATH ERROR>";
+  double last = x, ans = x;
+  for (size_t i = 1; i < k; i++)
+  {
+    last = last * x * x * (i * 2 - 1) * (i * 2 - 1) / (i * 2 + 0.0) / (i * 2 + 1.0);
+    ans += last;
+  }
+  if (std::abs(last) > error)
+  {
+    throw std::logic_error(errmsg);
+  }
+  return ans;
 }
 
 double kos::stdf(double x)
@@ -23,15 +23,15 @@ double kos::stdf(double x)
 
 void kos::output(double x, size_t k, double error)
 {
-	const size_t field_size = 12;
-	std::cout << std::setw(field_size) << x << "    ";
-	try
-	{
-		std::cout << std::setw(field_size) << kos::f(x, k, error);
-	}
-	catch(const std::logic_error & e)
-	{
-		std::cout << std::setw(field_size) << e.what();
-	}
-	std::cout << std::setw(field_size) << kos::stdf(x) << "\n";
+  const size_t field_size = 12;
+  std::cout << std::setw(field_size) << x << "    ";
+  try
+  {
+    std::cout << std::setw(field_size) << kos::f(x, k, error);
+  }
+  catch(const std::logic_error & e)
+  {
+    std::cout << std::setw(field_size) << e.what();
+  }
+  std::cout << std::setw(field_size) << kos::stdf(x) << "\n";
 }
