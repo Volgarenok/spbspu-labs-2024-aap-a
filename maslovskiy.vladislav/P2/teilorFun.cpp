d6403231a55de949415da28a7edacbc988646175
vladislavmaslovskiy <vladuisik826@gmail.com>
2024-10-21 15:23:20 +0300

    Add teilorCos

---
+++ maslovskiy.vladislav/P2/teilorFun.cpp
@@ -0,0 +1,31 @@
+#include "teilorFun.hpp"
+
+double teilorCos(const double& x, const size_t& k)
+{
+    const double error = 0.001;
+    double next = 1;
+    double res = next;
+    double xPow = 1;
+    double xFact = 2;
+    double sign = 0;
+    for (size_t i = 1; i < k; ++i)
+    {
+        if (i % 2 == 1)
+        {
+            sign = -1;
+        }
+        else
+        {
+            sign = 1;
+        }
+        xPow = xPow * x * x;
+        next = sign * xPow / xFact;
+        xFact = xFact * (xFact + 1) * (xFact + 2);
+        res += next;
+    }
+    if (std::abs(res - std::cos(x)) > error)
+    {
+        throw std::logic_error("math-error");
+    }
+    return res;
+}
---
+++ maslovskiy.vladislav/P2/teilorFun.hpp
@@ -0,0 +1,7 @@
+#ifndef TEILOR_FUN_HPP
+#define TEILOR_FUN_HPP
+
+#include <cstddef>
+double teilorCos(const double& x, const size_t& k);
+
+#endif
