07c1530d1019ee20c8c6d9173d6e6e7523350360
hahahapolinka <gavrilova.pv@edu.spbstu.ru>
2024-10-22 20:24:22 +0300

    Initializing of variables and input

--- gavrilova.polina/P2/main.cpp
+++ gavrilova.polina/P2/main.cpp
@@ -1,3 +1,11 @@
+#include <iostream>
 int main()
-{}
+{
+  double begin = 0.0, end = 0.0;
+  size_t nMax = 0;
+  const step = 0.05;
+  const error = 0.001;
+
+  std::cin >> begin >> end >> nMax;
+}
 
