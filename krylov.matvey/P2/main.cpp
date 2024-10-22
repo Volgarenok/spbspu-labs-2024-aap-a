e73017693f5f7496a2aaf0f717ee631d325bd42c
root <root@kryloff.localdomain>
2024-10-23 00:15:54 +0300

    f initialized, secure input added

--- krylov.matvey/P2/main.cpp
+++ krylov.matvey/P2/main.cpp
@@ -1,2 +1,17 @@
+#include <iostream>
+
+double f(double x, size_t k, double error);
+
 int main()
-{}
+{
+  double left_border = 0.0, right_border = 0.0;
+  size_t max_terms_number = 0;
+  std::cin >> left_border >> right_border >> max_terms_number;
+  if (!std::cin || (left_border < -1) || (right_border > 1))
+  {
+    std::cerr << "Incorrect input!\n";
+    return 1;
+  }
+  const double error = 0.001;
+  const double step = 0.01;
+}
