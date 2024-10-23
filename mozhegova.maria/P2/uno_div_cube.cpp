917579ee96ca82f8e8406005d5da08458bb340b6
mipxe <mariamozegova8726@gmail.com>
2024-10-23 14:09:19 +0300

    adding definition uno_div_cube fuctions

---
+++ mozhegova.maria/P2/uno_div_cube.cpp
@@ -0,0 +1,24 @@
+#include "uno_div_cube.h"
+#include <cmath>
+#include <stdexcept>
+
+double uno_div_cube(double x, size_t k, double error)
+{
+  double next = 1;
+  double result = next;
+  for (size_t i = 1; i < k; i++)
+  {
+    next *= ((1 + i) * (2 + i)) / 2 * (- x);
+    result += next;
+  }
+  if (std::abs(next) > error)
+  {
+    throw std::logic_error("math_error");
+  }
+  return result;
+}
+
+double uno_div_cube_cmath(double x)
+{
+  return 1 / pow(1.0 + x, 3.0);
+}
---
+++ mozhegova.maria/P2/uno_div_cube.h
@@ -0,0 +1,5 @@
+#ifndef UNO_DIV_CUBE_H
+#define UNO_DIV_CUBE_H
+double uno_div_cube(double x, size_t k, double error);
+double uno_div_cube_cmath(double x);
+#endif
