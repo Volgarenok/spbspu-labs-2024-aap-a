543c942beef6516d45c548caa4e4d3a0422e222e
Z-1337 <un228der@gmail.com>
2024-10-23 18:36:58 +0300

    fixed redefinition of cmath

--- kushekbaev.danil/P2/f.cpp
+++ kushekbaev.danil/P2/f.cpp
@@ -1,5 +1,4 @@
 #include <iostream>
-#include <cmath>
 #include <stdexcept>
 #include "header.hpp"
 
--- kushekbaev.danil/P2/stdf.cpp
+++ kushekbaev.danil/P2/stdf.cpp
@@ -1,5 +1,4 @@
 #include <iostream>
-#include <cmath>
 #include "header.hpp"
 
 double kushekbaev::stdf(double x)
