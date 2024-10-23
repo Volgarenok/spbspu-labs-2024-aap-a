8b9de0022f224a8eb85677bb59dc3280c3363c8c
mipxe <mariamozegova8726@gmail.com>
2024-10-23 15:26:12 +0300

    fixed1

--- mozhegova.maria/P2/main.cpp
+++ mozhegova.maria/P2/main.cpp
@@ -7,8 +7,8 @@ namespace mozhegova
 {
   double uno_div_cube(double x, size_t k, double error);
   double uno_div_cube_cmath(double x);
-  void output_row(auto i);
-  void output_table(double left, double right, size_t k);
+  void output_row(double i, size_t k, double error);
+  void output_table(double left, double right, size_t k, double step, double error);
 }
 
 int main()
@@ -16,11 +16,8 @@ int main()
   double left = 0.0, right = 0.0;
   size_t k = 0;
   std::cin >> left >> right >> k;
-
-  const double error = 0.001;
   const double step = 0.05;
-  const size_t SecColWidth = std::strlen("<MATH ERROR>") + 2;
-  const size_t OtherCol = 8;
+  const double error = 0.001;
 
-  output_table(left, right, k);
+  mozhegova::output_table(left, right, k, step, error);
 }
