c0a1410d7d08285ab2eb75d487908aea2749df29
dmitei <dima9990860349@icloud.com>
2024-10-23 10:40:17 +0300

    idk

--- shabalin.dmitrij/P2/main.cpp
+++ shabalin.dmitrij/P2/main.cpp
@@ -1,4 +1,30 @@
+#include <iostream>
+#include <stdexcept>
+
+double f(double x, size_t k, double error);
+
+double f(double x, size_t k, double error)
+{
+  double next = cos(x);
+  double result = 0.0;
+  for (int i = 1; i < k; ++i)
+  {
+    result *= (-x * x) / ((2 * i - 1) * (2 * i));
+    next += result;
+  }
+  if (std::abs(next) > error)
+  {
+    throw std::logic_error("math-error");
+  }
+  return next;
+}
+
 int main()
 {
-  return 1;
+  double left = 0.0, right = 0.0;
+  size_t k = 0;
+  std::cin >> left >> right >> k;
+  const double error = 0.01;
+  const double step = 0.123;
+
 }
