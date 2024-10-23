9ac890a201762289ae2477da92bd7201169f320e
Sergey Dribas <serdribas@gmail.com>
2024-10-23 21:03:39 +0300

    deleted wrong directory

--- dribas.sergey/p2/taylor.hpp
+++
@@ -1,9 +0,0 @@
-#ifndef TAYLOR_HPP
-#define TAYLOR_HPP
-namespace dribas
-{
-  void out(double x, size_t koll, double error);
-  double taylor(double x,size_t koll,double error);
-  double uno_div_cube(double x);
-}
-#endif
