25ea7c33b769d467e93df54ac481d37f830af60c
mipxe <mariamozegova8726@gmail.com>
2024-10-23 15:37:41 +0300

    add input check

--- mozhegova.maria/P2/main.cpp
+++ mozhegova.maria/P2/main.cpp
@@ -16,6 +16,17 @@ int main()
   double left = 0.0, right = 0.0;
   size_t k = 0;
   std::cin >> left >> right >> k;
+  if (!std::cin)
+  {
+    std::cerr << "Not a number\n";
+    return 1;
+  }
+  if (left  <= -1 || right >= 1)
+  {
+    std::cerr << "Incorrect interval\n";
+    return 1;
+  }
+
   const double step = 0.05;
   const double error = 0.001;
 
