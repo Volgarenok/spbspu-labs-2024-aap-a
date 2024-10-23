5f4db86a4987c9674bbe21ffac1832dbb899e2b5
root <root@DESKTOP-KOVAQT8.localdomain>
2024-10-23 03:46:43 +0300

    I remembered about <MATH ERROR>

--- balashov.dima/P2/tableDisplay.cpp
+++ balashov.dima/P2/tableDisplay.cpp
@@ -8,7 +8,7 @@ namespace balashov
   void showRow(double step, int deepShow, size_t numberOfTerms, double error)
   {
     double x = 0;
-    if ((step + 0.001 > 0) && (step - 0.001 < 0))
+    if ((step + 0.0001 > 0) && (step - 0.0001 < 0))
     {
       x = 0;
     }
@@ -16,8 +16,17 @@ namespace balashov
     {
       x = step;
     }
-    std::cout << std::setw(5) << step;
-    std::cout << "\t" << std::setprecision(deepShow) << balashov::EXP_X(x, numberOfTerms,error)<< "\t" << exp(x) << "\n";
+
+    std::cout << std::setw(5) << x <<  "\t";
+    try
+    {
+      std::cout << std::setprecision(deepShow) << balashov::EXP_X(x, numberOfTerms, error);
+    }
+    catch (const std::logic_error & e)
+    {
+      std::cout << "<MATH ERROR>";
+    }
+    std::cout << "\t" << exp(x) << "\n";
   }
 
   void showTable(double minimumIntervalStep, double maximumIntervalStep,int numberOfTerms, double step, int deepShow, double error)
