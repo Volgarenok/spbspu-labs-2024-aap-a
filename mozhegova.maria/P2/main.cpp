f59b555933e2bd3aa142703f8b974bd1bffa1fbf
mipxe <mariamozegova8726@gmail.com>
2024-10-23 14:12:23 +0300

    adding all fuctions

--- mozhegova.maria/P2/main.cpp
+++ mozhegova.maria/P2/main.cpp
@@ -1,10 +1,13 @@
 #include <iostream>
+#include <cstring>
+#include "output.h"
+#include "uno_div_cube.h"
 
 namespace mozhegova
 {
   double uno_div_cube(double x, size_t k, double error);
   double uno_div_cube_cmath(double x);
-  void output_row(auto i, double taylor, double cmath);
+  void output_row(auto i);
   void output_table(double left, double right, size_t k);
 }
 
@@ -16,6 +19,8 @@ int main()
 
   const double error = 0.001;
   const double step = 0.05;
+  const size_t SecColWidth = std::strlen("<MATH ERROR>") + 2;
+  const size_t OtherCol = 8;
 
   output_table(left, right, k);
 }
