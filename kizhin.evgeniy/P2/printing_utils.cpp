84da07497e55bdc1a24d9f60c914623db0d93900
fox <e.kizhin@yandex.ru>
2024-10-24 15:55:54 +0300

    Update printing_utils
    Removed templates (used overloads instead)

--- kizhin.evgeniy/P2/printing_utils.cpp
+++ kizhin.evgeniy/P2/printing_utils.cpp
@@ -2,21 +2,27 @@
 
 #include <iomanip>
 
-template <typename T>
-void kizhin::printCell(std::ostream& os, const T& data, size_t width, size_t precision)
+void kizhin::printCell(std::ostream& os, double data, size_t width, size_t precision)
+{
+  os << std::setw(width) << std::left << std::setprecision(precision) << data;
+}
+void kizhin::printCell(std::ostream& os, const char* data, size_t width, size_t precision)
 {
   os << std::setw(width) << std::left << std::setprecision(precision) << data;
 }
 
-template <typename T>
-void kizhin::printRow(std::ostream& os, double argument, const T& computedValue,
+void kizhin::printRow(std::ostream& os, double argument, const char* errorMessage,
                       double expectedValue)
+{
+  printCell(os, argument);
+  printCell(os, errorMessage);
+  printCell(os, expectedValue);
+  os << '\n';
+}
+void kizhin::printRow(std::ostream& os, double argument, double computedValue, double expectedValue)
 {
   printCell(os, argument);
   printCell(os, computedValue);
   printCell(os, expectedValue);
   os << '\n';
 }
-
-template void kizhin::printRow<double>(std::ostream&, double, const double&, double);
-template void kizhin::printRow<char const*>(std::ostream&, double, const char* const&, double);
--- kizhin.evgeniy/P2/printing_utils.hpp
+++ kizhin.evgeniy/P2/printing_utils.hpp
@@ -7,12 +7,13 @@ namespace kizhin {
   constexpr size_t defaultColumnWidth = 20;
   constexpr size_t defaultPrecision = 10;
 
-  template <typename T>
-  void printCell(std::ostream& os, const T& data, size_t width = defaultColumnWidth,
+  void printCell(std::ostream& os, const char* data, size_t width = defaultColumnWidth,
+                 size_t precision = defaultPrecision);
+  void printCell(std::ostream& os, double data, size_t width = defaultColumnWidth,
                  size_t precision = defaultPrecision);
 
-  template <typename T>
-  void printRow(std::ostream& os, double argument, const T& computedValue, double expectedValue);
+  void printRow(std::ostream& os, double argument, double computedValue, double expectedValue);
+  void printRow(std::ostream& os, double argument, const char* errorMessage, double expectedValue);
 }
 
 #endif
