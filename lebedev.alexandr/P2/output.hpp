f56959e9ebb324a3699592488c700d22956f3c0e
mokkenz1 <lam1600@mail.ru>
2024-10-24 15:00:31 +0300

    Done output.hpp

---
+++ lebedev.alexandr/P2/output.hpp
@@ -0,0 +1,10 @@
+#ifndef OUTPUT_HPP
+#define OUTPUT_HPP
+#include "taylor_swift.hpp"
+
+namespace lebedev
+{
+    void output_raw (double x, size_t k, double error);
+    void output_table (double left, double right, size_t k, double error, double step);
+}
+#endif
