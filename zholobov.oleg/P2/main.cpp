9a0423e66d6d98a8380c86ba876b77ba766d556f
Oleg Zholobov <zholobov.of@edu.spbstu.ru>
2024-10-24 23:09:07 +0300

    almost ready

--- zholobov.oleg/P2/main.cpp
+++ zholobov.oleg/P2/main.cpp
@@ -1,15 +1,34 @@
 #include <iostream>
+#include <cmath>
+#include <iomanip>
+#include <cstddef>
+#include "taylor_arctg.hpp"
 
-double absolute_error = 1e-5;
+const double absolute_error = 1e-5;
+const int N = 20;
 
 int main() {
     double beg = 0.0;
     double end = 0.0;
-    int max_n = 0;
+    size_t max_n = 0;
     std::cin >> beg >> end >> max_n;
     if (!std::cin || beg >= end)
         return 1;
 
+    double step = (end - beg) / N;
+    for (int i = 0; i < N+1; i++) {
+        double x = beg + step * i;
+        double std_res = atan(x);
+        double my_res = 0;
+        try {
+            my_res = taylor_arctg(x, max_n, absolute_error);
+        }
+        catch (std::runtime_error&) {
+            std::cout << std::setprecision(8) << x << " <MATH ERROR> " << std_res << "\n";
+            continue;
+        }
+        std::cout << std::setprecision(8) << x << " " << my_res << " " << std_res << "    " << abs(my_res - std_res) << "\n";
 
+    }
     return 0;
 }
--- zholobov.oleg/P2/taylor_arctg.cpp
+++ zholobov.oleg/P2/taylor_arctg.cpp
@@ -8,6 +8,8 @@ static double abs(double x) {
 #define M_PI_2   1.57079632679489661923
 
 double taylor_arctg(double x, size_t k, double error) {
+    // Since Taylor series works for arctg(x) for abs(x) <= 1
+    // If abs(x) > 1, we use formula: arctg(x) = (sign(x) * pi/2 - arctg(1/x)
     bool argument_less_abs_1 = abs(x) <= 1.0;
     if (!argument_less_abs_1)
         x = 1 / x;
@@ -26,6 +28,6 @@ double taylor_arctg(double x, size_t k, double error) {
     if (abs(s) >= error) throw std::runtime_error("Accuracy not reached");
 
     if (!argument_less_abs_1)
-        sum = M_PI_2 - sum;
+        sum = ((x >= 0) ? M_PI_2 : -M_PI_2) - sum;
     return sum;    
 }
