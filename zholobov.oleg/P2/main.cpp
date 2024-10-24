d4186543a5ac6a84dcf4dcc7d40d66c70549f42c
Oleg Zholobov <zholobov.of@edu.spbstu.ru>
2024-10-24 22:20:38 +0300

    at least it compiles

--- zholobov.oleg/P2/main.cpp
+++ zholobov.oleg/P2/main.cpp
@@ -3,7 +3,13 @@
 double absolute_error = 1e-5;
 
 int main() {
+    double beg = 0.0;
+    double end = 0.0;
+    int max_n = 0;
+    std::cin >> beg >> end >> max_n;
+    if (!std::cin || beg >= end)
+        return 1;
 
 
-
+    return 0;
 }
--- zholobov.oleg/P2/taylor_arctg.cpp
+++ zholobov.oleg/P2/taylor_arctg.cpp
@@ -1,5 +1,31 @@
 #include <cstddef>
+#include <stdexcept>
+
+static double abs(double x) {
+    return (x < 0) ? -x : x;
+}
+
+#define M_PI_2   1.57079632679489661923
 
 double taylor_arctg(double x, size_t k, double error) {
+    bool argument_less_abs_1 = abs(x) <= 1.0;
+    if (!argument_less_abs_1)
+        x = 1 / x;
+    double sum = x;
+    int d = 1;
+    double c = x;
+    double s = x;
     
+    while (k-- > 1) {
+        c = -c * x * x;
+        d += 2;
+        s = c / d;
+        if (abs(s) < error) break;
+        sum += s;
+    }
+    if (abs(s) >= error) throw std::runtime_error("Accuracy not reached");
+
+    if (!argument_less_abs_1)
+        sum = M_PI_2 - sum;
+    return sum;    
 }
