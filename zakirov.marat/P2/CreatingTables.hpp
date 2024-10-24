c47c81a027dbaa35ab825d24975b8136d84e2801
ZakirovMS <mz.08.08.06@gmail.com>
2024-10-24 23:15:12 +0300

    Header files have been created, the main functions are defined in them.

---
+++ zakirov.marat/P2/CreatingTables.hpp
@@ -0,0 +1,9 @@
+#ifndef FINDINGTYLOR_HPP
+#define FINDINGTYLOR_HPP
+namespace zakirov
+{
+  double find_atanh(double x, size_t k, double error);
+  double use_atanh(double x);
+}
+
+#endif
---
+++ zakirov.marat/P2/FindingTylor.hpp
@@ -0,0 +1,10 @@
+#ifndef CREATINGTABLES_HPP
+#define CREATINGTABLES_HPP
+namespace zakirov
+{
+  int find_error();
+  void print_string(double left, double position, double right);
+  void print_table(double left, double position, double right);
+}
+
+#endif
