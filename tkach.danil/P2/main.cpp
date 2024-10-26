085dbf2bc0f2aa8adcc0171d360567295553be55
Danil3352 <danil.tkach2018@yandex.ru>
2024-10-26 20:17:42 +0300

    Input check

--- tkach.danil/P2/main.cpp
+++ tkach.danil/P2/main.cpp
@@ -9,9 +9,26 @@ int main()
   const double error = 0.001;
   const double step = 0.1;
   std::cout << step << "\n";
-  double start = 0, end = 0, maxkolvo=0;
+  double start = 0.0, end = 0.0;
+  size_t maxkolvo = 0;
   std::cin >> start >> end >> maxkolvo;
+  if (std::cin.eof())
+  {
+    std::cerr << "Input Error: not all in input\n";
+    return 1;
+  }
+  if (!std::cin.good())
+  {
+    std::cerr << "Incorrect input\n";
+    return 1;
+  }
+  if ((start<-1) || (start>1) ||  (end>1) || (end<-1))
+  {
+    std::cerr << "Incorrect input of interval value\n";
+    return 1;
+  } 
   std::cout <<  end << " " <<  maxkolvo << "\n";
   std::cout << TeylorRow(start,10,error) << "\n";
   std::cout << cosh(start) << "\n";
+  return 0;
 }
