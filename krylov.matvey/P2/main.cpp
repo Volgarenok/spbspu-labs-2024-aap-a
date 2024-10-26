8532818547045f81c81509c86e35b7a013db25a9
root <root@kryloff.localdomain>
2024-10-26 12:36:03 +0300

    Taylor function added

--- krylov.matvey/P2/main.cpp
+++ krylov.matvey/P2/main.cpp
@@ -1,6 +1,11 @@
 #include <iostream>
-
-double f(double x, size_t k, double error);
+#include <cmath>
+namespace krylov
+{
+  double f(double x, size_t k, double error);
+  double sinx_div_x(double x);
+  void line_output(
+}
 
 int main()
 {
@@ -15,3 +20,32 @@ int main()
   const double error = 0.001;
   const double step = 0.01;
 }
+
+double f(double x, size_t k, double error)
+{
+  double result = 1;
+  double previous_term = 1;
+  double current_term = 0;
+  int previous_factorial_value = 1;
+  for  (i = 1; i < k; i++)
+  {
+    int factorial = 1;
+    previous_term *= factorial;
+    for (j = 1; j <= previous_factorial_value + 2; j+=1)
+    {
+      factorial *= j;
+    }
+    previous_factorial_value+=2;
+    current_term = previous_term * x * x / factorial;
+    if (i%2==0)
+    {
+      result += current_term;
+      previous_term = current_term;
+    }
+    else
+    {
+      result -= current_term;
+      previous_term = current_term;
+    }
+    return result;
+  }
