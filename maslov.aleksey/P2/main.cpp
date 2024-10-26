45912974f53eb7195c87bd9dcccb5feb0bc9cced
A1exMas1ov <ae1ex.mas1ov@gmail.com>
2024-10-26 17:42:33 +0300

    Checking the input

--- maslov.aleksey/P2/main.cpp
+++ maslov.aleksey/P2/main.cpp
@@ -5,4 +5,20 @@ int main()
   double left = 0.0, right = 0.0;
   size_t k = 0;
   std::cin >> left >> right >> k;
+  if (!std::cin)
+  {
+    std::cerr << "The input could not be recognized\n";
+    return 1;
+  }
+  if (left > right)
+  {
+    std::cerr << "The interval is set incorrectly\n";
+    return 1;
+  }
+  if ((-1 < left && left < 1) &&
+      (-1 < right && right < 1))
+  {
+    std::cerr << "The interval is not in the definition area\n";
+    return 1;
+  }
 }
