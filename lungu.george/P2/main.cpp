853b7cf8bc143f2525aa5ed82f8fde0d74336b12
ProstoG-byte <asilda2019@gmail.com>
2024-10-26 16:09:54 +0300

    Initial commit for P2

---
+++ lungu.george/P2/main.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <iomanip>
+#include <cmath>
+#include "Tylor.h"
+int main() {
+    double absError=0.001;
+    int numberMax;
+    double start, end, step=0.05;
+    std::cin >> start >> end >> numberMax;
+    if (!std::cin){
+        std::cerr << ("Bad input") << "\n" <<"\a";
+        return 1;
+    }
+    if (step <= 0) {
+        std::cerr << "Step must be positive." << "\n" << "\a";
+        return 2;
+    }
+    if (end<start){
+        std::cerr << ("Logic error") << "\n" << "\a";
+        return 3;
+    }
+
+    std::cout << std::fixed << std::setprecision(6);
+    std::cout << std::string(10, ' ') << "X\tSUMM\tSTDfunction\n";
+
+    for (double x = start; x <= end; x += step) {
+        try {
+            double calculatedValue = lungu::TylorFunc(x, absError, numberMax);
+            double standardValue = std::exp(-x * x); 
+
+            std::cout << std::string(10, ' ') << x << "\t" << calculatedValue << "\t" << standardValue << "\n";
+        } catch (const std::exception& e) {
+            std::cout << std::string(10, ' ') << x << "\tMathError" << "\n";
+        }
+    }
+
+    return 0;
+}
