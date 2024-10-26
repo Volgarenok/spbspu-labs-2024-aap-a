1565258994871dfbedd837e66994baa356911178
Nekich06 <p.nikita.73526@gmail.com>
2024-10-26 14:18:04 +0300

    Added input to main.cpp
    Now user can input data

--- petrov.nikita/P2/main.cpp
+++ petrov.nikita/P2/main.cpp
@@ -1,3 +1,10 @@
+#include <iostream>
+
 int main()
 {
+  double start = 0.0, end = 0.0;
+  size_t max = 0;
+  std::cin >> start >> end >> max;
+  const double abs_error = 0.001;
+  const double step = 0.05;
 }
