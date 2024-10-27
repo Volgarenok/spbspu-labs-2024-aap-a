51f2c6d4d2e99aa962d1cbc8d1ac5744aaa4faf6
Sikireg <masj280906@gmail.com>
2024-10-27 13:26:31 +0300

    Good version

--- karnauhova.alexandra/P2/func.cpp
+++ karnauhova.alexandra/P2/func.cpp
@@ -3,24 +3,24 @@ double karnauhova::f(double x, size_t k, double error)
 {
   double next = x;
   double result = next;
-  for (size_t i = 1; i<k;++i)
+  for (size_t i = 1; i < k;++i)
   {
     next = 1.0;
-    for (size_t j = 0; j<(i*2+1); ++j)
+    for (size_t j = 0; j < (i*2+1); ++j)
     {
       next*=x;
     }
-    for (double j = 1; j<(i*2+2); ++j)
+    for (double j = 1; j < (i*2+2); ++j)
     {
       next/=j;
     }
-    if (i%2==0)
+    if (i%2 == 0)
     {
-      result+=next;
+      result += next;
     }
     else
     {
-      result-=next;
+      result -= next;
     }
   }
   if (std::abs(next) > error)
