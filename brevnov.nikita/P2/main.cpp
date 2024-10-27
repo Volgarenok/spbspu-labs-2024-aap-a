b830679fd19dedd8811f67336ccc10d7bf9b8605
brev1ch <flirktv@gmail.com>
2024-10-27 23:44:08 +0300

    Politovka 10

--- brevnov.nikita/P2/main.cpp
+++ brevnov.nikita/P2/main.cpp
@@ -15,7 +15,7 @@ int main()
         std::cerr << "Input error!\n";
         return 1;
     }
-    if (left>=right)
+    if ((left>=right) or (std::abs(left)>1) or (std::abs(right)>1))
     {
         std::cerr << "Incorrect input!\n";
         return 1;
