5ba03dc53aad2d1a5424dbec78c2311576741033
ProstoG-byte <asilda2019@gmail.com>
2024-10-27 17:34:20 +0300

    Initial commit for P2

--- lungu.george/P2/main.cpp
+++ lungu.george/P2/main.cpp
@@ -8,7 +8,7 @@ int main() {
     double start, end, step=0.05;
     std::cin >> start >> end >> numberMax;
     if (!std::cin) {
-        std::cerr << ("Worst possible input") << "\n";
+        std::cerr << ("Bad input") << "\n";
         return 1;
     }
     if (step <= 0) {
@@ -20,15 +20,15 @@ int main() {
         return 3;
     }
     std::cout << std::fixed << std::setprecision(6);
-    std::cout << std::string(10, ' ') << "X\tSUMM\tSTDfunction\n";
+    std::cout << std::string(10, ' ') << "X  SUMM  STDfunction\n";
     for (double x = start; x <= end; x += step) {
         try {
             double calculatedValue = lungu::TylorFunc(x, absError, numberMax);
             double standardValue = std::exp(-x * x); 
-            std::cout << std::string(10, ' ') << x << "\t" << calculatedValue << "\t" << standardValue << "\n";
+            std::cout << std::string(10, ' ') << x <<   << calculatedValue <<   << standardValue << "\n";
         } 
         catch (const std::exception& e) {
-            std::cout << std::string(10, ' ') << x << "\tMathError" << "\n";
+            std::cout << std::string(10, ' ') << x << "   MathError" << "\n";
         }
     }
     return 0;
