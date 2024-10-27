c75916d1d133078a63b771475c68bf34c9032eb4
Danil3352 <danil.tkach2018@yandex.ru>
2024-10-27 23:05:24 +0300

    fix setw with Math Error

--- tkach.danil/P2/output_row.cpp
+++ tkach.danil/P2/output_row.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <cstring>
 #include <iomanip>
 #include "tableoutput.h"
 #include "taylorrow.h"
@@ -6,6 +7,8 @@
 void tkach::outputRow(double start, size_t maxkolvo, const double error)
 {
   const size_t setwx = 13;
+  const char *errormsg = "<MATH ERROR>";
+  const size_t error_len = std::strlen(errormsg) + 5;
   std::cout << std::fixed;
   std::cout << std::setw(setwx) << std::setprecision(1) <<  start;
   std::cout << " ";
@@ -15,7 +18,9 @@ void tkach::outputRow(double start, size_t maxkolvo, const double error)
   }
   catch (const std::logic_error & e)
   {
-    std::cout << "<MATH ERROR>";
+    std::cout << std::setw(error_len) << "<MATH ERROR>";
+    std::cout << " ";
   }
-  std::cout << std::setw(setwx) << std::setprecision(4) << tkach::stdCosh(start) << "\n";
+  std::cout << std::setw(setwx) << std::setprecision(4) << tkach::stdCosh(start);
+  std::cout << "\n";
 }
