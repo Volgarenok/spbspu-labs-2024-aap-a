2f29db13023637866f000a3e2138cd593258090a
Sikireg <masj280906@gmail.com>
2024-10-27 13:02:13 +0300

    Cheking the input

--- karnauhova.alexandra/P2/main.cpp
+++ karnauhova.alexandra/P2/main.cpp
@@ -10,7 +10,16 @@ int main()
   double left = 0.0, right = 0.0;
   size_t k = 0;
   std::cin >> left >> right >> k;
-  
+  if (!std::cin.good())
+  {
+    std::cerr << "Input error";
+    return 1;
+  }
+  if ((left > right) or (left < -1) or (right > 1))
+  {
+    std::cerr << "Incorrect input";
+    return 1; 
+
   const double error = 0.001;
   const double step = 0.05;
   karnauhova::output_table(left, right, step, k, error, 13);  
