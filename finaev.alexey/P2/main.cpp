7f9b1c38b299cb6e795beea103c95c9d7abf6731
AlexeyFinaev02 <finaev0212212@mail.ru>
2024-10-27 05:33:18 +0300

    fixed mistakes 2

--- finaev.alexey/P2/main.cpp
+++ finaev.alexey/P2/main.cpp
@@ -1,22 +1,26 @@
 #include <iostream>
 #include "tyler_derden.h"
 #include <cstring>
-int main
+
+int main()
 {
-  double left = 0.0, right = 0.0;
+  double left = 0.0;
+  double right = 0.0;
   size_t k = 0;
   std::cin >> left >> right >> k;
   if (std::cin.fail())
   {
     std::cerr << "input error\n";
-    return 1; 
+    return 1;
   }
   if (left <= -1 || right >= 1 || left > right)
   {
     std::cerr << "input error\n";
     return 1;
   }
+
   const double error = 0.001;
   const double step = 0.05;
+
   finaev::final_output(left, right, k, error, step);
 }
