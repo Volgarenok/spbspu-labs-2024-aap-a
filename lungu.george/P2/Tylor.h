8ab573cfa8640ae491872265d809673fe62f5a61
ProstoG-byte <asilda2019@gmail.com>
2024-10-27 18:14:18 +0300

    Initial commit for P2

--- lungu.george/P2/Tylor.h
+++ lungu.george/P2/Tylor.h
@@ -1,6 +1,9 @@
 #ifndef TYLOR_H
 #define TYLOR_H
-namespace lungu {
-    double TylorFunc(double x, double absError, int numberMax);
+
+namespace lungu 
+{
+  double TylorFunc(double x, double absError, int numberMax);
 }
+
 #endif
