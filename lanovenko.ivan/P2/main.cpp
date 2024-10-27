b081b2e3f2021bdab0436b53443ce1ae2bdd96b7
Tuc0Sa1amanka <sparowj765@gmail.com>
2024-10-27 20:14:17 +0300

    main

--- lanovenko.ivan/P2/main.cpp
+++ lanovenko.ivan/P2/main.cpp
@@ -1,2 +1,27 @@
+#include "taylor_swift.h"
+#include <iostream>
+#include <cstring>
+
 int main()
-{}
+{
+  double left = 0.0, right = 0.0;
+  size_t k = 0;
+  std::cin >> left >> right >> k;
+
+  if (std::cin.fail())
+  {
+    std::cerr << "input error\n";
+    return 1;
+  }
+  if (left <= -1 || right >=1 || left > right)
+  {
+    std::cerr << "input error\n";
+    return 1;
+  }
+
+  const double error = 0.001;
+  const double step = 0.05;
+
+  lanovenko::laststro(left, right, k, error, step);
+}
+
