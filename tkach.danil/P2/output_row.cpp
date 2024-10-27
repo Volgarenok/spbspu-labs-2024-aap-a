1be807ad0ef3bfc58be445ceacfbc7c4feb167f8
Danil3352 <danil.tkach2018@yandex.ru>
2024-10-27 22:51:24 +0300

    + std::fixed

--- tkach.danil/P2/output_row.cpp
+++ tkach.danil/P2/output_row.cpp
@@ -6,11 +6,12 @@
 void tkach::outputRow(double start, size_t maxkolvo, const double error)
 {
   const size_t setwx = 13;
-  std::cout << std::setw(setwx) << start;
+  std::cout << std::fixed;
+  std::cout << std::setw(setwx) << std::setprecision(1) <<  start;
   std::cout << " ";
   try
   {
-    std::cout << std::setw(setwx) << std::setprecision(4) <<  tkach::coshTaylor(start, maxkolvo, error) << " ";
+    std::cout << std::setw(setwx)  << std::setprecision(4) <<  tkach::coshTaylor(start, maxkolvo, error) << " ";
   }
   catch (const std::logic_error & e)
   {
