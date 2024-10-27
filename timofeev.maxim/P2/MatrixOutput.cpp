fd876cb46494771c922c6408294320956f91e146
PigoDog <maksimtmfv@gmail.com>
2024-10-27 16:58:57 +0300

    writing MatrixOutput.cpp

---
+++ timofeev.maxim/P2/MatrixOutput.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <iomanip>
+#include "arctanTaylor.h"
+#include <cstring>
+#include <stdexcept>
+
+void timofeev::MatrixOutput(double start, double end, size_t numberMax, double absError, double step)
+{
+  const char * er_math = "<MATH ERROR>"; 
+  const size_t dlina_er = std::strlen(er_math);
+  for (double x = start; x < end; x += step)
+  {
+    std::cout << std::setw(dlina_er) << x;
+    std::cout << std::setw(dlina_er) << atan(x);
+    try
+    {
+      std::cout << std::setw(dlina_er) << timofeev::arctanTaylor(x, absError, numberMax);
+    }
+    catch (const std::logic_error & e)
+    {
+      std::cout << std::setw(dlina_er) << "<MATH ERROR>";
+    }
+    std::cout << "\n";
+  }
+}
