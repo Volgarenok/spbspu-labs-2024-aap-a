6d189a8d448055a89d5d9b0a15bee7d772b6fe69
GuseynovGuseynGG <ganx.gamer@gmail.com>
2024-10-27 14:52:07 +0300

    add input

--- guseynov.guseyn/P2/main.cpp
+++ guseynov.guseyn/P2/main.cpp
@@ -1,3 +1,14 @@
+#include <iostream>
+
 int main()
 {
+  double left = 0.0;
+  double right = 0.0;
+  size_t k = 0;
+  std::cin >> left >> right >> k;
+  if ((!std::cin) or (left < (-1.0)) or (right > 1.0))
+{
+    return 1;
 }
+}
+
