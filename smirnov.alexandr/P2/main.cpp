f9457615ca9c2b7fe899e51e5e919e40124ca2ef
A1exCRE <alsmmm2065@gmail.com>
2024-10-27 14:39:14 +0300

    Check input

--- smirnov.alexandr/P2/main.cpp
+++ smirnov.alexandr/P2/main.cpp
@@ -5,4 +5,19 @@ int main()
    double left = 0.0, right = 0.0;
    size_t k = 0;
    std::cin >> left >> right >> k;
+   if (!std::cin)
+   {
+     std::cerr << "The input data could not be recognized\n";
+     return 1;
+   }
+   if (left > right)
+   {
+     std::cerr << "The interval is set incorrectly\n";
+     return 1;
+   }
+   if (left <= -1.0 || right >= 1.0)
+   {
+     std::cerr << "The interval is not entirely within the scope of the function definition\n";
+     return 1;
+   }
 }
