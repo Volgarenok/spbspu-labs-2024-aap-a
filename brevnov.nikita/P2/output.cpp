3b1dd59062de9b390b7a2ccbcf462cc5b1c909f4
brev1ch <flirktv@gmail.com>
2024-10-27 22:43:00 +0300

    Politovka 5

--- brevnov.nikita/P2/output.cpp
+++ brevnov.nikita/P2/output.cpp
@@ -8,30 +8,32 @@ void brevnov::outputing(const double left, const double right, const size_t k)
     const size_t usually_width = 10;
     const size_t usu = usually_width;
     const size_t snd = second_width;
+    const size_t numberafterp = 5;
+    const size_t nap = numberafterp;
     for (auto i = left; i < right; i += step)
     {
-        std::cout << std::setprecision(usu) << std::setw(usu) << i;
+        std::cout << std::setprecision(nap) << std::setw(usu) << i;
         std::cout << " ";
         try
         {
-            std::cout << std::setprecision(snd) << std::setw(snd) << brevnov::f(i,k,error);
+            std::cout << std::setprecision(nap) << std::setw(snd) << brevnov::f(i,k,error);
         } 
         catch (std::logic_error & e)
         {
-            std::cout << std::setprecision(snd) << std::setw(snd) << "<MATH ERROR>";
+            std::cout << std::setprecision(nap) << std::setw(snd) << "<MATH ERROR>";
         }
-        std::cout << " " << std::setprecision(usu) << std::setw(usu) << atan(i); 
+        std::cout << " " << std::setprecision(nap) << std::setw(usu) << atan(i); 
         std::cout<< "\n";
     }
-    std::cout << std::setprecision(usu) << std::setw(usu) << right;
+    std::cout << std::setprecision(nap) << std::setw(usu) << right;
     try
     {
-        std::cout << " " << std::setprecision(usu) << std::setw(snd) << brevnov::f(right, k, error);
+        std::cout << " " << std::setprecision(nap) << std::setw(snd) << brevnov::f(right, k, error);
     } 
     catch (std::logic_error & e)
     {
-        std::cout << std::setprecision(usu) << std::setw(snd) << "<MATH ERROR>";
+        std::cout << std::setprecision(nap) << std::setw(snd) << "<MATH ERROR>";
     }
-    std::cout << " " << std::setprecision(usu) << std::setw(usu) <<atan(right);
+    std::cout << " " << std::setprecision(nap) << std::setw(usu) <<atan(right);
     std::cout << "\n";
 }
