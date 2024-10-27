6dc96ddf71297888c0fd4113c0d1a97b0a3eb569
ProstoG-byte <asilda2019@gmail.com>
2024-10-27 18:11:39 +0300

    Initial commit for P2

--- lungu.george/P2/main.cpp
+++ lungu.george/P2/main.cpp
@@ -2,34 +2,54 @@
 #include <iomanip>
 #include <cmath>
 #include "Tylor.h"
-int main() {
-    double absError=0.001;
-    int numberMax;
-    double start, end, step=0.05;
-    std::cin >> start >> end >> numberMax;
-    if (!std::cin) {
-        std::cerr << ("Bad input") << "\n";
-        return 1;
+
+int main() 
+{
+  const double absError = 0.001;
+  int numberMax;
+  double start = 0.0;
+  double end = 0.0;
+  const double step = 0.05;
+
+  std::cin >> start >> end >> numberMax;
+
+  if (!std::cin) 
+  {
+    std::cerr << "Bad input" << "\n";
+    return 1;
+  } 
+
+  if (step <= 0) 
+  {
+    std::cerr << "Step must be positive." << "\n";
+    return 2;
+  } 
+
+  if (end < start) 
+  {
+    std::cerr << "Logic error" << "\n";
+    return 3;
+  }
+
+  std::cout << std::fixed << std::setprecision(6);
+  std::cout << std::string(10, ' ') << "X  SUMM  STDfunctionn" << "\n";
+
+  for (double x = start; x <= end; x += step) 
+  {
+    try 
+    {
+      double calculatedValue = lungu::TylorFunc(x, absError, numberMax);
+      double standardValue = std::exp(-x * x);
+
+      std::cout << std::string(10, ' ') << x << "   "
+                << calculatedValue << "   "
+                << standardValue << "\n";
+    } 
+    catch (const std::exception& e) 
+    {
+      std::cout << std::string(10, ' ') << x << "   MathError" << "\n";
     }
-    if (step <= 0) {
-        std::cerr << "Step must be positive." << "\n";
-        return 2;
-    }
-    if (end<start) {
-        std::cerr << ("Logic error") << "\n";
-        return 3;
-    }
-    std::cout << std::fixed << std::setprecision(6);
-    std::cout << std::string(10, ' ') << "X  SUMM  STDfunction\n";
-    for (double x = start; x <= end; x += step) {
-        try {
-            double calculatedValue = lungu::TylorFunc(x, absError, numberMax);
-            double standardValue = std::exp(-x * x); 
-            std::cout << std::string(10, ' ') << x <<   << calculatedValue <<   << standardValue << "\n";
-        } 
-        catch (const std::exception& e) {
-            std::cout << std::string(10, ' ') << x << "   MathError" << "\n";
-        }
-    }
-    return 0;
+  }
+
+  return 0;
 }
