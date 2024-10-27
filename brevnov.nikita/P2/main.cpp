f76507582c74e65c092cea48cf7546d512a0d4a4
brev1ch <flirktv@gmail.com>
2024-10-27 16:49:14 +0300

    Check wrong work

--- brevnov.nikita/P2/main.cpp
+++ brevnov.nikita/P2/main.cpp
@@ -16,7 +16,6 @@ int main(){
     const double error = 0.001;
     const double step = 0.05;
     for (auto i = left; i < right; i += step){
-        double value = brevnov::f(i,k,error);
         std::cout << i;
         std::cout << " ";
         try{
