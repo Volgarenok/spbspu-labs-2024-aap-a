4adee2e5a2d9725ba29f4d7b559bf1bc92fc0694
shycoshy <neforscist@gmail.com>
2024-10-27 14:51:55 +0300

    cg 1.11 1.12

--- asafov.andrew/P2/header.h
+++ asafov.andrew/P2/header.h
@@ -1,12 +1,11 @@
 #ifndef HEADER_H
 #define HEADER_H
-
 #include <iostream> 
 #include <cmath>
 #include <stdexcept>
 namespace asafov {
-	double absError(double, std::size_t);
-	double f(double, std::size_t, double);
-	void table(double, double, double, std::size_t, double);
+  double absError(double, std::size_t);
+  double f(double, std::size_t, double);
+  void table(double, double, double, std::size_t, double);
 }
 #endif
