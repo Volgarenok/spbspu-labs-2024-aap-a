e71814a2df9b18d2d9b21fb1c14df7b122d29566
brev1ch <flirktv@gmail.com>
2024-10-27 21:13:35 +0300

    I hope this is last commit 2

--- brevnov.nikita/P2/f.cpp
+++ brevnov.nikita/P2/f.cpp
@@ -4,7 +4,7 @@ double brevnov::f(double x, size_t k, double error)
     double next = x;
     double result = next;
     for (size_t i = 1; i < k; ++i){
-        next = (pow(-1,i) * pow(x, 2*i+1) )/ (2*i + 1)
+        next = (pow(-1,i) * pow(x, 2*i+1) )/ (2*i + 1);
         result +=next;
     }
     if (std::abs(next) - std::abs(atan(x)) > error){
