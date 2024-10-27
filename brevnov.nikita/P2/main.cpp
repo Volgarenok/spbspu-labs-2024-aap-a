84a407506407a3ea7d845b363f74c869fd226ecb
brev1ch <flirktv@gmail.com>
2024-10-27 21:32:37 +0300

    Finally

--- brevnov.nikita/P2/main.cpp
+++ brevnov.nikita/P2/main.cpp
@@ -11,28 +11,6 @@ int main(){
     if ((left>=right)){
         std::cerr << "Incorrect input!\n";
     }
-    const double error = 0.001;
-    const double step = 0.05;
-    const char * errorsum = "<MATH ERROR>";
-    const size_t snd = std::strlen(errorsum);
-    const size_t usu = 10;
-    for (auto i = left; i < right; i += step){
-        std::cout << std::setw(usu) << i;
-        std::cout << " ";
-        try{
-            std::cout << std::setw(snd) << brevnov::f(i,k,error);
-        } catch(std::logic_error & e){
-            std::cout << std::setw(snd) << "<MATH ERROR>";
-        }
-        std::cout << " " << std::setw(usu) << atan(i); 
-        std::cout<< "\n";
-    }
-    std::cout << std::setw(usu) << right;
-    try{
-        std::cout << " " << std::setw(snd) << brevnov::f(right, k, error);
-    } catch (std::logic_error & e){
-        std::cout << std::setw(snd) << "<MATH ERROR>";
-    }
-    std::cout << " " << std::setw(usu) <<atan(right);
-    std::cout << "\n";
+    output(left, right, k);
+    return 0;
 }
---
+++ brevnov.nikita/P2/output.cpp
@@ -0,0 +1,27 @@
+#include "brevnov.hpp"
+void output(double left, double right, size_t k){
+    const double error = 0.001;
+    const double step = 0.05;
+    const char * errorsum = "<MATH ERROR>";
+    const size_t snd = std::strlen(errorsum);
+    const size_t usu = 10;
+    for (auto i = left; i < right; i += step){
+        std::cout << std::setw(usu) << i;
+        std::cout << " ";
+        try{
+            std::cout << std::setw(snd) << brevnov::f(i,k,error);
+        } catch(std::logic_error & e){
+            std::cout << std::setw(snd) << "<MATH ERROR>";
+        }
+        std::cout << " " << std::setw(usu) << atan(i); 
+        std::cout<< "\n";
+    }
+    std::cout << std::setw(usu) << right;
+    try{
+        std::cout << " " << std::setw(snd) << brevnov::f(right, k, error);
+    } catch (std::logic_error & e){
+        std::cout << std::setw(snd) << "<MATH ERROR>";
+    }
+    std::cout << " " << std::setw(usu) <<atan(right);
+    std::cout << "\n";
+}
