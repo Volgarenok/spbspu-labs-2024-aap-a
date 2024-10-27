38ec0fbb5d1f6d7d6a9c9fc188287e1db94d5657
fox <e.kizhin@yandex.ru>
2024-10-28 00:07:25 +0300

    Update taylor_cosh.cpp
    - Minor changes

--- kizhin.evgeniy/P2/taylor_cosh.cpp
+++ kizhin.evgeniy/P2/taylor_cosh.cpp
@@ -8,9 +8,9 @@ double kizhin::computeTaylorCosh(double argument, size_t maxOperands, double abs
   }
   double result = 1.0;
   double current = 1.0;
-  double error = 0.0;
+  double error = 1.0;
 
-  for (size_t i = 1; i < maxOperands; ++i) {
+  for (size_t i = 0; i < maxOperands; ++i) {
     current *= (argument * argument) / (2 * i * (2 * i - 1));
     result += current;
     error = current;
