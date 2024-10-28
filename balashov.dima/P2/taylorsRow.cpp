27c9dd373e93d5698a696da1612895a6589cc9ba
root <root@DESKTOP-KOVAQT8.localdomain>
2024-10-28 20:54:48 +0300

    change cpp namespace

--- balashov.dima/P2/taylorsRow.cpp
+++ balashov.dima/P2/taylorsRow.cpp
@@ -1,21 +1,18 @@
 #include "taylorsRow.hpp"
 #include <stdexcept>
 
-namespace balashov
+double balashov::expTaylor(const double x,const size_t numberOfСalculations,const double error)
 {
-  double expTaylor(const double x,const size_t numberOfСalculations,const double error)
+  double result = 1;
+  double summand = 1;
+  for (size_t i = 1; i < numberOfСalculations; i++)
   {
-    double result = 1;
-    double summand = 1;
-    for (size_t i = 1; i < numberOfСalculations; i++)
-    {
-      summand = (x * summand) / i;
-      result += summand;
-    }
-    if (std::abs(summand) > error)
-    {
-      throw std::logic_error("math-error");
-    }
-    return result;
+    summand = (x * summand) / i;
+    result += summand;
   }
+  if (std::abs(summand) > error)
+  {
+    throw std::logic_error("math-error");
+  }
+  return result;
 }
