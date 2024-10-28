d314bcf85de759210e46f70f851aecf27ccab212
mokkenz1 <lam1600@mail.ru>
2024-10-28 18:39:20 +0300

    Little fixes

--- lebedev.alexandr/P2/taylor_swift.hpp
+++
@@ -1,9 +0,0 @@
-#ifndef TEYLORSWIFT_HPP
-#define TEYLORSWIFT_HPP
-#include <cstddef>
-namespace lebedev
-{
-  double sinus(double x, size_t k, double error);
-  double sinus_cmath(double x);
-}
-#endif
