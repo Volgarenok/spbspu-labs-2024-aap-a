db5039c95b84aae84e9879898a6570bde9a76e2b
deonik3 <kis.sergey2006@gmail.com>
2024-10-30 16:45:17 +0300

    Fix check_interval

--- kiselev.sergey/P2/check_interval.cpp
+++ kiselev.sergey/P2/check_interval.cpp
@@ -2,7 +2,7 @@
 bool kiselev::check_interval(double left, double right)
 {
   bool check_gap = left <= -1 || left >= 1;
-  bool check_gap = check_gap || right >= 1 || right <= -1;
-  bool check_gap = check_gap || right < left;
+  check_gap = check_gap || right >= 1 || right <= -1;
+  check_gap = check_gap || right < left;
   return check_gap;
 }
