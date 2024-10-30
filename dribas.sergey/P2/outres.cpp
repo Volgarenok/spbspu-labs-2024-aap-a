f6c66bc1211c031dcb95a343d77bf1b991995068
Sergey Dribas <serdribas@gmail.com>
2024-10-30 10:42:30 +0300

    change for cg and little fix precision

--- dribas.sergey/P2/outres.cpp
+++ dribas.sergey/P2/outres.cpp
@@ -2,25 +2,22 @@
 #include "taylor.hpp"
 #include <stdexcept>
 #include <iomanip>
-#include <cmath>
 #include <iostream>
 #include <cstddef>
 #include <cstring>
 
 void dribas::outline(double x, size_t koll, double error, int sizeclm)
 {
-  double roundedX=round(x * 100000) / 100000;
-  double cmathvalue = round(dribas::getCmathValue(roundedX) * 100000) / 100000;
+  double cmathvalue = dribas::getCmathValue(x);
   try{
-    double value = round(dribas::getTaylor(roundedX, koll, error) * 100000) / 100000;
-
-    std::cout << std::setw(sizeclm) << roundedX;
-    std::cout << std::setw(sizeclm) << value;
-    std::cout << std::setw(sizeclm) << cmathvalue;
+    double value = dribas::getTaylor(x, koll, error);
+    std::cout << std::setw(sizeclm) << std::setprecision(3) << std::fixed << x;
+    std::cout << std::setw(sizeclm) << std::setprecision(6) << std::fixed << value;
+    std::cout << std::setw(sizeclm) << std::setprecision(6) << std::fixed << cmathvalue;
   }catch (const std::exception & e){
-    std::cerr << std::setw(sizeclm) << roundedX;
+    std::cerr << std::setw(sizeclm) << std::setprecision(3) << std::fixed << x;
     std::cerr << std::setw(sizeclm) << e.what();
-    std::cerr << std::setw(sizeclm) << cmathvalue;
+    std::cerr << std::setw(sizeclm) << std::setprecision(6) << std::fixed << cmathvalue;
   }
 }
 
