dcc3638df718d6c9331722e0710b41d75ba9aef5
deonik3 <kis.sergey2006@gmail.com>
2024-10-30 11:21:29 +0300

    Fix check_interval.cpp

--- kiselev.sergey/P2/check_interval.cpp
+++ kiselev.sergey/P2/check_interval.cpp
@@ -1,8 +1,8 @@
 #include "taylor.h"
 bool kiselev::check_interval(double left, double right)
 {
-  bool check_left = left < -1 || left >= 1;
-  bool check_right = right > 1 || right <= -1;
-  bool check_gap = check_right || check_left || right < left;
+  bool check_gap = left < -1 || left >= 1;
+  bool check_gap = check_gap || right > 1 || right <= -1;
+  bool check_gap = check_gap || right < left;
   return check_gap;
 }
