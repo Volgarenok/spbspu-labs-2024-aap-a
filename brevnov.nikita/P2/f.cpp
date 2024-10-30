405ebc048b073a77df3937031b61b8eddb078c1d
brev1ch <flirktv@gmail.com>
2024-10-30 18:13:42 +0300

    CG 3

--- brevnov.nikita/P2/f.cpp
+++ brevnov.nikita/P2/f.cpp
@@ -7,7 +7,7 @@ double brevnov::f(const double x, const size_t k, const double error)
   double result = next;
   for (size_t i = 1; i < k; ++i)
   {
-    next = (std::pow(-1, i) * pow(x, 2 * i + 1) ) / (2 * i + 1);
+    next = (std::pow(-1, i) * pow(x, 2 * i + 1)) / (2 * i + 1);
     result += next;
   }
   if (std::abs(next) - std::abs(atan(x)) > error)
