c886134cc6638f71f918081561b16d70ae285609
Sikireg <masj280906@gmail.com>
2024-10-31 23:08:40 +0300

    So sad

--- karnauhova.alexandra/P3/main.cpp
+++ karnauhova.alexandra/P3/main.cpp
@@ -1,7 +1,16 @@
 #include <iostream>
 #include <fstream>
 #include <cstdlib>
-
+#include <cctype>
+#include <cstddef>
+int* ct_table(size_t m,size_t n);
+std::istream & input_matrix(std::istream & in, int * t, size_t m, size_t n)
+{
+  for (size_t i = 0; i < (m * n); ++i)
+  {
+    in >> t[i];
+  }
+}
 int main(int argc, char ** argv)
 {
   int fix_matrix[10000] = {};
@@ -15,10 +24,21 @@ int main(int argc, char ** argv)
     std::cerr << "Too many arguments\n";
     return 1;
   }
-  if (!isdigit(argv[1]))
-  }
+  char * number = argv[1];
+  if (!isdigit(number[0]))
+  {
     std::cerr << "First parameter is not a number\n";
     return 1;
+  }
+  size_t x = 1;
+  while (number[x]!='\0')
+  {
+    if (!isdigit(number[x]))
+    {
+      std::cerr << "First parameter is not a number\n";
+      return 1;
+    }
+    x++;
   }
   const int way = std::atoi(argv[1]);
   if (way > 2 | way < 1)
@@ -27,18 +47,28 @@ int main(int argc, char ** argv)
     return 1;
   }
   std::ifstream input(argv[2]);
-  input >> m;
-  std::cout << m << "\n";
+  int m = 0,n = 0;
+  input >> m >> n;
+  std::cout << m << n << "\n";
   if (way == 2)
   {
-    //alloc(m, n);
+    int* t = ct_table(m,n);
   }
-
-
-  std::cout<<way<<"\n";
-  for (int i = 0; i < argc; ++i)
+  else
   {
-    std::cout << argv[i]<<"\n";
+    int* t = fix_matrix;
   }
-  std::cout << argc;
+}
+int* ct_table(size_t m,size_t n){
+  int* matrix = nullptr;
+  try
+  {
+    matrix = new int[m*n];
+  }
+  catch(const std::bad_alloc &e)
+  {
+    delete[] matrix;
+    throw;
+  }
+  return matrix;
 }
