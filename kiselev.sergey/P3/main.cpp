7b7ff8f3fe5f4166e630c62896b4f90f60190f09
deonik3 <kis.sergey2006@gmail.com>
2024-11-01 17:09:25 +0300

    Update main.cpp
    Checking the number of parameter inputs

--- kiselev.sergey/P3/main.cpp
+++ kiselev.sergey/P3/main.cpp
@@ -1,3 +1,14 @@
-int main()
+#include <iostream>
+int main(int argc, char** argv)
 {
+  if (argc < 4)
+  {
+    std::cerr << "Not enough arguments\n";
+    return 1;
+  }
+  if (argc > 4)
+  {
+    std::cerr << "To many arguments\n";
+  }
 }
+
