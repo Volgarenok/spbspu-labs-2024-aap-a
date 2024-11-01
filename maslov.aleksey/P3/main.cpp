3fd8f9be431bc9b81e2682ec8b2888c971e9d060
A1exMas1ov <ae1ex.mas1ov@gmail.com>
2024-11-02 00:09:15 +0300

    Input function

--- maslov.aleksey/P3/main.cpp
+++ maslov.aleksey/P3/main.cpp
@@ -2,6 +2,20 @@
 #include <fstream>
 #include <cstdlib>
 
+std::istream & inputMatrix(std::istream & in, int** t, size_t m, size_t n, size_t & read)
+{
+  for (size_t i = 0; i < m; i++)
+  {
+    for (size_t j = 0; j < n; j++)
+    {
+      in >> t[i][j];
+    }
+    ++read;
+  }
+  return in;
+}
+void outputMatrix(std::ostream &out);
+
 int main(int argc, char ** argv)
 {
   if (argc < 4)
@@ -14,7 +28,6 @@ int main(int argc, char ** argv)
     std::cerr << "Too many arguments\n";
     return 1;
   }
-  int mainNumber = 0;
   char* endptr = nullptr;
   long taskNumber = std::strtol(argv[1], &endptr, 10);
   if (endptr != "\0")
@@ -27,7 +40,20 @@ int main(int argc, char ** argv)
     std::cerr << "First parameter is out of range\n";
     return 1;
   }
-  
+  size_t m = 10, n = 10;
+  size_t read = 0;
+  int ** t = alloc(m, n);
   std::ifstream input(argv[2]);
+  try
+  {
+    std::cin.exceptions(std::ios::failbit | std::ios::badbit); 
+    inputMatrix(std::cin, t, m, n, read);
+  }
+  catch (const std::ios_base::failure & e)
+  {
+    std::cerr << "" << e.what() << "\n";
+    return 1;
+  }
   std::ofstream output(argv[3]);
+  outputMatrix(output);
 }
