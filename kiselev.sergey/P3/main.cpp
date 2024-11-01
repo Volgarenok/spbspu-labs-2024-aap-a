f175bc42f6c2ae52d161d888b7b1da3a68e19675
deonik3 <kis.sergey2006@gmail.com>
2024-11-01 19:22:31 +0300

    Updaye main.cpp
    If num = 1,creating a fixed array

--- kiselev.sergey/P3/main.cpp
+++ kiselev.sergey/P3/main.cpp
@@ -1,6 +1,21 @@
+#include <cstddef>
 #include <iostream>
 int main(int argc, char** argv)
 {
+  if (argv[1][0] == 1)
+  {
+
+    size_t k = 1000;
+    int* odnom_array = nullptr;
+    try
+    {
+      odnom_array = new int[k];
+    }
+    catch (const std::bad_alloc& e)
+    {
+      std::cerr << "Memory was not allocated\n";
+    }
+  }
   if (argc < 4)
   {
     std::cerr << "Not enough arguments\n";
