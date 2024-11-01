74b34b348e7ad84918dd13c7b9c764ac235cc0c4
deonik3 <kis.sergey2006@gmail.com>
2024-11-01 23:59:07 +0300

    Update main
    Added a file opening check and counted the number of columns and rows from itI

--- kiselev.sergey/P3/main.cpp
+++ kiselev.sergey/P3/main.cpp
@@ -1,24 +1,9 @@
 #include "checkFirst.h"
-#include <cstddef>
 #include <cstring>
 #include <fstream>
 #include <iostream>
 int main(int argc, char** argv)
 {
-  if (argv[1][0] == 1)
-  {
-
-    size_t k = 1000;
-    int* odnom_array = nullptr;
-    try
-    {
-      odnom_array = new int[k];
-    }
-    catch (const std::bad_alloc& e)
-    {
-      std::cerr << "Memory was not allocated\n";
-    }
-  }
   if (argc < 4)
   {
     std::cerr << "Not enough arguments\n";
@@ -34,7 +19,7 @@ int main(int argc, char** argv)
     std::cerr << "First parameter is not a number\n";
     return 1;
   }
-  else if (strlen(argv[1]) != 1 || (argv[1][0] != '1' && argv[1][0] != '2'))
+  else if (argv[1][1] != '\0' || (argv[1][0] != '1' && argv[1][0] != '2'))
   {
 
     std::cerr << "First parameter is out of range\n";
@@ -42,6 +27,13 @@ int main(int argc, char** argv)
   }
   char* inFile = argv[2];
   std::ifstream input(inFile);
+  if (!input.is_open())
+  {
+    std::cerr << "The file cannot be opened\n";
+    return 1;
+  }
+  size_t m = 0, n = 0;
+  input >> m >> n;
   char* outFile = argv[3];
   std::ofstream output(outFile);
 }
