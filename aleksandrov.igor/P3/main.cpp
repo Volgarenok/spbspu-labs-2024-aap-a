e8983425767e60a8f96f6c46431b91ecad552bb4
MrMels625 <MrMels625@gmail.com>
2024-11-01 23:10:55 +0300

    Output from file to file with 2 types of array added

--- aleksandrov.igor/P3/main.cpp
+++ aleksandrov.igor/P3/main.cpp
@@ -1,2 +1,45 @@
-int main()
-{}
+#include <iostream>
+#include <fstream>
+
+int main(int argc, char** argv)
+{
+  (void)argc;
+  std::ifstream input(argv[2]);
+  int m = 0, n = 0;
+  input >> m >> n;
+  const int size = m * n;
+  std::ofstream output(argv[3]);
+  if (argv[1][0] == '1')
+  {
+    constexpr int arraySize = 10000;
+    int array[arraySize] = {};
+    input >> array[0];
+    output << array[0];
+    for (int i = 1; i < size; ++i)
+    {
+      input >> array[i];
+      output << " " << array[i];
+    }
+    output << "\n";
+  }
+  else if (argv[1][0] == '2')
+  {
+    int *array = nullptr;
+    array = new int[size];
+    input >> array[0];
+    output << array[0];
+    for (int i = 1; i < size; ++i)
+    {
+      input >> array[i];
+      output << " " << array[i];
+    }
+    output << "\n";
+    delete[] array;
+  }
+  else
+  {
+    std::cerr << "Wrong first parameter!\n";
+    return 1;
+  }
+}
+
