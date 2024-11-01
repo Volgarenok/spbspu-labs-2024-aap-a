7c3330215b3a0592a7542f5ddeda24cf57fad9c2
vladislavmaslovskiy <vladuisik826@gmail.com>
2024-11-01 18:24:41 +0300

    Add input number logic

--- maslovskiy.vladislav/P3/main.cpp
+++ maslovskiy.vladislav/P3/main.cpp
@@ -1,2 +1,32 @@
-int main()
-{}
+#include <stdexcept>
+#include <string>
+#include <iostream>
+
+int main(int argc, char** argv)
+{
+  int num = 0;
+  try
+  {
+    if (argc != 4)
+    {
+      throw std::logic_error("Wrong number of parametres");
+    }
+    const char *str = argv[1];
+    char *str_end = nullptr;
+    long long num = 0;
+    num = std::strtoll(str, &str_end, 10);
+    if (*str_end != '\0' || num == 0)
+    {
+      throw std::logic_error("Can not parse value");
+    }
+    if (num != 1 && num != 2)
+    {
+      throw std::logic_error("Incorrect parametr");
+    }
+  }
+  catch (const std::logic_error &e)
+  {
+    std::cerr << e.what() << "\n";
+    return 1;
+  }
+}
