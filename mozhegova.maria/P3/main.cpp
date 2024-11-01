b6cfc0a29d47d38023689a648ea29bd36ea2123d
mipxe <mariamozegova8726@gmail.com>
2024-11-01 20:51:22 +0300

    checking command line parameters

--- mozhegova.maria/P3/main.cpp
+++ mozhegova.maria/P3/main.cpp
@@ -1,2 +1,26 @@
-int main()
-{}
+#include <iostream>
+#include <cctype>
+
+int main(int argc, char ** argv)
+{
+  if (argc < 4)
+  {
+    std::cerr << "Not enough arguments\n";
+    return 1;
+  }
+  if (argc > 4)
+  {
+    std::cerr << "Too many arguments\n";
+    return 1;
+  }
+  if (mozhegova::isNumber(argv[1]) && argv[1] != "1" && argv[1] != "2")
+  {
+    std::cerr << "First parameter is out of range\n";
+    return 1;
+  }
+  if (!mozhegova::isNumber(argv[1]))
+  {
+    std::cerr << "First parameter is not a number\n";
+    return 1;
+  }
+}
