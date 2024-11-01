d42c6c9333836531c54b549020b9b850b945f5ee
vladislavmaslovskiy <vladuisik826@gmail.com>
2024-11-01 19:14:21 +0300

    Add inputMatrix

--- maslovskiy.vladislav/P3/main.cpp
+++ maslovskiy.vladislav/P3/main.cpp
@@ -1,10 +1,34 @@
 #include <stdexcept>
 #include <string>
 #include <iostream>
+#include <fstream>
+
+void inputMatrix(std::istream &in, int *matrix, size_t matrixSize)
+{
+  for (size_t i = 0; i < matrixSize; ++i)
+  {
+    in >> matrix[i];
+  }
+  if (!in)
+  {
+    throw std::logic_error("Incorrect matrix");
+  }
+}
+void outMatrix(int *matrix, size_t rows, size_t cols)
+{
+  for (size_t i = 0; i < rows; ++i)
+  {
+    for(size_t j = 0; j < cols; ++j)
+    {
+      std::cout << matrix[i * rows + j] << ' ';
+    }
+    std::cout << '\n';
+  }
+}
 
 int main(int argc, char** argv)
 {
-  int num = 0;
+  long long num = 0;
   try
   {
     if (argc != 4)
@@ -13,7 +37,6 @@ int main(int argc, char** argv)
     }
     const char *str = argv[1];
     char *str_end = nullptr;
-    long long num = 0;
     num = std::strtoll(str, &str_end, 10);
     if (*str_end != '\0' || num == 0)
     {
@@ -28,5 +51,34 @@ int main(int argc, char** argv)
   {
     std::cerr << e.what() << "\n";
     return 1;
+  }
+  size_t rows = 0;
+  size_t cols = 0;
+  std::ifstream in(argv[2]);
+  in >> rows >> cols;
+  if (!in)
+  {
+    std::cerr << "Can not read size of matrix" << '\n';
+    return 2;
+  }
+  size_t matrixSize = cols * rows;
+  int matrix[10000] = {0};
+  try
+  {
+    if (num == 1)
+    {
+      inputMatrix(in, matrix, matrixSize);
+      outMatrix(matrix, rows, cols);
+    }
+    if (num == 2)
+    {
+      int *dynamicMatrix = new int[matrixSize];
+      inputMatrix(in, dynamicMatrix, matrixSize);
+    }
+  }
+  catch (std::logic_error &e)
+  {
+    std::cerr << e.what() << '\n';
+    return 3;
   }
 }
