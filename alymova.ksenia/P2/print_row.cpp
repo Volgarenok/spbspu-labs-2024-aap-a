0745e6a004738a3818019fc053b91ff44c813c08
Ksenia-rgb <ksenialymova@mail.ru>
2024-11-02 00:50:04 +0300

    fixing spaces

--- alymova.ksenia/P2/print_row.cpp
+++ alymova.ksenia/P2/print_row.cpp
@@ -2,14 +2,14 @@
 #include <stdexcept>
 void alymova::print_row(double x, int max_summand, double error)
 {
-    double res_math = alymova::arctg(x);
-    try
-    {
-        double res_taylor = alymova::taylor_row(x, max_summand, error);
-        alymova::print(x, res_taylor, res_math);
-    }
-    catch(const std::logic_error& e)
-    {
-        alymova::print_error(x, res_math);
-    }
+  double res_math = alymova::arctg(x);
+  try
+  {
+    double res_taylor = alymova::taylor_row(x, max_summand, error);
+    alymova::print(x, res_taylor, res_math);
+  }
+  catch(const std::logic_error& e)
+  {
+    alymova::print_error(x, res_math);
+  }
 }
