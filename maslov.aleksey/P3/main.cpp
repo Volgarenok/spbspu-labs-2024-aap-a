9ee65f7e9959f8dcb27ad4487393d43c0ea57f47
A1exMas1ov <ae1ex.mas1ov@gmail.com>
2024-11-01 23:08:12 +0300

    Checking arguments

--- maslov.aleksey/P3/main.cpp
+++ maslov.aleksey/P3/main.cpp
@@ -1,1 +1,33 @@
-int main() {}
+#include <iostream>
+#include <fstream>
+#include <cstdlib>
+
+int main(int argc, char ** argv)
+{
+  if (argc < 4)
+  {
+    std::cerr << "Not enough arguments\n";
+    return 1;
+  }
+  if (argc > 4)
+  {
+    std::cerr << "Too many arguments\n";
+    return 1;
+  }
+  int mainNumber = 0;
+  char* endptr = nullptr;
+  long taskNumber = std::strtol(argv[1], &endptr, 10);
+  if (endptr != "\0")
+  {
+    std::cerr << "First parameter is not a number\n";
+    return 1;
+  }
+  if(!(taskNumber == 1 || taskNumber == 2))
+  {
+    std::cerr << "First parameter is out of range\n";
+    return 1;
+  }
+  
+  std::ifstream input(argv[2]);
+  std::ofstream output(argv[3]);
+}
