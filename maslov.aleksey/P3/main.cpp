85e709bbe85d375cee63a28939b70543997d8665
A1exMas1ov <ae1ex.mas1ov@gmail.com>
2024-11-02 00:47:50 +0300

    Functions createMatrix, destroy, convert

--- maslov.aleksey/P3/main.cpp
+++ maslov.aleksey/P3/main.cpp
@@ -14,6 +14,45 @@ std::istream & inputMatrix(std::istream & in, int** t, size_t m, size_t n, size_
   }
   return in;
 }
+int * convert(const int* const* t, size_t m, size_t n)
+{
+  int array[10000];
+  for (size_t i = 0; i < m; i++)
+  {
+    for (size_t j = 0; j < n; j++)
+    {
+      array[i * n + j] = t[i][j];
+    }
+  }
+  return array;
+}
+void destroy(int** theMatrix, size_t m)
+{
+  for (size_t i = 0; i < m; ++i)
+  {
+    delete[] theMatrix[i];
+  }
+  delete[] theMatrix;
+}
+
+int** createMatrix(size_t m, size_t n)
+{
+  int** theMatrix = new int*[m];
+  size_t created = 0;
+  try
+  {
+    for(; created < m; ++created)
+    {
+      theMatrix[created] = new int[n];
+    }
+  }
+  catch(const std::bad_alloc & e)
+  {
+    destroy(theMatrix, created);
+    throw;
+  }
+  return theMatrix;
+}
 void outputMatrix(std::ostream &out);
 
 int main(int argc, char ** argv)
@@ -42,7 +81,17 @@ int main(int argc, char ** argv)
   }
   size_t m = 10, n = 10;
   size_t read = 0;
-  int ** t = alloc(m, n);
+  int ** t = nullptr;
+  try
+  {
+    t = createMatrix(m,n);
+  }
+  catch(const std::bad_alloc & e)
+  {
+    std::cerr << "Out of memory\n";
+    return 1;
+  }
+
   std::ifstream input(argv[2]);
   try
   {
@@ -53,6 +102,14 @@ int main(int argc, char ** argv)
   {
     std::cerr << "" << e.what() << "\n";
     return 1;
+  }
+  if (taskNumber == 1)
+  {
+    convert(t, m, n);
+    destroy(t,m);
+  }
+  else
+  {
   }
   std::ofstream output(argv[3]);
   outputMatrix(output);
