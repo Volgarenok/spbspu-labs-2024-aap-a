047f3435a5043ba8896e5f80b8cc4a2d1e47e8e6
root <root@DESKTOP-KOVAQT8.localdomain>
2024-11-03 01:10:21 +0300

    added arguments to main

--- balashov.dima/P3/main.cpp
+++ balashov.dima/P3/main.cpp
@@ -1,3 +1,18 @@
-int main()
+#include <iostream>
+#include <fstream>
+
+int main(int argc, char** argv)
 {
+  if (argc < 4)
+  {
+    std::cerr << "Not enough arguments\n";
+    return 1;
+  }
+  if (argc > 4)
+  {
+    std::cerr << "Too many arguments\n";
+    return 1;
+  }
+  std::ifstream input(argv[2]);
+  std::ofstream output(argv[3]);
 }
