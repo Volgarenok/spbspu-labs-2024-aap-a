2b2f784ac90a4c80e28664aadd82700348ba8007
sharksonk <sharksonk@gmail.com>
2024-11-02 06:31:20 +0300

    add errors and snyaga

--- sharifullina.sofia/P2/main.cpp
+++ sharifullina.sofia/P2/main.cpp
@@ -1,2 +1,24 @@
+#include <iostream>
+#include <cmath>
+
+namespace sharifullina
 int  main()
-{}
+{
+  double  left = 0.0, right = 0.0;
+  size_t k = 0;
+  std::cin >> left >> right >> k;
+
+  if (!std::cin)
+  {
+    std::cerr << "Not a number\";
+    return 1;
+  }
+  if (left <= -1 || right >= 1 || left > right)
+  {
+    std::cerr << "Incorrect interval\n";
+    return 1;
+  }
+
+  const double error = 0.001;
+  const double step = 0.05;
+}
