a17a9a2fa3c24a4032e1e3942e65878b6e71c5ac
alina <duhaninaalina3@gmail.com>
2024-11-02 12:31:11 +0300

    Input and input errors

--- duhanina.alina/P3/main.cpp
+++ duhanina.alina/P3/main.cpp
@@ -22,11 +22,32 @@ std::istream & input_matrix(std::istream & in, int * t, size_t m, size_t n, size
 
 int main(int argc, char ** argv)
 {
-  for (size_t i = 0; i < argc; ++i)
+  if (argc != 4)
   {
-    
+    std::cerr << "Incorrect amount of data entered";
+    return 1;
+  }
+  int num = 0;
+  try
+  {
+    num = std::stoi(argv[1]);
+  }
+  catch (const std::logic_error & e)
+  {
+    std::cerr << "Invalid num";
+    return 1;
+  }
+  if (num != 1 || num != 2)
+  {
+    std::cerr << "Invalid value num";
+    return 1;
   }
   size_t m = 0, n = 0;
   input >> m >> n;
+  if (!(input >> m >> n) || m == 0 || n == 0)
+  {
+    std::cerr << "Invalid table size data";
+    return 2;
+  }
   size_t read = 0;
 }
