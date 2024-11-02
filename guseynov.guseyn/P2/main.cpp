5172c009358fa74e620930a56fb5b421d1e2afff
GuseynovGuseynGG <ganx.gamer@gmail.com>
2024-11-02 22:39:31 +0300

    oof oof oof

--- guseynov.guseyn/P2/main.cpp
+++ guseynov.guseyn/P2/main.cpp
@@ -6,13 +6,18 @@ int main()
 {
   constexpr double error = 0.001;
   constexpr double step = 0.05;
-  constexpr double min = -1.0;
-  constexpr double max = 1.0;
   double left = 0.0;
   double right = 0.0;
   size_t k = 0;
   std::cin >> left >> right >> k;
-  if ((!std::cin) or (left <= min) or (right >= max) or (left > right))
+  if (!std::cin)
+  {
+    return 1;
+  }
+  bool protectionmin = left <= -1.0;
+  bool protectionmax = right >= 1.0;
+  bool protectionlogic = left > right;
+  if (protectionmin or protectionmax or protectionlogic)
   {
     return 1;
   }
