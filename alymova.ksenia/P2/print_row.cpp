1dc0c5d46e863009ae16d4d1d7edc9ab53c8e7cc
Ksenia-rgb <ksenialymova@mail.ru>
2024-11-02 16:20:37 +0300

    adding space

--- alymova.ksenia/P2/print_row.cpp
+++ alymova.ksenia/P2/print_row.cpp
@@ -8,7 +8,7 @@ void alymova::print_row(double x, int max_summand, double error)
     double res_taylor = alymova::taylor_row(x, max_summand, error);
     alymova::print(x, res_taylor, res_math);
   }
-  catch(const std::logic_error& e)
+  catch (const std::logic_error& e)
   {
     alymova::print_error(x, res_math);
   }
