f4e29cef21487f533d7836ccc1fcc34a93f449c0
Maksim_Shramko <maxramko@gmail.com>
2024-11-03 00:59:53 +0300

    Bugfix attemt 2

--- shramko.maksim/P3/main.cpp
+++ shramko.maksim/P3/main.cpp
@@ -20,23 +20,24 @@ int main(int argc, char** argv)
   }
 
   const char* str = argv[1];
-  char* pEnd;
-  long int number = 0;
-  number = std::strtol(str, &pEnd, 10);
 
-  if (number != "1" && number != "2")
-  {
-    std::cerr << "First parameter is out of range\n";
-    return 1;
-  }
-
-  for (size_t i = 0; i < strlen(str); ++i)
+  for (size_t i = 0; i < strlen(str); i++)
   {
     if (str[i] < '0' || str[i] > '9')
     {
       std::cerr << "First parameter is not a number\n";
       return 1;
     }
+  }
+
+  char* pEnd;
+  long int number = 0;
+  number = std::strtol(str, &pEnd, 10);
+
+  if (number != 1 && number != 2)
+  {
+    std::cerr << "First parameter is out of range\n";
+    return 1;
   }
 
   std::ifstream finput(argv[2]);
