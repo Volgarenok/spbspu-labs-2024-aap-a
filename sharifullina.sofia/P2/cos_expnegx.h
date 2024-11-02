d1f295b617b11460ceb961e7a6ed5b6dc3fc41b5
sharksonk <sharksonk@gmail.com>
2024-11-02 20:53:21 +0300

    add output.h and cos_expnegx.h

---
+++ sharifullina.sofia/P2/cos_expnegx.h
@@ -0,0 +1,13 @@
+#ifndef COS_EXPNEGX_H
+#define COS_EXPNEGX_H
+
+namespace sharifullina
+
+{
+  double cos(double x, size_t k, double error);
+  double exp_negx(double x, size_t k, double error);
+  double cos_cmath(double x);
+  double exp_negx_cmath(double x);
+}
+
+#endif
