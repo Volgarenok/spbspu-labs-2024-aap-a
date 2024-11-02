35859ed8b7dcd1b9b07771a5a19e8ca28894c17d
Maksim_Shramko <maxramko@gmail.com>
2024-11-02 20:18:24 +0300

    Creating conditions for invalid inputs

--- shramko.maksim/P3/main.cpp
+++ shramko.maksim/P3/main.cpp
@@ -1,2 +1,37 @@
-int main()
-{}
+#include <iostream>
+#include <cdtring>
+
+int main(int argc, char** argv)
+{
+  if (argc < 4)
+  {
+    std::cerr << "Not enough arguments\n";
+    return 1;
+  }
+
+  if (argc > 4)
+  {
+    std::cerr << "Too many arguments\n";
+    return 1;
+  }
+
+  const char* str = argv[1];
+  char* pEnd;
+  long int number = 0;
+  number = str::strtol(str, &pEnd, 10)
+
+  if (number != 1 && number != 2)
+  {
+    std::cerr << "First parameter is out of range\n";
+    return 1;
+  }
+
+  for (size_t i = 0; i < strlen(str); ++i)
+  {
+    if (str[i] < "0" || str[i] > "9")
+    {
+      std::cerr << "First parameter is not a number\n";
+      return 1;
+    }
+  }
+}
