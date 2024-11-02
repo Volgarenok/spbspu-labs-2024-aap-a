6bec31ae8ba2d28aae8dd3a1fbc8c8e59948a0e5
fox <e.kizhin@yandex.ru>
2024-11-02 22:34:17 +0300

    Update main.cpp
    - Changed headers order accoring to CG
    - Block with files input/output was moved to try-catch to avoid
      repeating return operator, deallocation and std::cerr using

--- kizhin.evgeniy/P3/main.cpp
+++ kizhin.evgeniy/P3/main.cpp
@@ -1,8 +1,7 @@
-#include "matrix_utils.hpp"
-#include "memory_utils.hpp"
-#include "parse_arguments.hpp"
 #include <fstream>
 #include <iostream>
+#include "matrix_utils.hpp"
+#include "parse_arguments.hpp"
 
 int main(int argc, char** argv)
 {
@@ -13,7 +12,7 @@ int main(int argc, char** argv)
   char* fileOut = nullptr;
   try {
     kizhin::checkArgumentsCount(argc);
-    mode = kizhin::parseProgrammMode(argv[1]);
+    mode = kizhin::parseProgramMode(argv[1]);
     fileIn = argv[2];
     fileOut = argv[3];
   } catch (const std::logic_error& error) {
@@ -21,32 +20,28 @@ int main(int argc, char** argv)
     return static_cast<int>(ExitCode::failArguments);
   }
 
-  std::ifstream in(fileIn);
-  if (!in.is_open()) {
-    std::cerr << "Can't open file: " << fileIn << '\n';
-    return static_cast<int>(ExitCode::failFile);
-  }
-  size_t rows = 0;
-  size_t columns = 0;
+  int* matrix = nullptr;
+  try {
+    std::ifstream in(fileIn);
+    if (!in.is_open()) {
+      throw std::runtime_error("Failed to open input file");
+    }
+    size_t rows = 0;
+    size_t columns = 0;
+    in >> rows >> columns;
+    matrix = kizhin::allocateArray(rows * columns, mode);
+    if (!kizhin::readArrayValues(in, matrix, rows * columns)) {
+      throw std::runtime_error("Failed to read matrix from file");
+    }
 
-  in >> rows >> columns;
-  int* matrix = kizhin::allocateArray(rows * columns, mode);
-  kizhin::readArrayValues(in, matrix, rows * columns);
-  if (!in.good()) {
-    std::cerr << "Error during reading matrix from file\n";
-    in.close();
-    kizhin::deallocateArray(matrix, mode);
+    std::ofstream out(fileOut);
+    if (!out.is_open()) {
+      throw std::runtime_error("Failed to open ouput file");
+    }
+    out << kizhin::countLocalMinimums(matrix, rows, columns);
+  } catch (const std::exception& error) {
+    std::cerr << error.what() << '\n';
     return static_cast<int>(ExitCode::failFile);
   }
-  in.close();
-
-  std::ofstream out(fileOut);
-  if (!out.is_open()) {
-    std::cerr << "Can't open file: " << fileOut << '\n';
-    kizhin::deallocateArray(matrix, mode);
-    return static_cast<int>(ExitCode::failFile);
-  }
-  out << kizhin::countLocalMinimums(matrix, rows, columns);
-  out.close();
   kizhin::deallocateArray(matrix, mode);
 }
