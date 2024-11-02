f103ea4bfbc961d01bb2c99cb8afd4caa061bd5b
alina <duhaninaalina3@gmail.com>
2024-11-02 13:05:11 +0300

    table in main

--- duhanina.alina/P3/main.cpp
+++ duhanina.alina/P3/main.cpp
@@ -2,18 +2,13 @@
 #include <fstream>
 
 
-std::istream & input_matrix(std::istream & in, int * t, size_t m, size_t n, size_t & read)
+std::istream & inputMatrix(std::istream & in, int * t, size_t m, size_t n, size_t & read)
 {
   size_t size_table = n * m;
   for (size_t i = 0; i < size_table; ++i)
   {
     in >> t[i];
     ++read;
-    if (!(in >> t[i]))
-    {
-      std::cerr << "Invalid input data";
-      return 2;
-    }
   }
   return in;
 }
@@ -42,6 +37,8 @@ int main(int argc, char ** argv)
     std::cerr << "Invalid value num";
     return 1;
   }
+  std::ifstream input(argv[2]);
+  std::ofstream output(arg[3]);
   size_t m = 0, n = 0;
   input >> m >> n;
   if (!(input >> m >> n) || m == 0 || n == 0)
@@ -49,5 +46,32 @@ int main(int argc, char ** argv)
     std::cerr << "Invalid table size data";
     return 2;
   }
+  if (num == 1)
+  {
+    int table[1000];
+    inputMatrix(input, table, m, n);
+    if (!(input_matrix(input, table, m, n)))
+    {
+      std::cerr << "Invalid input";
+      return 2;
+    }
+    outputMatrix(output, table, m, n);
+  }
+  else
+  {
+    int * table = new int[m * n];
+    input_matrix(input, table, m, n);
+    if (!(input_matrix(input, table, m, n)))
+    {
+      std::cerr << "Invalid input";
+      delete[] table;
+      return 2;
+    }
+    outputMatrix(output, table, m, n);
+    delete[] table;
+  }
   size_t read = 0;
+  return 0;
 }
+
+
