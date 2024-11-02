4b01b9c2aeaa731a2449694659523520f7821117
hahahapolinka <gavrilova.pv@edu.spbstu.ru>
2024-11-02 16:26:59 +0300

    Remove whitespaces 3.0

--- gavrilova.polina/P3/input_matrix.cpp
+++ gavrilova.polina/P3/input_matrix.cpp
@@ -5,6 +5,7 @@
 std::istream & input_matrix(std::istream & in, int * mtx, size_t m, size_t n, size_t & read){
   if (m == 0 || n == 0) {
     return in;
+  }
   for (size_t i = 0; i < m; ++i) {
     for (size_t j = 0; j < n; ++j) {
       if (in >> mtx[i * n + j]) {
@@ -15,5 +16,4 @@ std::istream & input_matrix(std::istream & in, int * mtx, size_t m, size_t n, si
     }
   }
     return in;
-  }
 }
