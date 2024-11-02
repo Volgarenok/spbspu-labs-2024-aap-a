d057f9c76da9b3b17af55516e72bb2778f95a531
lee <likachich12@gmail.com>
2024-11-02 17:45:44 +0300

    3

--- hismatova.elizaveta/P3/matrix.cpp
+++ hismatova.elizaveta/P3/matrix.cpp
@@ -3,7 +3,7 @@
 
 hismatova::std::istream & input_matrix(std::istream & in, int * matrix, size_t m, size_t n, size_t & read)
 {
-  for (size_t i = 0; i < (m * n); ++i) 
+  for (size_t i = 0; i < (m * n); ++i)
   {
     in >> matrix[i];
     ++read;
@@ -17,8 +17,6 @@ void output_matrix(std::ostream & out, int * matrix, size_t m, size_t n)
     out << matrix[i] << " ";
   }
 }
-int find_longest(int * matrix, int size) 
-{
-  
-}
+int find_longest(int * matrix, int size)
+{}
 
