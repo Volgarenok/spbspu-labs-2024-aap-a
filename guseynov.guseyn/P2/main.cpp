b0c3e7f1cf573f377d202f499f1bb2d0508641ff
GuseynovGuseynGG <ganx.gamer@gmail.com>
2024-11-02 17:55:22 +0300

    last correct for P2

--- guseynov.guseyn/P2/main.cpp
+++ guseynov.guseyn/P2/main.cpp
@@ -6,11 +6,13 @@ int main()
 {
   constexpr double error = 0.001;
   constexpr double step = 0.05;
+  constexpr double min = -1.0;
+  constexpr double max = 1.0;
   double left = 0.0;
   double right = 0.0;
   size_t k = 0;
   std::cin >> left >> right >> k;
-  if ((!std::cin) or (left <= (-1.0)) or (right >= 1.0) or (left > right))
+  if ((!std::cin) or (left <= min) or (right >= max) or (left > right))
   {
     return 1;
   }
