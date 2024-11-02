0ea2f27aed45b612c2e9ccc103af807a219b6c33
alina <duhaninaalina3@gmail.com>
2024-11-02 18:14:03 +0300

    error

--- duhanina.alina/P3/outputAnswer.cpp
+++ duhanina.alina/P3/outputAnswer.cpp
@@ -6,24 +6,24 @@
 void duhanina::outputAnswer(std::ostream & out, int * t, size_t m, size_t n)
 {
   int max_count_diag = m + n - 1;
-  int sums[max_count_diag] = {0};
-  int diag_count[max_count_diag] = {0};
+  int * sums = new int[max_count_diag];
+  int * diag_count = new int[max_count_diag];
   for (size_t i = 0; i < m; ++i)
   {
     for (size_t j = 0; j < m; ++j)
     {
       int index_diag = j - (m - i - 1);
-      sums[index_diag] += table[i * n + j];
+      sums[index_diag] += t[i * n + j];
       diag_count[index_diag]++;
     }
   }
-int min_sum = std::numeric_limits<int>::max();
-for (int i = 0; i < max_count_diag; ++i)
-{
-  if (diag_count[i] > 0 && sums[i] < min_sum)
+  int min_sum = std::numeric_limits<int>::max();
+  for (int i = 0; i < max_count_diag; ++i)
   {
-    min_sum = sums[i];
+    if (diag_count[i] > 0 && sums[i] < min_sum)
+    {
+      min_sum = sums[i];
+    }
   }
-}
-out << min_sum << "\n";
+  out << min_sum << "\n";
 }
