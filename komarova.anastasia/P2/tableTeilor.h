05bf6deb9419a3ab5a4bcdb10597a871854194b3
NastyKomar <komarova2.ae@edu.spbstu.ru>
2024-11-02 13:57:18 +0300

    mblast

--- komarova.anastasia/P2/tableTeilor.h
+++ komarova.anastasia/P2/tableTeilor.h
@@ -4,7 +4,7 @@
 namespace komarova
 {
   double teilor(double x, const size_t  k, const double error);
-  double ln_Teilor(double x);
+  double lnTeilor(double x);
   void matherr(double x, double y);
   void output(double x, double value, double y);
 }
