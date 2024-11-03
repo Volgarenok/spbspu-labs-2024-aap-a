cfcec69077369010aadf60101ab4d2ad337f2c8b
GuseynovGuseynGG <ganx.gamer@gmail.com>
2024-11-03 17:01:11 +0300

    CG fix

--- guseynov.guseyn/P3/main.cpp
+++ guseynov.guseyn/P3/main.cpp
@@ -18,10 +18,10 @@ int main(int argc, char **argv)
   }
   size_t m = 0;
   size_t n = 0;
-  std::ifstream inf(argv[2]);
-  std::ifstream outf(argv[3]);
-  inf >> n >> m;
-  if (!inf)
+  std::ifstream input(argv[2]);
+  std::ifstream output(argv[3]);
+  input >> n >> m;
+  if (!input)
   {
     return 2;
   }
@@ -30,27 +30,30 @@ int main(int argc, char **argv)
   if (argv[1][0] == 1)
   {
     int arr[10000];
-    if (!inputMtx(inf, arr, general, read) || (read != general))
+    if (!inputMtx(input, arr, general, read) || (read != general))
     {
       return 2;
     }
-    outf << searchNumLogMin(arr, general) << "\n";
+    output << searchNumLogMin(arr, general) << "\n";
   }
   else
   {
-    int *arr = nullptr;
-    try
+    if (argv[1][0] == 0)
     {
-      arr = new int[general];
+      int *arr = nullptr;
+      try
+      {
+        arr = new int[general];
+      }
+      catch (const std::logic_error &e)
+      {
+        return 2;
+      }
+      if (!inputMtx(input, arr, general, read) || (read != general))
+      {
+        return 2;
+      }
+      output << searchNumLogMin(arr, general) << "\n";
     }
-    catch (const std::logic_error &e)
-    {
-      return 2;
-    }
-    if (!inputMtx(inf, arr, general, read) || (read != general))
-    {
-      return 2;
-    }
-    outf << searchNumLogMin(arr, general) << "\n";
   }
 }
