9d4a9952f9363a35d2d4f17db8a7ca05c9b75e04
sharksonk <sharksonk@gmail.com>
2024-11-03 22:18:51 +0300

    add restriction

--- sharifullina.sofia/main.cpp
+++ sharifullina.sofia/main.cpp
@@ -1,3 +1,19 @@
-int main()
+#include <iostream>
+
+int main(int argc, char ** argv)
 {
+  if (argc < 4)
+  {
+    std::cerr << "Not enough arguments\n";
+    return 1;
+  }
+  if (argc > 4)
+  {
+    std::cerr << "Too many arguments\n";
+    return 1;
+  }
+  for (int i = 0; i < argc; i++)
+  {
+    std::cout << argv[i] << "\n"
+  }
 }
