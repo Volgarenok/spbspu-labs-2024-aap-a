e3543d8a694985fc2f35336b9ecb8d15233a7057
Maksim_Shramko <maxramko@gmail.com>
2024-11-03 14:12:12 +0300

    I forgot about brackets in countGoodColoumns.cpp

--- shramko.maksim/P3/countGoodColoumns.cpp
+++ shramko.maksim/P3/countGoodColoumns.cpp
@@ -8,7 +8,7 @@ size_t shramko::countGoodColoumns(const int* mtrx, size_t cnt_row, size_t cnt_co
     bool breakPoint = true;
     for (size_t j = 1; j < cnt_row; ++j)
     {
-      if (mtrx[j - 1 * cnt_col + i] == mtrx[j * cnt_col + i])
+      if (mtrx[(j - 1) * cnt_col + i] == mtrx[j * cnt_col + i])
       {
         breakPoint = false;
         break;
