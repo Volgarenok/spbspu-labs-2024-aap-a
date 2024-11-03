337dff3145b7d9d07b5da6e8c11f6d376af34d90
GuseynovGuseynGG <ganx.gamer@gmail.com>
2024-11-03 17:03:09 +0300

    fix

--- guseynov.guseyn/P3/matrix.hpp
+++ guseynov.guseyn/P3/matrix.hpp
@@ -5,6 +5,5 @@
 #include <cstddef>
 
 std::istream & inputMtx(std::istream &in, int *arr, size_t general, size_t & read);
-void outputMtx(std::ostream &out, const int *arr, size_t m, size_t n);
 size_t searchNumLogMin(const int *arr, size_t general);
 #endif
