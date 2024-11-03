7678bb2c9b8b552e0b9d34b5fdc70a396cc7e863
lee <likachich12@gmail.com>
2024-11-03 15:51:33 +0300

    commit 13

--- hismatova.elizaveta/P3/main.cpp
+++ hismatova.elizaveta/P3/main.cpp
@@ -32,8 +32,7 @@ int main(int argc, char ** argv)
   in_par >> m >> n;
   if (!in_par || m == 0 || n == 0)
   {
-    std::cerr << "wrong m|n\n";
-    return 1;
+    return 0;
   }
   out_par << m << " " << n << "\n";
   size_t read = 0;
@@ -55,11 +54,6 @@ int main(int argc, char ** argv)
       std::cerr << "not enough elements\n";
       return 1;
     }
-    if (m == 0 || n == 0)
-    {
-      std::cerr << "empty array\n";
-      return 1;
-    }
     hismatova::output_result(out_par, matrix, m, n);
   }
   else if (parametr == 2)
@@ -75,11 +69,6 @@ int main(int argc, char ** argv)
     {
       std::cerr << "not enough elements\n";
       delete[] matrix;
-      return 1;
-    }
-    if (m == 0 || n == 0 || matrix == nullptr)
-    {
-      std::cerr << "empty matrix\n";
       return 1;
     }
     hismatova::output_result(out_par, matrix, m, n);
