a58cc45f01235cc7996b88d3547ce267f2a2b1a6
A1exCRE <alsmmm2065@gmail.com>
2024-11-03 21:31:08 +0300

    Checking the number of arguments

--- smirnov.alexandr/P3/main.cpp
+++ smirnov.alexandr/P3/main.cpp
@@ -1,2 +1,29 @@
-int main()
-{}
+#include <iostream>
+#include <cctype>
+#include <cstdlib>
+
+int main(int argc, char ** argv)
+{
+  if (argc < 4)
+  {
+    std::cerr < "Not enough arguments\n";
+    return 1;
+  }
+  if (argc > 4)
+  {
+    std::cerr < "Too many arguments\n";
+    return 1;
+  }
+  int num = std::atoi(argv[1]);
+  if (num != 1 && num != 2)
+  {
+    std::cerr < "First parameter is out of range\n";
+    return 1;
+  }
+  for (int i = 0; argv[1][i] != '\0'; ++i)
+  {
+    if (!std::isdigit(argv[1][i]))
+    {
+      std::cerr < "First parameter is not a number\n";
+  }
+}
