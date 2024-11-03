15b77b43a29798c89475f0567634999ebd513b5f
dmitei <dima9990860349@icloud.com>
2024-11-03 20:09:20 +0300

    cg

--- shabalin.dmitrij/P3/main.cpp
+++ shabalin.dmitrij/P3/main.cpp
@@ -97,7 +97,7 @@ int main(int argc, char *argv[])
   }
   catch (const std::exception &e)
   {
-    std::cerr << e.what() << '\n'; 
+    std::cerr << e.what() << '\n';
     if (task == 2)
     {
       delete[] matrix;
