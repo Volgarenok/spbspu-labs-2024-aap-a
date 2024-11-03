68bbdb4d0dd18dda89e5d617d5e096dbf6cbc24d
sharksonk <sharksonk@gmail.com>
2024-11-03 23:25:42 +0300

    add trycatch shtuku

--- sharifullina.sofia/main.cpp
+++ sharifullina.sofia/main.cpp
@@ -16,4 +16,26 @@ int main(int argc, char ** argv)
   {
     std::cout << argv[i] << "\n"
   }
+  if (sharifullina::isNumber(argv[1] && argv[1] != "1" && argv[1] != "2")
+  {
+    std::cerr << "First parameter is out of range\n";
+    return 1;
+  }
+  if (!sharifullina::isNumber(argv[1]))
+  {
+    std::cerr << "First parameter is not a number\n";
+    return 1;
+  }
+  size_t m = 0, n = 0;
+  int ** t = nullptr;
+  try
+  {
+    t = sharifullina::inMatrix(m, n);
+  }
+  catch(const std::bad_alloc & e)
+  {
+    std::cerr << "Out of memory\n";
+    return 2;
+  }
 }
+
