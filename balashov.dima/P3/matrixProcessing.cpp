3e9c34e42737209d879752b127c8e39434d10f25
root <root@DESKTOP-KOVAQT8.localdomain>
2024-11-04 00:01:01 +0300

    spaces

--- balashov.dima/P3/matrixProcessing.cpp
+++ balashov.dima/P3/matrixProcessing.cpp
@@ -5,16 +5,16 @@ namespace balashov
   int minSumSideDiagonal(const int matrix[], size_t column, size_t row)
   {
     int minSumSideDiagonals = matrix[0];
-    for (size_t i = 0; i < column; ++i)
+    for(size_t i = 0; i < column; ++i)
     {
-      for (size_t j = 0; j < row; ++j)
+      for(size_t j = 0; j < row; ++j)
       {
         bool isSideDiagonal = (j + 1 == row) && (i == 0);
         bool isParallelSideDiagonal = (j + 1 == row) || (i == 0);
-        if (isParallelSideDiagonal && isSideDiagonal == false)
+        if(isParallelSideDiagonal && isSideDiagonal == false)
         {
           int sumDiagonalNow = diagonalCalculation(matrix, i, column, j, row);
-          if (minSumSideDiagonals > sumDiagonalNow)
+          if(minSumSideDiagonals > sumDiagonalNow)
           {
             minSumSideDiagonals = sumDiagonalNow;
           }
@@ -28,7 +28,7 @@ namespace balashov
     int rowNow, size_t rowMax)
   {
     int sumDiagonal = 0;
-    while (rowNow >= 0 && columnNow < static_cast<int>(columnMax))
+    while(rowNow >= 0 && columnNow < static_cast<int>(columnMax))
     {
       sumDiagonal += matrix[columnNow * rowMax + rowNow];
       rowNow--;
