010b27272134f72806479da810e6b7dfab2d1acd
root <root@DESKTOP-KOVAQT8.localdomain>
2024-11-04 01:53:52 +0300

    add space

--- balashov.dima/P3/stream.cpp
+++ balashov.dima/P3/stream.cpp
@@ -5,9 +5,9 @@ namespace balashov
   std::istream & inputMatrix(std::istream & in, int * matrix, size_t column, size_t row, size_t & read)
   {
     size_t count = row * column;
-    for(size_t i =0; i < count; ++i)
+    for (size_t i =0; i < count; ++i)
     {
-      if(!(in >> matrix[i]))
+      if (!(in >> matrix[i]))
       {
         ++read;
       }
