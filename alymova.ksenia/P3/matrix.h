d4da51d4244f2d3f8c43f8a6ca244c83278f8a20
Ksenia-rgb <ksenialymova@mail.ru>
2024-11-03 23:06:51 +0300

    fixing tabs

--- alymova.ksenia/P3/matrix.h
+++ alymova.ksenia/P3/matrix.h
@@ -4,9 +4,9 @@
 #include <istream>
 namespace alymova
 {
-  int alymova::check_size(size_t rows, size_t cols);
+  int check_size(size_t rows, size_t cols);
   std::istream& input_matrix(std::istream& input, int* const matrix, size_t& rows, size_t& cols);
   void print_matrix(std::ostream& output, const int* const matrix, size_t rows, size_t cols);
-  void change_matrix(int* const matrix, size_t rows, size_t cols);  
+  void change_matrix(int* const matrix, size_t rows, size_t cols);
 }
 #endif
