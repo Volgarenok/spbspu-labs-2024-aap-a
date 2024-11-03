67f3432752c0e556062b5cae787e36a9fef190c6
lee <likachich12@gmail.com>
2024-11-03 12:15:58 +0300

    functions

--- hismatova.elizaveta/P3/matrix.cpp
+++ hismatova.elizaveta/P3/matrix.cpp
@@ -1,5 +1,7 @@
 #include "matrix.h"
+
 #include <iostream>
+#include <fstream>
 
 hismatova::std::istream & input_matrix(std::istream & in, int * matrix, size_t m, size_t n, size_t & read)
 {
@@ -10,13 +12,43 @@ hismatova::std::istream & input_matrix(std::istream & in, int * matrix, size_t m
   }
   return in;
 }
-void output_matrix(std::ostream & out, int * matrix, size_t m, size_t n)
+
+void hismatova::output_matrix(std::ostream & out, int * matrix, size_t m, size_t n)
 {
   for (size_t i = 0; i < (m * n); ++i)
   {
     out << matrix[i] << " ";
   }
 }
-int find_longest(int * matrix, int size)
-{}
 
+int hismatova::find_longest(int * matrix, size_t m, size_t n)
+{
+  int ans = 0;
+  int count = 1;
+  int vr = 0;
+  for (size_t i = 0; i < m; i++)
+  {
+    for (size_t j = 0; j < n; j++)
+    {
+      vr = j + 1;
+      while (vr < n && matrix[vr][i] == matrix[j][i]) 
+      {
+        count++;
+      }
+      if (count < vr - j)
+      {
+        count = vr - j;
+        ans = i;
+      }
+    }
+  }
+  return ans;
+}
+
+bool hismatova::is_digit(char c)
+{
+  if (c >= "0" && c <= "9") {
+    return true;
+  }
+  return false;
+}
--- hismatova.elizaveta/P3/matrix.h
+++ hismatova.elizaveta/P3/matrix.h
@@ -8,8 +8,8 @@ namespace hismatova
 {
   std::istream & input_matrix(std::istream & in, int * matrix, size_t m, size_t n, size_t & read);
   void output_matrix(std::ostream & out, int * matrix, size_t m, size_t n);
-  void change_matrix(int * matrix, size_t m, size_t n);
-  int find_longest(int * matrix, int size);
+  int find_longest(int * matrix, size_t m, size_t n);
+  bool is_digit(char c);
 }
 
 #endif
