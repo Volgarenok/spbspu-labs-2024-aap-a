a0e2fe23717be0d78ec37ab03d8656815881cd59
dmitei <dima9990860349@icloud.com>
2024-11-03 20:08:26 +0300

    ds

--- shabalin.dmitrij/P3/matrix_operations.cpp
+++ shabalin.dmitrij/P3/matrix_operations.cpp
@@ -4,15 +4,21 @@
 void shabalin::spiralDecrease(int *matrix, size_t rows, size_t cols)
 {
   size_t i = 0, j = 0;
-  size_t rowBegin = 0, rowEnd = 0, colBegin = 0, colEnd = 0;
+  size_t rowStart = 0, rowEnd = 0, colStart = 0, colEnd = 0;
 
   for (size_t k = 1; k < rows * cols; ++k)
   {
     matrix[i * cols + j] = k;
-    (i == rowBegin && j < cols - colEnd - 1) ? ++j :
+    (i == rowStart && j < cols - colEnd - 1) ? ++j :
     (j == cols - colEnd - 1 && i < rows - rowEnd - 1) ? ++i :
-    (i == rows - rowEnd - 1 && j > colBegin) ? --j : --i;
-    (colBegin <= colEnd) ? ++colBegin : (rowBegin <= rowEnd) ? --rowEnd : throw;
+    (i == rows - rowEnd - 1 && j > colStart) ? --j : --i;
+    if ((i == rowStart + 1) && (j == colStart) && (colStart != cols - colEnd - 1))
+    {
+      ++rowStart;
+      ++rowEnd;
+      ++colStart;
+      ++colEnd;
+    }
   }
   return;
 }
