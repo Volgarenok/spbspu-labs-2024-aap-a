d1b0ef8fa1eba5e557ca901a7c035ddbe36a3987
Ksenia-rgb <ksenialymova@mail.ru>
2024-11-03 23:42:03 +0300

    fixing

--- alymova.ksenia/P3/print_matrix.cpp
+++ alymova.ksenia/P3/print_matrix.cpp
@@ -3,9 +3,12 @@
 #include <cstddef>
 void alymova::print_matrix(std::ostream& output, const int* const matrix, size_t rows, size_t cols)
 {
-  output << matrix[0];
-  for (size_t i = 1; i < rows * cols; i++)
+  output << rows << " " << cols;
+  if (rows != 0 && cols != 0)
   {
-    output << " " << matrix[i];
+    for (size_t i = 0; i < rows * cols; i++)
+    {
+      output << " " << matrix[i];
+    }
   }
 }
