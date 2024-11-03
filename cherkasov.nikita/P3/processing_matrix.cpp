25685f6f93bc1f5ddc870ea2b67012d2fb90662d
cherkasoov <cherkasov5509@gmail.com>
2024-11-03 16:52:12 +0300

    .

--- cherkasov.nikita/P3/processing_matrix.cpp
+++ cherkasov.nikita/P3/processing_matrix.cpp
@@ -13,43 +13,53 @@ void cherkasov::processFixedMatrix(const std::string & inputFile, const std::str
   }
   int rows, cols;
   inFile >> rows >> cols;
-  if (rows * cols > 10000)
+  if (inFile.fail() || rows <= 0 || cols <= 0 || rows * cols > 10000)
   {
-  std::cerr << "Error: Matrix size exceeds 10000 elements." << "\n";
-  exit(2);
+  std::cerr << "Error: Invalid matrix size or exceeds 10000 elements." << "\n";
+   exit(2);
   }
-  int matrix[10000];
-  for (int i = 0; i < rows * cols; ++i)
-  {
-    inFile >> matrix[i];
-  }
-  inFile.close();
-  int count = 0;
-  for (int d = 1; d < rows; ++d)
-  {
-    bool hasZero = false;
-    for (int i = 0; i < rows - d; ++i)
+    if (rows * cols > 10000)
     {
-      if (matrix[i * cols + (i + d)] == 0)
-      {
-        hasZero = true;
-        break;
-      }
-  }
-    if (!hasZero)
-    {
-      count++;
+    std::cerr << "Error: Matrix size exceeds 10000 elements." << "\n";
+    exit(2);
     }
-  }
-  std::ofstream outFile(outputFile);
-  if (!outFile)
-  {
-  std::cerr << "Error: Cannot open output file." << "\n";
-  exit(2);
-  }
-  outFile << count << "\n";
-  outFile.close();
-  }
+    int matrix[10000];
+    for (int i = 0; i < rows * cols; ++i)
+    {
+      inFile >> matrix[i];
+      if (inFile.fail())
+      {
+        std::cerr << "Error: Invalid matrix data." << "\n";
+        exit(2);
+      }
+    }
+      inFile.close();
+      int count = 0;
+      for (int d = 1; d < rows; ++d)
+      {
+        bool hasZero = false;
+        for (int i = 0; i < rows - d; ++i)
+        {
+          if (matrix[i * cols + (i + d)] == 0)
+          {
+            hasZero = true;
+            break;
+          }
+        }
+         if (!hasZero)
+         {
+           count++;
+         }
+    }
+      std::ofstream outFile(outputFile);
+      if (!outFile)
+      {
+        std::cerr << "Error: Cannot open output file." << "\n";
+        exit(2);
+      }
+        outFile << count << "\n";
+        outFile.close();
+}
 void cherkasov::processDynamicMatrix(const std::string& inputFile, const std::string& outputFile)
 {
   std::ifstream inFile(inputFile);
