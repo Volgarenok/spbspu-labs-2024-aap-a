2fb736ba6e694ee476e84bc7d28cc6c965f7fbe4
cherkasoov <cherkasov5509@gmail.com>
2024-11-03 15:50:31 +0300

    .

---
+++ cherkasov.nikita/P3/processing_matrix.h
@@ -0,0 +1,10 @@
+#ifndef MATRIX_PROCESSING_H
+#define MATRIX_PROCESSING_H
+#include <string>
+
+void processFixedSizeArray(const std::string& inputFile, const std::string& outputFile);
+
+void processDynamicMatrix(const std::string& inputFile, const std::string& outputFile);
+
+
+#endif
