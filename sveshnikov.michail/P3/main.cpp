fa25e252c8b9045548c34ec8a4e4e955a388e5b0
svesh3000 <russkij2006@mail.ru>
2024-11-03 22:29:20 +0300

    Corrections of "if" for arrays

--- sveshnikov.michail/P3/main.cpp
+++ sveshnikov.michail/P3/main.cpp
@@ -17,7 +17,7 @@ int main(int argc, char **argv)
   {
     if (argv[1][i] < '0' || argv[1][i] > '9')
     {
-      std::cerr << "ERROR: First parameter  is not a numberout\n";
+      std::cerr << "ERROR: First parameter is not a numberout\n";
       return 1;
     }
   }
@@ -31,7 +31,7 @@ int main(int argc, char **argv)
   input >> num_rows >> num_columns;
   size_t read = 0;
   size_t num_diagonal = 0;
-  if (argv[1][0] == '1' && argv[1][0] == '\0')
+  if (argv[1][0] == '1' && argv[1][1] == '\0')
   {
     const int LEN = 10000;
     int matrix1[LEN] = {};
@@ -47,7 +47,7 @@ int main(int argc, char **argv)
     }
     num_diagonal = sveshnikov::cnt_nzr_dig(matrix1, num_rows, num_columns);
   }
-  else if (argv[1][0] == '2' && argv[1][0] == '\0')
+  else if (argv[1][0] == '2' && argv[1][1] == '\0')
   {
     size_t len = num_columns * num_rows;
     int *matrix2 = new int[len];
@@ -68,7 +68,7 @@ int main(int argc, char **argv)
   }
   else
   {
-    std::cerr << "ERROR: First parameter is  is out of range\n";
+    std::cerr << "ERROR: First parameter is out of range\n";
     return 1;
   }
   std::ofstream output(argv[3]);
--- sveshnikov.michail/P3/read_matrix.cpp
+++ sveshnikov.michail/P3/read_matrix.cpp
@@ -2,13 +2,9 @@
 
 std::istream &sveshnikov::read_matrix(std::istream &in, int *matrix, size_t num_rows, size_t num_columns, size_t &read)
 {
-  for (size_t i = 0; i < num_rows; i++)
+  for (size_t i = 0; i < num_rows*num_columns; i++)
   {
-    for (size_t j = 0; j < num_columns; j++)
-    {
-      in >> matrix[read];
-      read++;
-    }
+    in >> matrix[read];
+    read++;
   }
-  return in;
 }
