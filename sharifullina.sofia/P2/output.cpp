43fd154fce8ca00189d927dc81f6105dc8059029
sharksonk <sharksonk@gmail.com>
2024-11-03 17:15:01 +0300

    add more colonnes

--- sharifullina.sofia/P2/output.cpp
+++ sharifullina.sofia/P2/output.cpp
@@ -7,19 +7,40 @@
 
 void sharifullina::output_row(double i, size_t k, double error)
 {
-  const size_t WidthOfMathErr = std::strlen("<MATH ERROR>") + 2;
-  const size_t WidthOfCol = 12;
-  std::cout.precision(3);
-  std::cout << std::setw(WidthOfCol) << std::fixed << i << " ";
-  try
-  {
-    std::cout << std::setw(WidthOfMathErr) << sharifullina::cos(i, k, error);
-  }
-  catch (const std::logic_error & e)
-  {
-    std::cout << std::setw(WidthOfMathErr) << "<MATH ERROR>";
-  }
-  std::cout << " " << std::setw(WidthOfCol) << sharifullina::cos_cmath(i);
+    const size_t WidthOfMathErr = std::strlen("<MATH ERROR>") + 2;
+    const size_t WidthOfCol = 12;
+    std::cout.precision(3);
+    std::cout << std::setw(WidthOfCol) << std::fixed << i << " ";
+    try
+    {
+        std::cout << std::setw(WidthOfMathErr) << sharifullina::cos(i, k, error);
+    }
+    catch (const std::logic_error& e)
+    {
+        std::cout << std::setw(WidthOfMathErr) << "<MATH ERROR>";
+    }
+    try
+    {
+        std::cout << std::setw(WidthOfMathErr) << sharifullina::exp_negx(i, k, error);
+    }
+    catch (const std::logic_error& e)
+    {
+        std::cout << std::setw(WidthOfMathErr) << "<MATH ERROR>";
+    }
+    try
+    {
+        std::cout << std::setw(WidthOfMathErr) << sharifullina::cos(i, k, error) + sharifullina::exp_negx(i, k, error);
+    }
+    catch (const std::logic_error& e)
+    {
+        std::cout << std::setw(WidthOfMathErr) << "<MATH ERROR>";
+    }
+
+    std::cout << " " << std::setw(WidthOfCol) << sharifullina::cos_cmath(i);
+    
+    std::cout << " " << std::setw(WidthOfCol) << sharifullina::exp_negx_cmath(i);
+
+    std::cout << " " << std::setw(WidthOfCol) << sharifullina::exp_negx_cmath(i) + cos_cmath(i);
 }
 
 void sharifullina::output_table(double left, double right, size_t k, double step, double error)
