cf30892468944d4e345d3d83a56940a06049e9e5
PigoDog <maksimtmfv@gmail.com>
2024-11-03 16:31:06 +0300

    Writed output function

---
+++ timofeev.maxim/P3/output_matrix.cpp
@@ -0,0 +1,18 @@
+#include "output_matrix.h"
+#include <iostream>
+
+void timofeev::output_matrix(std::ostream& out, int* matrix, size_t strk, size_t stl)
+{
+  int res = 0;
+  for (size_t i = 0; i < strk; i++)
+  {
+    for (size_t j = 1; j < (stl - 1); j++)
+    {
+      if (matrix[i * strk + j - 1] > matrix[i * strk + j] && matrix[i * strk + j + 1] > matrix[i * strk + j])
+      {
+        res += 1;
+      }
+    }
+  }
+  out << res << "\n";
+}
---
+++ timofeev.maxim/P3/output_matrix.h
@@ -0,0 +1,11 @@
+#ifndef OUTPUT_MATRIX_H
+#define OUTPUT_MATRIX_H
+
+#include <ostream>
+#include <cstddef>
+
+namespace timofeev
+{
+  void output_matrix(std::ostream& out, int* matrix, size_t strk, size_t stl);
+}
+#endif
