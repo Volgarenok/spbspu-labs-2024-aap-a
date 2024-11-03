063ce7bb6e07e184a67bdeb685b4b4e1d1fefb7f
PigoDog <maksimtmfv@gmail.com>
2024-11-03 17:41:23 +0300

    stil trying

--- timofeev.maxim/P3/input_matrix.cpp
+++ timofeev.maxim/P3/input_matrix.cpp
@@ -1,6 +1,6 @@
 #include "input_matrix.h"
 #include <stdexcept>
-#include <fstream>
+
 std::istream & timofeev::input_matrix(std::istream& in, int* matrix, size_t size_matrix)
 {
   for (size_t i = 0; i < size_matrix; i++)
