c28bd27d5c85f80fbf64f0cf8726dafa31b0cc92
dmitei <dima9990860349@icloud.com>
2024-11-03 19:52:22 +0300

    optim

--- shabalin.dmitrij/P3/matrix_operations.cpp
+++ shabalin.dmitrij/P3/matrix_operations.cpp
@@ -3,39 +3,21 @@
 
 void shabalin::spiralDecrease(int *matrix, size_t rows, size_t cols)
 {
-  size_t k = 1;
-  size_t rowBegin = 0, rowEnd = rows - 1, colBegin = 0, colEnd = cols - 1;
+  size_t i = 0, j = 0;
+  size_t rowBegin = 0, rowEnd = 0, colBegin = 0, colEnd = 0;
 
-  while (rowBegin <= rowEnd && colBegin <= colEnd)
+  for (size_t k = 1; k < rows * cols; ++k)
   {
-    for (size_t j = colBegin; j <= colEnd; ++j)
+    matrix[i * cols + j] = k;
+    (i == rowBegin && j < cols - colEnd - 1) ? ++j : (j == cols - colEnd - 1 && i < rows - rowEnd - 1)
+    ? ++i : (i == rows - rowEnd - 1 && j > colBegin) ? --j : --i;
+    if ((i == rowBegin + 1) && (j == colBegin) && (colBegin != cols - colEnd - 1))
     {
-      matrix[rowBegin * cols + j] = k++;
-    }
-    rowBegin++;
-
-    for (size_t i = rowBegin; i <= rowEnd; ++i)
-    {
-      matrix[i * cols + colEnd] = k++;
-    }
-    colEnd--;
-
-    if (rowBegin <= rowEnd)
-    {
-      for (size_t j = colEnd; j >= colBegin; --j)
-      {
-        matrix[rowEnd * cols + j] = k++;
-      }
-      rowEnd--;
-    }
-
-    if (colBegin <= colEnd)
-    {
-      for (size_t i = rowEnd; i >= rowBegin; --i)
-      {
-        matrix[i * cols + colBegin] = k++;
-      }
-      colBegin++;
+      ++rowBegin;
+      ++rowEnd;
+      ++colBegin;
+      ++colEnd;
     }
   }
+  return;
 }
