deccc9c9b259f1eae4a4451158c445ecacd32df9
Ksenia-rgb <ksenialymova@mail.ru>
2024-11-03 23:03:20 +0300

    fixing

--- alymova.ksenia/P3/matrix.h
+++ alymova.ksenia/P3/matrix.h
@@ -7,7 +7,6 @@ namespace alymova
   int alymova::check_size(size_t rows, size_t cols);
   std::istream& input_matrix(std::istream& input, int* const matrix, size_t& rows, size_t& cols);
   void print_matrix(std::ostream& output, const int* const matrix, size_t rows, size_t cols);
-  void change_matrix(int* const matrix, size_t rows, size_t cols);
-  
+  void change_matrix(int* const matrix, size_t rows, size_t cols);  
 }
 #endif
