a65aa41cdc5953551d7d3588b61a56d2adb85fa7
Nekich06 <p.nikita.73526@gmail.com>
2024-11-03 21:21:20 +0300

    Added processing matrix according my variant; added output; added some conditions
    Now program is ready to be tested

--- petrov.nikita/P3/main.cpp
+++ petrov.nikita/P3/main.cpp
@@ -10,18 +10,35 @@ int main(int argc, char ** argv)
     return 1;
   }
   std::ifstream input(argv[2]);
+  std::ofstream output(argv[3]);
   if (std::atoi(argv[1]) == 1)
   {
     size_t number_of_rows = 0, rows_length = 0;
     input >> number_of_rows;
     input >> rows_length;
-    int matrix[10000] = {0};
-    int * ptr_matrix = matrix;
-    if (!petrov::input_matrix_static(input, ptr_matrix, number_of_rows, rows_length))
+    if (!input)
     {
       std::cerr << "Error: Invalid data\n";
       return 2;
     }
+    if (number_of_rows == 0 && rows_length == 0)
+    {
+      output << 0;
+      return 0;
+    }
+    int matrix[10000] = {0};
+    int * ptr_matrix = matrix;
+    if ((number_of_rows == 0 && rows_length != 0) || (number_of_rows != 0 && rows_length == 0))
+      {
+        std::cerr << "Error: Invalid data\n";
+        return 2;
+      }
+    else if (!petrov::input_matrix_static(input, ptr_matrix, number_of_rows, rows_length))
+    {
+      std::cerr << "Error: Invalid data\n";
+      return 2;
+    }
+    output << petrov::process_matrix_data(ptr_matrix, number_of_rows, rows_length);
     for (size_t i = 0; i < number_of_rows * rows_length; i++)
     {
       std::clog << matrix[i];
@@ -32,11 +49,26 @@ int main(int argc, char ** argv)
     size_t number_of_rows = 0, rows_length = 0;
     input >> number_of_rows;
     input >> rows_length;
+    if (!input)
+    {
+      std::cerr << "Error: Invalid data\n";
+      return 2;
+    }
+    if (number_of_rows == 0 && rows_length == 0)
+    {
+      output << 0;
+      return 0;
+    }
     int * ptr_matrix = nullptr;
     try
     {
       ptr_matrix = new int[number_of_rows * rows_length];
-      if (!petrov::input_matrix_dynamic(input, ptr_matrix, number_of_rows, rows_length))
+      if ((number_of_rows == 0 && rows_length != 0) || (number_of_rows != 0 && rows_length == 0))
+      {
+        std::cerr << "Error: Invalid data\n";
+        return 2;
+      }
+      else if (!petrov::input_matrix_dynamic(input, ptr_matrix, number_of_rows, rows_length))
       {
         std::cerr << "Error: Invalid data\n";
         return 2;
@@ -47,6 +79,7 @@ int main(int argc, char ** argv)
       std::cerr << "Out of memory\n";
       return 3;
     }
+    output << petrov::process_matrix_data(ptr_matrix, number_of_rows, rows_length);
     for (size_t i = 0; i < number_of_rows * rows_length; i++)
     {
       std::clog << ptr_matrix[i];
--- petrov.nikita/P3/process_the_matrix.hpp
+++ petrov.nikita/P3/process_the_matrix.hpp
@@ -7,6 +7,7 @@ namespace petrov
   int check_arguments(int argc, char ** argv);
   std::istream & input_matrix_static(std::istream & in, int * ptr_matrix, size_t number_of_rows, size_t rows_length);
   std::istream & input_matrix_dynamic(std::istream & in, int * ptr_matrix, size_t number_of_rows, size_t rows_length);
+  int process_matrix_data(int * ptr_matrix, size_t number_of_rows, size_t rows_length);
 }
 #endif
 
