2dad9d501ad2319387c871b19ea30a7b5395dc48
Savishna <bravissimo28@mail.ru>
2024-11-05 00:58:51 +0300

    2 full remake

--- averenkov.eugene/P3/arrayFunc.cpp
+++ averenkov.eugene/P3/arrayFunc.cpp
@@ -4,7 +4,7 @@
 std::istream& averenkov::input_matrix(std::istream& in, int* t, size_t m, size_t n, size_t& read)
 {
   read = 0;
-  for (size_t i = 0; i < m; ++i)
+  for (size_t i = 0; i < m * n; ++i)
   {
     if (in >> t[i])
     {
@@ -50,7 +50,7 @@ size_t averenkov::num_col_lsr(int* t, size_t m, size_t n)
 
 void averenkov::output_matrix(std::ostream& out, int* t, size_t m, size_t n)
 {
-  for (size_t i = 0; i < m; ++i) {
+  for (size_t i = 0; i < m * n; ++i) {
     out << t[i] << " ";
   }
 }
