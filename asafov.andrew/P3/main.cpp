bcc2cc9092f5958a2c8510471597af397637205b
shycoshy <neforscist@gmail.com>
2024-11-04 03:18:53 +0300

    pol raboti sdelano

--- asafov.andrew/P3/main.cpp
+++ asafov.andrew/P3/main.cpp
@@ -1,68 +1,102 @@
 #include <iostream>
 #include <fstream>
 
-void stackMatrixHandler(const char input[], const char output[]) {
-    int mtx[100][100] = {};
+void autoMatrixHandler(char input[],char output[]) {
+    int mtx[10000] = {};
     std::ifstream fin(input);
     int n = 0;
     fin >> n;
     int m = 0;
     fin >> m;
     
-    for (int i = 0; i < n; i++) {
-        for (int e = 0; e < m; e++) {
-            fin >> mtx[i][e];
-        }
+    for (int i = 0; i < n*m; i++) {
+        fin >> mtx[i];
     }
     fin.close();
     int count = 1;
-    int i = 0;
-    int j = 0;
-    for (int e = 0; e < (m >= n) ? (n) : (m); e++) {
-        if (e % 2 == 0) {
-            for (int z = 0; z < m - e - 1;z++) {
-                mtx[i][j] -= count;
-                count++;
-                j++;
-            }
-            for (int z = 0; z < n - e - 1;z++) {
-                mtx[i][j] -= count;
-                count++;
-                i++;
-            }
+    
+    int e = n;
+    int r = m;
+    int a = -1;
+    while (e * r != 0)
+    {
+        for (int i = 0; i < r; i++)
+        {
+            a++;
+            mtx[a] -= count;
+            count++;
         }
-        else {
-            for (int z = 0; z < m - e - 1;z++) {
-                mtx[i][j] -= count;
-                count++;
-                j--;
-            }
-            for (int z = 0; z < n - e - 1;z++) {
-                mtx[i][j] -= count;
-                count++;
-                i--;
-            }
+        if (e * r == 0)
+        {
+            break;
         }
+        e--;
+        for (int i = 0; i < e; i++)
+        {
+            a += m;
+            mtx[a] -= count;
+            count++; 
+        }
+        if (e * r == 0)
+        {
+            break;
+        }
+        r--;
+        for (int i = 0; i < r; i++)
+        {
+            a--;
+            mtx[a] -= count;
+            count++;
+        }
+        if (e * r == 0)
+        {
+            break;
+        }
+        e--;
+        for (int i = 0; i < e; i++)
+        {
+            a -= m;
+            mtx[a] -= count;
+            count++;
+        }
+        if (e * r == 0)
+        {
+            break;
+        }
+        r--;
     }
     std::ofstream fout(output);
-    for (int i = 0; i < n; i++) {
-        for (int e = 0; e < m; e++) {
-            fout << mtx[i][e] << ' ';
-        }
+    fout << n << ' ' << m << ' ';
+    for (int i = 0; i < n * m; i++) {
+        fout << mtx[i] << ' ';
     }
 }
 
-int main()
-{
-    int num = 0;
-    std::cin >> num;
-    char input[50] = {};
-    std::cin >> input;
-    char output[50] = {};
-    std::cin >> output;
+void dynamicalMatrixHandler(char input[],char output[]) {
+    std::ifstream fin(input);
+    int n = 0;
+    fin >> n;
+    int m = 0;
+    fin >> m;
+    fin.close();
+    
+    int* mtx = new int[m * n];
 
-    if (num == 1) {
-        stackMatrixHandler(input, output);
+    for (int i = 0; i < n * m; i++) {
+        fin >> mtx[i];
+    }
+    
+}
+
+int main(int argc, char** argv)
+{
+    if (argv[0] == "1")
+    {
+        autoMatrixHandler(argv[1], argv[2]);
+    }
+    else if (argv[0] == "2")
+    {
+        dynamicalMatrixHandler(argv[1], argv[2]);
     }
     return 0;
 }
