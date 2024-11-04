85724903eaa69fa69965435d5ee9a94b430cfc1f
aldyn-d <aldyndirti@gmail.com>
2024-11-04 15:20:00 +0300

    Input checking

--- dirti.aldyn/P2/main.cpp
+++ dirti.aldyn/P2/main.cpp
@@ -1,2 +1,16 @@
 int main()
-{}
+{
+  double left = 0.0, right = 0.0;
+  size_t k = 0;
+  std::cin >> left >> right >> k;
+  if (!std::cin)
+  {
+    std::cerr << "Wrong input\n";
+    return 1;
+  }
+  if (left >= right || left < -1 || right > 1)
+  {
+    std::cerr << "Wrong input\n";
+    return 1;
+  }
+}
