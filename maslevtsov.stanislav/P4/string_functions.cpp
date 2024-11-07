0220680db282d3c5945fc241a1ac260e467efa8a
jambii1 <s.maslevtsov@gmail.com>
2024-11-07 22:04:39 +0300

    CG fix

--- maslevtsov.stanislav/P4/string_functions.cpp
+++ maslevtsov.stanislav/P4/string_functions.cpp
@@ -2,53 +2,53 @@
 
 char* maslevtsov::getline(std::istream& in, char* string, std::size_t capacity, char stop)
 {
-    char symbol = '\0';
-    std::size_t stringIndex = 0;
+  char symbol = '\0';
+  std::size_t stringIndex = 0;
 
-    std::noskipws(in);
-    while ((in >> symbol) && (symbol != stop))
+  std::noskipws(in);
+  while ((in >> symbol) && (symbol != stop))
+  {
+    if (stringIndex == capacity)
     {
-        if (stringIndex == capacity)
-        {
-            string[stringIndex] = '\0';
-            maslevtsov::expandString(string, capacity);
-        }
-        string[stringIndex++] = symbol;
+      string[stringIndex] = '\0';
+      maslevtsov::expandString(string, capacity);
     }
-    std::skipws(in);
+    string[stringIndex++] = symbol;
+  }
+  std::skipws(in);
 
-    return string;
+  return string;
 }
 
 void maslevtsov::expandString(char* string, std::size_t& capacity)
 {
-    capacity *= 2;
-    char* newString = new char[capacity];
+  capacity *= 2;
+  char* newString = new char[capacity];
 
-    for (std::size_t i = 0; string[i] != '\0'; ++i)
-    {
-        newString[i] = string[i];
-    }
-    delete[] string;
-    string = newString;
+  for (std::size_t i = 0; string[i] != '\0'; ++i)
+  {
+    newString[i] = string[i];
+  }
+  delete[] string;
+  string = newString;
 }
 
 std::size_t maslevtsov::isSameSymbols(const char *const str1, const char *const str2)
 {
-    std::size_t counterArray[256] = {0};
+  std::size_t counterArray[256] = {0};
 
-    for (std::size_t i = 0; str1[i] != '\0'; ++i)
+  for (std::size_t i = 0; str1[i] != '\0'; ++i)
+  {
+    ++counterArray[str1[i]];
+  }
+  for (std::size_t i = 0; str2[i] != '\0'; ++i)
+  {
+    if (counterArray[str2[i]])
     {
-        ++counterArray[str1[i]];
+      return 1;
+      break;
     }
-    for (std::size_t i = 0; str2[i] != '\0'; ++i)
-    {
-        if (counterArray[str2[i]])
-        {
-            return 1;
-            break;
-        }
-    }
+  }
 
-    return 0;
+  return 0;
 }
