dd183c1d058b085ad66a3c77da5d039577dd845b
jambii1 <s.maslevtsov@gmail.com>
2024-11-07 20:24:49 +0300

    isSameSymbol function for finding if same symbols in 2 strings

--- maslevtsov.stanislav/P4/main.cpp
+++ maslevtsov.stanislav/P4/main.cpp
@@ -1,2 +1,24 @@
+#include <iostream>
+
+std::size_t isSameSymbols(const char *const str1, const char *const str2)
+{
+  size_t counterArray[256] = {0};
+
+  for (size_t i = 0; str1[i] != '\0'; ++i)
+  {
+    ++counterArray[str1[i]];
+  }
+  for (size_t i = 0; str2[i] != '\0'; ++i)
+  {
+    if (counterArray[str2[i]])
+    {
+      return 1;
+      break;
+    }
+  }
+
+  return 0;
+}
+
 int main()
 {}
