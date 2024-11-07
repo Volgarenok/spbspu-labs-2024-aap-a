4455c4af973d196789e243d9b174a5c8399ae56c
TvoyBatyA1234 <maxdem06@gmail.com>
2024-11-07 19:13:04 +0300

    add base of string alloc + signature of input func

--- demehin.maxim/P4/main.cpp
+++ demehin.maxim/P4/main.cpp
@@ -1,3 +1,19 @@
 int main()
 {
+  constexpr size_t base_size = 100;
+  char* str = nullptr;
+  try
+  {
+    str = new char[base_size);
+  }
+  catch (const std::bad_alloc& e)
+  {
+    std::cerr << "Out of memory\n";
+    return 1;
+  }
+
+
+
+
+  delete[] str;
 }
---
+++ demehin.maxim/P4/string.h
@@ -0,0 +1,10 @@
+#ifndef STRING_H
+#define STRING_H
+#include <iostream>
+
+namespace demehin
+{
+  char* inputString(std::istream& in, char stop);
+}
+
+#endif
