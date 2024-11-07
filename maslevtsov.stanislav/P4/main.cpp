34ff7307c55ab3edae68d0b679c47b802f3cbbfe
jambii1 <s.maslevtsov@gmail.com>
2024-11-07 20:53:09 +0300

    Strings initialization, symbols input and result output in main function

--- maslevtsov.stanislav/P4/main.cpp
+++ maslevtsov.stanislav/P4/main.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 
-std::size_t isSameSymbols(const char *const str1, const char *const str2)
+std::size_t isSameSymbols(const char* const str1, const char* const str2)
 {
   size_t counterArray[256] = {0};
 
@@ -21,4 +21,32 @@ std::size_t isSameSymbols(const char *const str1, const char *const str2)
 }
 
 int main()
-{}
+{
+  constexpr char constString[25] = "~9R hg,3hX i%htQ/K*Y.35T";
+  char symbol = '\0';
+  constexpr std::size_t stringCapacity = 10;
+  std::size_t stringIndex = 0;
+  char* dynamicString = nullptr;
+
+  try
+  {
+    dynamicString = new char[stringCapacity + 1];
+  }
+  catch(const std::bad_alloc& e)
+  {
+    std::cerr << e.what() << '\n';
+    return 1;
+  }
+
+  std::noskipws(std::cin);
+  while ((std::cin >> symbol) && (symbol != '\n'))
+  {
+    if (stringIndex == stringCapacity)
+    {}
+     dynamicString[stringIndex] = symbol;
+  }
+  std::skipws(std::cin);
+
+  std::cout << isSameSymbols(constString, dynamicString);
+  std::cout << '\n';
+}
