709e062c8f452ebeb9120b682c863c3d8c7b7e06
jambii1 <s.maslevtsov@gmail.com>
2024-11-07 21:39:10 +0300

    expandString function and getline function fix

--- maslevtsov.stanislav/P4/main.cpp
+++ maslevtsov.stanislav/P4/main.cpp
@@ -1,14 +1,47 @@
 #include <iostream>
 
+char *getline(std::istream &in, char *string, std::size_t capacity, char stop)
+{
+  char symbol = '\0';
+  std::size_t stringIndex = 0;
+
+  std::noskipws(in);
+  while ((in >> symbol) && (symbol != stop))
+  {
+    if (stringIndex == capacity)
+    {
+      string[stringIndex] = '\0';
+      expandString(string, capacity);
+    }
+    string[stringIndex++] = symbol;
+  }
+  std::skipws(in);
+
+  return string;
+}
+
+void expandString(char* string, std::size_t& capacity)
+{
+  capacity *= 2;
+  char* newString = new char[capacity];
+
+  for (std::size_t i = 0; string[i] != '\0'; ++i)
+  {
+    newString[i] = string[i];
+  }
+  delete[] string;
+  string = newString;
+}
+
 std::size_t isSameSymbols(const char* const str1, const char* const str2)
 {
-  size_t counterArray[256] = {0};
+  std::size_t counterArray[256] = {0};
 
-  for (size_t i = 0; str1[i] != '\0'; ++i)
+  for (std::size_t i = 0; str1[i] != '\0'; ++i)
   {
     ++counterArray[str1[i]];
   }
-  for (size_t i = 0; str2[i] != '\0'; ++i)
+  for (std::size_t i = 0; str2[i] != '\0'; ++i)
   {
     if (counterArray[str2[i]])
     {
@@ -18,24 +51,6 @@ std::size_t isSameSymbols(const char* const str1, const char* const str2)
   }
 
   return 0;
-}
-
-char* getline(std::istream& in, char* string, size_t capacity, char stop)
-{
-  char symbol = '\0';
-  std::size_t stringIndex = 0;
-  constexpr std::size_t stringCapacity = 10;
-
-  std::noskipws(in);
-  while ((in >> symbol) && (symbol != stop))
-  {
-    if (stringIndex == stringCapacity)
-    {}
-    string[stringIndex++] = symbol;
-  }
-  std::skipws(in);
-
-  return string;
 }
 
 int main()
@@ -58,4 +73,5 @@ int main()
 
   std::cout << isSameSymbols(constString, dynamicString);
   std::cout << '\n';
+  delete[] dynamicString;
 }
