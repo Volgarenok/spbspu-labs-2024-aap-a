10876f0ef765a6e3af92f78d62e9bddf7f9972cf
vladislavmaslovskiy <vladuisik826@gmail.com>
2024-11-08 21:08:09 +0300

    Add merge to strings

--- maslovskiy.vladislav/P4/main.cpp
+++ maslovskiy.vladislav/P4/main.cpp
@@ -1,2 +1,37 @@
+#include <iostream>
+
+void fillStr(char* res, const char* str, size_t pos)
+{
+  size_t i = 0;
+  for (i = pos * 2; str[pos] != '\0'; i++)
+  {
+    res[i] = str[pos];
+    pos++;
+  }
+  res[i] = '\0';
+}
+
+void mergeStr(char* res, const char *first, const char *second)
+{
+  size_t i = 0;
+  for (; first[i] != '\0' && second[i] != '\0'; i++)
+  {
+    res[i * 2] = first[i];
+    res[i * 2 + 1] = second[i];
+  }
+  const char* temp = first[i] != '\0' ? first : second;
+  fillStr(res, temp, i);
+}
+
 int main()
-{}
+{
+  const char *str1 = "135";
+  const char *str2 = "246789";
+  char *res = new char[10000];
+  mergeStr(res, str1, str2);
+  for (size_t i = 0; res[i] != '\0'; i++)
+  {
+    std::cout << res[i];
+  }
+  std::cout << '\n';
+}
