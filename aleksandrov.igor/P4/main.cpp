becce3f2de47e200de61f026fb5b0f2b03b09eaa
MrMels625 <MrMels625@gmail.com>
2024-11-09 16:57:48 +0300

    String input added

--- aleksandrov.igor/P4/main.cpp
+++ aleksandrov.igor/P4/main.cpp
@@ -1,3 +1,32 @@
-main()
-{}
+#include <iostream>
+#include <ios>
+
+int main()
+{
+  std::size_t max = 2;
+  constexpr const int coef = 2;
+  char* str = new char[max + 1];
+
+  std::size_t size = 0;
+  char c = '\0';
+  std::noskipws(std::cin);
+
+  while ((std::cin >> c) && (c != '\n'))
+  {
+    if (size == max)
+    {
+      max += coef;
+      char* newStr = new char[max + 1];
+      for (size_t i = 0; i < max; ++i)
+      {
+        newStr[i] = str[i];
+      }
+      delete[] str;
+      str = newStr;
+    }
+    str[size++] = c;
+  }
+  std::skipws(std::cin);
+  std::cout << str << "\n";
+}
 
