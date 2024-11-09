673fa1bf41e8f7448dc909935b18251eaf3458a8
ysffmn <andrewnikonov387@gmail.com>
2024-11-09 18:33:11 +0300

    added output functions

---
+++ nikonov.andrew/P4/lineoutput.cpp
@@ -0,0 +1,10 @@
+#include <lineoutput.hpp>
+#include <iostream>
+void printLine(char* line)
+{
+  size_t iter = 0;
+  while (line[iter] != '\n')
+  {
+    std::cout << line[iter];
+  }
+}
---
+++ nikonov.andrew/P4/lineoutput.hpp
@@ -0,0 +1,6 @@
+#ifndef LINEOUPUT_HPP
+#define LINEOUPUT_HPP
+#include <iostream>
+#include <cstddef>
+void printLine(char* line);
+#endif
