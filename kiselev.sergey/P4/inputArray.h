5c926642cec801286064fb08c8f5862f2ecda9e6
deonik3 <kis.sergey2006@gmail.com>
2024-11-09 15:21:10 +0300

    Transfer inputArray to main.cpp

--- kiselev.sergey/P4/inputArray.h
+++
@@ -1,8 +0,0 @@
-#ifndef INPUTARRAY_H
-#define INPUTARRAY_H
-#include <cstddef>
-namespace kiselev
-{
-  char* inputArr(char* arr, size_t k);
-}
-#endif
--- kiselev.sergey/P4/main.cpp
+++ kiselev.sergey/P4/main.cpp
@@ -1,3 +1,32 @@
+#include <ios>
+#include <iostream>
+#include "transferArray.h"
 int main()
 {
+  size_t k = 3;
+  char* arr = static_cast<char*>(malloc(k * sizeof(char)));
+  size_t count = 0;
+  std::noskipws(std::cin);
+  for (size_t j = 0; j != '\n'; ++j)
+  {
+    if (count >= k - 1)
+    {
+      size_t m = k;
+      k += 5;
+      char* arr2 = static_cast<char*>(malloc(k * sizeof(char)));
+      kiselev::transferArr(arr2, arr, m);
+      free(arr);
+      char* arr = static_cast<char*>(malloc(k * sizeof(char)));
+      kiselev::transferArr(arr, arr2, m);
+      free(arr2);
+    }
+    std::cin >> arr[j];
+    if (!std::cin >> arr[j])
+    {
+      std::cerr << "Incorrect input\n";
+      return 1;
+    }
+    count++;
+  }
+  std::skipws(std::cin);
 }
