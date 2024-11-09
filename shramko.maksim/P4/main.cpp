cf463933ffd97d89515ecbd35d6b9926b57b7896
Maksim_Shramko <maxramko@gmail.com>
2024-11-09 18:12:21 +0300

    Creating simple code with input string and checks for it

--- shramko.maksim/P4/main.cpp
+++ shramko.maksim/P4/main.cpp
@@ -1,2 +1,26 @@
+#include <iostream>
+#include <ios>
+#include <cstddef>
+#include <fstream>
+
 int main()
-{}
+{
+	constexpr size_t MEMORY = 10;
+	char* str = reinterpret_cast< char* >(malloc((MEMORY + 1) * sizeof(char)));
+	if (str == nullptr)
+	{
+		std::cerr << "Malloc error!\n";
+		free(str);
+		return 1;
+	}
+
+	size_t size = 0;
+	char c = '\n';
+
+	std::noskipsw(std::cin);
+	while ((std::cin >> c) && (c != '\n'))
+	{
+		str[size++] = c;
+	}
+	std::skipsw(std::cin);
+}
