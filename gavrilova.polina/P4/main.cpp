f88950d0b1c759dbc0ced5e99d55242b57138f5e
hahahapolinka <gavrilova.pv@edu.spbstu.ru>
2024-11-09 18:17:21 +0300

    Main function for P4

--- gavrilova.polina/P4/main.cpp
+++ gavrilova.polina/P4/main.cpp
@@ -1,3 +1,17 @@
+#include <iostream>
+#include <cctype>
+
+char * rm_digits (const char * original, char * new_str)
+{
+  size_t j = 0;
+  for (size_t i = 0; i != "\0"; ++i) {
+    if (!isdigit) {
+      new_str[j] = original[i];
+    }
+    ++j;
+  }
+}
+
 int main()
 {
 }
