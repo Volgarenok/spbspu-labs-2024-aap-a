397ee6c7baa284c153ebb7c8392b13632186e65f
shycoshy <neforscist@gmail.com>
2024-11-09 17:43:59 +0300

    spaces

--- asafov.andrew/P2/absError.cpp
+++ asafov.andrew/P2/absError.cpp
@@ -1,13 +1,13 @@
-#include "func.h"
+#include "func.hpp"
 
 namespace asafov {
   double absError(double x, size_t k)
   {
-      double res = (k % 2 == 0) ? (-1) : (1);
-      for (size_t i = k; i > 0; i--)
-      {
-          res = res * x * x / i;
-      }
-      return res;
+    double res = (k % 2 == 0) ? (-1) : (1);
+    for (size_t i = k; i > 0; i--)
+    {
+      res = res * x * x / i;
+    }
+    return res;
   }
 }
--- asafov.andrew/P2/main.cpp
+++ asafov.andrew/P2/main.cpp
@@ -1,32 +1,31 @@
-#include "func.h"
+#include "func.hpp"
 #include <iostream>
 
 int main()
 {
-    const double error = 0.001;
-    const double step = 0.05;
+  const double error = 0.001;
+  const double step = 0.05;
 
-    double left = -1;
-    double right = 1;
-    size_t numberMax = 2;
+  double left = -1;
+  double right = 1;
+  size_t numberMax = 2;
+  std::cin >> left >> right >> numberMax;
 
-    std::cin >> left >> right >> numberMax;
-
-    if (std::cin.fail())
-    {
-        std::cerr << "Input fail" << '\n';
-        return 1;
-    }
-    if (right < left)
-    {
-        std::cerr << "wrong borders" << '\n';
-        return 1;
-    }
-    if ((right > 1) || (left < -1))
-    {
-        std::cerr << "incorrect borders" << '\n';
-        return 1;
-    }
-    asafov::table(left, right, step, numberMax, error);
-    return 0;
+  if (std::cin.fail())
+  {
+    std::cerr << "Input fail" << '\n';
+    return 1;
+  }
+  if (right < left)
+  {
+    std::cerr << "wrong borders" << '\n';
+    return 1;
+  }
+  if ((right > 1) || (left < -1))
+  {
+    std::cerr << "incorrect borders" << '\n';
+    return 1;
+  }
+  asafov::table(left, right, step, numberMax, error);
+  return 0;
 }
