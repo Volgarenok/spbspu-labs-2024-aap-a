b2b412b9aa6fc68f1428934b91b491bee0fd5781
GuseynovGuseynGG <ganx.gamer@gmail.com>
2024-11-09 20:07:40 +0300

    fix

--- guseynov.guseyn/P4/main.cpp
+++ guseynov.guseyn/P4/main.cpp
@@ -8,7 +8,7 @@ int main()
   const size_t num = 26;
   arr = guseyn::inputLine(std::cin, stop);
   char uniqueChars[num] = {'a', 'b', 'c', 'd','e', 'f', 'g', 'h',
-'i', 'j', 'k', 'l','m', 'n', 'o',
-'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
+  'i', 'j', 'k', 'l','m', 'n', 'o',
+  'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
   std::cout << guseyn::identifyUniqueChar(arr, uniqueChars, num) << "\n";
 }
