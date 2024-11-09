76365138e655a4876791da5c931bc50411837e02
MrMels625 <MrMels625@gmail.com>
2024-11-09 18:01:10 +0300

    Alloc handling exceptions& input check added

--- aleksandrov.igor/P4/main.cpp
+++ aleksandrov.igor/P4/main.cpp
@@ -6,18 +6,43 @@ int main()
 {
   std::size_t max = 2;
   constexpr const int coef = 2;
-  char* str = new char[max + 1];
-
+  char* str = nullptr;
+  try
+  {
+    str = new char[max + 1];
+  }
+  catch (const std::bad_alloc& e)
+  {
+    std::cerr << "ERROR: Out of memory!\n";
+    delete[] str;
+    return 1;
+  }
   std::size_t size = 0;
   char c = '\0';
   std::noskipws(std::cin);
-
   while ((std::cin >> c) && (c != '\n'))
   {
+    if (!std::cin)
+    {
+      std::cerr << "ERROR: Input was incorrect!\n";
+      delete[] str;
+      return 1;
+    }
     if (size == max)
     {
       max += coef;
-      char* newStr = new char[max + 1];
+      char* newStr = nullptr;
+      try
+      {
+        newStr = new char[max + 1];
+      }
+      catch (const std::bad_alloc& e)
+      {
+        std::cerr << "ERROR: Out of memory!\n";
+        delete[] newStr;
+        delete[] str;
+        return 1;
+      }
       for (size_t i = 0; i < max; ++i)
       {
         newStr[i] = str[i];
