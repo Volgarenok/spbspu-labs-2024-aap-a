ca39672b9d4b878a48df85be55a395e3821b5961
lee <likachich12@gmail.com>
2024-11-10 14:44:33 +0300

    main.cpp

--- hismatova.elizaveta/P4/main.cpp
+++ hismatova.elizaveta/P4/main.cpp
@@ -1,2 +1,20 @@
+#include "fu_string.h"
+
 int main()
-{}
+{
+  char str1 = hismatova::getline();
+  char str2 = hismatoba::getline();
+  if (str1 == nullptr || str2 == nullptr)
+  {
+    std::cerr << "run without input\n";
+    return 1;
+  }
+  char * res = hismatova::uniqueChar(str1, str2);
+  if (res != nullptr)
+  {
+    std::cout << res << "\n";
+    free(res);
+  }
+  free(str1);
+  free(str2);
+}
