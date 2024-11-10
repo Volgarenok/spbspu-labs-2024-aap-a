3fa5997498fb19726ffd291ee98e2852becc721e
ZakirovMS <mz.08.08.06@gmail.com>
2024-11-10 19:34:41 +0300

    Correction of the get_line function

--- zakirov.marat/P4/line_processing.cpp
+++ zakirov.marat/P4/line_processing.cpp
@@ -18,7 +18,25 @@ char * zakirov::get_line(std::istream & in, char * line, size_t start, size_t fi
   for (; start < finish; ++start)
   {
     in >> line[start];
+    if (line[start] = '\0')
+    {
+      break;
+    }
+  }
+
+  if (line[start] = '\0')
+  {
+    start += 1;
+    for (; start < finish; ++start)
+    {
+      line[start] = '\n';
+    }
   }
 
   return line;
+}
+
+char * zakirov::change_line(char * line, char stop)
+{
+
 }
