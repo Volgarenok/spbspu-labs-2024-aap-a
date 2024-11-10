761ab41bb4c946228af02acf0d4306a6dab3af96
15446-rus75 <vladabramov06@yandex.ru>
2024-11-10 13:54:52 +0300

    add getting line in main.cpp

--- abramov.vladislav/P4/main.cpp
+++ abramov.vladislav/P4/main.cpp
@@ -1,3 +1,16 @@
+#include "str_functions.hpp"
+
 int main()
 {
+  constexpr const char *str2 = "def ";
+  char *str1 = nullptr;
+  try
+  {
+    str1 = abramov::getStr(std::cin);
+  }
+  catch (const std::bad_alloc &e)
+  {
+    std::cerr << "Memory fail\n";
+    return 2;
+  }
 }
