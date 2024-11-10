f96f79df5a85ae577ec1bd81f619e174f9537cd2
ProstoG-byte <asilda2019@gmail.com>
2024-11-11 02:07:36 +0300

    Initial commit for P3 + sanctions

---
+++ lungu.george/P3/count_columns.h
@@ -0,0 +1,8 @@
+#ifndef COUNT_COLUMNS_H
+#define COUNT_COLUMNS_H
+
+namespace lungu {
+    int countColumnsWithoutConsecutiveDuplicates(int** matrix, int rows, int cols);
+}
+
+#endif // COUNT_COLUMNS_H
