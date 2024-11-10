07ac14454cd517c247c168d8f0cb310d32063010
15446-rus75 <vladabramov06@yandex.ru>
2024-11-10 13:24:16 +0300

    add str_functions.hpp

---
+++ abramov.vladislav/P4/str_functions.hpp
@@ -0,0 +1,8 @@
+#ifndef STR_FUNCTIONS_HPP
+#define STR_FUNCTIONS_HPP
+#include <iostream>
+namespace abramov
+{
+  char *getStr(std::istream &in);
+  char *unionStrs(char *destination, char *source);
+}
