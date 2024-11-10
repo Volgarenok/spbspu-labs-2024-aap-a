293b353fb086d58fe9c298ae02d1a6c37d876c01
Z-1337 <un228der@gmail.com>
2024-11-11 01:49:52 +0300

    Initial commit for P4

---
+++ kushekbaev.danil/P4/arrays_reading.hpp
@@ -0,0 +1,10 @@
+#ifndef ARRAYS_READING
+#define ARRAYS_READING
+#include <cstddef>
+
+namespace kushekbaev
+{
+  char* readInputArray(size_t& size, size_t& max);
+}
+
+#endif
---
+++ kushekbaev.danil/P4/main.cpp
@@ -0,0 +1,34 @@
+#include "arrays_reading.hpp"
+#include <iostream>
+
+int main()
+{
+  size_t max = 10;
+  size_t size1 = 0;
+  size_t size2 = 0;
+
+  char * str1 = kushekbaev::readInputArray(size1, max);
+
+  char * str2 = kushekbaev::readInputArray(size2, max);
+
+  bool hasCommonChars = false;
+
+  for (size_t i = 0; i < size1; i++)
+  {
+    for (size_t j = 0; j < size2; j++)
+    {
+      if (str1[i] == str2[j])
+      {
+        hasCommonChars = true;
+        break;
+      }
+    }
+      if (hasCommonChars) break;
+    }
+
+    std::cout << (hasCommonChars ? 1 : 0) << std::endl;
+
+  delete[] str1;
+  delete[] str2;
+  return 0;
+}
