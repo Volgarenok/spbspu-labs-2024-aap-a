e8df489ed1bcbec5c9717edd2e0b4304d3619e16
Maksim_Shramko <maxramko@gmail.com>
2024-11-10 21:57:25 +0300

    Bugfix 5.1: correcting code after Build fail

--- shramko.maksim/P3/inputMtrx.cpp
+++ shramko.maksim/P3/inputMtrx.cpp
@@ -5,7 +5,7 @@ std::istream & shramko::inputMtrx(std::istream & in, int* mtrx, size_t sizeMtrx,
   for (size_t i = 0; i < sizeMtrx; ++i)
   {
     in >> mtrx[i];
-    ++read_count
+    ++read_count;
   }
 
   return in;
--- shramko.maksim/P3/main.cpp
+++ shramko.maksim/P3/main.cpp
@@ -63,9 +63,11 @@ int main(int argc, char** argv)
   size_t read_count = 0;
   if(read_count != sizeMtrx)
   {
+    delete[] mtrx;
     std::cerr << "Matrix data error!\n";
+    return 2;
   }
-  else if(!shramko::inputMtrx(input, mtrx, sizeMtrx))
+  else if(!shramko::inputMtrx(input, mtrx, sizeMtrx, read_count))
   {
     delete[] mtrx;
     std::cerr << "ERROR!\n";
