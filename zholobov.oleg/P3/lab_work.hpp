7a511750df91b79d297b48d0f4f10d30b4857f08
Oleg Zholobov <zholobov.of@edu.spbstu.ru>
2024-11-10 20:19:34 +0300

    fix per comments

--- zholobov.oleg/P3/lab_work.hpp
+++
@@ -1,6 +0,0 @@
-#ifndef LAB_WORK_H
-#define LAB_WORK_H
-
-void do_lab_work(int num, const char* input_filename, const char* output_filename);
-
-#endif
--- zholobov.oleg/P3/main.cpp
+++ zholobov.oleg/P3/main.cpp
@@ -1,7 +1,6 @@
-#include <cstring>
+#include <cstdlib>
 #include <iostream>
-
-#include "lab_work.hpp"
+#include <fstream>
 #include "min_sum_mdg.hpp"
 
 int main(int argc, char** argv)
@@ -13,20 +12,57 @@ int main(int argc, char** argv)
     std::cerr << "Too many arguments\n";
     return 1;
   }
-  int num = 0;
+  long num = 0;
   try {
-    size_t pos = 0;
-    num = std::stoi(argv[1], &pos);
-    if (pos < std::strlen(argv[1])) {
-      throw std::invalid_argument("Invalid argument");
+    char* endptr = nullptr;
+    num = std::strtol(argv[1], &endptr, 10);
+    if (*endptr != 0) {
+      throw std::runtime_error("Not a number");
     }
-  } catch (const std::exception&) {
+    if (num != 1 && num != 2) {
+      throw std::invalid_argument("Out of range");
+    }
+  } catch (const std::runtime_error&) {
     std::cerr << "First parameter is not a number\n";
+    return 1;
+  } catch (const std::invalid_argument&) {
+    std::cerr << "First parameter is out of range\n";
     return 1;
   }
 
   try {
-    do_lab_work(num, argv[2], argv[3]);
+    size_t rows = 0;
+    size_t cols = 0;
+    std::ifstream input_file(argv[2]);
+    input_file >> rows >> cols;
+    if (!input_file) {
+      throw std::runtime_error("Wrong data in input file");
+    }
+    std::ofstream output_file(argv[3]);
+    size_t elements_number = rows * cols;
+    int result = 0;
+    if (elements_number == 0) {
+      output_file << "0";
+    } else {
+      if (num == 1) {
+        int matrix[10000] = {0};
+        zholobov::read_matrix(input_file, matrix, elements_number);
+        result = zholobov::calc_min_sum_mdg(matrix, rows, cols);
+      } else if (num == 2) {
+        int* matrix = new int[elements_number]{};
+        try {
+          zholobov::read_matrix(input_file, matrix, elements_number);
+        } catch (const std::exception&) {
+          delete[] matrix;
+          throw;
+        }
+        result = zholobov::calc_min_sum_mdg(matrix, rows, cols);
+        delete[] matrix;
+      } else {
+        throw std::invalid_argument("First parameter is invalid. It should be either 1 or 2");
+      }
+      output_file << result;
+    }
   } catch (const std::invalid_argument& e) {
     std::cerr << "Exception: " << e.what() << "\n";
     return 1;
