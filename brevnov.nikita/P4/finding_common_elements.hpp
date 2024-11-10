3115f92ba7117b15d881d9f8798831f578ae5481
brev1ch <flirktv@gmail.com>
2024-11-10 12:54:42 +0300

    Whitespace fix

--- brevnov.nikita/P4/finding_common_elements.hpp
+++ brevnov.nikita/P4/finding_common_elements.hpp
@@ -1,6 +1,6 @@
 #ifndef FINDING_COMMON_ELEMENTS_HPP
 #define FINDING_COMMON_ELEMENTS_HPP
-namespace brevnov 
+namespace brevnov
 {
   void finding_common_elements(char * first_line, char * second_line);
 }
--- brevnov.nikita/P4/getline.hpp
+++ brevnov.nikita/P4/getline.hpp
@@ -1,7 +1,7 @@
 #ifndef GETLINE_HPP
 #define GETLINE_HPP
 #include <cstddef>
-namespace brevnov 
+namespace brevnov
 {
   char * getline(std::istream& in, char stop);
   char * newmemory(char * line, std::size_t max_size);
