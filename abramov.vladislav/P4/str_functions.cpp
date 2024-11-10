5cdd9c01a260aaf7fafbc038706a3ba502380e49
15446-rus75 <vladabramov06@yandex.ru>
2024-11-10 13:46:09 +0300

    add union of 2 strings

--- abramov.vladislav/P4/str_functions.cpp
+++ abramov.vladislav/P4/str_functions.cpp
@@ -28,3 +28,30 @@ char *abramov::getStr(std::istream &in)
   std::skipws(in);
   return str;
 }
+
+char *abramov::unionStrs(char *destination, char *source)
+{
+  char str[32768] = "\0";
+  size_t i = 0;
+  size_t k = 0;
+  while (destination[i] != '\0' && source[i] != '\0')
+  {
+    str[k++] = destrination[i];
+    str[k++] = source[i];
+    ++i;
+  }
+  while (destination[i] != '\0')
+  {
+    str[k++] = destination[i++];
+  }
+  while (source[i] != '\0')
+  {
+    str[k++] = source[i++];
+  }
+  str[k] = '\0';
+  for (size_t j = 0; j < k + 1; ++j)
+  {
+    destination[j] = str[j];
+  }
+  return destination;
+}
