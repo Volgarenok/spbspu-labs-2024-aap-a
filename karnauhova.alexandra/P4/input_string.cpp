d5d947641b276f5d89ea65b6cdbbc74ad8961cb4
Sikireg <masj280906@gmail.com>
2024-11-10 16:23:37 +0300

    Input

---
+++ karnauhova.alexandra/P4/input_string.cpp
@@ -0,0 +1,34 @@
+#include "input_string.hpp"
+#include <stdexcept>
+#include <fstream>
+#include <cstddef>
+char* karnauhova::input_string(std::ifstream & in)
+{
+  size_t n = 20;
+  char* str = reinterpret_cast< char* >(malloc(sizeof(char) * n));
+  if (str == nullptr)
+    {
+      throw std::logic_error("out of memory");
+    }
+  std::noskipws(in);
+  for (size_t i = 0; (in>>str[i]) && (str[i] != '\0'); i++)
+    {
+      if ((str[i + 1] != '\0') && (i + 1) >= 19)
+      {
+        n = n * 2;
+        char* str1 = reinterpret_cast< char* >(malloc(sizeof(char) * n));
+        if (str1 == nullptr)
+        {
+          throw std::logic_error("out of memory");
+        }
+        for (size_t j = 0; j < (n / 2); j++)
+        {
+          str1[j] = str[j];
+        }
+        free(str);
+        char* str = str1;
+      }
+    }
+    std::skipws(in);
+    return str;
+}
---
+++ karnauhova.alexandra/P4/input_string.hpp
@@ -0,0 +1,9 @@
+#ifndef INPUT_STRING_HPP
+#define INPUT_STRING_HPP
+#include <fstream>
+
+namespace karnauhova
+{
+  char* input_string(std::ifstream & in);
+}
+#endif
