17162ef10b8682b656d9e0840b429365932db43a
A1exMas1ov <ae1ex.mas1ov@gmail.com>
2024-11-10 15:31:30 +0300

    main

--- maslov.aleksey/P4/main.cpp
+++ maslov.aleksey/P4/main.cpp
@@ -3,4 +3,12 @@
 
 int main()
 {
+  char * string1 = maslov::inputLine(std::cin, '\n');
+  if (string1 == nullptr)
+  {
+    return 1;
+  }
+  const char * string2 = "abc"; //изменить на более сложный
+  std::cout << maslov::excludeCharacters(string1, string2) << "\n";
+  delete[] string1;
 }
