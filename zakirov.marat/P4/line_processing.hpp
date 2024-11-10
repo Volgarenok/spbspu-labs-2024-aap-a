c956c3cd7737649213508e269084da70f5a0899d
ZakirovMS <mz.08.08.06@gmail.com>
2024-11-10 14:20:55 +0300

    Main function implementation without function implementation

---
+++ zakirov.marat/P4/line_processing.hpp
@@ -0,0 +1,7 @@
+#include <iostream>
+namespace zakirov
+{
+  char * get_line(std::istream & in, char * line, size_t start, size_t finish);
+  char * expand_line(char * line, size_t real_lenght, size_t add_length);
+  char * change_line(char * line, char stop);
+}
--- zakirov.marat/P4/main.cpp
+++ zakirov.marat/P4/main.cpp
@@ -1,4 +1,47 @@
+#include <stdlib.h>
+#include <iostream>
+#include "line_processing.hpp"
+
 int main()
 {
+  size_t start = 1, finish = 1;
+  constexpr size_t step = 15;
+  char last_symbol = '\0';
+  std::cin >> last_symbol;
+  char * line = (char *)malloc(sizeof(char));
+  if (last_symbol == '\0')
+  {
+    std::cerr << "ERROR: Empty line" << '\n';
+    return 1;
+  }
+  else if (line == nullptr)
+  {
+    std::cerr << "ERROR: Out of memory" << '\n';
+    return 1;
+  }
 
+  line[0] = last_symbol;
+  while (last_symbol != '\0')
+  {
+    zakirov::expand_line(line, finish, step);
+    if (line == nullptr)
+    {
+      std::cerr << "ERROR: Out of memory" << '\n';
+      return 1;
+    }
+
+    finish += step;
+    zakirov::get_line(std::cin, line, start, finish);
+    start += step;
+    last_symbol = line[finish - 1];
+  }
+
+  char * answer = zakirov::change_line(line, '\n');
+  size_t location = 0;
+  while (answer[location] != '\0')
+  {
+    std::cout << answer[location];
+  }
+
+  std::cout << '\n';
 }
