5b5968d62aafe1bbe455cc3ec4bf540c09c98a8f
Sikireg <masj280906@gmail.com>
2024-11-10 21:54:41 +0300

    New check

--- karnauhova.alexandra/P4/main.cpp
+++ karnauhova.alexandra/P4/main.cpp
@@ -4,13 +4,19 @@
 int main()
 {
   char* t = nullptr;
+  constexpr char end = '\n';
   try
   {
-    t = karnauhova::input_string(std::cin, '\n');
+    t = karnauhova::input_string(std::cin, end);
   }
   catch (const std::bad_alloc &e)
   {
-    std::cerr << "Error: out of memory";
+    std::cerr << "Error: out of memory\n";
+    return 1;
+  }
+  if (t[0] == end)
+  {
+    std::cerr << "Error: empty string\n";
     return 1;
   }
   std::cout << karnauhova::dubl_char(t) << "\n";
