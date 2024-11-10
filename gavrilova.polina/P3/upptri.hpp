78614300a69b412cc2be32c27135a1977822fb39
hahahapolinka <gavrilova.pv@edu.spbstu.ru>
2024-11-10 17:13:53 +0300

    Valgrin bug fixing 4.0

--- gavrilova.polina/P3/upptri.hpp
+++ gavrilova.polina/P3/upptri.hpp
@@ -7,7 +7,7 @@ namespace gavrilova
   int checkNumOfArguments(int argc);
   int checkFirstArgument(unsigned long int num, const char * end);
   std::istream & input_matrix(std::istream & in, int * mtx, size_t m, size_t n, size_t & read);
-  int output_matrix(std::ostream & out, std::istream & in, const int * mtx, size_t read, size_t m, size_t n, unsigned long int num);
+  int output_matrix(std::ostream & out, std::istream & in, const int * mtx, size_t read, size_t m, size_t n);
   bool isUpperTriangMtx(const int * mtx, size_t m, size_t n);
 }
 #endif
