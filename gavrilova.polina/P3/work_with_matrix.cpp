484fcc85a34e964b7c3bfb3dda89c1c6e4ed8274
hahahapolinka <gavrilova.pv@edu.spbstu.ru>
2024-11-10 16:32:12 +0300

    Valgrin bug fixing

--- gavrilova.polina/P3/work_with_matrix.cpp
+++ gavrilova.polina/P3/work_with_matrix.cpp
@@ -20,8 +20,12 @@ std::istream & gavrilova::input_matrix(std::istream & in, int * mtx, size_t m, s
 }
 int gavrilova::output_matrix(std::ostream & out, std::istream & in, const int * mtx, size_t read, size_t m, size_t n, unsigned long int num)
 {
-  if (in && read == m * n) {
+  if (in && read == m * n && num == 1) {
     out << gavrilova::isUpperTriangMtx(mtx, m, n) << "\n";
+    return 1;
+  } else if (in && read == m * n && num == 2) {
+    out << gavrilova::isUpperTriangMtx(mtx, m, n) << "\n";
+    delete [] mtx;
     return 1;
   } else if (num == 2) {
     delete [] mtx;
