9af3a800d730a44a1d611954fd62e6d33d03ef4d
ZakirovMS <mz.08.08.06@gmail.com>
2024-11-10 14:46:30 +0300

    expand_line function implementation

--- zakirov.marat/P4/line_processing.cpp
+++ zakirov.marat/P4/line_processing.cpp
@@ -1,7 +1,19 @@
 #include "line_processing.hpp"
 #include <iostream>
 
-char * get_line(std::istream & in, char * line, size_t start, size_t finish)
+char * zakirov::expand_line(char * regular_line, size_t real_lenght, size_t add_length)
+{
+  char * expand_line = (char *)malloc((sizeof(char) * real_lenght) + (sizeof(char) * add_length));
+  for (size_t i = 0; i < real_lenght; ++i)
+  {
+    expand_line[i] = regular_line[i];
+  }
+
+  free(regular_line);
+  return expand_line;
+}
+
+char * zakirov::get_line(std::istream & in, char * line, size_t start, size_t finish)
 {
   for (; start < finish; ++start)
   {
