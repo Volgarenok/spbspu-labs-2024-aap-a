ec178e48234ac5143820bd2ce2b63cde18226dd4
Nekich06 <p.nikita.73526@gmail.com>
2024-11-11 21:13:09 +0300

    Added input for the first string
    It is necessary part of the program

--- petrov.nikita/P4/main.cpp
+++ petrov.nikita/P4/main.cpp
@@ -1,2 +1,7 @@
+#include "making_new_cstring.hpp"
+
 int main()
-{}
+{
+  char * cstring_1 = nullptr;
+  petrov::inputCSring(std::cin, cstring_1);
+}
---
+++ petrov.nikita/P4/making_new_cstring.hpp
@@ -0,0 +1,9 @@
+#ifndef MAKING_NEW_STRING_HPP
+#define MAKING_NEW_STRING_HPP
+#include <iostream>
+
+namespace petrov 
+{
+  std::istream & inputCSring(std::istream & in, char * string);
+}
+#endif
