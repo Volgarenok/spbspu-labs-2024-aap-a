4fd9535038c9cc0fb8ff03fee18dd107de10c70d
brev1ch <flirktv@gmail.com>
2024-11-11 06:49:08 +0300

    Problen with P3

---
+++ brevnov.nikita/P3/input_output_matrix.hpp
@@ -0,0 +1,10 @@
+#ifndef INPUT_OUPUT_MATRIX_HPP
+#define INPUT_OUPUT_MATRIX_HPP
+#include <cstddef>
+#include <fstream>
+namespace brevnov
+{
+  int input_matrix(std::ifstream & in, int * t, size_t m, size_t n);
+  void output_matrix(std::ofstream & out, int * t, size_t m, size_t n);
+}
+#endif
