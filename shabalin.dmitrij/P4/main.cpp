8ee7743f76ce64685abf50691d2ea43e1a48287d
dmitei <dima9990860349@icloud.com>
2024-11-11 18:28:43 +0300

    input string

--- shabalin.dmitrij/P4/main.cpp
+++ shabalin.dmitrij/P4/main.cpp
@@ -1,3 +1,45 @@
+#include <iostream>
+
+char *inputOfString(std::istream &input, size_t &sizeOfString)
+{
+  char someCharacter = 0;
+  size_t index = 0;
+  char *initialString = (char *)malloc(sizeOfString * sizeof(char));
+  input >> std::noskipws;
+
+  while (input >> someCharacter)
+  {
+    if (!input)
+    {
+      free(initialString);
+      throw std::logic_error("Input error");
+    }
+    if (index == sizeOfString - 1)
+    {
+      try
+      {
+        sizeOfString *= 2;
+        char *intermediateString = (char *)malloc(sizeOfString * sizeof(char));
+        for (size_t i = 0; i < index; ++i)
+        {
+          intermediateString[i] = initialString[i];
+        }
+
+        free(initialString);
+        initialString = intermediateString;
+      }
+      catch (std::bad_alloc &e)
+      {
+        free(initialString);
+        throw;
+      }
+    }
+    initialString[index] = someCharacter;
+    index++;
+  }
+  return initialString;
+}
+
 int main()
 {
   return 0;
