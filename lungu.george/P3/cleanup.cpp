8670c1d385033e1c1e953225f5837bfac6d45865
ProstoG-byte <asilda2019@gmail.com>
2024-11-11 07:35:12 +0300

    Sep-ed storage cleanup + man fix

---
+++ lungu.george/P3/cleanup.cpp
@@ -0,0 +1,11 @@
+#include "cleanup.h"
+namepace lungu {
+
+  void cleanup(int** matrix, int rows) {
+      if (matrix) {
+          for (int i = 0; i < rows; ++i) {
+              delete[] matrix[i];
+          }
+          delete[] matrix;
+      }
+}
---
+++ lungu.george/P3/cleanup.h
@@ -0,0 +1,6 @@
+ifndef "CLEANUP"
+define "CLEANUP"
+namespace lungu {
+  void cleanup(int** matrix, int rows);
+}
+#endif
