2bd4fa314fbf64bfb4eef7934cafe315b003ec9b
Danil3352 <danil.tkach2018@yandex.ru>
2024-11-12 00:21:58 +0300

    logic fix + CG fix

--- tkach.danil/P3/inputmtx.cpp
+++ tkach.danil/P3/inputmtx.cpp
@@ -1,12 +1,12 @@
 #include "filemtx.h"
 
-std::istream & tkach::inputMtx(std::istream & in, int* const mtx1, const size_t str, const size_t stl)
+std::istream & tkach::inputMtx(std::istream & in, int* const mtx1, const size_t row_size, const size_t column_size)
 {
-  for (size_t i = 0; i < str; ++i)
+  for (size_t i = 0; i < row_size; ++i)
   {
-    for (size_t j = 0; j < stl; ++j)
+    for (size_t j = 0; j < column_size; ++j)
     {
-      in >> mtx1[i * stl + j];
+      in >> mtx1[i * column_size + j];
     }
   }
   return in;
--- tkach.danil/P3/mtxcreate.cpp
+++ tkach.danil/P3/mtxcreate.cpp
@@ -1,64 +1,55 @@
 #include "mtxlogic.h"
-#include <stdexcept>
 
-double* tkach::createMtx2(const int* const mtx, const size_t str, const size_t stl)
+double* tkach::createMtx2(const int* const mtx, const size_t row_size, const size_t column_size)
 {
-  double* mtx2 = nullptr;
-  try
+  double* mtx2 = new double[row_size * column_size];
+  for (size_t i = 0; i < row_size; ++i)
   {
-    mtx2 = new double[str * stl];
-  }
-  catch (const std::bad_alloc & e)
-  {
-    return nullptr;
-  }
-  for (size_t i = 0; i < str; ++i)
-  {
-    for (size_t j = 0; j < stl; ++j)
+    for (size_t j = 0; j < column_size; ++j)
     {
       double num = 0, sum = 0, sr = 0;
       if (i > 0)
       {
         num++;
-        sum += mtx[i * stl + j - stl];
+        sum += mtx[i * column_size + j - column_size];
         if (j > 0)
         {
           num++;
-          sum += mtx[i * stl + j - stl - 1];
+          sum += mtx[i * column_size + j - column_size - 1];
         }
-        if (j + 1 < stl)
+        if (j + 1 < column_size)
         {
           num++;
-          sum += mtx[i * stl + j - stl + 1];
+          sum += mtx[i * column_size + j - column_size + 1];
         }
       }
       if (j > 0)
       {
         num++;
-        sum += mtx[ i * stl + j - 1];
+        sum += mtx[ i * column_size + j - 1];
       }
-      if (j + 1 < stl)
+      if (j + 1 < column_size)
       {
         num++;
-        sum += mtx[i * stl + j + 1];
+        sum += mtx[i * column_size + j + 1];
       }
-      if (i < str - 1)
+      if (i < row_size - 1)
       {
         num++;
-        sum += mtx[i * stl + j + stl];
+        sum += mtx[i * column_size + j + column_size];
         if (j > 0)
         {
           num++;
-          sum += mtx[i * stl + j + stl - 1];
+          sum += mtx[i * column_size + j + column_size - 1];
         }
-        if (j + 1 < stl)
+        if (j + 1 < column_size)
         {
           num++;
-          sum += mtx[i * stl + j + stl + 1];
+          sum += mtx[i * column_size + j + column_size + 1];
         }
       }
       sr = sum / num;
-      mtx2[i * stl + j] = sr;
+      mtx2[i * column_size + j] = sr;
     }
   }
   return mtx2;
