482e7b8d22077f1976a5c5271a0e9b9667fb80ed
root <root@LAPTOP-FGITSRCQ.localdomain>
2024-11-11 12:17:14 +0300

    Try inizialization fix

--- brevnov.nikita/P4/getline.cpp
+++ brevnov.nikita/P4/getline.cpp
@@ -4,6 +4,7 @@ char * brevnov::getline(std::istream& in, char stop)
 {
   std::size_t max_size = 50;
   char * line = new char[max_size + 1];
+  line[0] = 0;
   std::size_t current_size = 0;
   char c = '\0';
   std::noskipws(in);
