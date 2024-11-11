659047c252069ee36c91ee86417b7c4390e5cca6
mipxe <mariamozegova8726@gmail.com>
2024-11-11 21:41:47 +0300

    fix main

--- mozhegova.maria/P3/main.cpp
+++ mozhegova.maria/P3/main.cpp
@@ -26,19 +26,15 @@ int main(int argc, char ** argv)
   input >> m >> n;
   size_t read = 0;
   std::ofstream output(argv[3]);
+
+  int * arr = nullptr;
+  int v[10000];
   if (std::strcmp(argv[1], "1") == 0)
   {
-    int arr[10000] = {};
-    if (!mozhegova::inputMatrix(input, arr, m, n, read))
-    {
-      std::cerr << "Invalid matrix element\n";
-      return 2;
-    }
-    output << mozhegova::uppTriMtx(arr, m, n) << "\n";
+    arr = v;
   }
   else
   {
-    int * arr = nullptr;
     try
     {
       arr = new int[m * n];
@@ -48,13 +44,19 @@ int main(int argc, char ** argv)
       std::cerr << "Out of memory\n";
       return 2;
     }
-    if (!mozhegova::inputMatrix(input, arr, m, n, read))
+  }
+  if (!mozhegova::inputMatrix(input, arr, m, n, read))
+  {
+    std::cerr << "Invalid matrix element\n";
+    if (std::strcmp(argv[1], "2") == 0)
     {
       delete[] arr;
-      std::cerr << "Invalid matrix element\n";
-      return 2;
     }
-    output << mozhegova::uppTriMtx(arr, m, n) << "\n";
+    return 2;
+  }
+  output << mozhegova::uppTriMtx(arr, m, n) << "\n";
+  if (std::strcmp(argv[1], "2") == 0)
+  {
     delete[] arr;
   }
 }
