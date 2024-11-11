d254eb3264110574a4f748fb74d63810b5125540
root <root@kryloff.localdomain>
2024-11-12 02:15:35 +0300

    Line check added

--- krylov.matvey/P4/main.cpp
+++ krylov.matvey/P4/main.cpp
@@ -1,2 +1,18 @@
+#include <iostream>
+
 int main()
-{}
+{
+  char * line = nullptr;
+  line = krylov::inputLine(std::cin);
+  if (line == nullptr)
+  {
+    std::cerr << "Memory allocation failure!\n";
+    return 1;
+  }
+  if (line[0] == '\0')
+  {
+    std::cerr << "No characters to convert\n";
+    delete[] line;
+    return 1;
+  }
+}
