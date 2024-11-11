c98138b366ce369f6ee636d8a75b73d255945b88
Tuc0Sa1amanka <sparowj765@gmail.com>
2024-11-11 23:18:38 +0300

    mainc

--- lanovenko.ivan/P4/main.cpp
+++ lanovenko.ivan/P4/main.cpp
@@ -1,6 +1,24 @@
 #include <iostream>
+#include "strconversion.h"
 
 int main()
 {
-
+  char* istr = nullptr;
+  try
+  {
+    istr = lanovenko::inputstring(std::cin, '\n');
+  }
+  catch (const std::bad_alloc & e)
+  {
+    std::cerr << "No memory" << '\n';
+    return 1;
+  }
+  if(istr[0] == '\0')
+  {
+    delete[] istr;
+    std::cerr << "Empty string" << '\n';
+    return 1;
+  }
+  std::cout << lanovenko::transformationstr(istr) << '\n';
+  delete[] istr;
 }
