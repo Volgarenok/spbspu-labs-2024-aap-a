2070c2ba32a03631d56450111139c539ceada7a3
PigoDog <maksimtmfv@gmail.com>
2024-11-13 00:16:29 +0300

    add namespace

--- timofeev.maxim/P4/add_memory.cpp
+++ timofeev.maxim/P4/add_memory.cpp
@@ -1,6 +1,6 @@
 #include "functions.h"
 
-char* add_memory(size_t capacity)
+char* timofeev::add_memory(size_t capacity)
 {
   char* memory = reinterpret_cast<char*>(malloc(sizeof(char) * capacity));
   return memory;
