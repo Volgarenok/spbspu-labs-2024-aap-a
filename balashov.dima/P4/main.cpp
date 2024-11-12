3b956d4fe3cd13d789075cbf3626f69e41e9dd43
root <root@DESKTOP-KOVAQT8.localdomain>
2024-11-13 00:40:08 +0300

    add line

--- balashov.dima/P4/main.cpp
+++ balashov.dima/P4/main.cpp
@@ -1,4 +1,12 @@
+#include<iostream>
+
 int main()
 {
-
+  size_t capacity = 5;
+  char* line = reinterpret_cast<char*>(malloc(sizeof(char) * (capacity)));
+  if (line == nullptr)
+  {
+    std::cerr << "Memory was not allocated\n";
+    return 1;
+  }
 }
