b1e2a19b4417140b47af59d74be653145eb8100f
A1exCRE <alsmmm2065@gmail.com>
2024-11-12 23:34:19 +0300

    Create lines

--- smirnov.alexandr/P4/main.cpp
+++ smirnov.alexandr/P4/main.cpp
@@ -1,2 +1,20 @@
 int main()
-{}
+{
+  char * str1 = nullptr;
+  try
+  {
+    str1 = smirnov::getLine(std::cin, '\n');
+  }
+  catch (const std::bad_alloc & e)
+  }
+    std::cerr << "Out of memory\n";
+    return 1;
+  }
+  if (str1[0] == '\n')
+  {
+    delete[] str1;
+    std::cerr << "Error: empty string\n";
+    return 1;
+  }
+  const char * str2 = "def ";
+}
