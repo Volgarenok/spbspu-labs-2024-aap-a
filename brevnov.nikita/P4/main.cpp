58de183e1c98568ee90846e38a39e4e7e0900c2d
brev1ch <flirktv@gmail.com>
2024-11-12 07:20:38 +0300

    Change file

--- brevnov.nikita/P4/main.cpp
+++ brevnov.nikita/P4/main.cpp
@@ -6,7 +6,7 @@ int main()
 {
   constexpr char stop = '\n';
   char * first_line = nullptr;
-  char * second_line = nullptr;
+  char second_line[] = {'a', 'b', 'c', ' ', 'e', 'f', 0};
   try
   {
     first_line = brevnov::getline(std::cin, stop);
@@ -21,7 +21,6 @@ int main()
     std::cerr << "Empty input \n";
     return 1;
   }
-  second_line = "abc_ef";
   brevnov::finding_common_elements(std::cout, first_line, second_line);
   std::cout << "\n";
   delete[] first_line;
