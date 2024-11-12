f110631b0f0e25a99d8311be3bd44e78562356a9
lee <likachich12@gmail.com>
2024-11-12 19:51:52 +0300

    getline function

--- hismatova.elizaveta/P4/main.cpp
+++ hismatova.elizaveta/P4/main.cpp
@@ -6,13 +6,24 @@
 
 int main()
 {
-  char str1[100];
-  char str2[100];
-  std::cin.getline(str1, sizeof(str1));
-  std::cin.getline(str2, sizeof(str2));
+  char *str1 = nullptr;
+  char *str2 = nullptr;
+  if (hismatova::getLine(&str1) == 1)
+  {
+    std::cerr << "error in reading first line\n";
+    return 1;
+  }
+  if (hismatova::getLine(&str2) == 1)
+  {
+    std::cerr << "error in reading second line\n";
+    free(str1);
+    return 1;
+  }
   if (str1[0] == '\0' && str2[0] == '\0')
   {
     std::cerr << "empty strings\n";
+    free(str1);
+    free(str2);
     return 1;
   }
   char* result = hismatova::uniqueChar(str1, str2);
@@ -24,7 +35,11 @@ int main()
   else
   {
     std::cerr << "result is nullptr\n";
+    free(str1);
+    free(str2);
     return 1;
   }
+  free(str1);
+  free(str2);
   return 0;
 }
