db5208382509471f7aa17ab95f750a4c739fe5f3
root <root@DESKTOP-KOVAQT8.localdomain>
2024-11-13 00:44:05 +0300

    add all to main

--- balashov.dima/P4/main.cpp
+++ balashov.dima/P4/main.cpp
@@ -1,12 +1,33 @@
-#include<iostream>
+#include <iostream>
+#include "processingString.hpp"
 
 int main()
 {
   size_t capacity = 5;
-  char* line = reinterpret_cast<char*>(malloc(sizeof(char) * (capacity)));
+  char * line = reinterpret_cast<char*>(malloc(sizeof(char) * (capacity)));
   if (line == nullptr)
   {
     std::cerr << "Memory was not allocated\n";
     return 1;
   }
+
+  try
+  {
+    line = balashov::enteringLine(capacity);
+  }
+    catch(const std::bad_alloc& e)
+  {
+    free(line);
+    std::cerr << "Memory was not allocated\n";
+    return 1;
+  }
+  if (line[0] == '\0')
+  {
+    free(line);
+    std::cerr << "The absence of a line\n";
+    return 1;
+  }
+  std::cout << balashov::removeSpaces(line) << "\n";
+  free(line);
+  return 0;
 }
