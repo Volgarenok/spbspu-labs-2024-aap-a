1d87ee851977b4797d04fe3f05da5fc6cbd7be23
alina <duhaninaalina3@gmail.com>
2024-11-12 22:06:14 +0300

    fixes fixes

--- duhanina.alina/P3/minSumMdg.cpp
+++ duhanina.alina/P3/minSumMdg.cpp
@@ -1,27 +1,6 @@
 #include "functionMatrix.h"
 #include <algorithm>
 #include <cstring>
-/*
-int duhanina::minSumMdg(const int * t, size_t m, size_t n)
-{
-  size_t max_count_diag = m + n - 1;
-  int min_sum = std::numeric_limits<int>::max();
-  for (size_t sum = 0; sum < max_count_diag; ++sum)
-  {
-    int current_sum = 0;
-    for (size_t i = 0; i < m; ++i)
-    {
-      size_t j = sum - i;
-      if (j >= 0 && j < n_int)
-      {
-        current_sum += t[i * n + j];
-      }
-    }
-    min_sum = std::min(min_sum, current_sum);
-  }
-  return min_sum;
-}*/
-#include <climits>
 
 int duhanina::minSumMdg(const int* matrix, size_t rows, size_t cols)
 {
@@ -30,14 +9,14 @@ int duhanina::minSumMdg(const int* matrix, size_t rows, size_t cols)
     return 0;
   }
   size_t minSum = 0;
-  for (size_t i = 0, j = cols - 1; i < rows && j >= 0; ++i, --j)
+  for (size_t i = 0, j = cols - 1; i < rows && j < cols; ++i, --j)
   {
     minSum += matrix[i * cols + j];
   }
-  for (size_t startCol = cols - 2; startCol >= 0; --startCol)
+  for (size_t startCol = cols - 2; startCol < cols; --startCol)
   {
     size_t sum = 0;
-    for (size_t row = 0, col = startCol; row < rows && col >= 0; ++row, --col)
+    for (size_t row = 0, col = startCol; row < rows && col < cols; ++row, --col)
     {
       sum += matrix[row * cols + col];
     }
@@ -46,7 +25,7 @@ int duhanina::minSumMdg(const int* matrix, size_t rows, size_t cols)
   for (size_t startRow = 1; startRow < rows; ++startRow)
   {
     size_t sum = 0;
-    for (size_t row = startRow, col = cols - 1; row < rows && col >= 0; ++row, --col)
+    for (size_t row = startRow, col = cols - 1; row < rows && col < cols; ++row, --col)
     {
       sum += matrix[row * cols + col];
     }
