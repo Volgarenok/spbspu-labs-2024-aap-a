49715939abcc38e4465f80c8b40036470c8e1aa2
Sikireg <masj280906@gmail.com>
2024-11-13 11:05:26 +0300

    Iuiu

--- karnauhova.alexandra/P3/incomplete_matrix.cpp
+++ karnauhova.alexandra/P3/incomplete_matrix.cpp
@@ -3,7 +3,7 @@
 
 float* karnauhova::smooth_matrix(int* t, size_t m, size_t n)
 {
-  n_mtx = new float[m * n];
+  float* n_mtx = new float[m * n];
   int sum = 0;
   size_t k = 0;
   float count = 0;
