b57de6b30cadb659182f93de84373d55302b4242
root <root@DESKTOP-KOVAQT8.localdomain>
2024-11-13 23:05:52 +0300

    remove return 1 when not input

--- balashov.dima/P4/main.cpp
+++ balashov.dima/P4/main.cpp
@@ -22,12 +22,6 @@ int main()
     std::cerr << "Memory was not allocated\n";
     return 1;
   }
-  if (line[0] == '\0')
-  {
-    free(line);
-    std::cerr << "The absence of a line\n";
-    return 1;
-  }
   std::cout << balashov::removeSpaces(line) << "\n";
   free(line);
   return 0;
