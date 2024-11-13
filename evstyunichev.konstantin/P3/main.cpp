1414567b2c622f4ceb3746e162d127f74b251587
evstyunichev.konstantin <korombodis.gmail.com>
2024-11-13 11:31:48 +0300

    fixes, CG

--- evstyunichev.konstantin/P3/main.cpp
+++ evstyunichev.konstantin/P3/main.cpp
@@ -2,7 +2,7 @@
 #include <fstream>
 #include "matrix.hpp"
 
-int main(int argc, char** argv)
+int main(int argc, char **argv)
 {
   constexpr size_t StaticArrSize = 1e4;
   if (argc < 4)
@@ -17,57 +17,51 @@ int main(int argc, char** argv)
   }
   std::ifstream fin(argv[2]);
   std::ofstream fout(argv[3]);
-  size_t m = 0, n = 0;
-  fin >> m >> n;
+  size_t n = 0, m = 0;
+  fin >> n >> m;
   if (!fin)
   {
     std::cout << "((\n";
     return 1;
   }
-  if ((evstyunichev::cstring_len(argv[1]) == 1))
+  int p = 0;
+  p = evstyunichev::CastToDigit(argv[1]);
+  if (!p)
   {
-    int num = 0;
-    num = atoi(argv[1]);
-    int* arr = nullptr;
-    if (num == 1)
+    std::cerr << "is not 1 or 2 btw\n";
+    return 1;
+  }
+  int num = p;
+  int *arr = nullptr, *d_arr = nullptr;
+  if (num == 1)
+  {
+    static int static_arr[StaticArrSize];
+    arr = static_arr;
+  }
+  else if(num == 2)
+  {
+    try
     {
-      int static_arr[StaticArrSize];
-      arr = static_arr;
+      d_arr = new int[n * m];
+      arr = d_arr;
     }
-    else if(num == 2)
+    catch (const std::exception& e)
     {
-      try
-      {
-        int* d_arr = new int[m * n];
-        arr = d_arr;
-      }
-      catch(const std::exception& e)
-      {
-        std::cerr << e.what() << "\n";
-        evstyunichev::ddel(arr, num);
-        return 1;
-      }
-    }
-    else
-    {
-      std::cerr << "out of range\n";
+      std::cerr << e.what() << "\n";
+      delete[] d_arr;
       return 1;
     }
-    size_t read = 0;
-    if (evstyunichev::input(fin, arr, m, n, read) && (read == n * m))
-    {
-      std::cout << evstyunichev::CntRowsNsm(arr, m, n) << "\n";
-    }
-    else
-    {
-      evstyunichev::ddel(arr, num);
-      return 2;
-    }
-    evstyunichev::ddel(arr, num);
+  }
+  size_t read = 0;
+  if (evstyunichev::Input(fin, arr, n, m, read) && (read == n * m) && (read))
+  {
+    fout << evstyunichev::CntRowsNsm(arr, n, m) << "\n";
   }
   else
   {
-    std::cout << "is not 1 or 2 btw\n";
+    delete[] d_arr;
+    return 2;
   }
+  delete[] d_arr;
   return 0;
 }
--- evstyunichev.konstantin/P3/matrix.cpp
+++ evstyunichev.konstantin/P3/matrix.cpp
@@ -1,14 +1,20 @@
 #include "matrix.hpp"
 #include <iostream>
 
-size_t evstyunichev::cstring_len(char* s)
+int evstyunichev::CastToDigit(char* s)
 {
-  size_t len = 0;
-  for (; s[len] != 0; len++);
-  return len;
+  int ans = 0;
+  if (s[1] == 0)
+  {
+    if ((s[0] <= '2') && (s[0] >= '1'))
+    {
+      ans = s[0] - '0';
+    }
+  }
+  return ans;
 }
 
-std::istream & evstyunichev::input(std::istream & fin, int* arr, size_t m, size_t n, size_t &read)
+std::istream & evstyunichev::Input(std::istream & fin, int* arr, size_t m, size_t n, size_t &read)
 {
   int num = 0;
   for (size_t i = 0; i < m; i++)
@@ -25,12 +31,12 @@ std::istream & evstyunichev::input(std::istream & fin, int* arr, size_t m, size_
   return fin;
 }
 
-bool evstyunichev::isRowNsm(int* arr, size_t i, size_t n)
+bool evstyunichev::IsRowNsm(int* arr, size_t i, size_t m)
 {
   bool isNsm = true;
-  for(size_t j = 1; j < n; j++)
+  for(size_t j = 1; j < m; j++)
   {
-    if (arr[i * n + j] == arr[i * n + j - 1])
+    if (arr[i * m + j] == arr[i * m + j - 1])
     {
       isNsm = 0;
     }
@@ -38,22 +44,12 @@ bool evstyunichev::isRowNsm(int* arr, size_t i, size_t n)
   return isNsm;
 }
 
-size_t evstyunichev::CntRowsNsm(int* arr, size_t m, size_t n)
+size_t evstyunichev::CntRowsNsm(int* arr, size_t n, size_t m)
 {
   size_t ans = 0;
-  for (size_t i = 0; i < m; i++)
+  for (size_t i = 0; i < n; i++)
   {
-    ans += evstyunichev::isRowNsm(arr, i, n);
+    ans += evstyunichev::IsRowNsm(arr, i, m);
   }
   return ans;
-}
-
-void evstyunichev::ddel(int* arr, int num)
-{
-  constexpr int dinamic_flag = 2;
-  if (num == dinamic_flag)
-  {
-    delete[] arr;
-  }
-  return;
 }
