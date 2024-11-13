3d0cd96ddb8cef1a6f13695675855e96a9c4506c
Nekich06 <p.nikita.73526@gmail.com>
2024-11-13 13:07:03 +0300

    Moved checkMatrix function to main.cpp and removed check_matrix.cpp
    Now it looks better

--- petrov.nikita/P3/main.cpp
+++ petrov.nikita/P3/main.cpp
@@ -1,11 +1,15 @@
 #include <iostream>
 #include <fstream>
-#include <stdlib.h>
+#include <cstring>
+#include <cctype>
+#include <cstdlib>
 #include "process_the_matrix.hpp"
+
+int checkArguments(int argc, const char ** argv);
 
 int main(int argc, const char ** argv)
 {
-  if (petrov::checkArguments(argc, argv) == 1)
+  if (checkArguments(argc, argv) == 1)
   {
     return 1;
   }
@@ -82,3 +86,35 @@ int main(int argc, const char ** argv)
   }
 }
 
+int checkArguments(int argc, const char ** argv)
+{
+  if (argc != 4)
+  {
+    if (argc < 4)
+    {
+      std::cerr << "Not enough arguments\n";
+      return 1;
+    }
+    else if (argc > 4)
+    {
+      std::cerr << "Too many arguments\n";
+      return 1;
+    }
+  }
+  size_t size_of_first_argument = std::strlen(argv[1]);
+  for (size_t i = 0; i < size_of_first_argument; i++)
+  {
+    if (std::isdigit(argv[1][i]) == false)
+    {
+      std::cerr << "First parameter is not a number\n";
+      return 1;
+    }
+  }
+  int check_varuable = std::atoi(argv[1]);
+  if (check_varuable != 1 && check_varuable != 2)
+  {
+    std::cerr << "First parameter is out of range\n";
+    return 1;
+  }
+  return 0;
+}
--- petrov.nikita/P3/process_the_matrix.hpp
+++ petrov.nikita/P3/process_the_matrix.hpp
@@ -4,7 +4,6 @@
 
 namespace petrov
 {
-  int checkArguments(int argc, const char ** argv);
   std::istream & inputMatrix(std::istream & in, int * ptr_matrix, size_t number_of_rows, size_t rows_length);
   int countNotZeroDiagonals(int * ptr_matrix, size_t number_of_rows, size_t rows_length);
 }
