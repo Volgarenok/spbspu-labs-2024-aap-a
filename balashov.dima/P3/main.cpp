6b647d68204ed0491e0b0ef2e280b4fd53cc5bf8
root <root@DESKTOP-KOVAQT8.localdomain>
2024-11-13 18:26:37 +0300

    change return

--- balashov.dima/P3/main.cpp
+++ balashov.dima/P3/main.cpp
@@ -66,7 +66,7 @@ int main(int argc, char** argv)
             delete[] matrixDynamic;
             delete[] matrix;
             std::cerr << "Out of memory\n";
-            return 2;
+            return 1;
         }
         matrix = matrixDynamic;
     }
