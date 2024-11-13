a4c29758815207007af61ec45a937e62cca2581a
Nekich06 <p.nikita.73526@gmail.com>
2024-11-13 13:22:23 +0300

    Trying to remove duplication of logic part 1
    It helps program be more readable and it is more optimized variant

--- petrov.nikita/P3/main.cpp
+++ petrov.nikita/P3/main.cpp
@@ -15,29 +15,29 @@ int main(int argc, const char ** argv)
   }
   std::ifstream input(argv[2]);
   std::ofstream output(argv[3]);
+  size_t number_of_rows = 0, rows_length = 0;
+  input >> number_of_rows;
+  input >> rows_length;
+  if (!input)
+  {
+    std::cerr << "Error: Invalid data\n";
+    return 2;
+  }
+  if (number_of_rows == 0 && rows_length == 0)
+  {
+    output << 0;
+    return 0;
+  }
+  if ((number_of_rows == 0 && rows_length != 0) || (number_of_rows != 0 && rows_length == 0))
+  {
+    std::cerr << "Error: Invalid data\n";
+    return 2;
+  }
   if (std::atoi(argv[1]) == 1)
   {
-    size_t number_of_rows = 0, rows_length = 0;
-    input >> number_of_rows;
-    input >> rows_length;
-    if (!input)
-    {
-      std::cerr << "Error: Invalid data\n";
-      return 2;
-    }
-    if (number_of_rows == 0 && rows_length == 0)
-    {
-      output << 0;
-      return 0;
-    }
     int matrix[10000] = {0};
     int * ptr_matrix = matrix;
-    if ((number_of_rows == 0 && rows_length != 0) || (number_of_rows != 0 && rows_length == 0))
-    {
-      std::cerr << "Error: Invalid data\n";
-      return 2;
-    }
-    else if (!petrov::inputMatrix(input, ptr_matrix, number_of_rows, rows_length))
+    if (!petrov::inputMatrix(input, ptr_matrix, number_of_rows, rows_length))
     {
       std::cerr << "Error: Invalid data\n";
       return 2;
@@ -46,30 +46,11 @@ int main(int argc, const char ** argv)
   }
   else
   {
-    size_t number_of_rows = 0, rows_length = 0;
-    input >> number_of_rows;
-    input >> rows_length;
-    if (!input)
-    {
-      std::cerr << "Error: Invalid data\n";
-      return 2;
-    }
-    if (number_of_rows == 0 && rows_length == 0)
-    {
-      output << 0;
-      return 0;
-    }
     int * ptr_matrix = nullptr;
     try
     {
       ptr_matrix = new int[number_of_rows * rows_length];
-      if ((number_of_rows == 0 && rows_length != 0) || (number_of_rows != 0 && rows_length == 0))
-      {
-        std::cerr << "Error: Invalid data\n";
-        delete[] ptr_matrix;
-        return 2;
-      }
-      else if (!petrov::inputMatrix(input, ptr_matrix, number_of_rows, rows_length))
+      if (!petrov::inputMatrix(input, ptr_matrix, number_of_rows, rows_length))
       {
         std::cerr << "Error: Invalid data\n";
         delete[] ptr_matrix;
