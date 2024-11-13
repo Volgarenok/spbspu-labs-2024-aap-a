a03065d40ad8f131f17f7ef800948be50e8dbdf6
vladislavmaslovskiy <vladuisik826@gmail.com>
2024-11-13 21:05:47 +0300

    Add mergeStrs

---
+++ maslovskiy.vladislav/P4/mergeStrs.cpp
@@ -0,0 +1,23 @@
+#include "mergeStrs.hpp"
+
+void mergeStrs(char *res, const char *first, const char *second)
+{
+  size_t i = 0;
+  for (; first[i] != '\0' && second[i] != '\0'; ++i)
+  {
+    res[i * 2] = first[i];
+    res[i * 2 + 1] = second[i];
+  }
+  const char *temp = first[i] != '\0' ? first : second;
+  fillStr(res, temp, i);
+}
+void fillStr(char *res, const char *str, size_t pos)
+{
+  size_t i = 0;
+  for (i = pos * 2; str[pos] != '\0'; ++i)
+  {
+    res[i] = str[pos];
+    ++pos;
+  }
+  res[i] = '\0';
+}
---
+++ maslovskiy.vladislav/P4/mergeStrs.hpp
@@ -0,0 +1,7 @@
+#ifndef MERGE_STRS_HPP
+#define MERGE_STRS_HPP
+
+void fillStr(char *res, const char *str, size_t pos);
+void mergeStrs(char *res, const char *first, const char *second);
+
+#endif
