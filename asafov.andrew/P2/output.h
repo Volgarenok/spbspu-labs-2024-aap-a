2d0fd1984cf3942a71e55c86949b24cfb4b99db5
shycoshy <neforscist@gmail.com>
2024-11-14 08:47:02 +0000

    namespace

--- asafov.andrew/P2/output.h
+++ asafov.andrew/P2/output.h
@@ -5,7 +5,7 @@ namespace asafov
 {
 
   void output(double left, double right, double step, std::size_t numberMax, double error);
-  void asafov::writeLine(double x, size_t k, double error);
-  void asafov::makeLine(int a, char b);
+  void writeLine(double x, size_t k, double error);
+  void makeLine(int a, char b);
 }
 #endif
