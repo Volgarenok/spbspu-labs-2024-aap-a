e02cd3097b72ad53858465b403097ab7e190b31f
alina <duhaninaalina3@gmail.com>
2024-11-14 19:26:26 +0300

    sddfg

--- duhanina.alina/P3/inputTable.cpp
+++ duhanina.alina/P3/inputTable.cpp
@@ -3,8 +3,7 @@
 
 std::istream & duhanina::inputMatrix(std::istream & in, int * t, size_t m, size_t n, size_t & read)
 {
-  size_t size_table = n * m;
-  for (size_t i = 0; i < size_table; ++i)
+  for (size_t i = 0; i < (n * m); ++i)
   {
     if (in >> t[i])
     {
