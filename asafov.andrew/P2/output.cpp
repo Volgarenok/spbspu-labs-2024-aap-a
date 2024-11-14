48a353692e69a89ed6e84175652a1cdc35d1fa38
shycoshy <neforscist@gmail.com>
2024-11-14 08:54:43 +0000

    x

--- asafov.andrew/P2/output.cpp
+++ asafov.andrew/P2/output.cpp
@@ -24,8 +24,8 @@ double value = 0;
   {
     std::cout << "<MATH ERROR>";
   }
-  (value < 0) ? (std::cout << std::fixed << "|") : (std::cout << std::fixed << "| ");
-  std::cout << std::fixed << right << " | " << value << " | " << std::exp(-1.0 * right * right) << " | \n";
+  (x < 0) ? (std::cout << std::fixed << "|") : (std::cout << std::fixed << "| ");
+  std::cout << std::fixed << x << " | " << value << " | " << std::exp(-1.0 * right * right) << " | \n";
 }
 
 void asafov::output(double left, double right, double step, size_t numberMax, double error)
