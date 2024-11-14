ab7babc7979632b283fe2af88c7fb0db3053bac4
alina <duhaninaalina3@gmail.com>
2024-11-15 00:23:43 +0300

    valgr

--- duhanina.alina/P4/main.cpp
+++ duhanina.alina/P4/main.cpp
@@ -6,19 +6,26 @@ int main()
 {
   size_t size = 20;
   char* str = new char[size + 1];
+  if (str == nullptr)
+  {
+    delete[] str;
+    std::cerr << "Error memory\n";
+    return 1;
+  }
   str[size] = '\0';
   char c = '\0';
   size_t index = 0;
   while (std::cin >> c && c != '\n')
   {
-    if (index >= size - 1)
+    if (index >= (size - 1))
     {
       size *= 2;
       char* newStr = new char[size + 1];
-      if (!newStr)
+      if (str_new == nullptr)
       {
-        std::cerr << "Memory error\n";
         delete[] str;
+        delete[] str_new;
+        std::cerr << "Error memory\n";
         return 1;
       }
       newStr = duhanina::createNewArray(str, newStr);
