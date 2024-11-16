bee84c2d7b85733ab2e31e187d4d7592a46ca969
Maksim_Shramko <maxramko@gmail.com>
2024-11-16 19:34:22 +0300

    Correcting for CG 1.10

--- shramko.maksim/P3/countColoumnsWithNonRepeatingNumbers.hpp
+++ shramko.maksim/P3/countColoumnsWithNonRepeatingNumbers.hpp
@@ -5,7 +5,7 @@
 
 namespace shramko
 {
-  size_t countColoumnsWithNonRepeatingNumbers(const int * const mtrx, size_t cnt_row, size_t cnt_col);
+  size_t countColoumnsWithNonRepeatingNumbers(const int * const mtrx, const size_t cnt_row, const size_t cnt_col);
 }
 
 #endif
