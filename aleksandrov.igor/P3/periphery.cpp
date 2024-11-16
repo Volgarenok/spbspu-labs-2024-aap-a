83e57190f8515b93ca9112b001d8b4a4455ee53d
MrMels625 <MrMels625@gmail.com>
2024-11-16 17:38:47 +0300

    Trying to fix the periphery function 2

--- aleksandrov.igor/P3/periphery.cpp
+++ aleksandrov.igor/P3/periphery.cpp
@@ -2,6 +2,10 @@
 
 void aleksandrov::periphery(int* array, size_t m, size_t n)
 {
+  if (m == 0 || n == 0)
+  {
+    return;
+  }
   size_t p1 = 0, p2 = m - 1, q1 = 0, q2 = n - 1;
   size_t count = 1;
   do
