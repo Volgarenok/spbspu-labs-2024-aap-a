be7bea577ac1aec0264bf34258e04663fe96c18a
sharksonk <sharksonk@gmail.com>
2024-11-17 20:28:09 +0300

    trash

--- sharifullina.sofia/P2/cos_expnegx.hpp
+++ sharifullina.sofia/P2/cos_expnegx.hpp
@@ -6,10 +6,10 @@ namespace sharifullina
 {
   double cos(double x, size_t k, double error);
   double exp_negx(double x, size_t k, double error);
-  double cos_and_exp(double x, size_t k, double error)
+  double cos_and_exp(double x, size_t k, double error);
   double cos_cmath(double x);
   double exp_negx_cmath(double x);
-  double cos_and_exp_cma(double x)
+  double cos_and_exp_cma(double x);
 
   using FuncPtr = double(*)(double, size_t, double);
   extern FuncPtr funcs[];
