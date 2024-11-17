10c46b89e5b1a2686248c8de558e4f9ddb176d82
Z-1337 <un228der@gmail.com>
2024-11-17 17:02:46 +0300

    updated hpp file

--- kushekbaev.danil/P3/kushekbaev_matrix.hpp
+++ kushekbaev.danil/P3/kushekbaev_matrix.hpp
@@ -5,8 +5,8 @@
 namespace kushekbaev
 {
   int * createMatrix(size_t rows, size_t columns);
-  void deleteMatrix(int * matrix);
-  int & getElement(int * matrix, size_t columns, size_t row, size_t column);
+  bool isMinInRow(int* matrix, size_t columns, size_t row, int element);
+  bool isMaxInColumn(int* matrix, size_t rows, size_t columns, size_t column, int element);
   int countSaddlePoints(int * matrix, size_t rows, size_t columns);
 }
 
