a9327e6a3084c88715f96385ab8350b011a4e365
sharksonk <sharksonk@gmail.com>
2024-11-17 19:44:19 +0300

    aaaaaa

--- sharifullina.sofia/P2/output.cpp
+++ sharifullina.sofia/P2/output.cpp
@@ -24,7 +24,7 @@ void sharifullina::output_row(double i, size_t k, double error)
     }
   std::cout << " " << std::setw(WidthOfCol) << cos_cmath(i);
   std::cout << " " << std::setw(WidthOfCol) << exp_negx_cmath(i);
-  std::cout << " " << std::setw(WidthOfCol) << cos_and_negx_cmath(i);
+  std::cout << " " << std::setw(WidthOfCol) << cos_and_negx_cma(i);
 }
 
 void sharifullina::output_table(double left, double right, size_t k, double step, double error)
