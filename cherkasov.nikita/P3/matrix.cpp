510cc095b550d286072481d6e4df9109b7a634ba
cherkasoov <cherkasov5509@gmail.com>
2024-11-18 02:40:01 +0300

    ..

--- cherkasov.nikita/P3/matrix.cpp
+++ cherkasov.nikita/P3/matrix.cpp
@@ -46,12 +46,13 @@ int cherkasov::readMatrix(const char* inputFile, size_t& rows, size_t& cols, boo
     {
       if (!(inFile >> (*matrix)[i][j]))
       {
+        std::cerr << "Error: Invalid matrix format.\n";
         for (size_t k = 0; k <= i; ++k)
         {
           delete[] (*matrix)[k];
         }
           delete[] *matrix;
-          return 0;
+          return 1;
       }
     }
   }
