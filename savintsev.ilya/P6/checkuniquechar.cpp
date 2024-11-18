a44bc0f56ccd5059430dd48200fd6dc11cf492b4
faxryzen <cheburek2006@gmail.com>
2024-11-18 22:25:51 +0300

    Created all functions. CG not followed

---
+++ savintsev.ilya/P6/checkuniquechar.cpp
@@ -0,0 +1,92 @@
+#include "checkuniquechar.h"
+#include <iostream>
+
+bool savintsev::isOneOfTheLetters(char c)
+{
+  return ((c >= 97 && c <= 102) || (c >= 120 && c <= 122));
+}
+
+bool savintsev::isUnsignedInteger(const char * s, int & pos)
+{ 
+  if (!s[pos] || !std::isdigit(s[pos]))
+  {
+    return false;
+  }
+  pos++;
+  if (!s[pos] || !std::isdigit(s[pos]))
+  {
+    return true;
+  }
+  return isUnsignedInteger(s, pos);
+}
+
+bool savintsev::isIdentifier(const char* s, int & pos)
+{
+    if (isOneOfTheLetters(s[pos])) 
+    {
+        pos++;
+        return true;
+    }
+    return false;
+}
+
+bool savintsev::isMultiplier(const char * s, int & pos)
+{
+  int start = pos;
+  if (isUnsignedInteger(s, pos) || isIdentifier(s, pos)) {
+      return true;
+  }
+  if (s[pos] == '(') {
+      pos++;
+      if (isExpression(s, pos) && s[pos] == ')') {
+          pos++;
+          return true;
+    }
+  }
+  pos = start;
+  return false;
+}
+
+bool savintsev::isTerm(const char* s, int& pos) {
+    int start = pos;
+
+    if (isMultiplier(s, pos)) {
+        return true;
+    }
+
+    if (s[pos] == '(') {
+        pos++;
+        if (isMultiplier(s, pos)) {
+            if (s[pos] == '*' || s[pos] == '/') {
+                pos++;
+                if (isTerm(s, pos) && s[pos] == ')') {
+                    pos++;
+                    return true;
+                }
+            }
+        }
+    }
+
+    pos = start; // Откат позиции
+    return false;
+}
+
+bool savintsev::isExpression(const char* s, int & pos) {
+    int start = pos;
+
+    if (isTerm(s, pos)) {
+        return true;
+    }
+
+    if (isTerm(s, pos)) {
+        if (s[pos] == '+' || s[pos] == '-') {
+            pos++;
+            if (isExpression(s, pos)) {
+                return true;
+            }
+        }
+    }
+
+    pos = start; // Откат позиции
+    return false;
+}
