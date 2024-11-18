6a31f235c08b1336836e240b268d527c5b83555a
Z-1337 <un228der@gmail.com>
2024-11-18 18:54:25 +0300

    fixed parenthesis and deleted cycles

--- kushekbaev.danil/P6/isValidExpression.cpp
+++ kushekbaev.danil/P6/isValidExpression.cpp
@@ -27,9 +27,7 @@ const char * has_digit(const char * str)
         has_character(str, 'B') ? str + 1 :
         has_character(str, 'C') ? str + 1 :
         has_character(str, 'D') ? str + 1 :
-        has_character(str, 'E') ? str + 1 :
-        has_character(str, '(') ? str + 1 :
-        has_character(str, ')') ? str + 1 : nullptr;
+        has_character(str, 'E') ? str + 1 : nullptr;
 }
 
 const char * has_unsigned_int(const char * str)
@@ -39,34 +37,42 @@ const char * has_unsigned_int(const char * str)
     return str;
   }
   const char * next = has_digit(str);
-  while (next)
-  {
-    str = next;
-    next = has_digit(str);
-  }
-  return str;
-}
-
-const char * has_multiplier(const char * str)
-{
-  if (!str)
+  if (!next)
   {
     return str;
   }
 
-  auto next = has_unsigned_int(str);
-  if (next)
-  {
-    return next;
-  }
+  return has_unsigned_int(next);
+}
 
-  next = has_digit(str);
-  if (next)
-  {
-    return next;
-  }
+const char * has_multiplier(const char * str)
+{
+    if (!str)
+    {
+        return str;
+    }
 
-  next = has_expression(str);
+    auto next = has_unsigned_int(str);
+    if (next)
+    {
+        return next;
+    }
+
+    next = has_expression(str);
+    return next;
+}
+
+const char * has_parentheses(const char * str)
+{
+  if (!str || *str != '(')
+  {
+    return nullptr;
+  }
+  const char * next = has_expression(str + 1);
+  if (next && *next == ')')
+  {
+    return next + 1;
+  }
   return next;
 }
 
@@ -77,19 +83,15 @@ const char * has_term(const char * str)
     return str;
   }
   const char * next = has_multiplier(str);
-
-  while (true)
+  if (!next)
   {
-    const char * tmp_next = has_character(next, '*');
-    if (!tmp_next)
-    {
-      return next;
-    }
-    next = has_multiplier(tmp_next);
-    if (!next)
-    {
-      return next;
-    }
+    return str;
+  }
+
+  const char * hchar_next = has_character(next, '*');
+  if (hchar_next)
+  {
+    return has_term(hchar_next);
   }
   return next;
 }
@@ -100,26 +102,37 @@ const char * has_expression(const char * str)
   {
     return str;
   }
-  const char * next = has_term(str);
-  while (true)
-  {
-    const char * plus_next = has_character(next, '+');
-    const char * minus_next = has_character(next, '-');
 
-    if (plus_next)
+  const char * next = has_term(str);
+  if (!next)
+  {
+    return str;
+  }
+
+  const char * plus_next = has_character(next, '+');
+  if (plus_next)
+  {
+    const char * term_next = has_term(plus_next);
+    if (term_next)
     {
-      next = has_term(plus_next);
-      if (!next) return nullptr;
+      return has_expression(term_next);
     }
-    else if (minus_next)
+  }
+
+  const char * minus_next = has_character(next, '-');
+  if (minus_next)
+  {
+    const char * term_next = has_term(minus_next);
+    if (term_next)
     {
-      next = has_term(minus_next);
-      if (!next) return nullptr;
+      return has_expression(term_next);
     }
-    else
-    {
-      break;
-    }
+  }
+
+  const char * parentheses_next = has_parentheses(next);
+  if (parentheses_next)
+  {
+    return has_expression(parentheses_next);
   }
   return next;
 }
