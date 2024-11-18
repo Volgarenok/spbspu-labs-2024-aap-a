2f8840f5a14e67af00722ba5442e826e3496b2ff
mokkenz1 <lam1600@mail.ru>
2024-11-18 14:15:18 +0300

    Revert "Merge branch 'lebedev.alexandr/P3'"
    This reverts commit 521d8e38fefed0f7654e9788cfa8c6ccee0038d2, reversing
    changes made to 1c2db125918ff2cde9fe896024c69de22052bfde.

--- lebedev.alexandr/P3/matrix.hpp
+++
@@ -1,9 +0,0 @@
-#ifndef TABLE_HPP
-#define TABLE_HPP
-#include <fstream>
-namespace lebedev
-{
-  std::istream& input(std::istream& inputFile, int* arr, size_t m);
-  bool lwrTriMtx(const int* arr, size_t m);
-}
-#endif
