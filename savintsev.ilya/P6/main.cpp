a4a8a92c418b04660abf0031705cd7a12c0c7dfc
faxryzen <cheburek2006@gmail.com>
2024-11-18 23:32:56 +0300

    Changed main.cpp

--- savintsev.ilya/P6/main.cpp
+++ savintsev.ilya/P6/main.cpp
@@ -1,8 +1,22 @@
 #include <iostream>
 #include <dynamicstrings.h>
+#include "checkuniquechars.h"
 
 int main()
 {
-  s
+  char * line = savintsev::inputNewlineTerminatedStr(std::cin);
+  if (line == nullptr)
+  {
+    std::cerr << "Failure to allocate memory\n";
+    return 1;
+  }
+  if (line[0] == '\0')
+  {
+    std::cerr << "No characters to convert\n";
+    delete[] line;
+    return 1;
+  }
+  int pos = 0;
+  std::cout << savintsev::isExpression(line, pos) << '\n';
   return 0;
 }
