d0cf9569a608f13c90a02c13527b54fe977e3bdd
shycoshy <neforscist@gmail.com>
2024-11-19 21:37:51 +0000

    fix no.12

--- asafov.andrew/P3/matrixAutoInitialization.cpp
+++ asafov.andrew/P3/matrixAutoInitialization.cpp
@@ -6,7 +6,15 @@ void asafov::matrixAutoInitialization(int* mtx, char* input, size_t& n, size_t& 
 {
   std::ifstream fin(input);
   fin >> n;
+  if (fin.fail() || fin.eof()) {
+    throw std::logic_error("err");
+    fin.close();
+  }
   fin >> m;
+  if (fin.fail() || fin.eof()) {
+    throw std::logic_error("err");
+    fin.close();
+  }
   for (size_t i = 0; i < n * m; i++) {
     fin >> mtx[i];
     if (fin.fail() || fin.eof()) {
--- asafov.andrew/P3/matrixDynamicInitialization.cpp
+++ asafov.andrew/P3/matrixDynamicInitialization.cpp
@@ -6,12 +6,21 @@ void asafov::matrixDynamicInitialization(int*& mtx, char* input, size_t& n, size
 {
   std::ifstream fin(input);
   fin >> n;
+  if (fin.fail() || fin.eof()) {
+    throw std::logic_error("err");
+    fin.close();
+  }
   fin >> m;
+  if (fin.fail() || fin.eof()) {
+    throw std::logic_error("err");
+    fin.close();
+  }
   mtx = new int[n * m];
   for (size_t i = 0; i < n * m; i++) {
     fin >> mtx[i];
     if (fin.fail() || fin.eof()) {
       throw std::logic_error("err");
+      fin.close();
     }
   }
   fin.close();
