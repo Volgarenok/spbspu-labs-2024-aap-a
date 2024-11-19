e4b89f518907a334a63856f00e6489b9fa7e53b0
mokkenz1 <lam1600@mail.ru>
2024-11-19 18:50:20 +0300

    Little fix

--- lebedev.alexandr/P4/main.cpp
+++ lebedev.alexandr/P4/main.cpp
@@ -13,12 +13,11 @@ int main()
     std::cerr << "Memory allocation error!\n";
     return 2;
   }
-  // if (str == nullptr)
-  // {
-  //   std::cerr << "Incorrect input!\n";
-  //   return 2;
-  // }
-
+  if (!std::cin)
+  {
+    std::cerr << "Incorrect input!\n";
+    return 1;
+  }
   lebedev::removeVowels(str);
   std::cout << str << '\n';
   delete[] str;
