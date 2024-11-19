54255441bff1b748d5f5a073e9f3d9eca34740db
ZakirovMS <mz.08.08.06@gmail.com>
2024-11-19 20:47:30 +0300

    Fix of constancy according to the CG

--- zakirov.marat/P3/mtx_work.hpp
+++ zakirov.marat/P3/mtx_work.hpp
@@ -6,7 +6,7 @@ namespace zakirov
 {
   std::istream & input_mtx(std::istream & in, int * mtx, size_t columns, size_t rows);
   void mtx_spiral_decrease(int * mtx, size_t columns, size_t rows);
-  void output_mtx(std::ostream & out, int const * const mtx, size_t columns, size_t rows);
+  void output_mtx(std::ostream & out, const int * mtx, size_t columns, size_t rows);
 }
 
 #endif
