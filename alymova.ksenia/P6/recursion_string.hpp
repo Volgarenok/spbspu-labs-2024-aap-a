6230efff1eec7dfe146646fdac8c7a43b60c4cfc
Ksenia-rgb <ksenialymova@mail.ru>
2024-11-20 21:25:17 +0300

    fixing CG

--- alymova.ksenia/P6/recursion_string.hpp
+++ alymova.ksenia/P6/recursion_string.hpp
@@ -3,13 +3,13 @@
 namespace alymova
 {
   const char* has_e(const char* str);
-	const char* has_dot(const char* str);
-	const char* has_sign(const char* str);
-	const char* has_number(const char* str);
-	const char* has_unsigned_int(const char* str);
-	const char* has_order(const char* str);
-	const char* has_mantissa(const char* str);
-	const char* has_double(const char* str);
-	bool is_double(const char* str);
+  const char* has_dot(const char* str);
+  const char* has_sign(const char* str);
+  const char* has_number(const char* str);
+  const char* has_unsigned_int(const char* str);
+  const char* has_order(const char* str);
+  const char* has_mantissa(const char* str);
+  const char* has_double(const char* str);
+  bool is_double(const char* str);
 }
 #endif
