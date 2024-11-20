81da63b6ed83a87c6cacf3561dac899aa1b8e15d
NastyKomar <komarova2.ae@edu.spbstu.ru>
2024-11-20 19:20:30 +0300

    fix08

--- komarova.anastasia/P3/main.cpp
+++ komarova.anastasia/P3/main.cpp
@@ -53,7 +53,7 @@ int main(int argc, char ** argv)
     return 2;
   }
   size_t size_arr = m * n;
-  if (atoi(st) == 1)
+  if (std::atoi(st) == 1)
   {
     int array_1[10000] = {};
     if (!(komarova::input_m(input, array_1, size_arr)))
@@ -67,7 +67,7 @@ int main(int argc, char ** argv)
       komarova::checktri(output, array_1, m);
     }
   }
-  if (atoi(st) == 2)
+  if (std::atoi(st) == 2)
   {
     int* array_2 = new int[size_arr];
     if (!(komarova::input_m(input, array_2, size_arr)))
