310892c2d296532b8cf49d0fe5ce4a3759530d48
Tuc0Sa1amanka <sparowj765@gmail.com>
2024-11-20 21:58:38 +0300

    matrixhm

---
+++ lanovenko.ivan/P3/modification_matrix.h
@@ -0,0 +1,9 @@
+#ifndef MODIFICATION_MATRIX_H
+#define MODIFICATION_MATRIX_H
+#include <cstddef>
+
+namespace lanovenko
+{
+  void changeto_LFT_TOP_CLK(int* matrix, size_t rows, size_t cols);
+}
+#endif
