2851e013423349470b8bf292e9b6d0a6b1cb244d
root <root@LAPTOP-FGITSRCQ.localdomain>
2024-11-20 17:54:56 +0300

    function create

--- brevnov.nikita/P6/isexpression.cpp
+++ brevnov.nikita/P6/isexpression.cpp
@@ -1,14 +1,5 @@
 #include "isexpression.hpp"
 
-bool brevnov::isExpreession(const char * str)
-{
-  if (!str)
-  {
-    return false;
-  }
-  return false;
-}
-
 const char * brevnov::isLetter(const char * str)
 {
   if (!str)
@@ -25,4 +16,63 @@ const char * brevnov::isDigit(const char * str)
     return nullptr;
   }
   return ((*str == '1') || (*str == '0')) ? (str + 1) : nullptr;
+}
+
+const char * brevnov::isUint(const char * str)
+{
+  if (!str)
+  {
+    return nullptr;
+  }
+  auto next = brevnov::isDigit(str);
+  if (next == nullptr)
+  {
+    return next;
+  }
+  else
+  {
+    return brevnov::isUint(next);
+  }
+}
+
+const char * brevnov::isMultiplier(const char * str)
+{
+  if (!str)
+  {
+    return str;
+  }
+  auto next = brevnov::isUint(str);
+  if (next)
+  {
+    return next;
+  }
+  return brevnov::isLetter(str);
+}
+
+const char * brevnov::isTerm(const char * str)
+{
+  if (!str)
+  {
+    return str;
+  }
+  auto next = brevnov::isMultiplier(str);
+  if (brevnov::isMultiplier(next))
+  {
+    return brevnov::isTerm(next);
+  }
+  return next;
+}
+
+const char * brevnov::isExpreession(const char * str)
+{
+  if (!str)
+  {
+    return str;
+  }
+  auto next = isTerm(str);
+  if (isExpreession(next))
+  {
+    return isExpreession(next);
+  }
+  return next;
 }
--- brevnov.nikita/P6/isexpression.hpp
+++ brevnov.nikita/P6/isexpression.hpp
@@ -2,8 +2,11 @@
 #define ISEXPRESSION_HPP
 namespace brevnov 
 {
-  bool isExpreession(const char * str);
+  const char * isExpreession(const char * str);
   const char * isLetter(const char * str);
   const char * isDigit(const char * str);
+  const char * isUint(const char * str);
+  const char * isMultiplier(const char * str);
+  const char * isTerm(const char * str);
 }
 #endif
