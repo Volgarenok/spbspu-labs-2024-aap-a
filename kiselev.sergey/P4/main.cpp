96ec21a7d3f23900b082272c8c15dbdf8bfd08c5
deonik3 <kis.sergey2006@gmail.com>
2024-11-21 18:25:22 +0300

    Added a check for entering an empty string

--- kiselev.sergey/P4/main.cpp
+++ kiselev.sergey/P4/main.cpp
@@ -4,9 +4,10 @@
 int main()
 {
   char* string = kiselev::inputString();
-  if (string == nullptr)
+  if (string == nullptr || string[0] == '\0')
   {
     std::cerr << "Failed string reading\n";
+    return 1;
   }
   const char secondString[] = "abs";
   size_t countIdentical = kiselev::identicalChar(string, secondString);
