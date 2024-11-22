4a92f81df8a3eddb781db7a435d4652ece7a2a2f
aldyn-d <aldyndirti@gmail.com>
2024-11-22 16:50:10 +0300

    added matrix input function

---
+++ dirti.aldyn/P3/input_matrix.cpp
@@ -0,0 +1,15 @@
+#include "input_matrix.hpp"
+
+
+std::istream & dirti::input_matrix(std::istream & in, int ** t, size_t m, size_t n, size_t & read)
+{
+  for (size_t i = 0; i < m; ++i)
+  {
+    for (size_t j = 0; j < n; ++j)
+    {
+      in >> t[i][j];
+      ++read;
+    }
+  }
+  return in;
+}
