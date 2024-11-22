0648d48021cc23106acbc96c5473b47137d4154a
aldyn-d <aldyndirti@gmail.com>
2024-11-22 21:37:37 +0300

    matrix input corrections

--- dirti.aldyn/P3/input_matrix.cpp
+++ dirti.aldyn/P3/input_matrix.cpp
@@ -1,13 +1,13 @@
 #include "input_matrix.hpp"
 
 
-std::istream & dirti::input_matrix(std::istream & in, int ** t, size_t m, size_t n, size_t & read)
+std::istream & dirti::input_matrix(std::istream & in, int * t, size_t m, size_t n, size_t & read)
 {
   for (size_t i = 0; i < m; ++i)
   {
     for (size_t j = 0; j < n; ++j)
     {
-      in >> t[i][j];
+      in >> t[n * i + j];
       ++read;
     }
   }
--- dirti.aldyn/P3/main.cpp
+++ dirti.aldyn/P3/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <fstream>
+#include "input_matrix.hpp"
 
 
 int main(int argc, char ** argv)
