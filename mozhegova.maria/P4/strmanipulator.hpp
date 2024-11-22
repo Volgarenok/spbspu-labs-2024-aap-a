c5d053ff2ca6434886a31a4250149de6c5fba345
mipxe <mariamozegova8726@gmail.com>
2024-11-22 21:43:44 +0300

    create common, fix P4 and add input string in P6

--- mozhegova.maria/P4/strmanipulator.hpp
+++ mozhegova.maria/P4/strmanipulator.hpp
@@ -1,12 +1,8 @@
 #ifndef STRMANIPULATOR_HPP
 #define STRMANIPULATOR_HPP
 
-#include <iostream>
-
 namespace mozhegova
 {
-  char * inputString(std::istream & in, char stop);
-  char * changeSize(char * nowstr, size_t length);
   char * spcRmv(char * str);
 }
 
--- mozhegova.maria/P6/main.cpp
+++ mozhegova.maria/P6/main.cpp
@@ -1,2 +1,24 @@
+#include <strinput.hpp>
+
+
 int main()
-{}
+{
+  char * str = nullptr;
+  try
+  {
+    str = mozhegova::inputString(std::cin, '\n');
+  }
+  catch (const std::exception& e)
+  {
+    std::cerr << "Out of memory\n";
+    return 1;
+  }
+  if (str[0] == '\0')
+  {
+    delete[] str;
+    std::cerr << "Empty input\n";
+    return 1;
+  }
+
+  delete[] str;
+}
