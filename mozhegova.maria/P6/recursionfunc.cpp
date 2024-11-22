79b79ea0b6eb49b2ea547b71e236db2cfa2069a2
mipxe <mariamozegova8726@gmail.com>
2024-11-22 23:08:13 +0300

    create function hasIdentifier and hasUnsignedInt

--- mozhegova.maria/P6/recursionfunc.cpp
+++ mozhegova.maria/P6/recursionfunc.cpp
@@ -17,3 +17,22 @@ const char * mozhegova::hasNumber(const char * str)
   }
   return (*str >= '0' && *str <= '9') ? (str + 1) : nullptr;
 }
+
+const char * mozhegova::hasIdentifier(const char * str)
+{
+  return mozhegova::hasLetter(str);
+}
+
+const char * mozhegova::hasUnsignedInt(const char * str)
+{
+  if (!str)
+  {
+    return str;
+  }
+  auto next = mozhegova::hasNumber(str);
+  if (auto continues = mozhegova::hasUnsignedInt(next))
+  {
+    return continues;
+  }
+  return next;
+}
