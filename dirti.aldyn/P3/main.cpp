588fd74a2538cd95713c4508df3a8d798645dce8
aldyn-d <aldyndirti@gmail.com>
2024-11-22 17:12:37 +0300

    added number of arguments checking

--- dirti.aldyn/P3/main.cpp
+++ dirti.aldyn/P3/main.cpp
@@ -8,6 +8,21 @@ int main(int argc, char ** argv)
   {
     std::cout << argv[i] << "\n";
   }
+  
+  if (argc < 4)
+  {
+    std::cerr << "Not enough arguments" << "\n";
+    return 1;
+  }
+  
+  if (argc > 4)
+  {
+    std::cerr << "Too many arguments" << "\n";
+    return 1;
+  }
+  
+  std::cout << argv[1] << "\n";
+  
   std::ifstream input(argv[2]);
   //...
   std::ofstream output(argv[3]);
