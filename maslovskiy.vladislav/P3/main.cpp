e469ea3f73c34488348f6eeac93ecd9d2691912d
vladislavmaslovskiy <vladuisik826@gmail.com>
2024-11-24 00:20:41 +0300

    Fix

--- maslovskiy.vladislav/P3/main.cpp
+++ maslovskiy.vladislav/P3/main.cpp
@@ -11,22 +11,23 @@ int main(int argc, char** argv)
   using namespace maslovskiy;
   long long num = 0;
   int cntCol = 0;
+  int* matrixPointer = nullptr;
   try
   {
     if (argc != 4)
     {
-      throw std::logic_error("Wrong number of parametres");
+      throw std::logic_error("Wrong number of parameters");
     }
     const char *str = argv[1];
     char *str_end = nullptr;
     num = std::strtoll(str, std::addressof(str_end), 10);
     if (*str_end != '\0' || num == 0)
     {
-      throw std::logic_error("Can not parse value");
+      throw std::logic_error("Cannot parse value");
     }
     if (num != 1 && num != 2)
     {
-      throw std::logic_error("Incorrect parametr");
+      throw std::logic_error("Incorrect parameter");
     }
   }
   catch (const std::logic_error &e)
@@ -34,31 +35,50 @@ int main(int argc, char** argv)
     std::cerr << e.what() << "\n";
     return 1;
   }
+
   size_t rows = 0;
   size_t cols = 0;
   std::ifstream in(argv[2]);
   in >> rows >> cols;
   if (!in)
+  {
+    std::cerr << "Cannot read size of matrix\n";
+    return 2;
+  }
+  size_t matrixSize = cols * rows;
+  int matrix[10000] = {0};
+  try
+  {
+    if (num == 1)
     {
-      delete[] matrixPointer;
-      std::cerr << "Not enough data to fill the matrix\n";
-      return 2;
+      matrixPointer = matrix;
     }
-    int cntCol = countNoDuplicates(matrixPointer, rows, cols);
+    else
+    {
+      matrixPointer = new int[matrixSize];
+    }
+
+    inputMatrix(in, matrixPointer, matrixSize);
+    if (!in)
+    {
+      throw std::logic_error("Not enough data to fill the matrix");
+    }
+    cntCol = countNoDuplicates(matrixPointer, rows, cols);
     std::ofstream output(argv[3]);
     output << cntCol << "\n";
-
-    delete[] matrixPointer;
   }
   catch (const std::bad_alloc &e)
   {
     std::cerr << "Cannot allocate memory for matrix\n";
+    delete[] matrixPointer;
     return 2;
   }
   catch (const std::exception &e)
   {
-    delete[] matrixPointer;
     std::cerr << e.what() << "\n";
+    delete[] matrixPointer;
     return 2;
   }
+  delete[] matrixPointer;
+  return 0;
 }
