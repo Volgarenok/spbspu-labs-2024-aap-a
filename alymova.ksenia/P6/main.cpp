ca002d79b9b3e2c7a905c2ba47f8d5eb64ac45c6
Ksenia-rgb <ksenialymova@mail.ru>
2024-11-23 15:30:16 +0300

    fixing for new P4

--- alymova.ksenia/P6/main.cpp
+++ alymova.ksenia/P6/main.cpp
@@ -4,13 +4,9 @@
 #include "recursion_string.h"
 int main()
 {
-  size_t size = 10;
   constexpr int ratio = 2;
-  size_t size_now = 0;
-  char delim = '\n';
-  char* str = nullptr;
-  std::cin >> std::noskipws;
-  str = alymova::get_string(size, size_now, ratio, delim);
+  size_t capacity = 10, size = 0;
+  char* str = alymova::get_string(size, size_now, ratio, '\n');
   if (str == nullptr)
   {
     std::cerr << "Error: memory not allocate for string\n";
@@ -22,7 +18,6 @@ int main()
     std::cerr << "Empty string\n";
     return 1;
   }
-  str[size_now] = '\0';
   std::cout << std::boolalpha << alymova::is_double(str) << "\n";
   free(str);
 }
