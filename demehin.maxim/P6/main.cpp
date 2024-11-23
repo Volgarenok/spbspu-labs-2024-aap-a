9575e0c4367870f3ddc4d3cf4a161d8311909ec6
TvoyBatyA1234 <maxdem06@gmail.com>
2024-11-23 14:27:11 +0300

    add input of string

--- demehin.maxim/P6/main.cpp
+++ demehin.maxim/P6/main.cpp
@@ -1,3 +1,19 @@
+#include <string.h>
+
 int main()
 {
+  char* str = nullptr;
+  constexpr size_t max_size = 100;
+  constexpr char stop = '\n';
+
+  try
+  {
+    str = demehin::getString(std::cin, stop, str, max_size);
+  }
+  catch (const std::bad_alloc& e)
+  {
+    std::cerr << "Out of memory\n";
+    delete[] str;
+    return 1;
+  }
 }
