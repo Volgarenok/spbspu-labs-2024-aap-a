6350ece1eb5fce70052dfaadc04421231f243d3e
15446-rus75 <vladabramov06@yandex.ru>
2024-11-24 01:58:22 +0300

    rewrite main.cpp

--- abramov.vladislav/P4/main.cpp
+++ abramov.vladislav/P4/main.cpp
@@ -1,14 +1,25 @@
-#include "str_functions.hpp"
+#include <cstring>
+#include "unionStrs.hpp"
 #include <getString.hpp>
 int main()
 {
   constexpr const char *str2 = "def ";
   char *str1 = nullptr;
+  char *destination = nullptr;
   try
   {
     str1 = abramov::getStr(std::cin);
+    if (str1 != nullptr)
+    {
+      destination = new char [std::strlen(str1) + std::strlen(str2)];
+    }
   }
   catch (const std::bad_alloc &e)
+  {
+    std::cerr << "Memory fail\n";
+    return 2;
+  }
+  if (str1 == nullptr)
   {
     std::cerr << "Memory fail\n";
     return 2;
@@ -19,13 +30,8 @@ int main()
     delete[] str1;
     return 1;
   }
-  str1 = abramov::unionStrs(str1, str2);
-  size_t i = 0;
-  while (str1[i] != '\0')
-  {
-    std::cout << str1[i];
-    ++i;
-  }
-  std::cout << "\n";
+  destination = abramov::unionStrs(destination, str1, str2);
+  std::cout << destination << "\n";
   delete[] str1;
+  delete[] destination;
 }
