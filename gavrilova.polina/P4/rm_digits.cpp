d2c7e1510d3cd5e32e05eebed32cd80287d8e992
Гаврилова Полина <polina.gavrilova2022@mail.ru>
2024-11-23 16:54:02 +0300

    I stil hope..

---
+++ gavrilova.polina/P4/rm_digits.cpp
@@ -0,0 +1,15 @@
+#include "rm_digits.hpp"
+#include <cstddef>
+#include <cctype>
+
+char * rm_digits (const char * original, char * new_str)
+{
+  size_t j = 0;
+  for (size_t i = 0; original[i] != '\0'; ++i) {
+    if (!isdigit(original[i])) {
+      new_str[j++] = original[i];
+    }
+  }
+  new_str[j] = '\0';
+  return new_str;
+}
---
+++ gavrilova.polina/P4/rm_digits.hpp
@@ -0,0 +1,4 @@
+#ifndef RM_DIGITS_H
+#define RM_DIGITS_H
+char * rm_digits (const char * original, char * new_str);
+#endif
