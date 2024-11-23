5534fc371551fe390473cb2ec18fc8c46a975870
Maksim_Shramko <maxramko@gmail.com>
2024-11-23 22:43:09 +0300

    Trying to init arrays again...

--- shramko.maksim/P4/modifyString.cpp
+++ shramko.maksim/P4/modifyString.cpp
@@ -31,7 +31,7 @@ char* shramko::inputStr(std::istream& in)
 {
   size_t memory = 10;
   char* str = reinterpret_cast< char* >(malloc((memory + 1) * sizeof(char)));
-  str = {'a'};
+  str = {};
   if (str == nullptr)
   {
     return str;
@@ -49,7 +49,7 @@ char* shramko::inputStr(std::istream& in)
       constexpr size_t multiplier = 2;
       memory *= multiplier;
       char* new_str = shramko::createBiggerStrMemory(memory, str);
-      new_str = {'a'};
+      new_str = {};
       free(str);
       if (new_str == nullptr)
       {
