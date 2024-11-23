8bf2c4b8e77f246fb4b75422d063137abda531c5
Maksim_Shramko <maxramko@gmail.com>
2024-11-23 21:51:18 +0300

    New modification

--- shramko.maksim/P4/modifyString.cpp
+++ shramko.maksim/P4/modifyString.cpp
@@ -3,7 +3,7 @@
 
 char* shramko::createBiggerStrMemory(size_t memory, const char* str)
 {
-  char* new_str = reinterpret_cast< char* >(malloc((memory + 1) * sizeof(char)));
+  char* new_str = reinterpret_cast< char* >(malloc(memory * sizeof(char)));
 
   for (size_t i = 0; i < (memory/2); i++)
   {
@@ -16,11 +16,6 @@ char* shramko::createBiggerStrMemory(size_t memory, const char* str)
 size_t shramko::countConsecutiveIdenticalSymbols(const char* str)
 {
   size_t result = 0;
-
-  if (str == nullptr)
-  {
-    return 0;
-  }
 
   for (size_t i = 0; str[i] != '\0'; ++i)
   {
@@ -35,7 +30,7 @@ size_t shramko::countConsecutiveIdenticalSymbols(const char* str)
 char* shramko::inputStr(std::istream& in)
 {
   size_t memory = 10;
-  char* str = reinterpret_cast< char* >(malloc((memory + 1) * sizeof(char)));
+  char* str = reinterpret_cast< char* >(malloc(memory * sizeof(char)));
   if (str == nullptr)
   {
     return str;
@@ -53,12 +48,11 @@ char* shramko::inputStr(std::istream& in)
       constexpr size_t multiplier = 2;
       memory *= multiplier;
       char* new_str = shramko::createBiggerStrMemory(memory, str);
+      free(str);
       if (new_str == nullptr)
       {
-        free(str);
         return new_str;
       }
-      free(str);
       str = new_str;
     }
     str[currentSize++] = c;
