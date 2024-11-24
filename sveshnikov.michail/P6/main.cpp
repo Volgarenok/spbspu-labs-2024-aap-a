a6798be78e67382b3abcf1214cd4f1f503cecae7
svesh3000 <russkij2006@mail.ru>
2024-11-24 22:37:47 +0300

    Main function

--- sveshnikov.michail/P6/main.cpp
+++ sveshnikov.michail/P6/main.cpp
@@ -1,4 +1,29 @@
-int main()
+#include <iostream>
+#include <ios>
+#include <getline.hpp>
+
+bool isRealNumber(const char *str)
 {
   
+}
+
+int main()
+{
+  char *str = nullptr;
+  try
+  {
+    str = sveshnikov::read_str(std::cin);
+  }
+  catch (const std::bad_alloc &e)
+  {
+    std::cerr << "ERROR: BAD_ALLOC! " << e.what() << '\n';
+    return 1;
+  }
+  if (str[0] == '\0')
+  {
+    std::cerr << "ERROR: EMPTY INPUT\n";
+    delete[] str;
+    return 1;
+  }
+  std::cout << std::boolalpha << isRealNumber(str) << "\n";
 }
