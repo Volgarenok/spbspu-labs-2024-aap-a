004888dd1a880d3a7693f27270d2094e6fad0ff7
Maksim_Shramko <maxramko@gmail.com>
2024-11-24 19:59:33 +0300

    Bugfix

--- shramko.maksim/P4/modifyString.cpp
+++ shramko.maksim/P4/modifyString.cpp
@@ -5,7 +5,7 @@ char* shramko::createBiggerStrMemory(size_t memory, const char* str)
 {
   char* new_str = reinterpret_cast< char* >(malloc((memory + 1) * sizeof(char)));
 
-  for (size_t i = 0; str[i] != '\n'; ++i)
+  for (size_t i = 0; str[i] != '\0'; ++i)
   {
     new_str[i] = str[i];
   }
