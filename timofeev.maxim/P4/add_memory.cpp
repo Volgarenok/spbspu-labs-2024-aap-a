4cfa6554eecb78fd8ff5a5169ca55de98db663af
PigoDog <maksimtmfv@gmail.com>
2024-11-25 01:26:53 +0300

    add_memory go to dick

--- timofeev.maxim/P4/add_memory.cpp
+++
@@ -1,7 +0,0 @@
-#include "functions.h"
-
-char* timofeev::add_memory(size_t capacity)
-{
-  char* memory = reinterpret_cast<char*>(malloc(sizeof(char) * capacity));
-  return memory;
-}
--- timofeev.maxim/P4/input_line.cpp
+++ timofeev.maxim/P4/input_line.cpp
@@ -4,15 +4,7 @@
 char* timofeev::input_line(std::istream& in)
 {
   size_t capacity = 40;
-  char* line = nullptr;
-  try
-  {
-    line = add_memory(capacity);
-  }
-  catch(const std::bad_alloc& e)
-  {
-    return nullptr;
-  }
+  char* line = reinterpret_cast<char*>(malloc(sizeof(char) * capacity));
   size_t cur_writing_lit = 0;
   char cur_sym = '\0';
   std::noskipws(in);
