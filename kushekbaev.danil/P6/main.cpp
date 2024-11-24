bc01884200bb88fb3bd95f0931427710b5279358
Z-1337 <un228der@gmail.com>
2024-11-24 22:59:58 +0300

    added check on size

--- kushekbaev.danil/P6/main.cpp
+++ kushekbaev.danil/P6/main.cpp
@@ -7,6 +7,11 @@ int main()
     size_t size1 = 0;
     size_t max = 10;
     std::cin >> size1;
+    if (size1 == 0)
+    {
+      std::cerr << "Dry run";
+      return 1;
+    }
     char * s1 = kushekbaev::readInputArray(size1, max);
     std::cout << kushekbaev::isValidExpression(s1) << "\n";
     delete[] s1;
