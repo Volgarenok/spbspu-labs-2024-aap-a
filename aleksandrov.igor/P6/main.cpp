f7e62af2b37849a933bc9ca97be4c2f612285f44
MrMels625 <MrMels625@gmail.com>
2024-11-24 14:41:44 +0300

    Trying to fix run without input

--- aleksandrov.igor/P6/main.cpp
+++ aleksandrov.igor/P6/main.cpp
@@ -23,6 +23,12 @@ int main()
     delete[] str;
     return 1;
   }
+  if (str[0] == '\0')
+  {
+    std::cerr << "ERROR: Input was incorrect!\n";
+    delete[] str;
+    return 1;
+  }
   std::cout << std::boolalpha << aleksandrov::isFloatLiteral(str) << "\n";
   delete[] str;
 }
