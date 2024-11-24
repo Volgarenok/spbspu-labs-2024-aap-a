f952b62d90a9629f7880783ff4e3c4104ed8664a
lee <likachich12@gmail.com>
2024-11-24 13:45:24 +0300

    remake isvalidstring

--- hismatova.elizaveta/P6/functions.h
+++ hismatova.elizaveta/P6/functions.h
@@ -1,10 +1,10 @@
 #ifndef FUNCTIONS_H
 #define FUNCTIONS_H
-#include <string>
+#include <cstddef>
 
 namespace hismatova
 {
-  bool isValidString(const std::string& str, size_t& position);
+  bool isValidString(const char* str, size_t& position);
   const char* isTerm(const char* str);
   const char* isMultiplier(const char* str);
   const char* isUnsignedInt(const char* str);
