92c69d335e67e03241e7e4f371d65a2c0f313235
LuckyBichar <enbocharov2006@gmail.com>
2024-11-24 22:58:20 +0300

    Kak bulo

--- bocharov.egor/P3/input_matrix.cpp
+++ bocharov.egor/P3/input_matrix.cpp
@@ -3,7 +3,7 @@
 #include <cstddef>
 std::istream & bocharov::input_matrix(std::istream & in, int * const t,  size_t m, size_t n)
 {
-  for (size_t i = 0; i < ((m * n) + 2) && in.good(); i++)
+  for (size_t i = 0; i < (m * n) && in.good(); i++)
   {
     in >> t[i];
   }
--- bocharov.egor/P3/main.cpp
+++ bocharov.egor/P3/main.cpp
@@ -4,34 +4,6 @@
 #include <cstddef>
 #include <stdexcept>
 #include <cstdlib>
-namespace bocharov
-{
-  void check_argc(int argc)
-  {
-    if (argc - 1 > 3)
-    {
-      throw std::logic_error("Too many arguments");
-    }
-    if (argc - 1 < 3)
-    {
-      throw std::logic_error("Not enough arguments");
-    }
-  }
-  long int check_number(const char* const argv1)
-  {
-    char* endptr = nullptr;
-    long int number = std::strtol(argv1, std::addressof(endptr), 10);
-    if (*endptr != '\0')
-    {
-      throw std::logic_error("First parameter is not a number");
-    }
-    else if (number < 1 || number > 2)
-    {
-      throw std::logic_error("First parameter is out of range");
-    }
-    return number;
-  }
-}
 int main(int argc, char ** argv)
 {
   long int number = 0;
