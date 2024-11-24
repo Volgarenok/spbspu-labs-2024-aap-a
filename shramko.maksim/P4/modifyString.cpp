32c79bc4947bea2b1ecb392844c16897be349002
Maksim_Shramko <maxramko@gmail.com>
2024-11-24 19:52:53 +0300

    Correct of stupid mistake

--- shramko.maksim/P4/modifyString.cpp
+++ shramko.maksim/P4/modifyString.cpp
@@ -5,7 +5,7 @@ char* shramko::createBiggerStrMemory(size_t memory, const char* str)
 {
   char* new_str = reinterpret_cast< char* >(malloc((memory + 1) * sizeof(char)));
 
-  for (size_t i = 0; i < str[i] != '\n'; ++i)
+  for (size_t i = 0; str[i] != '\n'; ++i)
   {
     new_str[i] = str[i];
   }
