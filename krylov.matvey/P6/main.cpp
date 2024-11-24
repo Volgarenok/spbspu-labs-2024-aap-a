da9459fc26cef36cd4ca3e54796bfa2737d26fd5
root <root@kryloff.localdomain>
2024-11-24 04:04:44 +0300

    Line input in main

--- krylov.matvey/P6/main.cpp
+++ krylov.matvey/P6/main.cpp
@@ -1,2 +1,19 @@
+#include <iostream>
+#include <processLine.hpp>
+#include "replaceLetters.hpp"
+
 int main()
-{}
+{
+  char * line = krylov::inputLine(std::cin);
+  if (line == nullptr)
+  {
+    std::cerr << "Memory allocation failure!\n";
+    return 1;
+  }
+  if (line[0] == '\0')
+  {
+    std::cerr << "The line is empty!\n";
+    delete[] line;
+    return 1;
+  }
+}
