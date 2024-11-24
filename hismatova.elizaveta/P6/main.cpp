b72294f2e526937796b8363e65079ce1c72aca3b
lee <likachich12@gmail.com>
2024-11-24 13:42:13 +0300

    main.cpp

--- hismatova.elizaveta/P6/main.cpp
+++ hismatova.elizaveta/P6/main.cpp
@@ -1,1 +1,24 @@
-int main() {}
+#include <iostream>
+#include "fu_string.h"
+#include "functions.h"
+
+int main()
+{
+  char* str = nullptr;
+  if (!hismatova::getLine(&str))
+  {
+    std::cerr << "get line failed!\n";
+    return 1;
+  }
+  size_t position = 0;
+  if (hismatova::isValidString(str, position) && position == strlen(str))
+  {
+    std::cout << "true\n";
+  }
+  else
+  {
+    std::cout << "false\n";
+  }
+  free(str);
+  return 0;
+}
