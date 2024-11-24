088097f5e588676f3d4add17014709f5d7ae8fd6
TvoyBatyA1234 <maxdem06@gmail.com>
2024-11-24 11:01:20 +0300

    exception handling in string funcs

--- demehin.maxim/common/string_utils.cpp
+++ demehin.maxim/common/string_utils.cpp
@@ -5,6 +5,7 @@ char* demehin::copyString(char* str, size_t size)
 {
   size_t temp_size = 0;
   char* new_str = new char[size];
+
   if (!new_str)
   {
     throw std::bad_alloc();
@@ -15,6 +16,7 @@ char* demehin::copyString(char* str, size_t size)
     new_str[temp_size] = str[temp_size];
     temp_size += 1;
   }
+
   delete[] str;
   return new_str;
 }
@@ -22,22 +24,40 @@ char* demehin::copyString(char* str, size_t size)
 
 char* demehin::getString(std::istream& in, char stop, size_t max_size)
 {
-  char* str = nullptr;
-  str = new char[max_size];
+  char* str = new char[max_size];
+  if (!str)
+  {
+    throw std::bad_alloc();
+  }
+
   str[0] = '\0';
   size_t size = 0;
   char temp = '\0';
   std::noskipws(in);
+
   while ((in >> temp) && (temp != stop))
   {
     if (size + 1 == max_size)
     {
       str[max_size - 1] = '\0';
       max_size *= 2;
-      str = copyString(str, max_size);
+      char* new_str = nullptr;
+
+      try
+      {
+        new_str = copyString(str, max_size);
+      }
+      catch (const std::bad_alloc& e)
+      {
+        delete[] str;
+        throw;
+      }
+      str = new_str;
     }
+
     str[size++] = temp;
   }
+
   str[size] = '\0';
   return str;
 }
