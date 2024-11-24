2c22544ccfea3590be721ec653e8d2c720cc4e79
lee <likachich12@gmail.com>
2024-11-24 14:09:56 +0300

    functions.h remade2

--- hismatova.elizaveta/P6/functions.h
+++ hismatova.elizaveta/P6/functions.h
@@ -5,9 +5,9 @@
 namespace hismatova
 {
   bool isValidString(const char* str, size_t& position);
-  const char* isTerm(const char* str);
-  const char* isMultiplier(const char* str);
-  const char* isUnsignedInt(const char* str);
+  const char* isTerm(const char* str, size_t& position);
+  const char* isMultiplier(const char* str, size_t& position);
+  const char* isUnsignedInt(const char* str, size_t& position);
   const char* isIdentificator(const char* str);
   bool isDigit(char c);
   bool isLetter(char c);
