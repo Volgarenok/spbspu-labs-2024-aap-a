6fb21a5f7149e2884c43d3b23156e57975c34214
ZakirovMS <mz.08.08.06@gmail.com>
2024-11-25 02:59:15 +0300

    Convert tabs to spaces

--- zakirov.marat/P6/CheckRealNumber.cpp
+++ zakirov.marat/P6/CheckRealNumber.cpp
@@ -2,88 +2,90 @@
 
 const char * zakirov::compare_literals(const char * line, char literal)
 {
-	if (!line)
+  if (!line)
   {
     return line;
   }
 
-	return (* line == literal) ? (line + 1) : nullptr;
+  return (* line == literal) ? (line + 1) : nullptr;
 }
 
 const char * zakirov::check_sign(const char * line)
 {
-	if (!line)
+  if (!line)
   {
     return line;
   }
 
-	if (compare_literals(line, '+') || compare_literals(line, '-'))
-	{
-		return line + 1;
-	}
-	else
-	{
-		return nullptr;
-	}
+  if (compare_literals(line, '+') || compare_literals(line, '-'))
+  {
+    return line + 1;
+  }
+  else
+  {
+    return nullptr;
+  }
 }
 
 const char * zakirov::check_integer(const char * line)
 {
-	if (!line)
+  if (!line)
   {
     return line;
-	}
+  }
 
-	return (* line >= '0' && * line <= '9') ? (line + 1) : nullptr;
+  return (* line >= '0' && * line <= '9') ? (line + 1) : nullptr;
 }
 
 const char * check_uinteger(const char * line)
 {
-	if (!line)
-	{
-		return line;
-	}
+  if (!line)
+  {
+    return line;
+  }
 
-	auto next = zakirov::check_integer(line);
-	if (!next)
-	{
-		return zakirov::check_integer(line);
-	}
-	else
-	{
-		return zakirov::check_uinteger(next);
-	}
+  auto next = zakirov::check_integer(line);
+  if (!next)
+  {
+    return zakirov::check_integer(line);
+  }
+  else
+  {
+    return zakirov::check_uinteger(next);
+  }
 }
 
 const char * zakirov::check_sinteger(const char * line)
 {
-	if (!line)
+  if (!line)
   {
     return line;
   }
 
-	auto next = zakirov::check_sign(line);
-	if (!next)
-	{
-		return zakirov::check_uinteger(line);
-	}
-	else
-	{
-		return zakirov::check_uinteger(next);
-	}
+  auto next = zakirov::check_sign(line);
+  if (!next)
+  {
+    return zakirov::check_uinteger(line);
+  }
+  else
+  {
+    return zakirov::check_uinteger(next);
+  }
 }
 
 const char * zakirov::check_order(const char * line)
 {
-	if (!line)
+  if (!line)
   {
     return line;
   }
 
-	auto next = compare_literals(line, 'E');
-	if (!next)
-	{
-		return next;
-	}
-	return check_sinteger(next);
+  auto next = compare_literals(line, 'E');
+  if (!next)
+  {
+    return next;
+  }
+  return check_sinteger(next);
 }
+
+
