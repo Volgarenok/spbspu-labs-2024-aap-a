2e0fae70403e240a5cad52c68b3c00510b0f1564
mipxe <mariamozegova8726@gmail.com>
2024-11-24 14:04:36 +0300

    fix conflict

--- mozhegova.maria/P4/strmanipulator.hpp
+++ mozhegova.maria/P4/strmanipulator.hpp
@@ -3,11 +3,6 @@
 
 namespace mozhegova
 {
-<<<<<<< HEAD
-=======
-  char * inputString(std::istream & in, char stop);
-  char * changeSize(const char * nowstr, size_t length);
->>>>>>> master
   char * spcRmv(char * str);
 }
 
