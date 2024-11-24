5c6e128ec5f5758c9924b396d712226c8e12bb61
lee <likachich12@gmail.com>
2024-11-24 14:11:21 +0300

    fix main.cpp

--- hismatova.elizaveta/P6/main.cpp
+++ hismatova.elizaveta/P6/main.cpp
@@ -1,17 +1,18 @@
 #include <iostream>
+#include <cstring>
 #include "fu_string.h"
 #include "functions.h"
 
 int main()
 {
   char* str = nullptr;
-  if (!hismatova::getLine(&str))
+  if (hismatova::getLine(&str))
   {
     std::cerr << "get line failed!\n";
     return 1;
   }
   size_t position = 0;
-  if (hismatova::isValidString(str, position) && position == strlen(str))
+  if (hismatova::isValidString(str, position) && position == std::strlen(str))
   {
     std::cout << "true\n";
   }
