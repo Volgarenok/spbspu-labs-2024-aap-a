45d2229be603e4b4f57878766888e9f1779d0f9a
15446-rus75 <vladabramov06@yandex.ru>
2024-11-24 03:15:56 +0300

    return old getString

--- abramov.vladislav/common/getString.cpp
+++ abramov.vladislav/common/getString.cpp
@@ -7,7 +7,7 @@ char *abramov::getStr(std::istream &in)
   char c = '\0';
   char *str = new char[max + 1];
   size_t new_max = max;
-  size_t size = 1;
+  size_t size = 0;
   std::noskipws(in);
   while (in >> c && c != '\n')
   {
@@ -32,10 +32,6 @@ char *abramov::getStr(std::istream &in)
       str = new_str;
     }
     str[size++] = c;
-  }
-  if (size == 0)
-  {
-    return nullptr;
   }
   str[size] = '\0';
   std::skipws(in);
