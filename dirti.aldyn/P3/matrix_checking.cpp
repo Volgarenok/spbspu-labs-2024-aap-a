9127e158044170652358b3f31d27663738b46941
aldyn-d <aldyndirti@gmail.com>
2024-11-25 12:26:34 +0300

    added matrix checking func

---
+++ dirti.aldyn/P3/matrix_checking.cpp
@@ -0,0 +1,18 @@
+#include "matrix_checking.hpp"
+
+{
+  bool matrix_checking(int * matrix, size_t m, size_t n)
+  {
+    for (size_t i = 1; i < (m - 1); ++i)
+    {
+      for (size_t j = 1; j < (n - 1); ++j)
+      {
+        if (matrix[i * n + j] != 0)
+        {
+          return false;
+        }
+      }
+    }
+    return true;
+  }
+}
