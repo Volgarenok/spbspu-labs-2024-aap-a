97bd71d38eec54335d3286fcb4fe96f024102177
Tuc0Sa1amanka <sparowj765@gmail.com>
2024-11-25 06:34:37 +0300

    mainu

--- lanovenko.ivan/P6/main.cpp
+++ lanovenko.ivan/P6/main.cpp
@@ -1,7 +1,20 @@
 #include <iostream>
-
+#include <strinput.h>
+#include "string_chek.h"
 int main()
 {
- int a = 10;
- int b = 10; 
+  char* str = lanovenko::input_string(std::cin, '\n');
+  if (str == nullptr)
+  {
+    std::cerr << "Error! An attempt was made to access a row that is nullptr" << '\n';
+    return 1;
+  }
+  if (str[0] == '\0')
+  {
+    delete[] str;
+    std::cerr << "Line is empty!" << '\n';
+    return 1;
+  }
+  std::cout << std::boolalpha << lanovenko::is_valid_float_string(str) << '\n';
+  delete[] str;
 }
