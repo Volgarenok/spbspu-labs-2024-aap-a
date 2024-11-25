fae671378c0f894ee8748602d2f1fbe37a5bbd2c
sharksonk <sharksonk@gmail.com>
2024-11-25 07:46:44 +0300

    fix

---
+++ sharifullina.sofia/P3/matrix.hpp
@@ -0,0 +1,12 @@
+#ifndef MATRIX_HPP
+#define MATRIX_HPP
+#include <cstddef>
+#include <fstream>
+
+namespace sharifullina
+{
+  std::istream & inputMatrix(std::istream & in, int * t, size_t m, size_t n, size_t & read);
+  int countMaxSumMdg(const int * t, size_t m, size_t n);
+  size_t countLocMax(const int * t, size_t m, size_t n);
+}
+#endif
