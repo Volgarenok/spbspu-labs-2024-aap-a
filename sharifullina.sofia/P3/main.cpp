325374ca4f4e22ee0b2881b5af0d9dfab9522e82
sharksonk <sharksonk@gmail.com>
2024-11-25 08:12:08 +0300

    fix exceptions

--- sharifullina.sofia/P3/main.cpp
+++ sharifullina.sofia/P3/main.cpp
@@ -1,2 +1,36 @@
-int main()
-{}
+#include <iostream>
+
+int main(int argc, char ** argv)
+{
+  if (argc < 4)
+  {
+    std::cerr << "Not enough arguments\n";
+    return 1;
+  }
+  if (argc > 4)
+  {
+    std::cerr << "Too many arguments\n";
+    return 1;
+  }
+  int num = std::strtol(argv[1]);
+  if (num != 1 && num != 2)
+  {
+    std::cerr << "First parameter is out of range\n";
+    return 1;
+  }
+  if (argv[1][1] != '\0')
+  {
+    std::cerr << "First parameter is out of range\n";
+    return 1;
+  }
+  std::ifstream input(argv[2]);
+  size_t rows = 0;
+  size_t columns = 0;
+  input >> rows >> columns;
+  if (!input)
+  {
+    std::cerr << "Rows or columns are not a number\n";
+    return 2;
+  }
+  std::ofstream output(argv[3]);
+}
