77467a8a2799e4ab1cf7e02729b3cf6d7ce9fab3
Гаврилова Полина <polina.gavrilova2022@mail.ru>
2024-11-26 21:12:50 +0300

    main.cpp

--- gavrilova.polina/P6/main.cpp
+++ gavrilova.polina/P6/main.cpp
@@ -1,3 +1,15 @@
+#include <iostream>
+#include <istream>
+#include <inputStr>
+#include "expressions.hpp"
+
 int main()
 {
+  size_t len = 0;
+  char* str = inputStr(std::cin, len);
+  if (!str || len == 0) {
+    std::cerr << "Wrong input!" << "\n";
+    return 1;
+  }
+  std::cout << std::boolalpha << gavrilova::is_expresiion(str);
 }
