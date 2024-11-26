d347cd7a5445be2172eb14216725f3bdb527ab24
PigoDog <maksimtmfv@gmail.com>
2024-11-26 12:34:20 +0300

    fix

--- timofeev.maxim/P4/same_symbols.cpp
+++ timofeev.maxim/P4/same_symbols.cpp
@@ -5,7 +5,7 @@ int timofeev::same_symbols(const char* first_line, const char* second_line)
 {
   for (size_t i = 0; first_line[i] != '\0'; i++)
   {
-    for (size_t j = 0; second_line[j] != '\0'; j++)
+    for (*second_line != '\0'; second_line++)
     {
       if (first_line[i] == second_line[j])
       {
