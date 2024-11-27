415a486d2188fc8d3acf98d682cda38aea287631
Z-1337 <un228der@gmail.com>
2024-11-27 20:21:52 +0300

    CG fixes

--- kushekbaev.danil/P3/count_saddle_point.hpp
+++ kushekbaev.danil/P3/count_saddle_point.hpp
@@ -1,12 +1,9 @@
-#ifndef COUNT_SADDLE_POINT
-#define COUNT_SADDLE_POINT
+#ifndef COUNT_SADDLE_POINT_HPP
+#define COUNT_SADDLE_POINT_HPP
 #include <cstddef>
 
 namespace kushekbaev
 {
-  int & getElement(int * matrix, size_t columns, size_t row, size_t column);
-  bool isMinInRow(int* matrix, size_t columns, size_t row, int element);
-  bool isMaxInColumn(int* matrix, size_t rows, size_t columns, size_t column, int element);
   int countSaddlePoints(int * matrix, size_t rows, size_t columns);
 }
 
--- kushekbaev.danil/P3/main.cpp
+++ kushekbaev.danil/P3/main.cpp
@@ -1,81 +1,72 @@
-#include "count_saddle_point.hpp"
 #include <iostream>
 #include <fstream>
 #include <stdexcept>
+#include "count_saddle_point.hpp"
 
 int main(int argc, char **argv)
 {
   if (argc < 4)
   {
-    std::cerr << "Not enough arguments\n";
-    return 1;
-  }
-  else if (argc > 4)
-  {
-    std::cerr << "Too many arguments\n";
-    return 1;
-  }
-  else if ((argv[1][0] != '1' && argv[1][0] != '2') || argv[1][1] != '\0')
-  {
-    std::cerr << "First parameter is out of range\n";
-    return 1;
+    std::cerr << "Not enough arguments" << "\n";
+    return 2;
   }
 
   std::string input_filename = argv[2];
   std::string output_filename = argv[3];
 
   std::ifstream infile(input_filename);
-  size_t rows = 0, columns = 0;
-
   if (!infile.is_open())
   {
-    std::cerr << "Error while opening input file\n";
+    std::cerr << "Error while opening input file" << "\n";
     return 1;
   }
 
-  infile >> rows >> columns;
-  if (infile.fail())
+  infile.seekg(0, std::ios::end);
+  if (infile.tellg() == 0)
   {
-    std::cerr << "Incorrect parameters\n";
-    return 2;
+    std::cerr << "Input file is empty" << "\n";
+    return 1;
+  }
+  infile.seekg(0);
+
+  size_t rows = 0, columns = 0;
+  if (!(infile >> rows >> columns))
+  {
+    std::cerr << "Error reading matrix dimensions" << "\n";
+    return 1;
   }
 
-  if (rows == 0 || columns == 0)
+  int* matrix = new int [rows * columns];
+  if (!matrix)
   {
-    std::ofstream outfile(output_filename);
-    outfile << "0\n";
-    return 0;
+    std::cerr << "Error allocating memory for matrix" << "\n";
+    return 1;
   }
 
-  int* matrix = nullptr;
   try
   {
-    matrix = new int[rows * columns]();
-  }
-  catch (const std::bad_alloc& e)
-  {
-    std::cerr << "Out of memory\n";
-    return 2;
-  }
-
-  size_t count = 0;
-  for (size_t row = 0; row < rows; row++)
-  {
-    for (size_t col = 0; col < columns; col++)
+    for (size_t i = 0; i < rows * columns; i++)
     {
-      if (!(infile >> matrix[count]))
+      if (!(infile >> matrix[i]))
       {
-        std::cerr << "Not enough elements in file at row " << row << ", column " << col << "\n";
-        delete[] matrix;
-        return 1;
+        throw std::runtime_error("Error reading matrix data");
       }
-      count++;
     }
-  }
+    infile.close();
 
-  if (count != (rows * columns))
+    int saddle_points = kushekbaev::countSaddlePoints(matrix, rows, columns);
+
+    std::ofstream outfile(output_filename);
+    if (!outfile.is_open())
+    {
+      throw std::runtime_error("Error while opening output file");
+    }
+    outfile << saddle_points << "\n";
+    outfile.close();
+  }
+  catch (const std::runtime_error& e)
   {
-    std::cerr << "Expected " << (rows * columns) << " elements, but read only " << count << "\n";
+    std::cerr << e.what() << "\n";
     delete[] matrix;
     return 1;
   }
