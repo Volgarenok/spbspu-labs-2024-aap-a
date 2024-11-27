e6d20eb5543d527f13ff09257084c818c410fd5d
ProstoG-byte <asilda2019@gmail.com>
2024-11-27 21:31:17 +0300

    Fixed the placement of curly braces in functions and namespaces

--- lungu.george/P2/Tylor.h
+++ lungu.george/P2/Tylor.h
@@ -1,7 +1,7 @@
 #ifndef TYLOR_H
 #define TYLOR_H
-namespace lungu {
-  double TylorFunc(double x, double absError, int numberMax);
+namespace lungu
+{
+  double func_Tylor(double x, double absError, int numberMax);
 }
-
 #endif
