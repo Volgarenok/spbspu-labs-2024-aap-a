d9c85525da414c7feb89a0bfe120c7bcb695bce5
ysffmn <andrewnikonov387@gmail.com>
2024-11-27 23:08:14 +0300

    changed output message on true/false

--- nikonov.andrew/P6/main.cpp
+++ nikonov.andrew/P6/main.cpp
@@ -4,17 +4,17 @@
 int main()
 {
   size_t capacity = 1;
-  char* arr = nikonov::getLine(std::cin, capacity);
+  const char* arr = nikonov::getLine(std::cin, capacity);
   if (!arr)
   {
     return 1;
   }
   if (!nikonov::is_float(arr))
   {
-    std::cout << "NAH\n";
+    std::cout << "false\n";
   }
   else
   {
-    std::cout << "YEAH\n";
+    std::cout << "true\n";
   }
 }
