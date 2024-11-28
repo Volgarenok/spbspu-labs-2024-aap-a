0add44b4f8b164efbd1173a7bc9926bd982fad7b
ysffmn <andrewnikonov387@gmail.com>
2024-11-28 21:14:38 +0300

    changed input check

--- nikonov.andrew/common/dynamicArray.cpp
+++ nikonov.andrew/common/dynamicArray.cpp
@@ -30,7 +30,7 @@ char* nikonov::getLine(std::istream& input, size_t& capacity)
     ++index;
   }
   *(line + index) = '\0';
-  if (!input)
+  if (*line == '\0')
   {
     free(line);
     std::skipws(input);
