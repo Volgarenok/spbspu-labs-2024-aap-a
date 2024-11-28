69d301d8d2f124139182e93a0ebd6ac29e5c3b38
deonik3 <kis.sergey2006@gmail.com>
2024-11-28 16:53:27 +0300

    Fix

--- kiselev.sergey/P6/isExpression.cpp
+++ kiselev.sergey/P6/isExpression.cpp
@@ -1,158 +1,160 @@
 #include "isExpression.h"
-const char* hasExpression(const char* string);
-const char* hasSymbol(const char* string, char s)
+namespace
 {
-  if (!string)
+  const char* hasExpression(const char* string);
+  const char* hasSymbol(const char* string, char s)
   {
-    return string;
-  }
-  return (*string == s) ? (string + 1) : nullptr;
-}
-const char* hasX(const char* string)
-{
-  return hasSymbol(string, 'x');
-}
-const char* hasY(const char* string)
-{
-  return hasSymbol(string, 'y');
-}
-const char* hasZ(const char* string)
-{
-  return hasSymbol(string, 'z');
-}
-const char* hasNullorOne(const char* string)
-{
-  const char* next = hasSymbol(string, '1');
-  return next ? next : hasSymbol(string, '0');
-}
-const char* hasID(const char* string)
-{
-  if (!string)
-  {
-    return string;
-  }
-  const char* next = hasX(string);
-  if (next)
-  {
-    return next;
-  }
-  next = hasY(string);
-  if (next)
-  {
-    return next;
-  }
-  next = hasZ(string);
-  return next;
-}
-const char* hasUnsignedNumber(const char* string)
-{
-  if (!string)
-  {
-    return string;
-  }
-  const char* next = hasNullorOne(string);
-  if (!next)
-  {
-    return nullptr;
-  }
-  if (const char* continues = hasUnsignedNumber(next))
-  {
-    return continues;
-  }
-  return next;
-}
-const char* hasMultiplier(const char* string)
-{
-  if (!string)
-  {
-    return string;
-  }
-  const char* next = hasUnsignedNumber(string);
-  if (next)
-  {
-    return next;
-  }
-  next = hasID(string);
-  if (next)
-  {
-    return next;
-  }
-  if (*string == '(')
-  {
-    string++;
-    next = hasExpression(string);
-    if (next && *next == ')')
+    if (!string)
     {
-      return next + 1;
+      return string;
     }
-    return nullptr;
+    return (*string == s) ? (string + 1) : nullptr;
   }
-  return nullptr;
-}
-const char* hasTerm(const char* string)
-{
-  if (!string)
+  const char* hasX(const char* string)
   {
-    return string;
+    return hasSymbol(string, 'x');
   }
-  const char* next = hasMultiplier(string);
-  if (!next)
+  const char* hasY(const char* string)
   {
+    return hasSymbol(string, 'y');
+  }
+  const char* hasZ(const char* string)
+  {
+    return hasSymbol(string, 'z');
+  }
+  const char* hasNullorOne(const char* string)
+  {
+    const char* next = hasSymbol(string, '1');
+    return next ? next : hasSymbol(string, '0');
+  }
+  const char* hasID(const char* string)
+  {
+    if (!string)
+    {
+      return string;
+    }
+    const char* next = hasX(string);
+    if (next)
+    {
+      return next;
+    }
+    next = hasY(string);
+    if (next)
+    {
+      return next;
+    }
+    next = hasZ(string);
     return next;
   }
-  if (*next == '*')
+  const char* hasUnsignedNumber(const char* string)
   {
-    next++;
-    const char* continues = hasTerm(next);
-    if (!continues)
+    if (!string)
+    {
+      return string;
+    }
+    const char* next = hasNullorOne(string);
+    if (!next)
     {
       return nullptr;
     }
-    return continues;
-  }
-  if (*next == '(')
-  {
-    next++;
-    const char* continues = hasMultiplier(next);
-    if (!continues)
+    if (const char* continues = hasUnsignedNumber(next))
     {
+      return continues;
+    }
+    return next;
+  }
+  const char* hasMultiplier(const char* string)
+  {
+    if (!string)
+    {
+      return string;
+    }
+    const char* next = hasUnsignedNumber(string);
+    if (next)
+    {
+      return next;
+    }
+    next = hasID(string);
+    if (next)
+    {
+      return next;
+    }
+    if (*string == '(')
+    {
+      string++;
+      next = hasExpression(string);
+      if (next && *next == ')')
+      {
+        return next + 1;
+      }
       return nullptr;
     }
-    if (*continues == '+')
+    return nullptr;
+  }
+  const char* hasTerm(const char* string)
+  {
+    if (!string)
     {
-      continues++;
-      const char* nextTerm = hasTerm(continues);
-      if (!nextTerm || *nextTerm != ')')
+      return string;
+    }
+    const char* next = hasMultiplier(string);
+    if (!next)
+    {
+      return next;
+    }
+    if (*next == '*')
+    {
+      next++;
+      const char* continues = hasTerm(next);
+      if (!continues)
       {
         return nullptr;
       }
-      return nextTerm + 1;
-    }
-    if (*next != ')')
-    {
-      return nullptr;
-    }
-    return continues + 1;
-  }
-  return next;
-}
-const char* hasExpression(const char* string)
-{
-  if (!string)
-  {
-    return string;
-  }
-  const char* next = hasTerm(string);
-  if (next)
-  {
-    if (*next == '+' || *next == '-')
-    {
-      next++;
-      const char* continues = hasExpression(next);
       return continues;
     }
+    if (*next == '(')
+    {
+      next++;
+      const char* continues = hasMultiplier(next);
+      if (!continues)
+      {
+        return nullptr;
+      }
+      if (*continues == '+')
+      {
+        continues++;
+        const char* nextTerm = hasTerm(continues);
+        if (!nextTerm || *nextTerm != ')')
+        {
+          return nullptr;
+        }
+        return nextTerm + 1;
+      }
+      if (*next != ')')
+      {
+        return nullptr;
+      }
+      return continues + 1;
+    }
+    return next;
   }
-  return next;
+  const char* hasExpression(const char* string)
+  {
+    if (!string)
+    {
+      return string;
+    }
+    const char* next = hasTerm(string);
+    if (next)
+    {
+      if (hasSymbol(next, '+') != nullptr || hasSymbol(next, '-') != nullptr)
+      {
+        const char* continues = hasExpression(next);
+        return continues;
+      }
+    }
+    return next;
+  }
 }
 bool kiselev::isExpression(const char* string)
 {
