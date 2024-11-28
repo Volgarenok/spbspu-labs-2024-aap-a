a15fb55c4169bd788b3328c9156ed6bf145a23b0
PigoDog <maksimtmfv@gmail.com>
2024-11-28 21:05:30 +0300

    delete line input files

--- timofeev.maxim/P4/functions.h
+++ timofeev.maxim/P4/functions.h
@@ -1,14 +1,8 @@
 #ifndef FUNCTIONS_H
 #define FUNCTIONS_H
 
-#include <iostream>
-#include <fstream>
-#include <ios>
-
 namespace timofeev
 {
-  char* boost_size(size_t capacity);
-  char* input_line(std::istream& in);
   int same_symbols(const char* first_line, const char* second_line);
 }
 
