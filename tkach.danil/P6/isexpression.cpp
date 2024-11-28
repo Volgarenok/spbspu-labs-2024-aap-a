1221e35db6f87ed6eda4c877be9dc67677fabe14
Danil3352 <danil.tkach2018@yandex.ru>
2024-11-28 23:19:36 +0300

    try namespace

--- tkach.danil/P6/isexpression.cpp
+++ tkach.danil/P6/isexpression.cpp
@@ -2,7 +2,7 @@
 #include <cctype>
 #include <cstddef>
 
-namespace tkach
+namespace
 {
   bool isNeededSymbol(const char* symbol);
   bool isDigit(const char** str);
@@ -11,70 +11,70 @@ namespace tkach
   bool hasNumber(const char** str);
   bool hasMultiplier(const char** str);
   bool hasTerm(const char** str);
-}
 
-bool tkach::isNeededSymbol(const char* symbol)
-{
-  return (*symbol >= 'a' && *symbol <= 'e');
-}
-
-bool tkach::isDigit(const char** str)
-{
-  if (std::isdigit(**str))
+  bool isNeededSymbol(const char* symbol)
   {
-    ++(*str);
-    return true;
+    return (*symbol >= 'a' && *symbol <= 'e');
   }
-  return false;
-}
 
-bool tkach::isSymbol(const char** str)
-{
-  if (isNeededSymbol(*str))
+  bool isDigit(const char** str)
   {
-    ++(*str);
-    return true;
-  }
-  return false;
-}
-
-bool tkach::hasId(const char** str)
-{
-  return isSymbol(str);
-}
-
-bool tkach::hasNumber(const char** str)
-{
-  bool result = isDigit(str);
-  if (result)
-  {
-    hasNumber(str);
-  }
-  return result;
-}
-
-bool tkach::hasMultiplier(const char** str)
-{
-  return hasNumber(str) || hasId(str);
-}
-
-bool tkach::hasTerm(const char** str)
-{
-  if (hasMultiplier(str))
-  {
-    if (**str == '*')
+    if (std::isdigit(**str))
     {
-      (*str)++;
-      return hasTerm(str);
+      ++(*str);
+      return true;
     }
-    return true;
+    return false;
   }
-  return false;
+
+  bool isSymbol(const char** str)
+  {
+    if (::isNeededSymbol(*str))
+    {
+      ++(*str);
+      return true;
+    }
+    return false;
+  }
+
+  bool hasId(const char** str)
+  {
+    return isSymbol(str);
+  }
+
+  bool hasNumber(const char** str)
+  {
+    bool result = isDigit(str);
+    if (result)
+    {
+      hasNumber(str);
+    }
+    return result;
+  }
+
+  bool hasMultiplier(const char** str)
+  {
+    return hasNumber(str) || hasId(str);
+  }
+
+  bool hasTerm(const char** str)
+  {
+    if (hasMultiplier(str))
+    {
+      if (**str == '*')
+      {
+        (*str)++;
+        return hasTerm(str);
+      }
+      return true;
+    }
+    return false;
+  }
 }
 
 bool tkach::hasExpression(const char** str)
 {
-  if (hasTerm(str))
+  if (::hasTerm(str))
   {
     if ((**str == '+') || (**str == '-'))
     {
