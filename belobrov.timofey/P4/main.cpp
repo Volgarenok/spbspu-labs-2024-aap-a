f9b1a72a39b8ac5dc4736bd055aadf49a84e6a09
Durshl4k <belobrovtimofej@gmail.com>
2024-11-29 22:54:17 +0300

    input check

--- belobrov.timofey/P4/main.cpp
+++ belobrov.timofey/P4/main.cpp
@@ -1,1 +1,15 @@
-int main(){}
+#include <iostream>
+
+int main() {
+  char* line = belobrov::inputLine(std::cin);
+  if (line == nullptr) {
+    std::cerr << "Memory allocation error!\n";
+    return 1;
+  }
+
+  if (line[0] == '\0') {
+    std::cerr << "Line is empty!\n";
+    free(line);
+    return 1;
+  }
+}
