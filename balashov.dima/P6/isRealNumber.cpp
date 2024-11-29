2dc9bc5a39a83ad33ce7e70d47fadc278840adf0
root <root@DESKTOP-KOVAQT8.localdomain>
2024-11-29 09:56:14 +0300

    add func

---
+++ balashov.dima/P6/isRealNumber.cpp
@@ -0,0 +1,13 @@
+#include "isRealNumber.hpp"
+
+namespace balashov
+{
+const char* hasSymbol(const char* str, char symbol);
+const char* hasFigure(const char * str);
+const char* hasUnsigned(const char* str);
+const char * hasSign(const char * str);
+const char * hasSign(const char * str);
+const char* hasOrder(const char* str);
+const char* hasMantissa(const char* str);
+}
+
