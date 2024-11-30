a01bb7af914f25f3f75d2346aba0d94623c88489
15446-rus75 <vladabramov06@yandex.ru>
2024-11-30 12:14:37 +0300

    add new argument to function

--- abramov.vladislav/P6/main.cpp
+++ abramov.vladislav/P6/main.cpp
@@ -5,9 +5,10 @@
 int main()
 {
   char *str = nullptr;
+  size_t size_of_str = 0;
   try
   {
-    str = abramov::getStr(std::cin);
+    str = abramov::getStr(std::cin, size_of_str);
   }
   catch (const std::bad_alloc &e)
   {
