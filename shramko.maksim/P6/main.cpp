aa57437ea91fea89db674bffa023e1a5897a40f7
Maksim_Shramko <maxramko@gmail.com>
2024-11-30 22:02:42 +0300

    Creating carcase of future programm

--- shramko.maksim/P6/main.cpp
+++ shramko.maksim/P6/main.cpp
@@ -1,3 +1,22 @@
+#include <iostream>
+#include <inputString.hpp>
+
 int main()
 {
+  char* str = shramko::inputString(std::cin);
+  if (str == nullptr)
+  {
+    std::cerr << "String error!\n";
+    return 1;
+  }
+
+  if (str[0] == '\0')
+  {
+    free(str);
+    std::cerr << "String is empty!\n";
+    return 1;
+  }
+
+  std::cout << std::boolalpha << shramko::isExpression(str) << '\n';
+  free(str);
 }
