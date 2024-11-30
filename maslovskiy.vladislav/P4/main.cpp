d1be0e4ca61272d6e9969435f5c4ccaf2668886c
vladislavmaslovskiy <vladuisik826@gmail.com>
2024-11-30 23:43:01 +0300

    Revert "."
    This reverts commit 6473514a6edf4a237a9b55527ebaefa1535fafbb.

---
+++ maslovskiy.vladislav/P4/main.cpp
@@ -0,0 +1,46 @@
+#include <cstdlib>
+#include <iostream>
+#include <inputString.hpp>
+#include "mergeStrs.hpp"
+
+int main()
+{
+  using namespace maslovskiy;
+  char *str1 = nullptr;
+  const char *str2 = "246789";
+  try
+  {
+   str1 = inputString(std::cin);
+  }
+  catch (const std::bad_alloc &)
+  {
+    std::cerr << "incorrect memory allocation\n";
+    return 1;
+  }
+  if (*str1 == '\0')
+  {
+    free(str1);
+    std::cerr << "input string is null\n";
+    return 2;
+  }
+  char * united = nullptr;
+  try
+  {
+    united = static_cast<char*>(malloc(sizeof(char) * 512));
+  }
+  catch (const std::bad_alloc &)
+  {
+    free(str1);
+    free(united);
+    std::cerr << "Cannot create new strings\n";
+    return 3;
+  }
+  mergeStrs(united, str1, str2);
+  for (size_t i = 0; united[i] != '\0'; i++)
+  {
+    std::cout << united[i];
+  }
+  std::cout << '\n';
+  free(str1);
+  free(united);
+}
---
+++ maslovskiy.vladislav/P4/mergeStrs.hpp
@@ -0,0 +1,11 @@
+#ifndef MERGE_STRS_HPP
+#define MERGE_STRS_HPP
+
+#include <cstddef>
+namespace maslovskiy
+{
+  void fillStr(char *res, const char *str, size_t pos);
+  void mergeStrs(char *res, const char *first, const char *second);
+}
+
+#endif
