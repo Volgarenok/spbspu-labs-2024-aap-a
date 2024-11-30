7c20f0e34b7ed9f89353fd0b655f1ae0e2814381
TvoyBatyA1234 <maxdem06@gmail.com>
2024-11-30 13:31:51 +0300

    delete cycles

--- demehin.maxim/P6/base_vocab.cpp
+++ demehin.maxim/P6/base_vocab.cpp
@@ -2,41 +2,23 @@
 
 const char* demehin::hasLetter(const char* str)
 {
-  char letters[] = "ABCDE";
   if (!str)
   {
     return str;
   }
 
-  bool isLetter = false;
-  for (int i = 0; i < 5; ++i)
-  {
-    if (*str == letters[i])
-    {
-      isLetter = true;
-      break;
-    }
-  }
+  bool isLetter = *str >= 'A' && *str <= 'E';
   return (isLetter) ? (str + 1) : nullptr;
 }
 
 const char* demehin::hasNumber(const char* str)
 {
-  char numbers[] = "0123456789";
   if (!str)
   {
     return str;
   }
 
-  bool isNumber = false;
-  for (int i = 0; i < 10; ++i)
-  {
-    if (*str == numbers[i])
-    {
-      isNumber = true;
-      break;
-    }
-  }
+  bool isNumber = *str >= '0' && *str <= '9';
   return (isNumber) ? (str + 1) : nullptr;
 }
 
