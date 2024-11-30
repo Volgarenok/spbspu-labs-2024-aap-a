547d85f21817c41dd6742c9ee7244a0d62038784
Nekich06 <p.nikita.73526@gmail.com>
2024-11-30 13:05:15 +0300

    Added input to main.cpp
    Now we can input a real number

--- petrov.nikita/P6/main.cpp
+++ petrov.nikita/P6/main.cpp
@@ -1,2 +1,18 @@
+#include <iostream>
+#include <input_cstring.hpp>
+
 int main()
-{}
+{
+  char * real_number = nullptr;
+  size_t capacity = 1;
+  try
+  {
+    real_number = petrov::inputCString(std::cin, capacity);
+    std::clog << real_number;
+  }
+  catch (const std::bad_alloc & e)
+  {
+    std::cerr << "ERROR: Out of memory\n";
+    return 1;
+  }
+}
