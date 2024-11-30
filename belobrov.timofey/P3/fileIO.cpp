3f00d5dfaccaed7637913e4989033bc71517806d
Durshl4k <belobrovtimofej@gmail.com>
2024-11-30 23:10:17 +0300

    matrix output check?

--- belobrov.timofey/P3/fileIO.cpp
+++ belobrov.timofey/P3/fileIO.cpp
@@ -18,12 +18,9 @@ void belobrov::saveMatrix(const std::string& filename, const int *matrix, size_t
   }
   outFile << rows << " " << cols << "\n";
   for (size_t i = 0; i < rows; ++i) {
-    for (size_t j = 0; j < cols; ++j) {
-      outFile << matrix[i * cols + j];
-      if (j < cols - 1) {
-        outFile << " ";
-      }
+    for (size_t j = 0; j < cols - 1; ++j) {
+      outFile << matrix[i * cols + j] << " ";
     }
-    outFile << "\n";
+    outFile << matrix[i * cols + cols - 1] << "\n";
   }
 }
