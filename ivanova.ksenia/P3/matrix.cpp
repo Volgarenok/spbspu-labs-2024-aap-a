1c8881245f99f306c593b19c653d0c3606107486
Robotophile <ksyunya.iv@gmail.com>
2024-11-30 17:49:03 +0300

    plaese

--- ivanova.ksenia/P3/matrix.cpp
+++ ivanova.ksenia/P3/matrix.cpp
@@ -18,10 +18,10 @@ size_t ivanova::num_col_lsr(const int * t, size_t m, size_t n)
   size_t max = 0;
   size_t result = 0;
   size_t col  = 0;
-  for (size_t i = 1; i < m; i++)
+  for (size_t i = 0; i < n; i++)
   {
     size_t count = 1;
-    for (size_t j = 0; j < n; j++)
+    for (size_t j = 0; j < m; j++)
     {
       if (t[j * n + i] == t[(j + 1) * n + i])
       {
