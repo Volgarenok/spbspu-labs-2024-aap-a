2c8052a42b5d3b8719ef3468dadbbc462bfc82de
Tuc0Sa1amanka <sparowj765@gmail.com>
2024-12-02 00:42:41 +0300

    CG fixed

--- lanovenko.ivan/P3/output_matrix.cpp
+++ lanovenko.ivan/P3/output_matrix.cpp
@@ -4,10 +4,10 @@ void lanovenko::outputMatrix(std::ostream& output, int* matrix, size_t rows, siz
 {
   for (size_t i = 0; i < rows; i++)
   {
-    output << matrix[i*rows];
+    output << matrix[i * rows];
     for (size_t j = 1; j < cols; j++)
     {
-      output << ' ' << matrix[i*cols+j];
+      output << ' ' << matrix[i * cols + j];
     }
     output << '\n';
   }
