b31611df7b05d3c6a6a607279634fae81b2c529b
A1exCRE <alsmmm2065@gmail.com>
2024-12-01 17:59:50 +0300

    Create string

--- smirnov.alexandr/P6/main.cpp
+++ smirnov.alexandr/P6/main.cpp
@@ -1,2 +1,23 @@
+#include <iostream>
+#include <getLine.hpp>
+
 int main()
-{}
+{
+  char * string = nullptr;
+  try
+  {
+    string = smirnov::getLine(std::cin, '\n');
+  }
+  catch (const std::bad_alloc & e)
+  {
+    std::cerr << "Out of memory\n";
+    return 1;
+  }
+  if (string[0] == '\0')
+  {
+    delete[] string;
+    std::cerr << "Error: empty string\n";
+    return 1;
+  }
+  delete[] string;
+}
