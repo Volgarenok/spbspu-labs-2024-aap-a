95a9f08e8b2afc674e72bea5c99876926faa2b94
dmitei <dima9990860349@icloud.com>
2024-12-01 14:50:13 +0300

    common

---
+++ shabalin.dmitrij/common/inputOfString.cpp
@@ -0,0 +1,33 @@
+#include "inputOfString.hpp"
+
+char *shabalin::inputOfString(std::istream &input, size_t &sizeOfString)
+{
+  char someCharacter = 0;
+  size_t index = 0;
+  char *initialString = reinterpret_cast< char* >(malloc(sizeof(char) * sizeOfString));
+
+  if (!initialString)
+  {
+    throw std::bad_alloc();
+  }
+
+  input >> std::noskipws;
+
+  while (input >> someCharacter)
+  {
+    if (index == sizeOfString - 1)
+    {
+      sizeOfString *= 2;
+      char *intermediateString = reinterpret_cast< char* >(realloc(initialString, sizeof(char) * sizeOfString));
+      if (!intermediateString)
+      {
+        free(initialString);
+        throw std::bad_alloc();
+      }
+      initialString = intermediateString;
+    }
+    initialString[index++] = someCharacter;
+  }
+  initialString[index] = '\0';
+  return initialString;
+}
---
+++ shabalin.dmitrij/common/inputOfString.hpp
@@ -0,0 +1,11 @@
+#ifndef INPUTOFSTRING_HPP
+#define INPUTOFSTRING_HPP
+#include <cstddef>
+#include <iostream>
+
+namespace shabalin
+{
+  char *inputOfString(std::istream &input, size_t &sizeOfString);
+}
+
+#endif
