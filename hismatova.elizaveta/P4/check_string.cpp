dc7a893696ba6f0fbb8f36332b106e4acc401634
lee <likachich12@gmail.com>
2024-12-01 13:38:46 +0300

    check string cpp fixes

--- hismatova.elizaveta/P4/check_string.cpp
+++ hismatova.elizaveta/P4/check_string.cpp
@@ -50,15 +50,31 @@ char* hismatova::getLine(std::istream& input)
     return nullptr;
   }
   size_t length = 0;
-  int c = 0;
-  while ((c = input.get()) != EOF && c != '\n')
+  char c;
+  while (input.get(c))
   {
+    if (c == '\n')
+    {
+      break;
+    }
     if (length + 1 >= n)
     {
+      size_t newSize = n * 2;
+      char *newStr = reinterpret_cast< char* >(malloc(sizeof(char) * newSize));
+      if (newStr == nullptr)
+      {
+        free(str);
+        return nullptr;
+      }
+      for (size_t i = 0; i < length; i++)
+      {
+        newStr[i] = str[i];
+      }
       free(str);
-      return nullptr;
+      str = newStr;
+      n = newSize;
     }
-    str[length++] = static_cast< char >(c);
+    str[length++] = c;
   }
   str[length] = '\0';
   return str;
