43d43d70e3a7ded043773b866aa61a4a3afd4081
A1exCRE <alsmmm2065@gmail.com>
2024-12-01 19:34:00 +0300

    Create isUnsignedInt, isMantissa, isOrder

--- smirnov.alexandr/P6/checkString.cpp
+++ smirnov.alexandr/P6/checkString.cpp
@@ -3,6 +3,9 @@ namespace smirnov
   const char * isSymbol(const char * string, char c);
   const char * isSigh(const char * string);
   const char * isNumber(const char * string);
+  const char * isUnsignedInt(const char * string);
+  const char * isOrder(const char * string);
+  const char * isMantissa(const char * string);
 }
 
 const char * smirnov::isSymbol(const char * string, char c)
@@ -31,3 +34,50 @@ const char * smirnov::isNumber(const char * string)
   }
   return (*string >= '0' && *string <= '9') ? (string + 1) : nullptr;
 }
+
+const char * smirnov::isUnsignedInt(const char * string)
+{
+  if (!string)
+  {
+    return string;
+  }
+  auto next = isNumber(string);
+  if (auto continues = isUnsignedInt(next))
+  {
+    return continues;
+  }
+  return next;
+}
+
+const char * smirnov::isOrder(const char * string)
+{
+  if (!string)
+  {
+    return string;
+  }
+  auto next = isSymbol(string, 'E');
+  if (auto continues = isSign(next))
+  {
+    return isUnsignedInt(continues);
+  }
+  return isUnsignedInt(next);
+}
+
+const char * smirnov::isMantissa(const char * string)
+{
+  if (!string)
+  {
+    return string;
+  }
+  if (*string == '.')
+  {
+    return isUnsignedInt(string + 1);
+  }
+  auto next = isUnsignedInt(string);
+  if (next && *next == '.')
+  {
+    return isUnsignedInt(next + 1);
+  }
+  return string;
+}
+
