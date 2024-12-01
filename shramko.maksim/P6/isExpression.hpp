0604e38220643d13e126230fe71b56e7fc31dd2e
Maksim_Shramko <maxramko@gmail.com>
2024-12-01 18:41:02 +0300

    Fixing namespacing

--- shramko.maksim/P6/isExpression.hpp
+++ shramko.maksim/P6/isExpression.hpp
@@ -3,16 +3,6 @@
 
 namespace shramko
 {
-  const char* has_symbol(const char* str, char s);
-  const char* has_x(const char* str);
-  const char* has_y(const char* str);
-  const char* has_z(const char* str);
-  const char* has_one_or_zero(const char* str);
-  const char* has_following_id(const char* str);
-  const char* has_unsigned_int(const char* str);
-  const char* has_multi(const char* str);
-  const char* has_term(const char* str);
-  const char* has_expression(const char* str);
   bool is_expression(const char* str);
 }
 
