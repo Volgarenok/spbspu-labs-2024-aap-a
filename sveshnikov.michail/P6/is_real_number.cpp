fb0628ab5541b51ed0c4dc4db77625b826db939c
svesh3000 <russkij2006@mail.ru>
2024-12-01 23:45:09 +0300

    Anonymous namespace

--- sveshnikov.michail/P6/is_real_number.cpp
+++ sveshnikov.michail/P6/is_real_number.cpp
@@ -1,6 +1,6 @@
 #include "is_real_number.hpp"
 
-namespace sveshnikov
+namespace
 {
   const char *hasSymbol(const char *str, const char c);
   const char *hasSign(const char *str);
@@ -8,6 +8,76 @@ namespace sveshnikov
   const char *hasUnsignedInteger(const char *str);
   const char *hasOrder(const char *str);
   const char *hasMantissa(const char *str);
+
+  const char *hasMantissa(const char *str)
+  {
+    if (!str)
+    {
+      return str;
+    }
+    if (const char *continues = hasUnsignedInteger(str))
+    {
+      if (const char *continues2 = hasSymbol(continues, '.'))
+      {
+        return hasUnsignedInteger(continues2);
+      }
+      return continues;
+    }
+    if (const char *continues = hasSymbol(str, '.'))
+    {
+      return hasUnsignedInteger(continues);
+    }
+    return nullptr;
+  }
+
+  const char *hasOrder(const char *str)
+  {
+    if (!str)
+    {
+      return str;
+    }
+    const char *next = hasSymbol(str, 'E');
+    next = hasSign(next);
+    return hasUnsignedInteger(next);
+  }
+
+  const char *hasUnsignedInteger(const char *str)
+  {
+    if (!str)
+    {
+      return str;
+    }
+    const char *next = hasDigit(str);
+    if (const char *continues = hasUnsignedInteger(next))
+    {
+      return continues;
+    }
+    return next;
+  }
+
+  const char *hasDigit(const char *str)
+  {
+    if (!str)
+    {
+      return str;
+    }
+    return (*str >= '0' && *str <= '9') ? (str + 1) : nullptr;
+  }
+
+  const char *hasSign(const char *str)
+  {
+    const char *next = hasSymbol(str, '+');
+    return next ? next : hasSymbol(str, '-');
+  }
+
+  const char *hasSymbol(const char *str, const char c)
+  {
+    if (!str)
+    {
+      return str;
+    }
+    return (*str == c) ? (str + 1) : nullptr;
+  }
 }
 
 bool sveshnikov::isRealNumber(const char *str)
@@ -29,74 +99,4 @@ const char *sveshnikov::hasRealNumber(const char *str)
   }
   const char *next = hasMantissa(str);
   return hasOrder(next);
-}
-
-const char *sveshnikov::hasMantissa(const char *str)
-{
-  if (!str)
-  {
-    return str;
-  }
-  if (const char *continues = hasUnsignedInteger(str))
-  {
-    if (const char *continues2 = hasSymbol(continues, '.'))
-    {
-      return hasUnsignedInteger(continues2);
-    }
-    return continues;
-  }
-  if (const char *continues = hasSymbol(str, '.'))
-  {
-    return hasUnsignedInteger(continues);
-  }
-  return nullptr;
-}
-
-const char *sveshnikov::hasOrder(const char *str)
-{
-  if (!str)
-  {
-    return str;
-  }
-  const char *next = hasSymbol(str, 'E');
-  next = hasSign(next);
-  return hasUnsignedInteger(next);
-}
-
-const char *sveshnikov::hasUnsignedInteger(const char *str)
-{
-  if (!str)
-  {
-    return str;
-  }
-  const char *next = hasDigit(str);
-  if (const char *continues = hasUnsignedInteger(next))
-  {
-    return continues;
-  }
-  return next;
-}
-
-const char *sveshnikov::hasDigit(const char *str)
-{
-  if (!str)
-  {
-    return str;
-  }
-  return (*str >= '0' && *str <= '9') ? (str + 1) : nullptr;
-}
-
-const char *sveshnikov::hasSign(const char *str)
-{
-  const char *next = hasSymbol(str, '+');
-  return next ? next : hasSymbol(str, '-');
-}
-
-const char *sveshnikov::hasSymbol(const char *str, const char c)
-{
-  if (!str)
-  {
-    return str;
-  }
-  return (*str == c) ? (str + 1) : nullptr;
 }
