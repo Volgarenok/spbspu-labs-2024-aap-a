05d12f38b8313b0f3691017eee13c1833b944707
15446-rus75 <vladabramov06@yandex.ru>
2024-12-01 14:20:58 +0300

    CG, hiding implementation details

--- abramov.vladislav/P6/recursionCheckStr.cpp
+++ abramov.vladislav/P6/recursionCheckStr.cpp
@@ -1,12 +1,23 @@
 #include "recursionCheckStr.hpp"
 
+namespace abramov
+{
+  const char *isSymbol(const char *str, char c);
+  const char *isSign(const char *str);
+  const char *isDigit(const char *str);
+  const char *hasUnsignedNumber(const char *str);
+  const char *hasOrder(const char *str);
+  const char *hasMantissa(const char *str);
+  const char *hasReal(const char *str);
+}
+
 const char *abramov::isSymbol(const char *str, char c)
 {
   if (!str)
   {
     return str;
   }
-  return (*str==c) ? (str + 1) : nullptr;
+  return (*str == c) ? (str + 1) : nullptr;
 }
 
 const char *abramov::isSign(const char *str)
@@ -15,7 +26,7 @@ const char *abramov::isSign(const char *str)
   {
     return str;
   }
-  return (*str == '+' || *str=='-') ? (str + 1) : nullptr;
+  return (*str == '+' || *str == '-') ? (str + 1) : nullptr;
 }
 
 const char *abramov::isDigit(const char *str)
@@ -24,7 +35,7 @@ const char *abramov::isDigit(const char *str)
   {
     return str;
   }
-  return (*str <= 57 && *str >= 48) ? (str + 1) : nullptr;
+  return (*str <= '9' && *str >= '0') ? (str + 1) : nullptr;
 }
 
 const char *abramov::hasUnsignedNumber(const char *str)
@@ -33,12 +44,12 @@ const char *abramov::hasUnsignedNumber(const char *str)
   {
     return str;
   }
-  auto next = abramov::isDigit(str);
+  auto next = isDigit(str);
   if (!next)
   {
     return next;
   }
-  if (auto continues = abramov::hasUnsignedNumber(next))
+  if (auto continues = hasUnsignedNumber(next))
   {
     return continues;
   }
@@ -51,17 +62,17 @@ const char *abramov::hasOrder(const char *str)
   {
     return str;
   }
-  auto next = abramov::isSymbol(str, 'E');
+  auto next = isSymbol(str, 'E');
   if (!next)
   {
     return next;
   }
-  next = abramov::isSign(next);
+  next = isSign(next);
   if (!next)
   {
     return next;
   }
-  next = abramov::hasUnsignedNumber(next);
+  next = hasUnsignedNumber(next);
   return next;
 }
 
@@ -71,14 +82,14 @@ const char *abramov::hasMantissa(const char *str)
   {
     return str;
   }
-  auto next = abramov::hasUnsignedNumber(str);
+  auto next = hasUnsignedNumber(str);
   if (!next)
   {
     return next;
   }
   if (*next=='.')
   {
-    return abramov::hasUnsignedNumber(next+1);
+    return hasUnsignedNumber(next+1);
   }
   return next;
 }
@@ -89,20 +100,20 @@ const char *abramov::hasReal(const char *str)
   {
     return str;
   }
-  if (auto next = abramov::isSign(str))
+  if (auto next = isSign(str))
   {
-    next = abramov::hasMantissa(next);
-    return abramov::hasOrder(next);
+    next = hasMantissa(next);
+    return hasOrder(next);
   }
   else
   {
-    next = abramov::hasMantissa(str);
-    return abramov::hasOrder(next);
+    next = hasMantissa(str);
+    return hasOrder(next);
   }
 }
 
 bool abramov::isReal(const char *str)
 {
-  auto next = abramov::hasReal(str);
+  auto next = hasReal(str);
   return next && (*next == '\0');
 }
--- abramov.vladislav/P6/recursionCheckStr.hpp
+++ abramov.vladislav/P6/recursionCheckStr.hpp
@@ -2,13 +2,6 @@
 #define RECURSIONCHECKSTR_HPP
 namespace abramov
 {
-  const char *isSymbol(const char *str, char c);
-  const char *isSign(const char *str);
-  const char *isDigit(const char *str);
-  const char *hasUnsignedNumber(const char *str);
-  const char *hasOrder(const char *str);
-  const char *hasMantissa(const char *str);
-  const char *hasReal(const char *str);
   bool isReal(const char *str);
 }
 #endif
