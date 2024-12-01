3e4118db5a98a765cf9ca8c9f97f0332a26f927b
Maksim_Shramko <maxramko@gmail.com>
2024-12-01 17:43:47 +0300

    Several corrections with ; and namespacing

--- shramko.maksim/P6/isExpression.cpp
+++ shramko.maksim/P6/isExpression.cpp
@@ -1,170 +1,174 @@
 #include "isExpression.hpp"
-
-const char* shramko::has_symbol(const char* str, char s)
+namespace
 {
-  if (!str)
+  const char* has_expression(const char* str);
+
+  const char* has_symbol(const char* str, char s)
   {
-    return str;
-  }
-  return (*str == s) ? (str++) : nullptr;
-}
-
-const char* shramko::has_x(const char* str)
-{
-  return has_symbol(str, 'x');
-}
-
-const char* shramko::has_y(const char* str)
-{
-  return has_symbol(str, 'y');
-}
-
-const char* shramko::has_z(const char* str)
-{
-  return has_symbol(str, 'z');
-}
-
-const char* shramko::has_one_or_zero(const char* str)
-{
-  const char* following = has_symbol(str, '1');
-  return following ? following : has_symbol(str, '0');
-}
-
-const char* shramko::has_following_id(const char* str)
-{
-  if (!str)
-  {
-    return str;
+    if (!str)
+    {
+      return str;
+    }
+    return (*str == s) ? (str++) : nullptr;
   }
 
-  const char* following = has_x(str);
-  if (following)
+  const char* has_x(const char* str)
   {
-    return following;
-  {
-  following = has_y(str);
-  if (following)
-  {
-    return following;
-  {
-  following = has_z(str);
-  return following;
-}
+    return has_symbol(str, 'x');
+  }
 
-const char* shramko::has_unsigned_int(const char* str)
-{
-  const char* i = *str;
-  if (!str)
+  const char* has_y(const char* str)
   {
-    return str;
+    return has_symbol(str, 'y');
   }
-  const char* following = has_zero_or_one(str);
-  if (!following)
-  {
-    return nullptr;
-  }
-  if (const char* proceed = has_unsigned_int(following))
-  {
-    return proceed;
-  }
-  return following;
-}
 
-const char* shramko::has_multi(const char* str)
-{
-  if (!str)
+  const char* has_z(const char* str)
   {
-    return str
+    return has_symbol(str, 'z');
   }
-  const char* following = has_unsigned_int(str);
-  if (following)
+
+  const char* has_one_or_zero(const char* str)
   {
+    const char* following = has_symbol(str, '1');
+    return following ? following : has_symbol(str, '0');
+  }
+
+  const char* has_following_id(const char* str)
+  {
+    if (!str)
+    {
+      return str;
+    }
+
+    const char* following = has_x(str);
+    if (following)
+    {
+      return following;
+    {
+    following = has_y(str);
+    if (following)
+    {
+      return following;
+    {
+    following = has_z(str);
     return following;
   }
-  following = has_following_id(str);
-  if (following)
+
+  const char* has_unsigned_int(const char* str)
   {
+    const char* i = *str;
+    if (!str)
+    {
+      return str;
+    }
+    const char* following = has_zero_or_one(str);
+    if (!following)
+    {
+      return nullptr;
+    }
+    if (const char* proceed = has_unsigned_int(following))
+    {
+      return proceed;
+    }
     return following;
   }
-  if ((str = has_symbol(str, '(')))
+
+  const char* has_multi(const char* str)
   {
-    following = has_expression(str);
-    if ((following = has_symbol(str, ')')))
+    if (!str)
+    {
+      return str;
+    }
+    const char* following = has_unsigned_int(str);
+    if (following)
     {
       return following;
     }
+    following = has_following_id(str);
+    if (following)
+    {
+      return following;
+    }
+    if ((str = has_symbol(str, '(')))
+    {
+      following = has_expression(str);
+      if ((following = has_symbol(str, ')')))
+      {
+        return following;
+      }
+      return nullptr;
+    }
     return nullptr;
   }
-  return nullptr;
-}
 
-const char* shramko::has_term(const char* str)
-{
-  if (!str)
+  const char* has_term(const char* str)
   {
-    return str;
-  }
-  const char* following = has_multi(str);
-  if (!following)
-  {
-    return following;
-  }
-  if (const char* nextFol = has_symbol(following, '*'))
-  {
-    const char* next = has_term(nextFol);
-    if (!next)
+    if (!str)
     {
-      return nullptr;
+      return str;
     }
-    return next;
-  }
-  if (const char* ay_nextFol = has_symbol(str, '('))
-  {
-    const char* next = has_multi(ay_nextFol);
-    if (!next)
+    const char* following = has_multi(str);
+    if (!following)
     {
-      return nullptr;
+      return following;
     }
-    if (const char* scnd_next = has_symbol(next, '+')
+    if (const char* nextFol = has_symbol(following, '*'))
     {
-      const char* next_term = has_term(scnd_next)
-      if (!next_term || !has_symbol(next_term, ')'))
+      const char* next = has_term(nextFol);
+      if (!next)
       {
         return nullptr;
       }
-      return next_term;
+      return next;
     }
-    if (!has_symbol(following, ')'))
+    if (const char* ay_nextFol = has_symbol(str, '('))
     {
-      return nullptr;
+      const char* next = has_multi(ay_nextFol);
+      if (!next)
+      {
+        return nullptr;
+      }
+      if (const char* scnd_next = has_symbol(next, '+')
+      {
+        const char* next_term = has_term(scnd_next)
+        if (!next_term || !has_symbol(next_term, ')'))
+        {
+          return nullptr;
+        }
+        return next_term;
+      }
+      if (!has_symbol(following, ')'))
+      {
+        return nullptr;
+      }
+      return ay_nextFol;
     }
-    return ay_nextFol;
+    return following;
   }
-  return following;
+
+  const char* has_expression(const char* str)
+  {
+    if (!str)
+    {
+      return str;
+    }
+    const char* following = has_term(str);
+    if (following)
+    {
+      if (const char* nextFol = has_symbol(following, '+'))
+      {
+        return has_expression(nextFol);
+      }
+      else if (const char* nextFol = has_symbol(following, '-'))
+      {
+        return has_expression(nextFol);
+      }
+    }
+    return following;
+  }
 }
 
-const char* shramko::has_expression(const char* str)
-{
-  if (!str)
-  {
-    return str;
-  }
-  const char* following = has_term(str);
-  if (following)
-  {
-    if (const char* nextFol = has_symbol(following, '+'))
-    {
-      return has_expression(nextFol);
-    }
-    else if (const char* nextFol = has_symbol(following, '-'))
-    {
-      return has_expression(nextFol)
-    }
-  }
-  return following;
-}
-
-bool shramko::is_expression(const char* str)
+bool is_expression(const char* str)
 {
   const char* next = has_expression(str);
   return next && (has_symbol(next, '\0'));
