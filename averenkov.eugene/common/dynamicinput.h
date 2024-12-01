574431933969e778d2b8ce52ed0afb3d9ced4e23
Savishna <bravissimo28@mail.ru>
2024-12-01 16:59:24 +0300

    last last fixes

--- averenkov.eugene/common/dynamicinput.h
+++ averenkov.eugene/common/dynamicinput.h
@@ -3,7 +3,7 @@
 #include <istream>
 namespace averenkov
 {
-  char* arrayresize(const char* str, char* new_str);
+  char* stringcopy(const char* str, char* new_str);
   char* stringInput(std::istream& in);
 }
 #endif
