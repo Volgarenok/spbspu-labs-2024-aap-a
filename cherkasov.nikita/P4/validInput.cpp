2bfac6b34b1ccc5caae2b293807e2e87edc45aa2
cherkasoov <cherkasov5509@gmail.com>
2024-12-02 23:23:57 +0300

    ..

--- cherkasov.nikita/P4/validInput.cpp
+++ cherkasov.nikita/P4/validInput.cpp
@@ -6,6 +6,7 @@ bool cherkasov::isValidInput(const char* input)
 {
   if (!input)
   {
+    std::cerr << "Error: The string is empty.\n";
     return false;
   }
   size_t length = 0;
@@ -13,10 +14,17 @@ bool cherkasov::isValidInput(const char* input)
   {
     if (!std::isprint(input[length]))
     {
+      std::cerr << "Error:no char string.\n";
       return false;
     }
     ++length;
   }
-  const size_t maxLength = 100;
-  return length <= maxLength;
+  const size_t maxLength = 1000;
+  if (length > maxLength)
+  {
+    std::cerr << "Error: line max leght." << maxLength << "\n";
+    return false;
+  }
+  return true;
 }
+
