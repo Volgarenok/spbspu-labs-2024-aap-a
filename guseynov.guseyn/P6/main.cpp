c3f9f217187df9e2822296aa79e327c300b40202
GuseynovGuseynGG <ganx.gamer@gmail.com>
2024-12-03 00:44:54 +0300

    fix

--- guseynov.guseyn/P6/main.cpp
+++ guseynov.guseyn/P6/main.cpp
@@ -5,11 +5,6 @@
 int main()
 {
   char * str = guseynov::getLine(std::cin);
-  if (str == nullptr)
-  {
-    std::cerr << "Fail to read\n";
-    return 1;
-  }
   std::cout << std::boolalpha << guseynov::isDouble(str) << "\n";
   free(str);
   return 0;
