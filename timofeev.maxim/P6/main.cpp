ea41ba9ac2921636174d4c8770352fc75301540d
PigoDog <maksimtmfv@gmail.com>
2024-12-03 00:19:55 +0300

    for you to see

--- timofeev.maxim/P6/main.cpp
+++ timofeev.maxim/P6/main.cpp
@@ -7,8 +7,7 @@ int main()
   char* line = timofeev::input_line(std::cin);
   if (line == nullptr)
   {
-    std::cerr << "Bad output\n";
-    free(line);
+    std::cerr << "Bad input\n";
     return 1;
   }
   std::cout << std::boolalpha << timofeev::is_real_number(line) << "\n";
