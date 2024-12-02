9f02c2508561d4207b76170b085f972ddfaaa97b
evstyunichev.konstantin <korombodis.gmail.com>
2024-12-02 20:19:13 +0300

    all is good now

--- evstyunichev.konstantin/P3/matrix.cpp
+++ evstyunichev.konstantin/P3/matrix.cpp
@@ -18,7 +18,8 @@ int evstyunichev::cast_to_digit(const char *s)
   return ans;
 }
 
-std::istream & evstyunichev::input(std::istream &fin, int *arr, size_t n, size_t m, size_t &read)
+std::istream & evstyunichev::input(std::istream &fin, int *arr, const size_t n,
+  const size_t m, size_t &read)
 {
   int num = 0;
   for (size_t i = 0; i < n; i++)
--- evstyunichev.konstantin/P3/matrix.hpp
+++ evstyunichev.konstantin/P3/matrix.hpp
@@ -4,10 +4,10 @@
 namespace evstyunichev
 {
   int cast_to_digit(const char *);
-  std::istream & input(std::istream &, int *, const size_t, const size_t, size_t &);
-  bool is_row_nsm(const int *, const size_t, const size_t);
-  size_t cnt_rows_nsm(const int *, const size_t, const size_t);
-  bool solve_print(std::istream &, std::ostream &, int *, const size_t, const size_t, size_t &);
-  void output(std::ostream &, const int *, const size_t, const size_t);
+  std::istream & input(std::istream &, int *, size_t, size_t, size_t &);
+  bool is_row_nsm(const int *, size_t, size_t);
+  size_t cnt_rows_nsm(const int *, size_t, size_t);
+  bool solve_print(std::istream &, std::ostream &, int *, size_t, size_t, size_t &);
+  void output(std::ostream &, const int *, size_t, size_t);
 }
 #endif
