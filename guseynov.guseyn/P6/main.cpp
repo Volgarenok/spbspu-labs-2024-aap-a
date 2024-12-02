4674c78010d02db6d68deb18070a004d53d46df2
GuseynovGuseynGG <ganx.gamer@gmail.com>
2024-12-02 23:03:44 +0300

    check nullptr

--- guseynov.guseyn/P6/main.cpp
+++ guseynov.guseyn/P6/main.cpp
@@ -5,6 +5,10 @@
 int main()
 {
   const char * str = guseynov::getLine(std::cin);
+  if (!str)
+  {
+    return 1;
+  }
   std::cout << std::boolalpha << guseynov::isDouble(str) << "\n";
   free(str);x
   return 0;
