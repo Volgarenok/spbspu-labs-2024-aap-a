abe88584b0bf8d504f54034f72397f3383e32c17
cherkasoov <cherkasov5509@gmail.com>
2024-12-02 23:50:20 +0300

    ..

---
+++ cherkasov.nikita/P6/readLine.cpp
@@ -0,0 +1,9 @@
+#include "readLine.h"
+#include "getLine.h"
+#include <iostream>
+#include <stdexcept>
+
+char* cherkasov::readLine(std::istream& inputStream)
+{
+  return cherkasov::inputLine(inputStream);
+}
---
+++ cherkasov.nikita/P6/readLine.h
@@ -0,0 +1,10 @@
+#ifndef READLINE_H
+#define READLINE_H
+#include <iostream>
+
+namespace cherkasov
+{
+  char* readLine(std::istream& inputStream);
+}
+
+#endif
