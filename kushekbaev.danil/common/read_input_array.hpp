5197927b92c4fca43b50869acedce8ecf1a8690e
Z-1337 <un228der@gmail.com>
2024-12-03 00:31:36 +0300

    doing bad things

---
+++ kushekbaev.danil/common/read_input_array.hpp
@@ -0,0 +1,10 @@
+#ifndef READ_INPUT_ARRAY_HPP
+#define READ_INPUT_ARRAY_HPP
+#include <cstddef>
+#include <istream>
+
+namespace kushekbaev
+{
+  char* readInputArray(std::istream& input);
+}
+#endif
