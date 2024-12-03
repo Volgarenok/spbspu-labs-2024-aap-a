2f682b80463d0cda45539d55ca8dc7865c0aa266
GuseynovGuseynGG <ganx.gamer@gmail.com>
2024-12-03 22:48:23 +0300

    try fix 2 test

--- guseynov.guseyn/P6/main.cpp
+++ guseynov.guseyn/P6/main.cpp
@@ -7,11 +7,13 @@ int main()
   char * str = guseynov::getLine(std::cin);
   if (str == nullptr)
   {
+    std::cerr << "Memory was not allocated\n";
     return 1;
   }
   if (str[0] == '\0')
   {
     free(str);
+    std::cerr << "The absence of a line\n";
     return 1;
   }
   std::cout << std::boolalpha << guseynov::isDouble(str) << "\n";
