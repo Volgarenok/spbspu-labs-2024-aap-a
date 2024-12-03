1631771ae3bf46caea9d42858b760c560f9aa2c2
Гаврилова Полина <polina.gavrilova2022@mail.ru>
2024-12-03 16:43:25 +0300

    .

--- gavrilova.polina/P3/upptri.cpp
+++ gavrilova.polina/P3/upptri.cpp
@@ -11,7 +11,7 @@ bool gavrilova::isUpperTriangMtx(const int * mtx, size_t m, size_t n)
   }
   for (size_t i = 0; i < m; ++i) {
     for (size_t j = 0; j < n; ++j) {
-      if (!is_need_element(i, j, mtx, n)) {
+      if (!is_upper_triangle_mtx_element(i, j, mtx, n)) {
         result = false;
       }
     }
