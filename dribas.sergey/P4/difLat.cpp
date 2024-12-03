221100be08b73264ce0ec7911b01588b133f5bd9
Sergey Dribas <serdribas@gmail.com>
2024-12-04 00:34:27 +0300

    return my p4

---
+++ dribas.sergey/P4/difLat.cpp
@@ -0,0 +1,28 @@
+#include "difLat.hpp"
+#include <cctype>
+#include <cstddef>
+
+size_t dribas::difLat(const char* str)
+{
+  size_t result = 0;
+  const char* ptrstr = str;
+  while (*ptrstr!= '\0') {
+    if (isalpha(*ptrstr)) {
+      bool check = true;
+      const char* prevptr = ptrstr - 1;
+      while (prevptr >= str) {
+        if (*ptrstr == *prevptr) {
+          check = false;
+          break;
+        }
+        prevptr--;
+      }
+      if (check) {
+        result++;
+      }
+    }
+    ptrstr++;
+  }
+  return result;
+}
+
