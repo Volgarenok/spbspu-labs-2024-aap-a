add2760c147481d1518922c8e589aa4f89224df0
15446-rus75 <vladabramov06@yandex.ru>
2024-12-04 14:44:46 +0300

    add rectangle.hpp

---
+++ abramov.vladislav/T1/rectangle.hpp
@@ -0,0 +1,20 @@
+#ifndef RECTANGLE_HPP
+#define RECTANGLE_HPP
+#include "shape.hpp"
+
+namespace abramov
+{
+  struct Rectangle: Shape
+  {
+  Rectangle(double x1, double y1, double x2, double y2);
+  double getArea() const override;
+  void getFrameRect() const override;
+  void move(point_t p) override;
+  void move(double dx, double dy) override;
+  void scale(double k) override;
+  private:
+    point_t pLeftLower;
+    point_t pRightUpper;
+  };
+}
+#endif
