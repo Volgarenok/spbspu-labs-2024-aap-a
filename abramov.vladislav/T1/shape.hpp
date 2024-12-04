a0eaaf4b2ee3f2cbd3c7c0e8330338cbef9e9775
15446-rus75 <vladabramov06@yandex.ru>
2024-12-04 14:29:04 +0300

    add shape.hpp

---
+++ abramov.vladislav/T1/shape.hpp
@@ -0,0 +1,17 @@
+#ifndef SHAPE_HPP
+#define SHAPE_HPP
+#include "base-types.hpp"
+
+namespace abramov
+{
+  struct Shape
+  {
+    virtual double getArea() = 0;
+    virtual rectangle_t getFrameRect() = 0;
+    virtual void move(point_t p) = 0;
+    virtual void move(double dx, double dy) = 0;
+    virtual void scale(double k) = 0;
+    virtual ~Shape() = default;
+  };
+}
+#endif
