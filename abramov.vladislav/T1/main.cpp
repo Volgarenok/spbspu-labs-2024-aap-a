ba834dd29c9c4efcc54af7e7844ca65d283aaac1
15446-rus75 <vladabramov06@yandex.ru>
2024-12-04 17:23:17 +0300

    CG

--- abramov.vladislav/T1/main.cpp
+++ abramov.vladislav/T1/main.cpp
@@ -1,3 +1,9 @@
+#include <iostream>
+#include <getString.hpp>
+#include "base-types.hpp"
+#include "shape.hpp"
+#include "rectangle.hpp"
+
 int main()
 {
 }
--- abramov.vladislav/T1/rectangle.hpp
+++ abramov.vladislav/T1/rectangle.hpp
@@ -6,12 +6,12 @@ namespace abramov
 {
   struct Rectangle: Shape
   {
-  Rectangle(double x1, double y1, double x2, double y2);
-  double getArea() const override;
-  rectangle_t getFrameRect() const override;
-  void move(point_t p) override;
-  void move(double dx, double dy) override;
-  void scale(double k) override;
+    Rectangle(double x1, double y1, double x2, double y2);
+    double getArea() const override;
+    rectangle_t getFrameRect() const override;
+    void move(point_t p) override;
+    void move(double dx, double dy) override;
+    void scale(double k) override;
   private:
     point_t pLeftLower;
     point_t pRightUpper;
