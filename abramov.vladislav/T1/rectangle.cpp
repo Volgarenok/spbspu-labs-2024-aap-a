bb6d32e8931aff131382c7c3db7fa3dd1f099612
15446-rus75 <vladabramov06@yandex.ru>
2024-12-04 15:45:23 +0300

    CG

--- abramov.vladislav/T1/rectangle.cpp
+++ abramov.vladislav/T1/rectangle.cpp
@@ -5,56 +5,57 @@
 
 namespace abramov
 {
-Rectangle::Rectangle(double x1, double y1, double x2, double y2)
-{
-  if (x1 >= x2 || y1 >= y2)
+  Rectangle::Rectangle(double x1, double y1, double x2, double y2)
   {
-    throw std::logic_error("Impossible to build a rectangle");
+    if (x1 >= x2 || y1 >= y2)
+    {
+      throw std::logic_error("Impossible to build a rectangle");
+    }
+    pLeftLower.x = x1;
+    pLeftLower.y = y1;
+    pRightUpper.x = x2;
+    pRightUpper.y = y2;
   }
-  pLeftLower.x = x1;
-  pLeftLower.y = y1;
-  pRightUpper.x = x2;
-  pRightUpper.y = y2;
-}
 
-double Rectangle::getArea() const
-{
-  return (pRightUpper.x - pLeftLower.x) * (pRightUpper.y - pLeftLower.y);
-}
+  double Rectangle::getArea() const
+  {
+    return (pRightUpper.x - pLeftLower.x) * (pRightUpper.y - pLeftLower.y);
+  }
 
-rectangle_t Rectangle::getFrameRect() const
-{
-  double x = (pRightUpper.x + pLeftLower.x) / 2;
-  double y = (pRightUpper.y + pLeftLower.y) / 2;
-  point_t pos;
-  pos.x = x;
-  pos.y = y;
-  rectangle_t frame_rect;
-  frame_rect.width = x;
-  frame_rect.height = y;
-  frame_rect.pos = pos;
-  return frame_rect;
-}
+  rectangle_t Rectangle::getFrameRect() const
+  {
+    double x = (pRightUpper.x + pLeftLower.x) / 2;
+    double y = (pRightUpper.y + pLeftLower.y) / 2;
+    point_t pos;
+    pos.x = x;
+    pos.y = y;
+    rectangle_t frame_rect;
+    frame_rect.width = x;
+    frame_rect.height = y;
+    frame_rect.pos = pos;
+    return frame_rect;
+  }
 
-void Rectangle::move(double dx, double dy)
-{
-  pRightUpper.x += dx;
-  pRightUpper.y += dy;
-  pLeftLower.x += dx;
-  pLeftLower.y += dy;
-}
+  void Rectangle::move(double dx, double dy)
+  {
+    pRightUpper.x += dx;
+    pRightUpper.y += dy;
+    pLeftLower.x += dx;
+    pLeftLower.y += dy;
+  }
 
-void Rectangle::move(point_t p)
-{
-  rectangle_t frame_rect = getFrameRect();
-  point_t center = frame_rect.pos;
-  double dx = p.x - center.x;
-  double dy = p.y - center.y;
-  move(dx, dy);
-}
+  void Rectangle::move(point_t p)
+  {
+    rectangle_t frame_rect = getFrameRect();
+    point_t center = frame_rect.pos;
+    double dx = p.x - center.x;
+    double dy = p.y - center.y;
+    move(dx, dy);
+  }
 
-void Rectangle::scale(double k)
-{
-  k = 0;
-}
+  void Rectangle::scale(double k)
+  {
+    
+  }
+
 }
