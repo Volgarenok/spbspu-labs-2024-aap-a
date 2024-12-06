683ec2aced48e1e1b217f792abaf007987145aba
deonik3 <kis.sergey2006@gmail.com>
2024-12-06 15:19:19 +0300

    Update input in main

--- kiselev.sergey/T1/main.cpp
+++ kiselev.sergey/T1/main.cpp
@@ -1,4 +1,6 @@
+#include <cctype>
 #include <iostream>
+#include <stdexcept>
 #include <string>
 #include <inputString.h>
 #include "base-types.hpp"
@@ -9,44 +11,58 @@ int main()
   kiselev::Shape* shapes[1000] = {};
   size_t countShape = 0;
   std::string titleShape;
-  while (std::cin >> titleShape)
+  try
   {
-    if (titleShape == "RECTANGLE")
+    while (std::cin >> titleShape)
     {
-      shapes[countShape++] = kiselev::make_rectangle(std::cin);
-      titleShape = "";
-    }
-    else if (std::cin.eof())
-    {
-      std::cerr << "The input ended with eof\n";
-      return 1;
-    }
-    else if (titleShape == "SCALE")
-    {
-      kiselev::point_t scale = kiselev::make_scale(std::cin);
-      double ratio = 0;
-      std::cin >> ratio;
-      if (ratio <= 0)
+      if (titleShape == "RECTANGLE")
       {
-        std::cerr << "Incorrect ratio\n";
+        shapes[countShape++] = kiselev::make_rectangle(std::cin);
+        titleShape = "";
+      }
+      else if (std::cin.eof())
+      {
+        std::cerr << "The input ended with eof\n";
         return 1;
       }
-      break;
-    }
-    else if (titleShape.empty())
-    {
-      continue;
-    }
-    else
-    {
-      char* unusedCoordinates = kiselev::inputString(std::cin, '\n');
-      if (!unusedCoordinates)
+      else if (titleShape == "SCALE")
       {
-        return 1;
+        kiselev::point_t scale = kiselev::make_scale(std::cin);
+        double ratio = 0;
+        std::cin >> ratio;
+        if (ratio <= 0)
+        {
+          std::cerr << "Incorrect ratio\n";
+          return 1;
+        }
+        break;
       }
-      free(unusedCoordinates);
-      titleShape = "";
+      else if (titleShape.empty())
+      {
+        continue;
+      }
+      else
+      {
+        char* unusedCoordinates = kiselev::inputString(std::cin, '\n');
+        if (!unusedCoordinates)
+        {
+          return 1;
+        }
+        free(unusedCoordinates);
+        titleShape = "";
+      }
     }
+  }
+  catch (const std::logic_error& e)
+  {
+    std::cerr << e.what();
+    kiselev::destroyShapePtr(shapes, countShape);
+    return 1;
+  }
+  catch (const std::bad_alloc& e)
+  {
+    std::cerr << "Memory was not allocated\n";
+    kiselev::destroyShapePtr(shapes, countShape);
   }
   kiselev::destroyShapePtr(shapes, countShape);
   return 0;
