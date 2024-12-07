4b9bf42316178faf485e6aede32a3b1f7f5b9ef4
evstyunichev.konstantin <korombodis.gmail.com>
2024-12-07 19:18:56 +0300

    nullptr check added

--- evstyunichev.konstantin/P6/main.cpp
+++ evstyunichev.konstantin/P6/main.cpp
@@ -5,6 +5,11 @@
 int main()
 {
   char *str1 = evstyunichev::get_string(std::cin);
+  if (!str1)
+  {
+    std::cerr << "error\n";
+    return 1;
+  }
   if (str1 && !str1[0])
   {
     std::cerr << "zero input\n";
