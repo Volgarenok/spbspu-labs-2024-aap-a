2da864b164d857e246172562a02b6aa89d114388
aldyn-d <aldyndirti@gmail.com>
2024-12-07 22:49:53 +0300

    matrix_checking corecting

--- dirti.aldyn/P3/matrix_checking.cpp
+++ dirti.aldyn/P3/matrix_checking.cpp
@@ -1,18 +1,17 @@
 #include "matrix_checking.hpp"
 
+
+bool dirti::matrix_checking(int * matrix, size_t m, size_t n)
 {
-  bool matrix_checking(int * matrix, size_t m, size_t n)
+  for (size_t i = 1; i < (m - 1); ++i)
   {
-    for (size_t i = 1; i < (m - 1); ++i)
+    for (size_t j = 1; j < (n - 1); ++j)
     {
-      for (size_t j = 1; j < (n - 1); ++j)
+      if (matrix[i * n + j] != 0)
       {
-        if (matrix[i * n + j] != 0)
-        {
-          return false;
-        }
+        return false;
       }
     }
-    return true;
   }
+  return true;
 }
