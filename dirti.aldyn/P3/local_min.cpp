098ed2a0b5ce5a91115dc79e01af92f109a2d4a0
aldyn-d <aldyndirti@gmail.com>
2024-12-07 22:36:32 +0300

    local_min mistake

--- dirti.aldyn/P3/local_min.cpp
+++ dirti.aldyn/P3/local_min.cpp
@@ -1,20 +1,18 @@
 #include "local_min.hpp"
 
+int dirti::local_min(int * matrix, size_t m, size_t n)
 {
-  int dirti::local_min(int * matrix, size_t m, size_t n)
+  int num = 0;
+  for (i = 1; i < (m - 1); ++i)
   {
-    int num = 0;
-    for (i = 1; i < (m - 1); ++i)
+    for (j = 1; j < (n - 1); ++j)
     {
-      for (j = 1; j < (n - 1); ++j)
+      size_t t = i * n + j;
+      if (matrix[t] < matrix[t - 1] && matrix[t] < matrix[t + 1] && matrix[t] < matrix[t - n] && matrix[t] < matrix[t + n])
       {
-        size_t t = i * n + j;
-        if (matrix[t] < matrix[t - 1] && matrix[t] < matrix[t + 1] && matrix[t] < matrix[t - n] && matrix[t] < matrix[t + n])
-        {
-          ++num;
-        }
+        ++num;
       }
     }
-    return num;
   }
+  return num;
 }
