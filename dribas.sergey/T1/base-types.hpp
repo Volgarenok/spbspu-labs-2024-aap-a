aad2ba592d1aaad5b491dfe76220368d4b3e56e9
Sergey Dribas <serdribas@gmail.com>
2024-12-07 07:53:11 +0300

    add struct for base types

---
+++ dribas.sergey/T1/base-types.hpp
@@ -0,0 +1,17 @@
+#ifndef BASE_TYPES_HPP
+#define BASE_TYPES_HPP
+namespace dribas
+{
+  struct Point_t
+  {
+    double x_;
+    double y_;
+  };
+  struct Rectange_t
+  {
+    double width_;
+    double height_;
+    double pos_;
+  };
+}
+#endif
--- dribas.sergey/T1/main.cpp
+++ dribas.sergey/T1/main.cpp
@@ -1,3 +1,14 @@
+#include <cstddef> 
+#include <iostream>
+#include "enterStr.hpp"
 int main()
 {
+  //string = enterStr();
+	constexpr size_t stackSize = 1000;
+	char * stack[stackSize] = {} ;
+	bool running = true;
+	for (size_t i = 0; char* str = dribas::enterStr(std::cin); i++) {
+		*(stack + i) = str;
+	}
+	std::cout << stack[0] << stack[1];
 }
