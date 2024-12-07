424df7aa03d9132adc3872d4351efe1c04507a2d
evstyunichev.konstantin <korombodis.gmail.com>
2024-12-07 17:59:56 +0300

    ??

--- evstyunichev.konstantin/P6/main.cpp
+++ evstyunichev.konstantin/P6/main.cpp
@@ -5,6 +5,11 @@
 int main()
 {
   char *str1 = evstyunichev::get_string(std::cin);
+  if (str1 && !str1[0])
+  {
+    std::cerr << "zero input\n";
+    return 1;
+  }
   std::cout << std::boolalpha << (bool)evstyunichev::IsExpressionCorrect(str1) << '\n';
   return 0;
 }
