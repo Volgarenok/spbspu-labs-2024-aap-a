2c70a86fb511cee2d13cff5c299cd7d707e2bcdb
GuseynovGuseynGG <ganx.gamer@gmail.com>
2024-12-08 20:09:07 +0300

    add shape.hpp

---
+++ guseynov.guseyn/T1/shape.hpp
@@ -0,0 +1,18 @@
+#ifndef SHAPE_HPP
+#define SHAPE_HPP
+
+#include "base-types.hpp"
+
+namespace guseynov
+{
+  class Shape
+  {
+    virtual double getArea();
+    virtual rectangle_t getFrameRect();
+    virtual void move(point_t pos);
+    virtual void move(double x, double y);
+    virtual void scale(double k);
+    virtual ~Shape() = default;
+}
+
+#endif
