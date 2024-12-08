7d566b52c21b2b3fd9b1ff686327a07dd16d3349
sharksonk <sharksonk@gmail.com>
2024-12-08 23:00:18 +0300

    add input function

--- sharifullina.sofia/P4/shrSym_uniTwo.cpp
+++ sharifullina.sofia/P4/shrSym_uniTwo.cpp
@@ -1,2 +1,46 @@
 #include "shrSym_uniTwo.hpp"
 
+char * sharifullina::inputString(std::istream & in, char stop)
+{
+  constexpr size_t capacity = 10;
+  char * str = new char[capacity];
+  size_t size = 0;
+  char ch = '\0';
+  std::noskipws(in);
+  while ((in >> ch) && (ch != stop))
+  {
+    if (capacity == size)
+    {
+      char * newstr = nullptr;
+      try
+      {
+        newstr = new char[capacity]
+      }
+      catch
+      {
+        delete[] str;
+        std::skipws(in);
+        throw;
+      }
+      for (size_t i = 0; i < size; ++i)
+      {
+        newstr[i] = str[i];
+      }
+      delete[] str;
+      str = newstr;
+    }
+    str[size++] = ch;
+  }
+  str[size] = '\0';
+  in >> std::skipws;
+  return str;
+}
+
+char * sharifullina::shrSym(const char * str1, const char * str2)
+{
+  
+}
+char * sharifullina::uniTwo(const char * str1, const char * str2)
+{
+  
+}
