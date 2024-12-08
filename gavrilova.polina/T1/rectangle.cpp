151a1ce2bdea83f7f81612963241f426f1f6692e
Гаврилова Полина <polina.gavrilova2022@mail.ru>
2024-12-08 18:06:34 +0300

    Realizing rectangle. cpp and hpp

---
+++ gavrilova.polina/T1/rectangle.cpp
@@ -0,0 +1,38 @@
+#include "rectangle.hpp"
+#include "base-types.hpp"
+
+gavrilova::Rectangle::Rectangle(point_t pBottomLeft, point_t pTopRight):
+  pBottomLeft_(pBottomLeft), pTopRight_(pTopRight) {}
+double gavrilova::Rectangle::getArea() const {
+  return (pTopRight_.x - pBottomLeft_.x) * (pTopRight_.y - pBottomLeft_.y);
+}
+gavrilova::rectangle_t gavrilova::Rectangle::getFrameRect() const {
+  rectangle_t resultRect;
+  resultRect.height = pTopRight_.y - pBottomLeft_.y;
+  resultRect.width = pTopRight_.x - pBottomLeft_.x;
+  resultRect.pos.x = (pTopRight_.x - pBottomLeft_.x)/2;
+  resultRect.pos.y = (pTopRight_.y - pBottomLeft_.y)/2;
+  return resultRect;
+}
+void gavrilova::Rectangle::move(point_t p) {
+  point_t center = getFrameRect().pos;
+  double difX = p.x - center.x;
+  double difY = p.y - center.y;
+  pBottomLeft_.x += difX;
+  pBottomLeft_.y += difY;
+  pTopRight_.x += difX;
+  pTopRight_.y += difY;
+}
+void gavrilova::Rectangle::move(double difX, double difY) {
+  pBottomLeft_.x += difX;
+  pBottomLeft_.y += difY;
+  pTopRight_.x += difX;
+  pTopRight_.y += difY;
+}
+void gavrilova::Rectangle::scale(double k) {
+  point_t center = getFrameRect().pos;
+  pBottomLeft_.x = center.x - getFrameRect().width / 2 * k;
+  pBottomLeft_.y = center.y - getFrameRect().height / 2 * k;
+  pTopRight_.x = center.x + getFrameRect().width / 2 * k;
+  pTopRight_.y = center.y + getFrameRect().height / 2 * k;
+}
---
+++ gavrilova.polina/T1/rectangle.hpp
@@ -0,0 +1,17 @@
+#ifndef RECTANGLE_HPP
+#define RECTANGLE_HPP
+#include "shape.hpp"
+namespace gavrilova {
+  class Rectangle : public Shape {
+    Rectangle(point_t pBottomLeft, point_t pTopRigh);
+    double getArea() const override;
+    rectangle_t getFrameRect() const override;
+    void move(point_t p) override;
+    void move(double difX, double difY) override;
+    void scale(double k) override;
+  private:
+    point_t pBottomLeft_;
+    point_t pTopRight_;
+  };
+}
+#endif
