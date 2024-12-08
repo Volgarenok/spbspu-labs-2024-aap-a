2ba9fb16d55edca00785df9f149d215afb9162f8
15446-rus75 <vladabramov06@yandex.ru>
2024-12-08 23:29:54 +0300

    probably done for rectangle

--- abramov.vladislav/T1/shape_transformations.cpp
+++ abramov.vladislav/T1/shape_transformations.cpp
@@ -3,7 +3,7 @@
 
 namespace abramov
 {
-  void printFrameRectCoords(const abramov::rectangle_t& r)
+  void printFrameRectCoords(const abramov::rectangle_t &r)
   {
     double x1 = r.pos.x - r.width / 2;
     double y1 = r.pos.y - r.height / 2;
@@ -11,5 +11,17 @@ namespace abramov
     double y2 = r.pos.y + r.height / 2;
     std::cout << x1 << " " << y1 << " ";
     std::cout << x2 << " " << y2 << " ";
+  }
+  void scaleRectangle(Shape *r, point_t p, double k)
+  {
+    point_t pos1 = r->getFrameRect().pos;
+    r->move(p);
+    point_t pos2 = r->getFrameRect().pos;
+    double dx = pos2.x - pos1.x;
+    double dy = pos2.y - pos1.y;
+    r->scale(k);
+    dx *= -1 * k;
+    dy *= -1 * k;
+    r->move(dx, dy);
   }
 }
--- abramov.vladislav/T1/shape_transformations.hpp
+++ abramov.vladislav/T1/shape_transformations.hpp
@@ -1,10 +1,11 @@
 #ifndef SHAPE_TRANSFORMATIONS_HPP
 #define SHAPE_TRANSFORMATIONS_HPP
 #include "base-types.hpp"
+#include "shape.hpp"
 
 namespace abramov
 {
-  void printFrameRectCoords(const rectangle_t& r);
-  void scaleRectangle(Rectangle &r, point_t p);
+  void printFrameRectCoords(const rectangle_t &r);
+  void scaleRectangle(Shape *r, point_t p, double k);
 }
 #endif
