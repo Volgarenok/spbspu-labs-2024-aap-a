b44093f6e7f11a5ef864b9b2211c3f275e414ab1
ysffmn <andrewnikonov387@gmail.com>
2024-12-08 18:27:43 +0300

    Rectangle done(?)

---
+++ nikonov.andrew/T1/rectangle.cpp
@@ -0,0 +1,30 @@
+#include "rectangle.hpp"
+#include "base-types.hpp"
+nikonov::Rectangle::Rectangle(point_t lbp, point_t rtp):
+  frameRect(lbp, rtp), 
+  width_(frameRect.width_), 
+  height_(frameRect.height_),
+  pos_(frameRect.pos_)
+{}
+float nikonov::Rectangle::getArea() const
+{
+  return (width_ * height_);
+}
+nikonov::rectangle_t nikonov::Rectangle::getFrameRect() const
+{
+  return frameRect;
+}
+void nikonov::Rectangle::move(point_t newPos)
+{
+  this->pos_ = newPos;
+}
+void nikonov::Rectangle::move(float x, float y)
+{
+  pos_.x_ += x;
+  pos_.y_ += y;
+}
+void nikonov::Rectangle::scale(float k)
+{
+  width_ *= k;
+  height_ *= k;
+}
---
+++ nikonov.andrew/T1/rectangle.hpp
@@ -0,0 +1,20 @@
+#ifndef RECTANGE_HPP
+#define RECTANGE_HPP
+#include "base-types.hpp"
+#include "shape.hpp"
+namespace nikonov
+{
+  struct Rectangle: Shape
+  {
+    Rectangle(point_t lbp, point_t rtp);
+    float getArea() const override;
+    nikonov::rectangle_t getFrameRect() const override;
+    void move(point_t a) override;
+    void move(float x, float y) override;
+    void scale(float k) override;
+    float width_, height_;
+    point_t pos_;
+    rectangle_t frameRect;
+  };
+}
+#endif
