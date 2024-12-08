ebac01d4c52b35167eeeed51b2b86140c7ce8b35
Гаврилова Полина <polina.gavrilova2022@mail.ru>
2024-12-08 17:04:40 +0300

    Header for shape

--- gavrilova.polina/T1/base-types.hpp
+++ gavrilova.polina/T1/base-types.hpp
@@ -1,16 +1,13 @@
 #ifndef BASE_TYPES_HPP
 #define BASE_TYPES_HPP
 
-namespace gavrilova
-{
-  struct point_t
-  {
+namespace gavrilova {
+  struct point_t {
     double x;
     double y;
   };
 
-  struct rectangle_t
-  {
+  struct rectangle_t {
     double width;
     double height;
     point_t pos;
