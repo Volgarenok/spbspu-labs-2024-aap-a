2fcda6decc4279ec9236f481dfdc352b5f69abbf
sharksonk <sharksonk@gmail.com>
2024-12-08 22:36:46 +0300

    add shrSym and uniTwo

---
+++ sharifullina.sofia/P4/shrSym_uniTwo.cpp
@@ -0,0 +1,2 @@
+#include "shrSym_uniTwo.hpp"
+
---
+++ sharifullina.sofia/P4/shrSym_uniTwo.hpp
@@ -0,0 +1,13 @@
+#ifndef SHRSYM_UNITWO_HPP
+#define SHRSYM_UNITWO_HPP
+
+#include <iostream>
+
+namespace sharifullina
+{
+  char * inputString(std::istream & in, char stop);
+  char * shrSym(const char * str1, const char str2);
+  char * uniTwo(const char * str1, const char str2);
+}
+
+#endif
