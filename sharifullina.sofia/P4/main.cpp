59ba3dac1e43027e9e024eaf0885ab14ba428367
sharksonk <sharksonk@gmail.com>
2024-12-09 00:10:43 +0300

    add main

--- sharifullina.sofia/P4/main.cpp
+++ sharifullina.sofia/P4/main.cpp
@@ -1,3 +1,25 @@
+#include <iostream>
+#include "shrSym_uniTwo.hpp"
+
 int main()
 {
+  char * str = nullptr;
+  try
+  {
+    str = sharifullina::inputString(std::cin, '\n');
+  }
+  catch (const std::bad_alloc & e)
+  {
+    std::cerr << "Out of memory\n";
+    return 1;
+  }
+  if (str[0] == '\0')
+  {
+    delete[] str;
+    std::cerr << "Empty input\n";
+    return 1;
+  }
+  std::cout << sharifullina::shrSym(res, str1, str2) << '\n';
+  std::cout << sharifullina::uniTwo(res, str1, str2) << '\n';
+  delete[] str;
 }
