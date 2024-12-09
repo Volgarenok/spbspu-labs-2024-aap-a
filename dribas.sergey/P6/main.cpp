261900ff312a027966a0602c452dab202c69d534
Sergey Dribas <serdribas@gmail.com>
2024-12-09 17:17:59 +0300

    reamke function for enter

--- dribas.sergey/P6/main.cpp
+++ dribas.sergey/P6/main.cpp
@@ -5,12 +5,8 @@
 int main()
 {
   char* str = dribas::enterStr(std::cin);
-  if (!str && std::cin.eof()) {
+  if (!str) {
     return 1;
-  }
-  if (std::cin.eof()) {
-    free(str);
-    return 0;
   }
   std::cout << std::boolalpha << dribas::isValidString(str) << '\n';
   free(str);
