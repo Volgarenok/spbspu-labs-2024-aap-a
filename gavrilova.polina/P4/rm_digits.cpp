d48d68a2b716e4cae9ddc1d0662bb5f9f7924fc8
Гаврилова Полина <polina.gavrilova2022@mail.ru>
2024-12-10 00:54:02 +0300

    Not conferance.

--- gavrilova.polina/P4/rm_digits.cpp
+++ gavrilova.polina/P4/rm_digits.cpp
@@ -7,13 +7,12 @@ char* gavrilova::rm_digits(const char* original, char* new_str)
   if (original == nullptr || new_str == nullptr) {
     return nullptr;
   }
-  size_t ind_new_str = 0;
-  for (size_t i = 0; *(original + i) != '\0'; ++i) {
-    if (!isdigit(*(original + i))) {
-      *(new_str + ind_new_str) = *(original + i);
-      ++ind_new_str;
+  for (; *original; ++original) {
+    if (!std::isdigit(*original)) {
+      *new_str = *original;
+      ++new_str;
     }
   }
-  new_str[ind_new_str] = '\0';
+  *new_str = '\0';
   return new_str;
 }
