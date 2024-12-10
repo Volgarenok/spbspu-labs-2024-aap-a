e7b3dd5e39a6bd3b3236bbee41a282ee5c91afaf
brev1ch <flirktv@gmail.com>
2024-12-10 06:32:02 +0300

    rectangle.hpp changes

--- brevnov.nikita/T1/base-types.hpp
+++ brevnov.nikita/T1/base-types.hpp
@@ -1,14 +1,17 @@
 #ifndef BASE_TYPES_HPP
 #define BASE_TYPES_HPP
-struct point_t
+namespace brevnov
 {
-  double x;
-  double y;
-};
-struct rectangle
-{
-  double width;
-  double height;
-  point_t pos;
-};
+  struct point_t
+  {
+    double x;
+    double y;
+  };
+  struct rectangle
+  {
+    double width;
+    double height;
+    point_t pos;
+  };
+}
 #endif
--- brevnov.nikita/T1/rectangle.hpp
+++ brevnov.nikita/T1/rectangle.hpp
@@ -1,19 +1,21 @@
 #ifndef RECTANGLE_HPP
 #define RECTANGLE_HPP
 #include "shape.hpp"
-class Rectangle: public Shape
+namespace brevnov
 {
-public:
-  double getArea();
-  rectangle getFrameRect();
-  void move(point_t k);
-  void move(double x, double y);
-  void scale(double n);
-private:
-  point_t a;
-  point_t b;
-  point_t c;
-  point_t d;
-  rectangle base_rectangle;
-};
+  class Rectangle: public Shape
+  {
+  public:
+    Rectangle(point_t left, point_t right);
+    double getArea();
+    rectangle_t getFrameRect();
+    void move(point_t k);
+    void move(double x, double y);
+    void scale(double n);
+  private:
+    point_t left;
+    point_t right;
+    rectangle_t base_rectangle;
+  };
+}
 #endif
