10e122e8c43e381002f8fb6b594ad60e5e80e397
ZakirovMS <mz.08.08.06@gmail.com>
2024-12-14 00:25:54 +0300

    Definition of common elements

--- zakirov.marat/T1/base-types.hpp
+++ zakirov.marat/T1/base-types.hpp
@@ -1,1 +1,19 @@
+#include <cstddef>
 
+namespace zakirov
+{
+  struct point_t
+  {
+    private:
+      double x;
+      double y;
+  };
+
+  struct rectangle_t
+  {
+    private:
+      double width;
+      double height;
+      point_t pos;
+  };
+}
--- zakirov.marat/T1/shape.hpp
+++ zakirov.marat/T1/shape.hpp
@@ -1,1 +1,14 @@
+#include "base-types.hpp"
 
+namespace zakirov
+{
+  struct Shape
+  {
+    public:
+      virtual double getArea() const = 0;
+      virtual rectangle_t getFrameRect() const = 0;
+      virtual void move(point_t target) = 0;
+      virtual void move(double bias_x, double bias_y) = 0;
+      virtual void scale(double k) = 0;
+  };
+}
