5dda4eb34c63b2a6e0677129ff8580181a974760
root <root@DESKTOP-4310O61.localdomain>
2024-12-13 05:48:29 +0300

    add  shape.hpp

---
+++ balashov.dima/T1/shape.hpp
@@ -0,0 +1,16 @@
+#ifndef SHAPE_HPP
+#define SHAPE_HPP
+#include "base-types.hpp"
+
+namespace balashov
+{
+  class Shape
+  {
+    virtual double getArea() const = 0;
+    virtual rectangle_t getFrameRect() const = 0;
+    virtual void move(point_t point) = 0;
+    virtual void move(double xPos, double yPos) = 0;
+    virtual void scale(double coefficient) = 0;
+  };
+}
+#endif
