b6a90068ae5a773fb2b225bd870d0c35eef8ca5f
15446-rus75 <vladabramov06@yandex.ru>
2024-12-13 23:52:06 +0300

    delete some trash

--- abramov.vladislav/T1/main.cpp
+++ abramov.vladislav/T1/main.cpp
@@ -30,40 +30,6 @@ int main()
     return 1;
   }
   abramov::printShapes(std::cout, shapes, rects, i, x, y, k);
-/*  double s = 0;
-  std::cout << std::fixed << std::setprecision(1);
-  for (size_t j = 0; j < i; ++j)
-  {
-    s += shapes[j]->getArea();
-    rects[j] = shapes[j]->getFrameRect();
-  }
-  std::cout << s << " ";
-  abramov::point_t p;
-  p.x = x;
-  p.y = y;
-  for (size_t j = 0; j < i - 1; ++j)
-  {
-    abramov::printFrameRectCoords(rects[j]);
-    std::cout << " ";
-    abramov::scaleFigure(shapes[j], p, k);
-  }
-  abramov::printFrameRectCoords(rects[i - 1]);
-  abramov::scaleFigure(shapes[i - 1], p, k);
-  std::cout << "\n";
-  s = 0;
-  for (size_t j = 0; j < i; ++j)
-  {
-    s += shapes[j]->getArea();
-    rects[j] = shapes[j]->getFrameRect();
-  }
-  std::cout << s << " ";
-  for (size_t j = 0; j < i - 1; ++j)
-  {
-    abramov::printFrameRectCoords(rects[j]);
-    std::cout << " ";
-  }
-  abramov::printFrameRectCoords(rects[i - 1]);
-  std::cout << "\n"; */
   if (wrong_figure == true)
   {
     std::cerr << "There was bad figure\n";
