93b8b49d7fbe2966594050e19fe079ea86647ed4
Nekich06 <p.nikita.73526@gmail.com>
2024-12-14 22:20:37 +0300

    Changed formulas
    Now it seems to be good; returning to last problem

--- petrov.nikita/T1/rectangle.cpp
+++ petrov.nikita/T1/rectangle.cpp
@@ -1,5 +1,6 @@
 #include "rectangle.hpp"
 #include <cmath>
+#include <iostream> // Remove after debug
 petrov::Rectangle::Rectangle(point_t p1, point_t p2): 
   p1_(p1),
   p3_(p2)
@@ -19,7 +20,7 @@ petrov::point_t petrov::Rectangle::getFrameRect()
 {
   frame_rect_.height_ = abs(p2_.y_ - p1_.y_);
   frame_rect_.width_ = abs(p4_.x_ - p1_.x_);
-  frame_rect_.pos_ = { ((2 * p1_.x_ + p3_.x_ - p1_.x_) / 2), ((2 * p1_.y_ + p3_.x_ - p1_.x_) / 2) };
+  frame_rect_.pos_ = { ((2 * p1_.x_ + p3_.x_ - p1_.x_) / 2), ((2 * p1_.y_ + p3_.y_ - p1_.y_) / 2) };
   return frame_rect_.pos_;
 }
 
@@ -29,6 +30,7 @@ void petrov::Rectangle::move(point_t concrete_point)
   frame_rect_.pos_.y_ = concrete_point.y_;
   p1_.x_ = frame_rect_.pos_.x_ - (frame_rect_.width_ / 2);
   p1_.y_ = frame_rect_.pos_.y_ - (frame_rect_.height_ / 2);
+  std::clog << p1_.x_ << " " << p1_.y_ << "\n"; // Remove after debug
   p2_.x_ = frame_rect_.pos_.x_ - (frame_rect_.width_ / 2);
   p2_.y_ = frame_rect_.pos_.y_ + (frame_rect_.height_ / 2);
   p3_.x_ = frame_rect_.pos_.x_ + (frame_rect_.width_ / 2);
@@ -53,14 +55,15 @@ void petrov::Rectangle::move(double dx, double dy)
 
 void petrov::Rectangle::scale(double k)
 {
+  p1_.x_ -= (frame_rect_.width_ / 2) * (k - 1);
+  p1_.y_ -= (frame_rect_.height_ / 2) * (k - 1);
+  std::clog << p1_.x_ << " " << p1_.y_ << "\n"; // Remove after debug
+  p2_.x_ -= (frame_rect_.width_ / 2) * (k - 1);
+  p2_.y_ += (frame_rect_.height_  / 2) * (k - 1);
+  p3_.x_ += (frame_rect_.width_ / 2) * (k - 1);
+  p3_.y_ += (frame_rect_.height_ / 2) * (k - 1);
+  p4_.x_ += (frame_rect_.width_ / 2) * (k - 1);
+  p4_.y_ -= (frame_rect_.height_ / 2) * (k - 1);
   frame_rect_.height_ *= k;
   frame_rect_.width_ *= k;
-  p1_.x_ -= frame_rect_.width_ * 0.25;
-  p1_.y_ -= frame_rect_.height_ * 0.25;
-  p2_.x_ -= frame_rect_.width_ * 0.25;
-  p2_.y_ += frame_rect_.height_ * 0.25;
-  p3_.x_ += frame_rect_.width_ * 0.25;
-  p3_.y_ += frame_rect_.height_ * 0.25;
-  p4_.x_ += frame_rect_.width_ * 0.25;
-  p4_.y_ -= frame_rect_.height_ * 0.25;
 }
--- petrov.nikita/T1/scale_isotropically.cpp
+++ petrov.nikita/T1/scale_isotropically.cpp
@@ -1,16 +1,18 @@
 #include "scale_isotropically.hpp"
 #include "base-types.hpp"
 #include "shape.hpp"
-#include <iostream>
+#include <iostream> // Remove after debug
 void petrov::scaleIsotropically(point_t scale_point, size_t k, Shape * shape)
 {
   point_t pos = shape[0].getFrameRect();
+  std::clog << pos.x_ << " " << pos.y_ << "\n";
   double mv_dx = pos.x_ - scale_point.x_;
   double mv_dy = pos.y_ - scale_point.y_;
+  std::clog << mv_dx << " " << mv_dy << "\n";
   shape[0].move(scale_point);
   shape[0].scale(k);
   shape[0].getArea();
   mv_dx *= k;
   mv_dy *= k;
-  shape[0].move(-mv_dx, -mv_dy);
+  shape[0].move(mv_dx, mv_dy);
 }
