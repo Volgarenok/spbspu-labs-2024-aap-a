0579716bf917b727324f00dd660aec495cfe4586
Zholobov Oleg <zholobov.of@edu.spbstu.ru>
2024-12-14 15:57:20 +0300

    implemented square

--- zholobov.oleg/T1/square.cpp
+++ zholobov.oleg/T1/square.cpp
@@ -1,1 +1,9 @@
 #include "square.hpp"
+
+Square::Square():
+  Rectangle()
+{}
+
+Square::Square(const point_t& a, float length):
+  Rectangle(a, point_t{a.x + length, a.y + length})
+{}
--- zholobov.oleg/T1/square.hpp
+++ zholobov.oleg/T1/square.hpp
@@ -1,5 +1,13 @@
 #ifndef SQUARE_HPP
 #define SQUARE_HPP
 
+#include "rectangle.hpp"
+
+class Square: public Rectangle {
+public:
+  Square();
+  Square(const point_t& a, float length);
+};
+
 
 #endif
