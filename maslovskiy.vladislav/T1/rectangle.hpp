19fa01262dda94d076fc5fad1a0fcb6105a65fc6
vladislavmaslovskiy <vladuisik826@gmail.com>
2024-12-14 23:26:13 +0300

    Add rectangle.hpp

---
+++ maslovskiy.vladislav/T1/rectangle.hpp
@@ -0,0 +1,22 @@
+#ifndef RECTANGLE_HPP
+#define RECTANGLE_HPP
+#include "shape.hpp"
+
+namespace maslovskiy
+{
+  class Rectangle : public Shape
+  {
+  public:
+    Rectangle(point_t leftCorner, point_t rightCormer);
+    double getArea() const override;
+    rectangle_t getFrameRect() const override;
+    void move(point_t p) override;
+    void move(double dx, double dy) override;
+    void scale(double k) override;
+  private:
+    point_t leftCorner_;
+    point_t rightCormer_;
+  };
+}
+
+#endif
