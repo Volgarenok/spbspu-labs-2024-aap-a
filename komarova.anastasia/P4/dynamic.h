134caf6de92a53b238d8c23e630ce83ee5037718
NastyKomar <komarova2.ae@edu.spbstu.ru>
2024-12-14 17:09:26 +0300

    dyn fix

--- komarova.anastasia/P4/dynamic.h
+++ komarova.anastasia/P4/dynamic.h
@@ -1,6 +1,7 @@
 #ifndef DYNAMIC_H
 #define DYNAMIC_H
-
+#include <istream>
+#include <cstddef>
 namespace komarova
 {
   char * read(std::istream & input, size_t & size_str, size_t & size_str_now, char end_str);
