ab760e7737b7b4284fc4d5c3e312c866c3837c35
root <root@kryloff.localdomain>
2024-12-15 00:45:59 +0300

    base-types added

---
+++ krylov.matvey/T1/base-types.hpp
@@ -0,0 +1,16 @@
+#ifndef BASE_TYPES_HPP
+#define BASE_TYPES_HPP
+
+struct point_t {
+    float x;
+    float y;
+};
+
+struct rectangle_t
+{
+  point_t pos;
+  float width;
+  float height;
+};
+
+#endif
