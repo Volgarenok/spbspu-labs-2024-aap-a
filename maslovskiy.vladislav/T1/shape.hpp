3bfc27b59ac1683bbb320c27e0791af5400e9c6d
vladislavmaslovskiy <vladuisik826@gmail.com>
2024-12-14 22:33:06 +0300

    Add shape.hpp

---
+++ maslovskiy.vladislav/T1/shape.hpp
@@ -0,0 +1,18 @@
+#ifndef SHAPE_HPP
+#define SHAPE_HPP
+#include "base-types.hpp"
+
+namespace maslovskiy
+{
+  class Shape
+  {
+    virtual double getArea() const = 0;
+    virtual rectangle_t getFrameRect() const = 0;
+    virtual void move(point_t p) = 0;
+    virtual void move(double dx, double dy) = 0;
+    virtual void scale(double k) = 0;
+    virtual ~Shape() = default;
+  };
+}
+
+#endif
