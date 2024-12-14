faa2f8c5782189bf5139b80065c1aff612bffffd
root <root@kryloff.localdomain>
2024-12-15 01:09:57 +0300

    Shape class added

--- krylov.matvey/T1/base-types.hpp
+++ krylov.matvey/T1/base-types.hpp
@@ -2,15 +2,15 @@
 #define BASE_TYPES_HPP
 
 struct point_t {
-    float x;
-    float y;
+    double x;
+    double y;
 };
 
 struct rectangle_t
 {
   point_t pos;
-  float width;
-  float height;
+  double width;
+  double height;
 };
 
 #endif
---
+++ krylov.matvey/T1/shape.hpp
@@ -0,0 +1,17 @@
+#ifndef SHAPE_HPP
+#define SHAPE_HPP
+
+#include "base-types.hpp"
+
+class Shape
+{
+public:
+  virtual ~Shape() = default;
+  virtual double getArea() const = 0;
+  virtual rectangle_t getFrameRect() const = 0;
+  virtual void move(const point_t& point) = 0;
+  virtual void move(double dx, double dy) = 0;
+  virtual void scale(double factor) = 0;
+};
+
+#endif
