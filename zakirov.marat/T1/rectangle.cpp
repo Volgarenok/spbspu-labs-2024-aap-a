b96255493f5dc597534b617f9c0cfcfa85d9f02a
ZakirovMS <mz.08.08.06@gmail.com>
2024-12-14 12:52:38 +0300

    Rectangle Definition

--- zakirov.marat/T1/rectangle.cpp
+++ zakirov.marat/T1/rectangle.cpp
@@ -1,1 +1,16 @@
-
+#include "shape.hpp"
+namespace zakirov
+{
+  class Rectangle : public Shape
+  {
+    public:
+      virtual double getArea();
+      virtual rectangle_t getFrameRect();
+      virtual void move(point_t target);
+      virtual void move(double bias_x, double bias_y);
+      virtual void scale(double k);
+    private:
+      point_t bottom_left;
+      point_t top_right;
+  };
+}
