df7181f0568bcebd9ed88e3775922bdb680795e0
Zholobov Oleg <zholobov.of@edu.spbstu.ru>
2024-12-14 15:00:37 +0300

    implemented base-types.hpp and shape.hpp

--- zholobov.oleg/T1/shape.hpp
+++ zholobov.oleg/T1/shape.hpp
@@ -1,5 +1,14 @@
 #ifndef SHAPE_HPP
 #define SHAPE_HPP
 
+#include "base-types.hpp"
+
+class Shape {
+  virtual float getArea() = 0;
+  virtual rectangle_t getFrameRect() = 0;
+  virtual void move(point_t p) = 0;
+  virtual void move(float dx, float dy) = 0;
+  virtual void scale(float k) = 0;
+};
 
 #endif
