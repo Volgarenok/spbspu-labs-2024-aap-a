6d96c3470a50c1bdf356509aa321eb33e1bd0f3c
mipxe <mariamozegova8726@gmail.com>
2024-12-15 15:28:00 +0300

    create class rectangle

---
+++ mozhegova.maria/T1/rectangle.hpp
@@ -0,0 +1,23 @@
+#ifndef RECTANGLE_HPP
+#define RECTANGLE_HPP
+
+#include "shape.hpp"
+
+namespace mozhegova
+{
+  class Rectangle : public Shape
+  {
+  public:
+    Rectangle(point_t lowLef, point_t uppRig);
+    double getArea() const override;
+    rectangle_t getFrameRect() const override;
+    void move(point_t p) override;
+    void move(double dx, double dy) override;
+    void scale(double k) override;
+  private:
+    point_t lowerLeft;
+    point_t upperRight;
+  };
+}
+
+#endif
