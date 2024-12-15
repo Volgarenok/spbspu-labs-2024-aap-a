7db68bd5a037b467e8a3aeba169df1fbf3f9acc3
A1exMas1ov <ae1ex.mas1ov@gmail.com>
2024-12-15 16:35:29 +0300

    change parallelogram

--- maslov.aleksey/T1/inputShape.cpp
+++ maslov.aleksey/T1/inputShape.cpp
@@ -131,7 +131,7 @@ maslov::Parallelogram * makeParallelogram(std::istream & in)
 {
   double x1 = 0, y1 = 0, x2 = 0, y2 = 0 , x3 = 0, y3 = 0;
   in >> x1 >> y1 >> x2 >> y2 >> x3 >> y3;
-  if (y1 != y2 && y2 != y3)
+  if ((y1 != y2 && y2 != y3) || (y1 == y3))
   {
     throw std::invalid_argument("Parallelogram is not parallel");
   }
