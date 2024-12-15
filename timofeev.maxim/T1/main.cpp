f54e67db43b937ff39cf1811b2723a11ab57af63
PigoDog <maksimtmfv@gmail.com>
2024-12-15 14:54:48 +0300

    ) fix

--- timofeev.maxim/T1/main.cpp
+++ timofeev.maxim/T1/main.cpp
@@ -50,7 +50,7 @@ int main()
       {
         incorrect_values = true;
       }
-      if (!timofeev::is_point_in_triangle({ x1, y1 }, { x2, y2 }, { x3, y3 }, { x4, y4 })))
+      if (!timofeev::is_point_in_triangle({ x1, y1 }, { x2, y2 }, { x3, y3 }, { x4, y4 }))
       {
         incorrect_values = true;
       }
