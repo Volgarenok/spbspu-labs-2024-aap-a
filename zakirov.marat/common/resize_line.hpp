f50444f970c106abdca3106b0305df26f14df962
ZakirovMS <mz.08.08.06@gmail.com>
2024-12-16 00:37:17 +0300

    Fix of declaration in common

--- zakirov.marat/common/resize_line.hpp
+++ zakirov.marat/common/resize_line.hpp
@@ -5,6 +5,6 @@ namespace zakirov
 {
   char * expand_line(char const * regular_line, size_t real_lenght, size_t add_length);
   void get_segment(std::istream & in, char * line, size_t start, size_t finish);
-  char * get_line(std::istream & in, size_t step, char interrupt_el = '\0');
+  char * get_line(std::istream & in, size_t step);
 }
 #endif
