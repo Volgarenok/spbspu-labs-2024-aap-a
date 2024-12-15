a1469e8bc52878cdb55bdccd2ba7cbfe2130276e
sharksonk <sharksonk@gmail.com>
2024-12-15 16:50:29 +0300

    gOOOOOOOOOOOOl?

--- sharifullina.sofia/P4/shrSym_uniTwo.cpp
+++ sharifullina.sofia/P4/shrSym_uniTwo.cpp
@@ -89,18 +89,18 @@ char * sharifullina::uniTwo(const char * str1, const char * str2)
   char * res = new char[length1 + length2 + 1];
   char * ptr = res;
   size_t i = 0;
-   while (i < length1 || i < length2)
-   {
-     if (i < length2)
-     {
-       *ptr++ = str2[i];
-     }
-     if (i < length1)
-     {
-       *ptr++ = str1[i];
-     }
-     i++;
-   }
-   *ptr = '\0';
-   return res;
+  while (i < length1 || i < length2)
+  {
+    if (i < length2)
+    {
+      *ptr++ = str2[i];
+    }
+    if (i < length1)
+    {
+      *ptr++ = str1[i];
+    }
+    i++;
+  }
+  *ptr = '\0';
+  return res;
 }
