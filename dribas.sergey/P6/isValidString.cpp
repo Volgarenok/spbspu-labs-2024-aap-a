f3af9651410e45c795096a89242cedd5237c91a8
Sergey Dribas <serdribas@gmail.com>
2024-12-15 17:03:18 +0300

    add recursion

--- dribas.sergey/P6/isValidString.cpp
+++ dribas.sergey/P6/isValidString.cpp
@@ -3,7 +3,7 @@
 #include <cctype>
 namespace dribas
 {
-  const char* isChar(const char* string, char symbol);
+  const char* isChar(const char* string, const char* symbol);
   const char* isLetter(const char* string);
   const char* isNumber(const char* string);
   const char* isUnsignInt(const char* string);
@@ -11,17 +11,20 @@ namespace dribas
   const char* isTerm(const char* string);
   const char* isExpression(const char* string);
 }
-
+const char* dribas::isChar(const char* string, const char* symbol)
+{
+  if (*symbol == '0') {
+    return nullptr;
+  }
+  return *string == *symbol ? string + 1: isChar(string,symbol + 1);
+}
 const char* dribas::isLetter(const char* string)
 {
+  const char * abc = "abcdefxyz0";
   if (!string) {
     return string;
   }
-  if (*string == 'a' || *string == 'b' || *string == 'c' || *string == 'd' || *string == 'e'
-    || *string == 'f' || *string == 'x' || *string == 'y' || *string == 'z') {
-      return string + 1;
-  }
-  return nullptr;
+  return isChar(string, abc);
 }
 
 const char* dribas::isNumber(const char* string)
