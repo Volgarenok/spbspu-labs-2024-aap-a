bbd7d78993e793acb90cedba4a000b7d6a9010aa
alina <duhaninaalina3@gmail.com>
2024-12-15 03:39:18 +0300

    body

---
+++ duhanina.alina/T1/main.cpp
@@ -0,0 +1,68 @@
+#include <iostream>
+#include "makeShape.hpp"
+#include "printResult.hpp"
+
+int main()
+{
+  duhanina::Shape* shapes[10000];
+  size_t shapeCount = 0;
+  std::string shapeType;
+  while (std::cin >> shapeType)
+  {
+    try
+    {
+      if (shapeType == "RECTANGLE")
+      {
+        shapes[shapeCount] = duhanina::makeRectangle(std::cin);
+        shapeCount++;
+      }
+      else if (shapeType == "CIRCLE")
+      {
+        shapes[shapeCount] = duhanina::makeCircle(std::cin);
+        shapeCount++;
+      }
+      else if (shapeType == "ELLIPSE")
+      {
+        shapes[shapeCount] = duhanina::makeEllipse(std::cin);
+        shapeCount++;
+      }
+      else if (shapeType == "SCALE")
+      {
+        double x = 0;
+        double y = 0;
+        duhanina::point_t point;
+        std::cin >> x >> y;
+        point.x = x;
+        point.y = y;
+        double scalingFactor = 0;
+        std::cin >> scalingFactor;
+        if (scalingFactor <= 0)
+        {
+          std::cerr << "Incorrect scalingFactor\n";
+          duhanina::destroy(shapes, shapeCount);
+          return 1;
+        }
+        duhanina::processScaling(shapes, shapeCount, point, scalingFactor);
+        break;
+      }
+      else
+      {
+        continue;
+      }
+    }
+    catch (const std::invalid_argument&)
+    {
+      std::cerr << "Error in figure description\n";
+      duhanina::destroy(shapes, shapeCount);
+      continue;
+    }
+    if (std::cin.eof())
+    {
+      std::cerr << "No scale\n";
+      duhanina::destroy(shapes, shapeCount);
+      return 1;
+    }
+  }
+  duhanina::destroy(shapes, shapeCount);
+  return 0;
+}
---
+++ duhanina.alina/T1/makeShape.cpp
@@ -0,0 +1,40 @@
+#include "makeShape.h"
+#include <iostream>
+
+duhanina::Rectangle* duhanina::makeRectangle(std::istream& in)
+{
+  double ltX = 0;
+  double ltY = 0;
+  double rtX = 0;
+  double rtY = 0;
+  in >> ltX >> ltY >> rtX >> rtY;
+  if (ltX >= rtX || ltY >= rtY)
+  {
+    throw std::invalid_argument("Error in parameters");
+  }
+  Rectangle* rect = new Rectangle({ ltX, ltY }, { rtX, rtY });
+  return rect;
+}
+
+duhanina::Circle* duhanina::makeCircle(std::istream& in)
+{
+  double x = 0;
+  double y = 0;
+  in >> x >> y;
+  double radius = 0;
+  in >> radius;
+  Circle* circ = new Circle({ x, y }, radius);
+  return circ;
+}
+
+duhanina::Ellipse* duhanina::makeEllipse(std::istream& in)
+{
+  double x = 0;
+  double y = 0;
+  in >> x >> y;
+  double vertRadius = 0;
+  double horizRadius = 0;
+  in >> vertRadius >> horizRadius;
+  Ellipse* ell = new Ellipse({ x, y }, horizRadius, vertRadius);
+  return ell;
+}
