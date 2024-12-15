0c836339b49d9f6046b266d26d2f1cfd5df20388
brev1ch <flirktv@gmail.com>
2024-12-15 03:00:10 +0300

    Diamond

--- brevnov.nikita/T1/makeShape.cpp
+++ brevnov.nikita/T1/makeShape.cpp
@@ -32,32 +32,32 @@ brevnov::Diamond * brevnov::make_diamond(std::istream & in)
   }
   if (x1 == x2 && y1 == y3)
   {
-    result = new Diamond({x1, y1}, {x2, y2}, {x3, y3});
+    result = new Diamond({x1, y1}, {x3, y3}, {x2, y2});
     return result;
   }
   else if (x1 == x3 && y1 == y2)
   {
-    result = new Diamond({x1, y1}, {x3, y3}, {x2, y2});
+    result = new Diamond({x1, y1}, {x2, y2}, {x3, y3});
     return result;
   }
   else if (x2 == x1 && y2 == y3)
   {
-    result = new Diamond({x2, y2}, {x1, y1}, {x3, y3});
+    result = new Diamond({x2, y2}, {x3, y3}, {x1, y1});
     return result;
   }
   else if (x2 == x3 && y2 == y1)
   {
-    result = new Diamond({x2, y2}, {x3, y3}, {x1, y1});
+    result = new Diamond({x2, y2}, {x1, y1}, {x3, y3});
     return result;
   }
   else if (x3 == x1 && y3 == y2)
   {
-    result = new Diamond({x3, y3}, {x1, y1}, {x2, y2});
+    result = new Diamond({x3, y3}, {x2, y2}, {x1, y1});
     return result;
   }
   else if (x3 == x2 && y3 == y1)
   {
-    result = new Diamond({x3, y3}, {x2, y2}, {x1, y1});
+    result = new Diamond({x3, y3}, {x1, y1}, {x2, y2});
     return result;
   }
   else
