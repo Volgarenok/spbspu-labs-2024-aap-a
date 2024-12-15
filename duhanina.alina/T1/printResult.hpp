9421434f350a61f845625f50ebc6f6902505ebf4
alina <duhaninaalina3@gmail.com>
2024-12-15 04:03:02 +0300

    aaaa

--- duhanina.alina/T1/printResult.hpp
+++ duhanina.alina/T1/printResult.hpp
@@ -7,8 +7,8 @@
 
 namespace duhanina
 {
-  void printFiguresInfo(Shape shapes, size_t shapeCount);
-  void processScaling(Shape shapes, size_t shapeCount, point_t point, double scalingFactor);
+  void printFiguresInfo(Shape** shapes, size_t shapeCount);
+  void processScaling(Shape** shapes, size_t shapeCount, point_t point, double scalingFactor);
 }
 
 #endif
