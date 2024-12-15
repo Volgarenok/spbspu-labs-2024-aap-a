ea1bd3147404e5f2319dc7474cdc4d98955889b7
vladislavmaslovskiy <vladuisik826@gmail.com>
2024-12-15 21:37:30 +0300

    Add geometric function for Triangle

---
+++ maslovskiy.vladislav/T1/geomFun.cpp
@@ -0,0 +1,29 @@
+#include <cmath>
+#include "geom_fun.hpp"
+
+namespace maslovskiy
+{
+  bool isCorrectTriangle(point_t vertexA, point_t vertexB, point_t vertexC)
+  {
+    double sideAB = std::sqrt(std::pow((vertexB.x - vertexA.x), 2) + std::pow((vertexB.y - vertexA.y), 2));
+    double sideBC = std::sqrt(std::pow((vertexC.x - vertexB.x), 2) + std::pow((vertexC.y - vertexB.y), 2));
+    double sideCA = std::sqrt(std::pow((vertexA.x - vertexC.x), 2) + std::pow((vertexA.y - vertexC.y), 2));
+    return (sideAB + sideBC > sideCA) && (sideAB + sideCA > sideBC) && (sideBC + sideCA > sideAB);
+  }
+  double calculateTriangleArea(point_t vertexA, point_t vertexB, point_t vertexC)
+  {
+    double sideAB = std::sqrt(std::pow((vertexB.x - vertexA.x), 2) + std::pow((vertexB.y - vertexA.y), 2));
+    double sideBC = std::sqrt(std::pow((vertexC.x - vertexB.x), 2) + std::pow((vertexC.y - vertexB.y), 2));
+    double sideCA = std::sqrt(std::pow((vertexA.x - vertexC.x), 2) + std::pow((vertexA.y - vertexC.y), 2));
+    double semiPerimeter = (sideAB + sideBC + sideCA) / 2;
+    return std::sqrt(semiPerimeter * (semiPerimeter - sideAB) * (semiPerimeter - sideBC) * (semiPerimeter - sideCA));
+  }
+  bool isPointInTriangle(point_t vertexA, point_t vertexB, point_t vertexC, point_t point)
+  {
+    double totalArea = calculateTriangleArea(vertexA, vertexB, vertexC);
+    double area1 = calculateTriangleArea(point, vertexA, vertexB);
+    double area2 = calculateTriangleArea(point, vertexB, vertexC);
+    double area3 = calculateTriangleArea(point, vertexC, vertexA);
+    return totalArea == (area1 + area2 + area3);
+  }
+}
---
+++ maslovskiy.vladislav/T1/geomFun.hpp
@@ -0,0 +1,14 @@
+#ifndef GEOM_FUN_HPP
+#define GEOM_FUN_HPP
+#include "shape.hpp"
+#include <cctype>
+#include <cmath>
+
+namespace maslovskiy
+{
+  bool isCorrectTriangle(point_t vertexA, point_t vertexB, point_t vertexC);
+  double calculateTriangleArea(point_t vertexA, point_t vertexB, point_t vertexC);
+  bool isPointInTriangle(point_t vertexA, point_t vertexB, point_t vertexC, point_t point);
+}
+
+#endif
