16dc29a56dcc90685b1cc5dbe2273035d7fc263f
Zholobov Oleg <zholobov.of@edu.spbstu.ru>
2024-12-15 22:52:31 +0300

    fix print_shapes

--- zholobov.oleg/T1/shape.cpp
+++ zholobov.oleg/T1/shape.cpp
@@ -7,10 +7,16 @@
 
 void zholobov::print_shapes(Shape** shapes, size_t shape_cnt)
 {
+  float total_area = 0.0f;
   for (size_t i = 0; i < shape_cnt; ++i) {
-    std::cout << shapes[i]->getArea() << " ";
-    rectangle_t rect = shapes[i]->getFrameRect();
-    std::cout << rect.pos.x - rect.width / 2.0f << " " << rect.pos.y - rect.height / 2.0f << " ";
-    std::cout << rect.pos.x + rect.width / 2.0f << " " << rect.pos.y + rect.height / 2.0f << "\n";
+    total_area += shapes[i]->getArea();
   }
+
+  std::cout << total_area;
+  for (size_t i = 0; i < shape_cnt; ++i) {
+    rectangle_t rect = shapes[i]->getFrameRect();
+    std::cout << " " << rect.pos.x - rect.width / 2.0f << " " << rect.pos.y - rect.height / 2.0f;
+    std::cout << " " << rect.pos.x + rect.width / 2.0f << " " << rect.pos.y + rect.height / 2.0f;
+  }
+  std::cout << "\n";
 }
