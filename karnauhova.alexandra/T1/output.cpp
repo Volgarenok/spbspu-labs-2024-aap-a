227261ac998679bc8e2323121d8f5fe6e60c616c
“Karnaukhova <masj280906@gmail.com>
2024-12-16 05:06:24 +0300

    Scale -

--- karnauhova.alexandra/T1/output.cpp
+++ karnauhova.alexandra/T1/output.cpp
@@ -120,10 +120,11 @@ void karnauhova::output(std::ostream & out, double* all_points, double* polygon,
 
 void karnauhova::scale_point(Shape& object, point_t t, double k)
 {
-  point_t centr_rec = object.getFrameRect().pos;
-  double x = t.x - centr_rec.x;
-  double y = t.y - centr_rec.y;
+  point_t centr_rec1 = object.getFrameRect().pos;
   object.move(t);
+  point_t centr_rec2 = object.getFrameRect().pos;
+  double x = centr_rec2.x - centr_rec1.x;
+  double y = centr_rec2.y - centr_rec1.y;
   object.scale(k);
   object.move(-(x * k), -(y * k));
 }
