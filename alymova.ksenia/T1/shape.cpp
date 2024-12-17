1e584561dfabcdd9bec1f7c7d9a6ec4503c415da
Ksenia-rgb <ksenialymova@mail.ru>
2024-12-17 17:51:11 +0300

    fixing func for checking rectangle triangle

--- alymova.ksenia/T1/shape.cpp
+++ alymova.ksenia/T1/shape.cpp
@@ -1,6 +1,8 @@
 #include "shape.hpp"
 #include <stdexcept>
 #include <cmath>
+#include <limits>
+constexpr double inaccuracy = 0.000000000000001;
 void alymova::scale(Shape** shapes, point_t s, double ratio)
 {
   if (ratio <= 0)
@@ -24,13 +26,14 @@ double alymova::getVector(point_t p1, point_t p2)
 {
   return std::sqrt(std::pow(p1.x - p2.x, 2) + std::pow(p1.y - p2.y, 2));
 }
+
 bool alymova::getRectanglurTriangle(point_t& pos, point_t& p2, point_t& p3)
 {
-  if (std::pow(getVector(p2, p3), 2) + std::pow(getVector(pos, p3), 2) == std::pow(getVector(pos, p2), 2))
+  if (std::abs(std::pow(getVector(p2, p3), 2) + std::pow(getVector(pos, p3), 2) - std::pow(getVector(pos, p2), 2)) < inaccuracy)
   {
     return true;
   }
-  else if (std::pow(getVector(p2, p3), 2) + std::pow(getVector(pos, p2), 2) == std::pow(getVector(pos, p3), 2))
+  else if (std::abs(std::pow(getVector(p2, p3), 2) + std::pow(getVector(pos, p2), 2) - std::pow(getVector(pos, p3), 2)) < inaccuracy)
   {
     point_t temp(p2);
     p2 = p3;
