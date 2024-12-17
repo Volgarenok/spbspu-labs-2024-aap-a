9c724c6bd1b567c91d458065180eaf07bf6b95fc
Nekich06 <p.nikita.73526@gmail.com>
2024-12-17 13:09:20 +0300

    Small patch
    This solves problem of building program (forgot to estblish default
    for <<Interface>> Shape)

--- petrov.nikita/T1/shape.hpp
+++ petrov.nikita/T1/shape.hpp
@@ -5,8 +5,8 @@ namespace petrov
 {
   struct Shape
   {
-    virtual double getArea() const;
-    virtual petrov::rectangle_t getFrameRect() const;
+    virtual double getArea() const = 0;
+    virtual petrov::rectangle_t getFrameRect() const = 0;
     virtual void move(petrov::point_t concrete_point) = 0;
     virtual void move(double dx, double dy) = 0;
     virtual void scale(double k) = 0;
