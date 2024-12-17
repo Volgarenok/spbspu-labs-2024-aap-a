2db59b7cae7ca542dd280662b32e67fa8c5120a5
Sergey Dribas <serdribas@gmail.com>
2024-12-17 11:00:34 +0300

    cg fix for conferancie

--- dribas.sergey/T1/main.cpp
+++ dribas.sergey/T1/main.cpp
@@ -3,11 +3,12 @@
 #include "getShapeInfo.hpp"
 #include "shape.hpp"
 
-void clearStr(char** stack, size_t stackSize){
+void clearStr(char** stack, size_t stackSize)
+{
   for (size_t i = 0; *(stack+i) && i <stackSize; i++) {
     free(*(stack + i));
-    }
-     }
+  }
+}
 
 int main()
 {
