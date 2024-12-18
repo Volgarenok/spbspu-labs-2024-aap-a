cc3feb2da73628bcdb21e056eff39784e4c4a640
sharksonk <sharksonk@gmail.com>
2024-12-18 05:16:19 +0300

    main napisala

--- sharifullina.sofia/P6/main.cpp
+++ sharifullina.sofia/P6/main.cpp
@@ -4,4 +4,22 @@
 int main()
 {
   char * str = nullptr;
+  try
+  {
+    str = sharifullina::inputString(std::cin, '\n');
+  }
+  catch (const std::exception& e)
+  {
+    std::cerr << "Out of memory\n";
+    return 1;
+  }
+  if (str[0] == '\0')
+  {
+    delete[] str;
+    std::cerr << "Empty input\n";
+    return 1;
+  }
+  std::cout << std::boolalpha;
+  std::cout << sharifullina::IsExpression(str) << "\n";
+  delete[] str;
 }
