9959e9a66739808ad89f988755aad68afef9ac5d
Tuc0Sa1amanka <sparowj765@gmail.com>
2024-12-19 20:07:16 +0300

    Revert "fixed matrix iteration"
    This reverts commit c721711600e9a93a460cf2d998abc3cf6f8f6ac8.

--- lanovenko.ivan/T1/rectangle.cpp
+++ lanovenko.ivan/T1/rectangle.cpp
@@ -40,8 +40,8 @@ void lanovenko::Rectangle::move(double dx, double dy)
   point_t* points[] = { &leftlower, &rightupper };
   for (size_t i = 0; i < 2; i++)
   {
-    points[i]->x += dx;
-    points[i]->y += dy;
+    points[0]->x += dx;
+    points[0]->y += dy;
   }
 }
 
