dc7c683d1690cca5508cf610655dda266ecf904f
NastyKomar <komarova2.ae@edu.spbstu.ru>
2024-12-19 21:18:26 +0300

    fix12

--- komarova.anastasia/P4/change.h
+++ komarova.anastasia/P4/change.h
@@ -4,7 +4,7 @@
 namespace komarova
 {
   char * rep_sym(const char * str, char * str_rep);
-  int vowel(char sym);
+  int vowel(const char* str);
   char * vowels(const char * str, char * str_res);
 }
 #endif
