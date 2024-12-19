acda186bcff06a036de4df8e62a862e8dfb38dae
Maksim_Shramko <maxramko@gmail.com>
2024-12-19 19:32:51 +0300

    Adding output for results

---
+++ shramko.maksim/T1/outRes.cpp
@@ -0,0 +1,17 @@
+#include "outRes.hpp"
+#include "createShape.hpp"
+#include <iomanip>
+
+void shramko::outRes(std::ostream& out, Shape** shapes, size_t, count)
+{
+  out << std::fixed << std::setprecision(1) << getArea(shapes, count);
+
+  for (size_t i = 0; i < count; i++)
+  {
+    out << " " << shapes[i]->getRectangleFrame().pos.x - (shapes[i]->getRectangleFrame().width) / 2.0f;
+    out << " " << shapes[i]->getRectangleFrame().pos.y - (shapes[i]->getRectangleFrame().height) / 2.0f;
+    out << " " << shapes[i]->getRectangleFrame().pos.x + (shapes[i]->getRectangleFrame().width) / 2.0f;
+    out << " " << shapes[i]->getRectangleFrame().pos.y + (shapes[i]->getRectangleFrame().height) / 2.0f;
+  }
+  out << '\n';
+}
---
+++ shramko.maksim/T1/outRes.hpp
@@ -0,0 +1,12 @@
+#ifndef OUTRES_HPP
+#define OUTRES_HPP
+
+#include <iostream>
+#include "shape.hpp"
+
+namespace shramko
+{
+  void outRes(std::ostream& out, Shape** shapes, size_t count);
+}
+
+#endif
