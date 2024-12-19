a2aa24698377a518ddfb29d6b28facf2ee87a7d7
Tuc0Sa1amanka <sparowj765@gmail.com>
2024-12-19 16:16:54 +0300

    added anonymus namespace

--- lanovenko.ivan/P6/string_check.cpp
+++ lanovenko.ivan/P6/string_check.cpp
@@ -1,81 +1,74 @@
 #include "string_check.h"
 #include <cctype>
 
-namespace lanovenko
+namespace
 {
-  const char* is_mantissa(const char* str);
-  const char* is_symbol(const char* str, char c);
-  const char* is_sign(const char* str);
-  const char* is_digit(const char* str);
-  const char* is_unsigned_int(const char* str);
-  const char* is_E_digit(const char* str);
-}
+  const char* is_symbol(const char* str, char c)
+  {
+    if (!str)
+    {
+      return str;
+    }
+    return (*str == c) ? (str + 1) : nullptr;
+  }
 
-const char* lanovenko::is_symbol(const char* str, char c)
-{
-  if (!str)
+  const char* is_sign(const char* str)
   {
-    return str;
+    if (!str)
+    {
+      return str;
+    }
+    auto next = is_symbol(str, '+');
+    return next ? next : is_symbol(str, '-');
   }
-  return (*str == c) ? (str + 1) : nullptr;
-}
 
-const char* lanovenko::is_sign(const char* str)
-{
-  if (!str)
+  const char* is_digit(const char* str)
   {
-    return str;
+    if (!str)
+    {
+      return str;
+    }
+    return std::isdigit(*str) ? (str + 1) : nullptr;
   }
-  auto next = is_symbol(str, '+');
-  return next ? next : is_symbol(str, '-');
-}
 
-const char* lanovenko::is_digit(const char* str)
-{
-  if (!str)
+  const char* is_unsigned_int(const char* str)
   {
-    return str;
+    if (!str)
+    {
+      return str;
+    }
+    auto next = is_digit(str);
+    if (auto continues = is_unsigned_int(next))
+    {
+      return continues;
+    }
+    return next;
   }
-  return std::isdigit(*str) ? (str + 1) : nullptr;
-}
 
-const char* lanovenko::is_unsigned_int(const char* str)
-{
-  if (!str)
+  const char* is_E_digit(const char* str)
   {
-    return str;
+    if (!str)
+    {
+      return str;
+    }
+    auto next = is_symbol(str, 'E');
+    if (auto continues = is_sign(next))
+    {
+      return is_unsigned_int(continues);
+    }
+    return is_unsigned_int(next);
   }
-  auto next = is_digit(str);
-  if (auto continues = is_unsigned_int(next))
-  {
-    return continues;
-  }
-  return next;
-}
 
-const char* lanovenko::is_E_digit(const char* str)
-{
-  if (!str)
+  const char* is_mantissa(const char* str)
   {
-    return str;
+    if (!str)
+    {
+      return str;
+    }
+    auto next = is_unsigned_int(str);
+    next = is_symbol(next, '.');
+    return is_unsigned_int(next);
   }
-  auto next = is_symbol(str, 'E');
-  if (auto continues = is_sign(next))
-  {
-    return is_unsigned_int(continues);
-  }
-  return is_unsigned_int(next);
-}
-
-const char* lanovenko::is_mantissa(const char* str)
-{
-  if (!str)
-  {
-    return str;
-  }
-  auto next = is_unsigned_int(str);
-  next = is_symbol(next, '.');
-  return is_unsigned_int(next);
 }
 
 bool lanovenko::is_valid_float_string(const char* str)
