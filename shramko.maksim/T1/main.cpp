54c45ee873dad2eb6ff6ad1226d6f3778f9698b5
Maksim_Shramko <maxramko@gmail.com>
2024-12-19 19:43:39 +0300

    Finishing with main

--- shramko.maksim/T1/main.cpp
+++ shramko.maksim/T1/main.cpp
@@ -1,2 +1,18 @@
+#include <cstddef>
+#include <iostream>
+#include "shape.hpp"
+#include "createShape.hpp"
+
 int main()
-{}
+{
+  constexpr size_t size = 10000;
+  shramko::Shape* shapes[size] = {};
+  size_t count = 0;
+  count = shramko::createShape(std::cin, std::cerr, std::cout, shapes);
+
+  if (count == -1)
+  {
+    return 1;
+  }
+  shramko::clear(shapes, count);
+}
