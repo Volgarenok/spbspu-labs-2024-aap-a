93e2aaa59953d18e77646419fb8b89089eb6a7bd
evstyunichev.konstantin <korombodis.gmail.com>
2024-12-21 00:41:11 +0300

    fixes...

--- evstyunichev.konstantin/T1/main.cpp
+++ evstyunichev.konstantin/T1/main.cpp
@@ -9,6 +9,7 @@
 namespace evstyunichev
 {
   void skip_to_sign(std::istream &, char end = '\n');
+  void destroy_shapes(Shape **, size_t);
 }
 
 void evstyunichev::skip_to_sign(std::istream &in, char end)
@@ -22,6 +23,15 @@ void evstyunichev::skip_to_sign(std::istream &in, char end)
   return;
 }
 
+void evstyunichev::destroy_shapes(Shape **shapes, size_t n)
+{
+  for (size_t i = 0; i < n; i++)
+  {
+    delete shapes[i];
+  }
+  return;
+}
+
 int main()
 {
   evstyunichev::Shape *shapes[10000]{};
@@ -30,16 +40,16 @@ int main()
   double totalSquare = 0;
   std::cout.precision(1);
   std::cout << std::fixed;
+  bool input_errors_flag = 0;
   while (std::cin >> s)
   {
     non_empty++;
     if (s == "RECTANGLE")
     {
       shapes[done] = evstyunichev::make_rectangle(std::cin);
-      if (shapes[done])
+      if (!shapes[done])
       {
-        totalSquare += shapes[done]->getArea();
-        done++;
+        input_errors_flag = 1;
       }
     }
     else if (s == "SCALE")
@@ -49,7 +59,13 @@ int main()
       std::cin >> mid.x >> mid.y >> k;
       if (k < 0)
       {
+        evstyunichev::destroy_shapes(shapes, done);
         std::cerr << "negative k!";
+        return 1;
+      }
+      if (!done)
+      {
+        std::cerr << "Nothing to scale!\n";
         return 1;
       }
       std::cout << evstyunichev::roundToOneSign(totalSquare) << ' ';
