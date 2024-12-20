43c9c163d06f70f157774284773b6ebee3b98885
root <root@kryloff.localdomain>
2024-12-21 01:07:31 +0300

    Finally fixed output

--- krylov.matvey/T1/main.cpp
+++ krylov.matvey/T1/main.cpp
@@ -1,5 +1,4 @@
 #include <iostream>
-#include <sstream>
 #include <iomanip>
 #include <stdexcept>
 #include "rectangle.hpp"
@@ -28,131 +27,149 @@ void processShapes()
   size_t shapeCount = 0;
   bool scaleCommandProcessed = false;
   bool invalidDescriptions = false;
-  std::string line = "";
-  while (std::getline(std::cin, line))
+
+  while (true)
   {
-    if (line.empty())
-    {
-      continue;
-    }
-    std::istringstream stream(line);
     std::string shapeType = "";
-    stream >> shapeType;
+    std::cin >> shapeType;
+    if (std::cin.eof())
+    {
+      for (size_t i = 0; i < shapeCount; ++i)
+      {
+        delete shapes[i];
+      }
+      throw std::invalid_argument("Missing SCALE before EOF\n");
+    }
     try
     {
       if (shapeType == "RECTANGLE")
       {
         double x1, y1, x2, y2;
-        stream >> x1 >> y1 >> x2 >> y2;
+        if (!(std::cin >> x1 >> y1 >> x2 >> y2))
+        {
+          throw std::invalid_argument("Invalid RECTANGLE parameters");
+        }
         shapes[shapeCount++] = new krylov::Rectangle({x1, y1}, {x2, y2});
       }
       else if (shapeType == "RING")
       {
         double x, y, outerRadius, innerRadius;
-        stream >> x >> y >> outerRadius >> innerRadius;
+        if (!(std::cin >> x >> y >> outerRadius >> innerRadius))
+        {
+          throw std::invalid_argument("Invalid RING parameters");
+        }
         shapes[shapeCount++] = new krylov::Ring({x, y}, outerRadius, innerRadius);
       }
       else if (shapeType == "TRIANGLE")
       {
-        double x1, y1, x2, y2, x3, y3;
-        stream >> x1 >> y1 >> x2 >> y2 >> x3 >> y3;
+        double x1 = 0.0, y1 = 0.0;
+        double x2 = 0.0, y2 = 0.0;
+        double x3 = 0.0, y3 = 0.0;
+        if (!(std::cin >> x1 >> y1 >> x2 >> y2 >> x3 >> y3))
+        {
+          throw std::invalid_argument("Invalid TRIANGLE parameters");
+        }
         shapes[shapeCount++] = new krylov::Triangle({x1, y1}, {x2, y2}, {x3, y3});
       }
       else if (shapeType == "SCALE")
       {
-        double x, y, factor;
-        stream >> x >> y >> factor;
+        if (shapeCount == 0)
+        {
+          throw std::logic_error("Nothing to scale");
+        }
+        double x = 0.0, y = 0.0, factor = 0.0;
+        if (!(std::cin >> x >> y >> factor))
+        {
+          throw std::invalid_argument("Invalid SCALE parameters");
+        }
         if (factor <= 0)
         {
-          throw std::invalid_argument("Invalid scale command");
+          throw std::invalid_argument("Scale factor must be positive");
         }
+
         double totalAreaBefore = 0.0;
         for (size_t i = 0; i < shapeCount; ++i)
         {
           totalAreaBefore += shapes[i]->getArea();
         }
+
         std::cout << std::fixed << std::setprecision(1) << totalAreaBefore << ' ';
         for (size_t i = 0; i < shapeCount; ++i)
         {
           krylov::rectangle_t frame = shapes[i]->getFrameRect();
-          std::cout << std::fixed << std::setprecision(1) << frame.pos.x - frame.width / 2 << ' ';
-          std::cout << std::fixed << std::setprecision(1) << frame.pos.y - frame.height / 2 << ' ';
-          std::cout << std::fixed << std::setprecision(1) << frame.pos.x + frame.width / 2 << ' ';
-          if (i == shapeCount - 1)
+          std::cout << frame.pos.x - frame.width / 2 << ' ';
+          std::cout << frame.pos.y - frame.height / 2 << ' ';
+          std::cout << frame.pos.x + frame.width / 2 << ' ';
+          std::cout << frame.pos.y + frame.height / 2;
+          if (i < shapeCount - 1)
           {
-            std::cout << std::fixed << std::setprecision(1) << frame.pos.y + frame.height / 2;
-          }
-          else
-          {
-            std::cout << std::fixed << std::setprecision(1) << frame.pos.y + frame.height / 2 << ' ';
+            std::cout << ' ';
           }
         }
-        std::cout << "\n";
+        std::cout << '\n';
+
+        krylov::point_t scaleCenter = {x, y};
         for (size_t i = 0; i < shapeCount; ++i)
         {
-          krylov::point_t shapeCenter = shapes[i]->getFrameRect().pos;
-          shapes[i]->move(x, y);
-          krylov::point_t newShapeCenter = shapes[i]->getFrameRect().pos;
-          double dx = newShapeCenter.x - shapeCenter.x;
-          double dy = newShapeCenter.y - shapeCenter.y;
+          krylov::rectangle_t frameBefore = shapes[i]->getFrameRect();
+          shapes[i]->move(scaleCenter);
           shapes[i]->scale(factor);
-          shapes[i]->move(-dx, -dy);
+          krylov::rectangle_t frameAfter = shapes[i]->getFrameRect();
+          shapes[i]->move((frameBefore.pos.x - frameAfter.pos.x) * factor, (frameBefore.pos.y - frameAfter.pos.y) * factor);
         }
+
         double totalAreaAfter = 0.0;
         for (size_t i = 0; i < shapeCount; ++i)
         {
           totalAreaAfter += shapes[i]->getArea();
         }
-        std::cout << std::fixed << std::setprecision(1) << totalAreaAfter << ' ';
+
+        std::cout << totalAreaAfter << ' ';
         for (size_t i = 0; i < shapeCount; ++i)
         {
           krylov::rectangle_t frame = shapes[i]->getFrameRect();
-          std::cout << std::fixed << std::setprecision(1) << frame.pos.x - frame.width / 2 << ' ';
-          std::cout << std::fixed << std::setprecision(1) << frame.pos.y - frame.height / 2 << ' ';
-          std::cout << std::fixed << std::setprecision(1) << frame.pos.x + frame.width / 2 << ' ';
-          if (i == shapeCount - 1)
+          std::cout << frame.pos.x - frame.width / 2 << ' ';
+          std::cout << frame.pos.y - frame.height / 2 << ' ';
+          std::cout << frame.pos.x + frame.width / 2 << ' ';
+          std::cout << frame.pos.y + frame.height / 2;
+          if (i < shapeCount - 1)
           {
-            std::cout << std::fixed << std::setprecision(1) << frame.pos.y + frame.height / 2;
-          }
-          else
-          {
-            std::cout << std::fixed << std::setprecision(1) << frame.pos.y + frame.height / 2 << ' ';
+            std::cout << ' ';
           }
         }
-        std::cout << "\n";
+        std::cout << '\n';
+
         scaleCommandProcessed = true;
         break;
       }
       else
       {
-        invalidDescriptions = true;
+        std::string skippedLines = "";
+        std::getline(std::cin, skippedLines);
       }
     }
     catch (...)
     {
       invalidDescriptions = true;
+      std::string skippedLines = "";
+      std::getline(std::cin, skippedLines);
     }
   }
+
   if (!scaleCommandProcessed)
   {
-    if (std::cin.eof())
-    {
-      for (size_t i = 0; i < shapeCount; ++i)
-      {
-      delete shapes[i];
-      }
-      throw std::invalid_argument("Missing SCALE before EOF\n");
-    }
     for (size_t i = 0; i < shapeCount; ++i)
     {
       delete shapes[i];
     }
     throw std::invalid_argument("Missing or invalid SCALE command");
   }
+
   if (invalidDescriptions)
   {
     std::cerr << "Warning: Some shapes had invalid descriptions\n";
   }
+
   for (size_t i = 0; i < shapeCount; ++i)
   {
     delete shapes[i];
