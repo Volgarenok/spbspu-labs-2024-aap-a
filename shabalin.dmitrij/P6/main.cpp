7bb7bc3c8b1165e23217a71d846bf77d8b1046c9
dmitei <dima9990860349@icloud.com>
2024-12-20 20:14:28 +0300

    Dsd

---
+++ shabalin.dmitrij/P6/main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <stdexcept>
+#include "checkString.hpp"
+#include <inputOfString.hpp>
+
+int main()
+{
+  const char end = '\n';
+  char *c = shabalin::inputOfString(std::cin, end);
+  if (c == nullptr)
+  {
+    std::cerr << "Error with input\n";
+    return 1;
+  }
+  if (c[0] == '\0')
+  {
+    std::cerr << "Empty string\n";
+    free(c);
+    return 1;
+  }
+  std::cout << std::boolalpha << shabalin::func(t) << '\n';
+  free(c);
+}
