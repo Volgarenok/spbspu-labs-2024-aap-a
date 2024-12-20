de3f45c82161567667dfa683019603db1f579739
dmitei <dima9990860349@icloud.com>
2024-12-20 21:03:46 +0300

    mua

--- shabalin.dmitrij/P6/input.cpp
+++ shabalin.dmitrij/P6/input.cpp
@@ -3,7 +3,8 @@
 char *shabalin::inputOfString(std::istream &input, char end)
 {
   size_t sizeOfString = 123;
-  size_t index = 0;
+  size_t index = 1;
+
   char *initialString = reinterpret_cast< char* >(malloc(sizeOfString));
 
   if (!initialString)
@@ -11,24 +12,30 @@ char *shabalin::inputOfString(std::istream &input, char end)
     return initialString;
   }
 
-  input >> std::noskipws;
+  std::noskipws(input);
   char someCharacter = 0;
+  input >> someCharacter;
   if (someCharacter == end)
   {
     return initialString;
   }
-
+  initialString[0] = someCharacter;
   while ((input >> someCharacter) && (someCharacter != end))
   {
-    if (index == sizeOfString - 1)
+    if (index >= sizeOfString)
     {
       sizeOfString *= 2;
       char *intermediateString = reinterpret_cast< char* >(malloc(sizeOfString));
-      if (!intermediateString)
+      if (intermediateString == nullptr)
       {
         free(initialString);
-        throw std::bad_alloc();
+        return intermediateString;
       }
+      for (size_t j = 0; j < (sizeOfString / 2); ++j)
+      {
+        intermediateString[j] = initialString[j];
+      }
+      free(initialString);
       initialString = intermediateString;
     }
     initialString[index++] = someCharacter;
