95fae1db4819bc563134763a72b79389cfe8a55c
Robotophile <ksyunya.iv@gmail.com>
2024-12-20 03:21:19 +0300

    looks terrible

--- ivanova.ksenia/P4/frequentSymbols.cpp
+++ ivanova.ksenia/P4/frequentSymbols.cpp
@@ -3,9 +3,17 @@
 
 char ivanova::frequentSymbol(const char* string)
 {
+  char dest[3] = {};
+  twoFrequentSymbol(string, dest);
+  return dest[0];
+}
+
+void ivanova::twoFrequentSymbol(const char* string, char* dest)
+{
   size_t count = 0;
-  size_t max = 0;
-  char maxSym = '\0';
+  size_t max1 = 0, max2 = 0;
+  char maxSym1 = '\0';
+  char maxSym2 = '\0';
   for (size_t i = 0; string[i] != '\0'; i++)
   {
     for (size_t j = 0; string[j] != '\0'; j++)
@@ -15,29 +23,20 @@ char ivanova::frequentSymbol(const char* string)
         count++;
       }
     }
-    if (max < count)
+    if (max1 < count)
     {
-      max = count;
-      maxSym = string[i];
-    }
-  }
-  return maxSym;
-}
+      max2 = max1;
+      maxSym2 = maxSym1;
 
-void ivanova::twoFrequentSymbol(const char* string, char* dest)
-{
-  char maxSym1 = frequentSymbol(string);
-  char strWithoutSym1[300] = {0};
-  size_t k = 0;
-  for (size_t i = 0; string[i] != '\0'; i++)
-  {
-    if (string[i] != maxSym1)
+      max1 = count;
+      maxSym2 = string[i];
+    }
+    else if (count > max2 && string[i] != maxSym1)
     {
-      strWithoutSym1[k++] = string[i];
+      max2 = count;
+      maxSym2 = string[i];
     }
   }
-  strWithoutSym1[k] = '\0';
-  char maxSym2 = frequentSymbol(strWithoutSym1);
   dest[0] = maxSym1;
   dest[1] = maxSym2;
   dest[2] = '\0';
