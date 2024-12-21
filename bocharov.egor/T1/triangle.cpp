31b3d4a060a6502a0d5b736275ad065af8b90603
LuckyBichar <enbocharov2006@gmail.com>
2024-12-21 20:02:21 +0300

    Triangle

---
+++ bocharov.egor/T1/triangle.cpp
@@ -0,0 +1,76 @@
+#include "triangle.hpp"
+#include <algorithm>
+#include <cmath>
+#include <stdexcept>
+
+bocharov::Triangle::Triangle(point_t a, point_t b, point_t c)
+{
+  if ((a.x * (b.y -c.y) + b.x * (c.y - a.y) + c.x * (a.y - b.y)) == 0) 
+  {
+    throw std::invalid_argument("error arguments for triangle\n");
+  }
+  a_ = a;
+  b_ = b;
+  c_ = c;
+}
+
+double bocharov::Triangle::getArea() const
+{
+    return std::abs(((b_.x - a_.x) * (c_.y - a_.y) - (c_.x - a_.x) * (b_.y - a_.y))) / 2.0;
+}
+
+bocharov::rectangle_t bocharov::Triangle::getFrameRect() const
+{
+  double maxX = std::max(a_.x, std::max(b_.x, c_.x));
+  double maxY = std::max(a_.y, std::max(b_.y, c_.y));
+  double minX = std::min(a_.x, std::min(b_.x, c_.x));
+  double minY = std::min(a_.y, std::min(b_.y, c_.y));
+  bocharov::rectangle_t result;
+  result.height = maxY - minY;
+  result.width = maxX - minX;
+  result.pos.x = minX + (result.width / 2.0);
+  result.pos.y = minY + (result.height / 2.0);
+  return result;
+}
+
+void bocharov::Triangle::move(point_t centerP)
+{
+  bocharov::point_t pos;
+  pos.x = std::abs(a_.x + b_.x + c_.x) / 3.0;
+  pos.y = std::abs(a_.y + b_.y + c_.y) / 3.0;
+  double moveX = centerP.x - pos.x;
+  double moveY = centerP.y - pos.y;
+  a_.x += moveX;
+  a_.y += moveY;
+  b_.x += moveX;
+  b_.y += moveY;
+  c_.x += moveX;
+  c_.y += moveY;
+}
+
+void bocharov::Triangle::move(double x, double y)
+{
+  a_.x += x;
+  a_.y += y;
+  b_.x += x;
+  b_.y += y;
+  c_.x += x;
+  c_.y += y;
+}
+
+void bocharov::Triangle::scale(double ratio)
+{
+  if (ratio<= 0)
+  {
+    throw std::invalid_argument("under zero ratio\n");
+  }
+  point_t pos;
+  pos.x = std::abs(a_.x + b_.x + c_.x) / 3.0;
+  pos.y = std::abs(a_.y + b_.y + c_.y) / 3.0;
+  a_.x = (a_.x - pos.x) * ratio + pos.x;
+  a_.y = (a_.y - pos.y) * ratio + pos.y;
+  b_.x = (b_.x - pos.x) * ratio + pos.x;
+  b_.y = (b_.y - pos.y) * ratio + pos.y;
+  c_.x = (c_.x - pos.x) * ratio + pos.x;
+  c_.y = (c_.y - pos.y) * ratio + pos.y;
+}
