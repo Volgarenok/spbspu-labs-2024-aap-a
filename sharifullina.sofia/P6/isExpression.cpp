25cb8489643b88bf062699838f4e17747a4f5b93
sharksonk <sharksonk@gmail.com>
2024-12-21 09:01:02 +0300

    fix namespace

--- sharifullina.sofia/P6/isExpression.cpp
+++ sharifullina.sofia/P6/isExpression.cpp
@@ -1,7 +1,7 @@
 #include "isExpression.hpp"
 #include <iostream>
 
-namespace sharifullina
+namespace
 {
   const char * isSymbol(const char * str, const char c);
   const char * hasExpression(const char * str);
@@ -11,121 +11,121 @@ namespace sharifullina
   const char * hasMultiplier(const char * str);
   const char * hasTerm(const char * str);
   const char * hasLetter(const char * str);
-}
 
-const char * sharifullina::isSymbol(const char * str, const char c)
-{
-  if (!str)
+  const char * isSymbol(const char * str, const char c)
   {
-    return str;
+    if (!str)
+    {
+      return str;
+    }
+    return (*str == c) ? (str + 1) : nullptr;
   }
-  return (*str == c) ? (str + 1) : nullptr;
+
+  const char * hasExpression(const char * str)
+  {
+    if (!str)
+    {
+      return str;
+    }
+    auto next = hasTerm(str);
+    if (!next)
+    {
+      return nullptr;
+    }
+    while (*next == '+' || *next == '-')
+    {
+      next = isSymbol(next, *next);
+      next = hasTerm(next);
+      if (!next)
+      {
+        return nullptr;
+      }
+    }
+    return next;
+  }
+
+  const char * hasTerm(const char * str)
+  {
+    if (!str)
+    {
+      return str;
+    }
+    auto next = hasMultiplier(str);
+    if (!next)
+    {
+      return nullptr;
+    }
+    while (*next == '*' || *next == '/')
+    {
+      next = isSymbol(next, *next);
+      next = hasMultiplier(next);
+      if (!next)
+      {
+        return nullptr;
+      }
+    }
+    return next;
+  }
+
+  const char * hasMultiplier(const char * str)
+  {
+    if (!str)
+    {
+      return str;
+    }
+    auto next = hasUnsignedInt(str);
+    if (next)
+    {
+      return next;
+    }
+    next = hasIdentifier(str);
+    if (next)
+    {
+      return next;
+    }
+    return nullptr;
+  }
+
+  const char * hasUnsignedInt(const char * str)
+  {
+    if (!str)
+    {
+      return str;
+    }
+    auto next = hasNumber(str);
+    if (auto continues = hasUnsignedInt(next))
+    {
+      return continues;
+    }
+    return next;
+  }
+
+  const char * hasIdentifier(const char * str)
+  {
+    return hasLetter(str);
+  }
+
+  const char * hasNumber(const char * str)
+  {
+    if (!str)
+    {
+      return str;
+    }
+    return (*str >= '0' && *str <= '9') ? (str + 1) : nullptr;
+  }
+
+  const char * hasLetter(const char * str)
+  {
+    if (!str)
+    {
+      return str;
+    }
+    return (*str >= 'a' && *str <= 'e') ? (str + 1) : nullptr;
+  }
 }
 
 bool sharifullina::isExpression(const char * str)
 {
   auto next = hasExpression(str);
   return next && (*next == '\0');
-}
-
-const char * sharifullina::hasExpression(const char * str)
-{
-  if (!str)
-  {
-    return str;
-  }
-  auto next = hasTerm(str);
-  if (!next)
-  {
-    return nullptr;
-  }
-  while (*next == '+' || *next == '-')
-  {
-    next = isSymbol(next, *next);
-    next = hasTerm(next);
-    if (!next)
-    {
-      return nullptr;
-    }
-  }
-  return next;
-}
-
-const char * sharifullina::hasTerm(const char * str)
-{
-  if (!str)
-  {
-    return str;
-  }
-  auto next = hasMultiplier(str);
-  if (!next)
-  {
-    return nullptr;
-  }
-  while (*next == '*' || *next == '/')
-  {
-    next = isSymbol(next, *next);
-    next = hasMultiplier(next);
-    if (!next)
-    {
-      return nullptr;
-    }
-  }
-  return next;
-}
-
-const char * sharifullina::hasMultiplier(const char * str)
-{
-  if (!str)
-  {
-    return str;
-  }
-  auto next = hasUnsignedInt(str);
-  if (next)
-  {
-    return next;
-  }
-  next = hasIdentifier(str);
-  if (next)
-  {
-    return next;
-  }
-  return nullptr;
-}
-
-const char * sharifullina::hasUnsignedInt(const char * str)
-{
-  if (!str)
-  {
-    return str;
-  }
-  auto next = hasNumber(str);
-  if (auto continues = hasUnsignedInt(next))
-  {
-    return continues;
-  }
-  return next;
-}
-
-const char * sharifullina::hasIdentifier(const char * str)
-{
-  return hasLetter(str);
-}
-
-const char * sharifullina::hasNumber(const char * str)
-{
-  if (!str)
-  {
-    return str;
-  }
-  return (*str >= '0' && *str <= '9') ? (str + 1) : nullptr;
-}
-
-const char * sharifullina::hasLetter(const char * str)
-{
-  if (!str)
-  {
-    return str;
-  }
-  return (*str >= 'a' && *str <= 'e') ? (str + 1) : nullptr;
 }
