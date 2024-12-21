c17366371bd00483daa18b7a1af2bf87652d74e4
LuckyBichar <enbocharov2006@gmail.com>
2024-12-21 20:04:13 +0300

    outputRes

---
+++ bocharov.egor/T1/outputRes.cpp
@@ -0,0 +1,16 @@
+#include "outputRes.hpp"
+#include "getShapeInfo.hpp"
+#include <iomanip>
+
+void bocharov::outputRes(std::ostream & output, Shape ** myShape, size_t shapeSize)
+{
+  output << std::fixed << std::setprecision(1) << getAllArea(myShape, shapeSize);
+  for (size_t i = 0; i < shapeSize; i++)
+  {
+    output << " " << myShape[i]->getFrameRect().pos.x - (myShape[i]->getFrameRect().width) / 2.0f;
+    output << " " << myShape[i]->getFrameRect().pos.y - (myShape[i]->getFrameRect().height) / 2.0f;
+    output << " " << myShape[i]->getFrameRect().pos.x + (myShape[i]->getFrameRect().width) / 2.0f;
+    output << " " << myShape[i]->getFrameRect().pos.y + (myShape[i]->getFrameRect().height) / 2.0f;
+  }
+  output << '\n';
+}
---
+++ bocharov.egor/T1/outputRes.hpp
@@ -0,0 +1,9 @@
+#ifndef OUTPUTRES_HPP
+#define OUTPUTRES_HPP
+#include <iostream>
+#include "shape.hpp"
+namespace bocharov
+{
+  void outputRes(std::ostream & output, Shape ** myShape, size_t shapeSize);
+}
+#endif
