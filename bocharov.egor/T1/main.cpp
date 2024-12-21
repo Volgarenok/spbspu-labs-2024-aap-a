79ea468350ce1f8a0bac84554eba2e7c3e4ab9b7
LuckyBichar <enbocharov2006@gmail.com>
2024-12-21 20:03:39 +0300

    main.cpp

---
+++ bocharov.egor/T1/main.cpp
@@ -0,0 +1,17 @@
+#include <cstddef>
+#include <iostream>
+#include "getShapeInfo.hpp"
+#include "shape.hpp"
+
+int main()
+{
+  constexpr size_t shapesSize = 10000;
+  bocharov::Shape * myShapes[shapesSize] = {};
+  int shapeCount = 0;
+  shapeCount = bocharov::getShapeInfo(std::cin, std::cerr, std::cout, myShapes);
+  if (shapeCount == -1) {
+    return 1;
+  }
+  bocharov::clear(myShapes, shapeCount);
+  return 0;
+}
