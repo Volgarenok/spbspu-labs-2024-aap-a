bd780a9cc6d914db2ab21836eee7a6af5a01ffcc
Asafow Andrew <neforscist@gmail.com>
2024-12-22 16:18:25 +0300

    common

---
+++ asafov.andrew/common/inputString.hpp
@@ -0,0 +1,7 @@
+#ifndef INPUT_STRING_HPP
+#define INPUT_STRING_HPP
+namespace asafov
+{
+  char* inputString();
+}
+#endif
---
+++ asafov.andrew/common/resize.hpp
@@ -0,0 +1,8 @@
+#ifndef RESIZE_HPP
+#define RESIZE_HPP
+#include <cstddef>
+namespace asafov
+{
+  char* resize(char* string, size_t lenght);
+}
+#endif
