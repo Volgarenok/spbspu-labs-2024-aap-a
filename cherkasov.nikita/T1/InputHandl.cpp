f852dd58a0a0a408b6c06646936509b5ee383025
cherkasoov <cherkasov5509@gmail.com>
2024-12-23 02:15:21 +0300

    ..

--- cherkasov.nikita/T1/InputHandl.cpp
+++ cherkasov.nikita/T1/InputHandl.cpp
@@ -30,7 +30,7 @@ cherkasov::Shape* parseShapeInput(const std::string& inputCommand, std::istream&
   {
     double x1, y1, x2, y2, x3, y3;
     input >> x1 >> y1 >> x2 >> y2 >> x3 >> y3;
-    if (input.fail.() || (x1 == x3 && y2 == y3) || (x2 == x3 && y1 == y3))
+    if (input.fail() || (x1 == x3 && y2 == y3) || (x2 == x3 && y1 == y3))
     {
       invalidInput = true;
     }
