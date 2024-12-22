3ade0897e65b06c772be2f960ea58e31d4bf000b
cherkasoov <cherkasov5509@gmail.com>
2024-12-22 22:04:50 +0300

    .....

--- cherkasov.nikita/T1/outputFrameCoordinates.cpp
+++ cherkasov.nikita/T1/outputFrameCoordinates.cpp
@@ -2,9 +2,14 @@
 #include <iostream>
 void cherkasov::outputFrameCoordinates(Shape* shapes[], size_t shapeCount)
 {
-  for (size_t i = 0; i < shapeCount; ++i)
+  for (size_t i = 0; i < shapeCount; i++)
   {
-    rectangle_t frame = shapes[i]->getFrameRect();
-    std::cout << " " << frame.width << " " << frame.height << " " << frame.pos.x << " " << frame.pos.y << "\n";
+    rectangle_t rectangle = shapes[i]->getFrameRect();
+    double x1 = rectangle.pos.x - rectangle.width / 2.0;
+    double y1 = rectangle.pos.y - rectangle.height / 2.0;
+    double x2 = rectangle.pos.x + rectangle.width / 2.0;
+    double y2 = rectangle.pos.y + rectangle.height / 2.0;
+    std::cout << " " << x1 << " " << y1 << " " << x2 << " " << y2;
   }
+  std::cout << "\n";
 }
