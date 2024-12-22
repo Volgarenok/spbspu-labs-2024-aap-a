8f05741b67f1161f749275759df6597c96cf41fa
cherkasoov <cherkasov5509@gmail.com>
2024-12-23 01:28:14 +0300

    Revert "......"
    This reverts commit ad27e3a6bb49bf8f976188f939065e3aa0ba960e.

--- cherkasov.nikita/T1/parallelogram.cpp
+++ cherkasov.nikita/T1/parallelogram.cpp
@@ -18,11 +18,9 @@ namespace cherkasov
   }
   double Parallelogram::getArea() const
   {
-    double vector1X = vertex2_.x - vertex1_.x;
-    double vector1Y = vertex2_.y - vertex1_.y;
-    double vector2X = vertex3_.x - vertex1_.x;
-    double vector2Y = vertex3_.y - vertex1_.y;
-    return std::abs(vector1X * vector2Y - vector1Y * vector2X);
+    double base = std::hypot(vertex2_.x - vertex1_.x, vertex2_.y - vertex1_.y);
+    double height = std::abs(vertex3_.y - vertex1_.y);
+    return base * height;
   }
   rectangle_t Parallelogram::getFrameRect() const
   {
