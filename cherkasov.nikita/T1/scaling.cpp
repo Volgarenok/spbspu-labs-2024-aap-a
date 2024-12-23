d58517f3c323a3f009512e733112922b17a904e1
cherkasoov <cherkasov5509@gmail.com>
2024-12-23 04:07:56 +0300

    ..

--- cherkasov.nikita/T1/scaling.cpp
+++ cherkasov.nikita/T1/scaling.cpp
@@ -5,10 +5,11 @@ void cherkasov::scaling(Shape* shapes[], size_t shapeCount, point_t p, double sc
   for (size_t i = 0; i < shapeCount; i++)
   {
     point_t startPoint = shapes[i]->getFrameRect().pos;
-    shapes[i]->scale(scalingFactor);
+    shapes[i]->move(p);
     point_t vector;
     vector.x = (startPoint.x - p.x) * (1 - scalingFactor);
     vector.y = (startPoint.y - p.y) * (1 - scalingFactor);
+    shapes[i]->scale(scalingFactor);
     shapes[i]->move(-vector.x, -vector.y);
   }
 }
