1788586cf1d883bbbb787c89156c723815618029
sharksonk <sharksonk@gmail.com>
2024-12-23 06:15:09 +0300

    add rectangle

---
+++ sharifullina.sofia/T1/rectangle.hpp
@@ -0,0 +1,21 @@
+#ifndef RECTANGLE_HPP
+#define RECTANGLE_HPP
+
+#include "shape.hpp"
+namespace sharifullina
+{
+  class Rectangle : public Shape {
+  public:
+    Rectangle(point_t lowLeft, point_t uppRight);
+    double getArea() const override;
+    rectangle_t getFrameRect() const override;
+    void move(const point_t p) override;
+    void move(double dx, double dy) override;
+    void scale(double k) override;
+  private:
+    point_t lowLeft_ = {0.0, 0.0};
+    point_t uppRight_ = {0.0, 0.0};
+  };
+}
+
+#endif
