ebad66e40c6d034ced92a9a7d726c7b8525eb027
aldyn-d <aldyndirti@gmail.com>
2024-12-23 19:53:13 +0300

    trying with static storage duration

--- dirti.aldyn/P4/main.cpp
+++ dirti.aldyn/P4/main.cpp
@@ -1,4 +1,20 @@
+#include <iostream>
+#include <ios>
+#include <cstddef>
 int main()
 {
+  size_t max = 10;
+  char str[max + 1] = {};
+  size_t size = 0;
+  char c = '\0';
+  std::noskipws(std::cin);
   
+  while ((std::cin >> c) && (c != '\n') && (size < max))
+  {
+    str[size++] = c;
+  }
+  std::skipws(std::cin);
+  
+  std::cout << size << "\n";
+  std::cout << str << "\n";
 }
