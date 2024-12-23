29c650cd00dd8effe05b214819d9f6974016f464
svesh3000 <russkij2006@mail.ru>
2024-12-24 00:26:26 +0300

    Add header for square

---
+++ sveshnikov.michail/T1/square.hpp
@@ -0,0 +1,23 @@
+#ifndef SQUARE_HPP
+#define SQUARE_HPP
+#include "shape.hpp"
+
+namespace sveshnikov
+{
+  class Square : public Shape
+  {
+  private:
+    point_t left_low_;
+    double side_;
+
+  public:
+    Square(point_t left_low_, double side_);
+    double getArea() const override;
+    rectangle_t getFrameRect() const override;
+    void move(point_t p) override;
+    void move(double dx, double dy) override;
+    void scale(double k) override;
+  };
+}
+
+#endif
