e264000376fa30c525a8b1d10ae3ca92c9c37ebb
cherkasoov <cherkasov5509@gmail.com>
2024-12-24 08:30:28 +0300

    ..

--- cherkasov.nikita/T1/procesShape.cpp
+++ cherkasov.nikita/T1/procesShape.cpp
@@ -5,7 +5,7 @@
 
 double cherkasov::calculArea(Shape** shapes, size_t shapeCount)
 {
-  double totalArea = 0.0;
+  double totalArea = 0;
   for (size_t i = 0; i < shapeCount; ++i)
   {
     totalArea += shapes[i]->getArea();
