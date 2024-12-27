7043ac2635b729d5455f308fc6bb5915c731d765
Гаврилова Полина <polina.gavrilova2022@mail.ru>
2024-12-27 16:21:38 +0300

    Realizing ellipse .hpp and .cpp

---
+++ gavrilova.polina/T1/ellipse.cpp
@@ -0,0 +1,40 @@
+#include "ellipse.hpp"
+#include <iostream>
+#include "base-types.hpp"
+gavrilova::Ellipse::Ellipse(point_t center, double radiusX, double radiusY) :
+  center_(center),
+  radiusX_(radiusX),
+  radiusY_(radiusY) 
+{
+  if (radiusX <= 0 || radiusY <= 0) {
+    throw std::logic_error("Invalid radiuses for ellipse");
+  }
+}
+
+double gavrilova::Ellipse::getArea() const {
+  return 3.14159 * radiusX_ * radiusY_;
+}
+
+gavrilova::rectangle_t gavrilova::Ellipse::getFrameRect() const {
+  rectangle_t frameRect;
+  frameRect.height = 2 * radiusY_;
+  frameRect.width = 2 * radiusX_;
+  frameRect.pos.x = center_.x;
+  frameRect.pos.y = center_.y;
+  return frameRect;
+}
+
+void gavrilova::Ellipse::move(point_t p) {
+  center_.x = p.x;
+  center_.y = p.y;
+}
+
+void gavrilova::Ellipse::move(double difX, double difY) {
+  center_.x += difX;
+  center_.y += difY;
+}
+
+void gavrilova::Ellipse::scale(double k) {
+  radiusX_ *= k;
+  radiusY_ *= k;
+}
---
+++ gavrilova.polina/T1/ellipse.hpp
@@ -0,0 +1,20 @@
+#ifndef ELLIPSE_HPP
+#define ELLIPSE_HPP
+#include "shape.hpp"
+namespace gavrilova {
+  class Ellipse : public Shape {
+  public:
+    Ellipse(point_t center, double radiusX, double radiusY);
+    double getArea() const override;
+    rectangle_t getFrameRect() const override;
+    void move(point_t p) override;
+    void move(double difX, double difY) override;
+    void scale(double k) override;
+
+  private:
+    point_t center_;
+    double radiusX_;
+    double radiusY_;
+  };
+}
+#endif
