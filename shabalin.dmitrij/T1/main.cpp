4e4e50773ca645538388232ba7a4d84af3cbb51e
dmitei <dima9990860349@icloud.com>
2024-12-30 21:18:40 +0300

    :))

--- shabalin.dmitrij/T1/main.cpp
+++ shabalin.dmitrij/T1/main.cpp
@@ -16,12 +16,12 @@ int main()
   using namespace shabalin;
   size_t maxShapesCount = 1000;
   size_t currentShapesCount = 0;
-  shabalin::Shape **myShapes = new shabalin::Shape *[maxShapesCount]{nullptr};
+  Shape **myShapes = new Shape *[maxShapesCount]{ nullptr };
 
   bool errorsFlagShapes = false;
   bool errorsFlagScale = false;
 
-  shabalin::point_t scalePoint = {0.0, 0.0};
+  point_t scalePoint = {0.0, 0.0};
   double scaleK = 0;
   const char *targetWordScale = "SCALE";
 
@@ -29,14 +29,16 @@ int main()
 
   bool scaleEntered = false;
   const char end = '\n';
+
   while (!scaleEntered)
   {
     if (!std::cin.good())
     {
-      shabalin::freeMem(myShapes, currentShapesCount);
-      std::cerr << "Somthing went wrong with input, might be EOF!\n";
+      freeMem(myShapes, currentShapesCount);
+      std::cerr << "Something went wrong with input, might be EOF!\n";
       return 1;
     }
+
     try
     {
       string = shabalin::inputOfString(std::cin, end);
@@ -47,18 +49,18 @@ int main()
     }
     catch (const std::exception &e)
     {
-      shabalin::freeMem(myShapes, currentShapesCount);
+      freeMem(myShapes, currentShapesCount);
       std::cerr << "Error: " << e.what() << "\n";
       return 1;
     }
 
-    const char * foundScale = std::strstr(string, targetWordScale);
+    const char *foundScale = std::strstr(string, targetWordScale);
     if (foundScale != nullptr)
     {
-      shabalin::inputScale(string, scalePoint, scaleK);
+      inputScale(string, scalePoint, scaleK);
       try
       {
-        shabalin::scaleFunction(myShapes, currentShapesCount, scalePoint, scaleK, std::cout);
+        scaleFunction(myShapes, currentShapesCount, scalePoint, scaleK, std::cout);
       }
       catch (const std::logic_error &e)
       {
@@ -72,7 +74,7 @@ int main()
       {
         if (string[0] != '\0')
         {
-          shabalin::defineAndCreateShape(myShapes, currentShapesCount, string);
+          defineAndCreateShape(myShapes, currentShapesCount, string);
         }
       }
       catch (const std::logic_error &e)
@@ -80,7 +82,7 @@ int main()
         errorsFlagShapes = true;
       }
 
-      if (myShapes[currentShapesCount] != nullptr)
+      if (currentShapesCount < maxShapesCount && myShapes[currentShapesCount] != nullptr)
       {
         currentShapesCount++;
       }
@@ -95,16 +97,16 @@ int main()
   if (errorsFlagScale)
   {
     std::cerr << "Something went wrong with scale process, might be incorrect k of scale!\n";
-    shabalin::freeMem(myShapes, currentShapesCount);
+    freeMem(myShapes, currentShapesCount);
     return 2;
   }
 
   if (currentShapesCount == 0)
   {
     std::cerr << "\nNothing to scale!\n";
-    shabalin::freeMem(myShapes, currentShapesCount);
+    freeMem(myShapes, currentShapesCount);
     return 2;
   }
-  shabalin::freeMem(myShapes, currentShapesCount);
+  freeMem(myShapes, currentShapesCount);
   return 0;
 }
