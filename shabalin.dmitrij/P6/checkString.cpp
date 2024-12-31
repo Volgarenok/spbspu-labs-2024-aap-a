335d901a22cd48588c0c847d6c744b01fd8bfc12
dmitei <dima9990860349@icloud.com>
2024-12-31 18:13:21 +0300

    Dsd

--- shabalin.dmitrij/P6/checkString.cpp
+++ shabalin.dmitrij/P6/checkString.cpp
@@ -1,110 +1,113 @@
 #include "checkString.hpp"
 
-const char *shabalin::isSign(const char *str)
+namespace shabalin
 {
-  if (!str)
+  const char *isSign(const char *str)
   {
-    return nullptr;
-  }
-  return ((*str == '+') || (*str == '-')) ? (str + 1) : nullptr;
-}
-
-bool shabalin::is_digit(char c, char cur)
-{
-  if (cur == '9' + 1)
-  {
-    return false;
-  }
-  if (cur == c)
-  {
-    return true;
-  }
-  return is_digit(c, cur + 1);
-}
-
-const char *shabalin::isUnsignedInt(const char *str)
-{
-  if (!str)
-  {
-    return nullptr;
-  }
-  while (*str)
-  {
-    if (!is_digit(*str, '0'))
+    if (!str)
     {
       return nullptr;
     }
-    str++;
+    return ((*str == '+') || (*str == '-')) ? (str + 1) : nullptr;
   }
-  return str;
-}
 
-const char *shabalin::isSymbol(const char *str, char ch)
-{
-  if (!str)
+  bool is_digit(char c, char cur)
   {
-    return nullptr;
+    if (cur == '9' + 1)
+    {
+      return false;
+    }
+    if (cur == c)
+    {
+      return true;
+    }
+    return is_digit(c, cur + 1);
   }
-  return (*str == ch) ? (str + 1) : nullptr;
-}
 
-const char *shabalin::isOrder(const char *str)
-{
-  if (!str)
+  const char *isUnsignedInt(const char *str)
   {
-    return nullptr;
+    if (!str)
+    {
+      return nullptr;
+    }
+    while (*str)
+    {
+      if (!is_digit(*str, '0'))
+      {
+        return nullptr;
+      }
+      str++;
+    }
+    return str;
   }
-  auto next = isSymbol(str, 'E');
-  if (!next)
-  {
-    return nullptr;
-  }
-  auto next2 = isSign(next);
-  if (next2 != nullptr)
-  {
-    next = next2;
-  }
-  next = isUnsignedInt(next);
-  return next;
-}
 
-const char *shabalin::isMantissa(const char *str)
-{
-  if (!str)
+  const char *isSymbol(const char *str, char ch)
   {
-    return nullptr;
+    if (!str)
+    {
+      return nullptr;
+    }
+    return (*str == ch) ? (str + 1) : nullptr;
   }
-  const char *next = isSymbol(str, '.');
-  if (next)
-  {
-    return isUnsignedInt(next);
-  }
-  next = isUnsignedInt(str);
-  if (const char *next2 = isSymbol(next, '.'))
-  {
-    return isUnsignedInt(next2);
-  }
-  return next;
-}
 
-const char *shabalin::hasRealNum(const char *str)
-{
-  if (!str)
+  const char *isOrder(const char *str)
   {
-    return nullptr;
+    if (!str)
+    {
+      return nullptr;
+    }
+    auto next = isSymbol(str, 'E');
+    if (!next)
+    {
+      return nullptr;
+    }
+    auto next2 = isSign(next);
+    if (next2 != nullptr)
+    {
+      next = next2;
+    }
+    next = isUnsignedInt(next);
+    return next;
   }
-  auto next = isSign(str);
-  auto next2 = isMantissa(next);
-  auto next3 = isOrder(next2);
-  return next3;
-}
 
-bool shabalin::isRealNumber(const char *str)
-{
-  if (!str)
+  const char *isMantissa(const char *str)
   {
-    return false;
+    if (!str)
+    {
+      return nullptr;
+    }
+    const char *next = isSymbol(str, '.');
+    if (next)
+    {
+      return isUnsignedInt(next);
+    }
+    next = isUnsignedInt(str);
+    if (const char *next2 = isSymbol(next, '.'))
+    {
+      return isUnsignedInt(next2);
+    }
+    return next;
   }
-  const char *next = hasRealNum(str);
-  return next && (*next == '\0');
+
+  const char *hasRealNum(const char *str)
+  {
+    if (!str)
+    {
+      return nullptr;
+    }
+    auto next = isSign(str);
+    auto next2 = isMantissa(next);
+    auto next3 = isOrder(next2);
+    return next3;
+  }
+
+  bool isRealNumber(const char *str)
+  {
+    if (!str)
+    {
+      return false;
+    }
+    const char *next = hasRealNum(str);
+    return next && (*next == '\0');
+  }
 }
--- shabalin.dmitrij/P6/checkString.hpp
+++ shabalin.dmitrij/P6/checkString.hpp
@@ -1,17 +1,8 @@
 #ifndef CHECKSTRING_HPP
 #define CHECKSTRING_HPP
 
-class shabalin
+namespace shabalin
 {
-public:
-  static const char *isSign(const char *str);
-  static bool is_digit(char c, char cur);
-  static const char *isUnsignedInt(const char *str);
-  static const char *isSymbol(const char *str, char ch);
-  static const char *isOrder(const char *str);
-  static const char *isMantissa(const char *str);
-  static const char *hasRealNum(const char *str);
-  static bool isRealNumber(const char *str);
-};
-
+  bool isRealNumber(const char *str);
+}
 #endif
