6299393e6e3757108c2695d524f4b0080664c518
ZakirovMS <mz.08.08.06@gmail.com>
2025-01-03 15:21:16 +0300

    Destructor for polygon

--- zakirov.marat/T1/polygon.hpp
+++ zakirov.marat/T1/polygon.hpp
@@ -10,6 +10,7 @@ namespace zakirov
   {
   public:
     Polygon(size_t size, point_t * points);
+    ~Polygon();
     double getArea() const override;
     rectangle_t getFrameRect() const override;
     void move(point_t target) override;
