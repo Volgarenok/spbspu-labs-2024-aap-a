849fe302d75c8db10cf43cb24c22686495d076bc
Asafow Andrew <neforscist@gmail.com>
2025-01-04 08:29:57 +0300

    fix no.33

--- asafov.andrew/T1/rectangle.cpp
+++ asafov.andrew/T1/rectangle.cpp
@@ -13,10 +13,10 @@ double asafov::Rectangle::getArea() const
 rectangle_t asafov::Rectangle::getFrameRect() const
 {
   rectangle_t frect;
-  frect.height = abs(lb.y - rt.y);
-  frect.width = abs(lb.x - rt.x);
-  frect.pos.x = abs(lb.x + rt.x) / 2;
-  frect.pos.y = abs(lb.y + rt.y) / 2;
+  frect.height = pow((lb.y - rt.y)*(lb.y - rt.y), 0.5);
+  frect.width = pow((lb.x - rt.x)*(lb.x - rt.x), 0.5);
+  frect.pos.x = pow((lb.x + rt.x)*(lb.x + rt.x), 0.5) / 2;
+  frect.pos.y = pow((lb.y + rt.y)*(lb.y + rt.y), 0.5) / 2;
   return frect;
 }
 
