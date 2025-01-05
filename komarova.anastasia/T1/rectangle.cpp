a0344e440f17d9d79c6ff74a0e9192486606ed06
NastyKomar <komarova2.ae@edu.spbstu.ru>
2025-01-05 20:59:31 +0300

    rect

---
+++ komarova.anastasia/T1/rectangle.cpp
@@ -0,0 +1,48 @@
+#include "rectangle.hpp"
+#include "base_types.hpp"
+
+komarova::Rectangle::Rectangle(point_t low_left, point_t up_right):
+  low_left_(low_left),
+  up_right_(up_right)
+{}
+
+double komarova::Rectangle::getArea() const
+{
+  double width_ = up_right_.x - low_left_.x;
+  double height_ = up_right_.y - low_left_.y;
+  return (width_ * height_);
+}
+
+komarova::rectangle_t komarova::Rectangle::getFrameRect() const
+{
+  double width_ = up_right_.x - low_left_.x;
+  double height_ = up_right_.y - low_left_.y;
+  point_t pos_ = {(low_left_.x + width_ / 2.0), (low_left_.y + height_ / 2.0)};
+  return {width_, height_, pos_};
+}
+
+void komarova::Rectangle::move(point_t point)
+{
+  double dx = point.x - getFrameRect().pos.x;
+  double dy = point.y - getFrameRect().pos.y;
+  low_left_ = {low_left_.x + dx, low_left_.y + dy};
+  up_right_ = {up_right_.x + dx, up_right_.y + dy};
+}
+void komarova::Rectangle::move(double dx, double dy)
+{
+  low_left_ = {low_left_.x + dx, low_left_.y + dy};
+  up_right_ = {up_right_.x + dx, up_right_.y + dy};
+}
+
+void komarova::Rectangle::scale(double coef)
+{
+  if (coef <= 0.0)
+  {
+    throw std::logic_error("incorrect coefficient");
+  }
+  point_t center = getFrameRect().pos;
+  low_left_.x = center.x + (low_left_.x - center.x) * coef;
+  low_left_.y = center.y + (low_left_.y - center.y) * coef;
+  up_right_.x = center.x + (up_right_.x - center.x) * coef;
+  up_right_.y = center.y + (up_right_.y - center.y) * coef;
+}
---
+++ komarova.anastasia/T1/rectangle.hpp
@@ -0,0 +1,23 @@
+#ifndef RECTANGLE_HPP
+#define RECTANGLE_HPP
+#include "base_types.hpp"
+#include "shape.hpp"
+
+namespace komarova
+{
+  struct Rectangle: public Shape
+  {
+  public:
+    Rectangle(point_t low_left, point_t up_right);
+    double getArea() const override;
+    rectangle_t getFrameRect() const override;
+    void move(double dx, double dy);
+    void move(point_t point) override;
+    void scale(double coef) override;
+  private:
+    point_t low_left_;
+    point_t up_right_;
+  }
+}
+
+#endif
