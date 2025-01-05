01b80ea582714c315ed4d7999e4689d5b847599e
Z-1337 <un228der@gmail.com>
2025-01-05 14:55:37 +0300

    idk why but there was copy of rectangle in concave.cpp instead of just concave code

--- kushekbaev.danil/T1/concave.cpp
+++ kushekbaev.danil/T1/concave.cpp
@@ -1,56 +1,88 @@
-#include "rectangle.hpp"
+#include "concave.hpp"
+#include <cmath>
 #include <stdexcept>
+#include <cctype>
 #include "base-types.hpp"
 
 namespace kushekbaev
 {
-  Rectangle::Rectangle(point_t lowerLeft,
-    point_t upperRight):
-    lowerLeft_(lowerLeft),
-    upperRight_(upperRight)
+  Concave::Concave(point_t first,
+    point_t second,
+    point_t third,
+    point_t final):
+    first_(first),
+    second_(second),
+    third_(third),
+    final_(final)
   {}
 
-  double Rectangle::getArea() const
+  double Concave::getArea() const
   {
-    return (upperRight_.x - lowerLeft_.x) * (upperRight_.y - lowerLeft_.y);
+    double standart = getAreaOfTriangle(first_, second_, third_);
+    double concavity = getAreaOfTriangle(second_, third_, final_);
+    return standart - concavity;
   }
 
-  rectangle_t Rectangle::getFrameRect() const
+  rectangle_t Concave::getFrameRect() const
   {
-    double middleForX = lowerLeft_.x + (upperRight_.x - lowerLeft_.x) / 2;
-    double middleForY = lowerLeft_.y + (upperRight_.y - lowerLeft_.y) / 2;
-    return { upperRight_.x - lowerLeft_.x, upperRight_.y - lowerLeft_.y, { middleForX, middleForY } };
+    point_t lowerLeft { 0, 0 };
+    lowerLeft.x = std::min(std::min(std::min(first_.x, second_.x), third_.x), final_.x);
+    lowerLeft.y = std::min(std::min(std::min(first_.y, second_.y), third_.y), final_.y);
+
+    point_t upperRight { 0, 0 };
+
+    upperRight.x = std::max(std::max(std::max(first_.x, second_.x), third_.x), final_.x);
+    upperRight.y = std::max(std::max(std::max(first_.y, second_.y), third_.y), final_.y);
+
+    rectangle_t frame_rect;
+    frame_rect.height = upperRight.y - lowerLeft.y;
+    frame_rect.width = upperRight.x - lowerLeft.x;
+    frame_rect.pos.x = lowerLeft.x + frame_rect.width / 2;
+    frame_rect.pos.y = lowerLeft.y + frame_rect.height / 2;
+    return frame_rect;
   }
 
-  void Rectangle::move(point_t Z)
+  void Concave::move(point_t Z)
   {
-    point_t middle = this->getFrameRect().pos;
+    point_t middle = getFrameRect().pos;
     double moveX = Z.x - middle.x;
     double moveY = Z.y - middle.y;
-    lowerLeft_.x += moveX;
-    lowerLeft_.y += moveY;
-    upperRight_.x += moveX;
-    upperRight_.y += moveY;
+    first_.x += moveX;
+    first_.y += moveY;
+    second_.x += moveX;
+    second_.y += moveY;
+    third_.x += moveX;
+    third_.y += moveY;
+    final_.x += moveX;
+    final_.y += moveY;
   }
 
-  void Rectangle::move(double dx, double dy)
+  void Concave::move(double dx, double dy)
   {
-    lowerLeft_.x += dx;
-    lowerLeft_.y += dy;
-    upperRight_.x += dx;
-    upperRight_.y += dy;
+    first_.x += dx;
+    first_.y += dy;
+    second_.x += dx;
+    second_.y += dy;
+    third_.x += dx;
+    third_.y += dy;
+    final_.x += dx;
+    final_.y += dy;
   }
 
-  void Rectangle::scale(double V)
+  void Concave::scale(double V)
   {
     if (V <= 0)
     {
       throw std::out_of_range("Scale coefficient should be greater than zero\n");
     }
-    point_t middle = this->getFrameRect().pos;
-    lowerLeft_.x = middle.x + (lowerLeft_.x - middle.x) * V;
-    lowerLeft_.y = middle.y + (lowerLeft_.y - middle.y) * V;
-    upperRight_.x = middle.x + (upperRight_.x - middle.x) * V;
-    upperRight_.y = middle.y + (upperRight_.y - middle.y) * V;
+    point_t middle = getFrameRect().pos;
+    first_.x = middle.x + (first_.x - middle.x) * V;
+    first_.y = middle.y + (first_.y - middle.y) * V;
+    second_.x = middle.x + (second_.x - middle.x) * V;
+    second_.y = middle.y + (second_.y - middle.y) * V;
+    third_.x = middle.x + (third_.x - middle.x) * V;
+    third_.y = middle.y + (third_.y - middle.y) * V;
+    final_.x = middle.x + (final_.x - middle.x) * V;
+    final_.y = middle.y + (final_.y - middle.y) * V;
   }
 }
