6d5e00c0692c4bdaee8e61da1978d740ecee4087
Nekich06 <p.nikita.73526@gmail.com>
2025-01-06 20:23:49 +0300

    Tried to improve concave

--- petrov.nikita/T1/concave.cpp
+++ petrov.nikita/T1/concave.cpp
@@ -49,7 +49,6 @@ double petrov::Concave::getArea() const
   double p = (a + b + c) / 2;
   double area_without_cave = sqrt(p * (p - a) * (p - b) * (p - c));
   a = sqrt(pow((p2_.x - p4_.x), 2) + pow((p2_.y - p4_.y), 2));
-  b = b;
   c = sqrt(pow((p4_.x - p3_.x), 2) + pow((p4_.y - p3_.y), 2));
   p = (a + b + c) / 2;
   double area_of_cave = sqrt(p * (p - a) * (p - b) * (p - c));
@@ -63,15 +62,15 @@ petrov::rectangle_t petrov::Concave::getFrameRect() const
   double xmin = std::min(std::min(p1_.x, p2_.x), p3_.x);
   double ymax = std::max(std::max(p1_.y, p2_.y), p3_.y);
   double ymin = std::min(std::min(p1_.y, p2_.y), p3_.y);
-  frame_rect.width = abs(xmax - xmin);
-  frame_rect.height = abs(ymax - ymin);
+  frame_rect.width = xmax - xmin;
+  frame_rect.height = ymax - ymin;
   frame_rect.pos = { ((2 * xmin + xmax - xmin) / 2.0), ((2 * ymin + ymax - ymin) / 2.0) };
   return frame_rect;
 }
 
 void petrov::Concave::move(point_t concrete_point)
 {
- rectangle_t frame_rect = getFrameRect();
+  rectangle_t frame_rect = getFrameRect();
   double dx = concrete_point.x - frame_rect.pos.x;
   double dy = concrete_point.y - frame_rect.pos.y;
   p1_.x += dx;
@@ -100,15 +99,15 @@ void petrov::Concave::scale(double k)
 {
   {
     rectangle_t frame_rect = getFrameRect();
+    frame_rect.pos.x = p4_.x;
+    frame_rect.pos.y = p4_.y;
     double dx = (frame_rect.width / 2) * std::abs(k - 1);
     double dy = (frame_rect.height / 2) * std::abs(k - 1);
     p1_.x > frame_rect.pos.x ? p1_.x += dx : (p1_.x < frame_rect.pos.x ? p1_.x -= dx : p1_.x += 0);
     p2_.x > frame_rect.pos.x ? p2_.x += dx : (p2_.x < frame_rect.pos.x ? p2_.x -= dx : p2_.x += 0);
     p3_.x > frame_rect.pos.x ? p3_.x += dx : (p3_.x < frame_rect.pos.x ? p3_.x -= dx : p3_.x += 0);
-    p4_.x > frame_rect.pos.x ? p4_.x += dx : (p4_.x < frame_rect.pos.x ? p4_.x -= dx : p4_.x += 0);
     p1_.y > frame_rect.pos.y ? p1_.y += dy : (p1_.y < frame_rect.pos.y ? p1_.y -= dy : p1_.y += 0);
     p2_.y > frame_rect.pos.y ? p2_.y += dy : (p2_.y < frame_rect.pos.y ? p2_.y -= dy : p2_.y += 0);
     p3_.y > frame_rect.pos.y ? p3_.y += dy : (p3_.y < frame_rect.pos.y ? p3_.y -= dy : p3_.y += 0);
-    p4_.y > frame_rect.pos.y ? p4_.y += dy : (p4_.y < frame_rect.pos.y ? p4_.y -= dy : p4_.y += 0);
   }
 }
