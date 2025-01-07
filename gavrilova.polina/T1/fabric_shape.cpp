f18859404b5f3788dc10a85c732c0fe9bc23defe
Гаврилова Полина <polina.gavrilova2022@mail.ru>
2025-01-07 13:04:45 +0300

    Delete[] line in other place

--- gavrilova.polina/T1/fabric_shape.cpp
+++ gavrilova.polina/T1/fabric_shape.cpp
@@ -33,14 +33,14 @@ namespace {
   gavrilova::Rectangle* make_rectangle(char* line, size_t& nSpaces, size_t& nError) {
     const int nVertRect = 2;
     if (nSpaces != 2* nVertRect) {
-      delete[] line;
+      //delete[] line;
       ++nError;
       return nullptr;
     }
 
     gavrilova::point_t verteces[nVertRect] = {};
     read_verteces_from_line(verteces, nVertRect);
-    delete[] line;
+    //delete[] line;
 
     gavrilova::Rectangle* rect = nullptr;
     try {
@@ -54,14 +54,14 @@ namespace {
   gavrilova::Triangle* make_triangle(char* line, size_t& nSpaces, size_t& nError) {
     const int nVertTriang = 3;
     if (nSpaces != 2* nVertTriang) {
-      delete[] line;
+      //delete[] line;
       ++nError;
       return nullptr;
     }
 
     gavrilova::point_t verteces[nVertTriang] = {};
     read_verteces_from_line(verteces, nVertTriang);
-    delete[] line;
+    //delete[] line;
 
     gavrilova::Triangle* triang = nullptr;
     try {
@@ -76,7 +76,7 @@ namespace {
     const int minNVertPolygon = 3;
     if (nSpaces < 2* minNVertPolygon || nSpaces % 2 != 0) {
       ++nError;
-      delete[] line;
+      //delete[] line;
       return nullptr;
     }
     size_t nPoints = nSpaces / 2;
@@ -85,11 +85,11 @@ namespace {
       verteces = new gavrilova::point_t[nPoints]{};
     } catch (const std::bad_alloc&) {
       ++nError;
-      delete[] line;
+      //delete[] line;
       return nullptr;
     }
     read_verteces_from_line(verteces, nPoints);
-    delete[] line;
+    //delete[] line;
     if (hasSameVerteces(verteces, nPoints)) {
       delete[] verteces;
       ++nError;
@@ -109,7 +109,7 @@ namespace {
   gavrilova::Ellipse* make_ellipse(char* line, size_t& nSpaces, size_t& nError) {
     if (nSpaces != 4) {
         ++nError;
-        delete[] line;
+        //delete[] line;
         return nullptr;
     }
 
@@ -120,7 +120,7 @@ namespace {
 
     if (!cXStr || !cYStr || !rXStr || !rYStr) {
         ++nError;
-        delete[] line;
+        //delete[] line;
         return nullptr;
     }
 
@@ -131,11 +131,11 @@ namespace {
 
     try {
         gavrilova::Ellipse* ellipse = new gavrilova::Ellipse({x, y}, radiusX, radiusY);
-        delete[] line;
+        //delete[] line;
         return ellipse;
     }catch (const std::exception&) {
         ++nError;
-        delete[] line;
+        //delete[] line;
         return nullptr;
     }
   }
@@ -172,9 +172,8 @@ gavrilova::Shape* gavrilova::make_shape(std::istream& in, gavrilova::point_t& ce
     center.y = std::atof(cYStr);
     char* koefStr = std::strtok(nullptr, " ");
     koef = std::atof(koefStr);
-    delete[] line;
-  } else {
-    delete[] line;
+    //delete[] line;
   }
+  delete[] line;
   return new_shape;
 }
