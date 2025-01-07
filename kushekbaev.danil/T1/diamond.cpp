1d9ce6a5ebdb43e433bcb8db58aa4ea50041fdb8
Z-1337 <un228der@gmail.com>
2025-01-07 08:24:35 +0300

    trying to fix diamond through parallelogram implementation

--- kushekbaev.danil/T1/diamond.cpp
+++ kushekbaev.danil/T1/diamond.cpp
@@ -5,64 +5,38 @@
 
 namespace kushekbaev
 {
-  Diamond::Diamond(point_t middle,
-    point_t diffX,
-  point_t diffY):
-  Parallelogram(
-    {middle.x - diffX.x, middle.y},
-    {middle.x + diffX.x, middle.y},
-    {middle.x, middle.y - diffY.y}
-  ),
-  middle_(middle),
-  diffX_(diffX),
-  diffY_(diffY)
+  Diamond::Diamond(point_t middle, point_t diffX, point_t diffY):
+    parallelogram_(
+      { middle.x - diffX.x, middle.y - diffX.y },
+      { middle.x + diffX.x, middle.y + diffX.y },
+      { middle.x + diffY.x, middle.y + diffY.y }
+    )
   {
 
   }
 
   double Diamond::getArea() const
   {
-    return std::fabs((middle_.x - diffX_.x) * (middle_.y - diffY_.y) * 2);
+    return parallelogram_.getArea();
   }
 
   rectangle_t Diamond::getFrameRect() const
   {
-    return { std::fabs((middle_.x - diffX_.x)) * 2, std::fabs((middle_.y - diffY_.y) * 2), middle_ };
+    return parallelogram_.getFrameRect();
   }
 
   void Diamond::move(point_t Z)
   {
-    point_t middle = getFrameRect().pos;
-    double dx = Z.x - middle.x;
-    double dy = Z.y - middle.y;
-
-    std::array<point_t*, 3> points = { &middle_, &diffX_, &diffY_, };
-
-    for (point_t* point : points)
-    {
-      point->x += dx;
-      point->y += dy;
-    }
+    return parallelogram_.move(Z);
   }
 
   void Diamond::move(double dx, double dy)
   {
-    std::array<point_t*, 3> points = { &middle_, &diffX_, &diffY_, };
-
-    for (point_t* point : points)
-    {
-      point->x += dx;
-      point->y += dy;
-    }
+    return parallelogram_.move(dx, dy);
   }
 
   void Diamond::scale(double V)
   {
-    if (V <= 0)
-    {
-      throw std::out_of_range("Scale coefficient should be greater than zero\n");
-    }
-    diffY_.y = middle_.y + (diffY_.y - middle_.y) * V;
-    diffX_.x = middle_.x + (diffX_.x - middle_.x) * V;
+    return parallelogram_.scale(V);
   }
 }
