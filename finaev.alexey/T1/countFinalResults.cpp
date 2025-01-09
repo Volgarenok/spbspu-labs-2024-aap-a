b564b33827cbc3b9caf9023f75fc3e1f9cb61394
AlexeyFinaev02 <finaev0212212@mail.ru>
2025-01-10 00:39:13 +0300

    fix space

--- finaev.alexey/T1/countFinalResults.cpp
+++ finaev.alexey/T1/countFinalResults.cpp
@@ -13,13 +13,19 @@ double finaev::getSumArea(Shape** shapes, const size_t size)
 }
 void finaev::printFrameRect(std::ostream& out, Shape** shapes, const size_t size)
 {
-  for (size_t i = 0; i < size; i++)
+  rectangle_t a = shapes[0]->getFrameRect();
+  double leftDownX = a.pos.x - (a.width / 2);
+  double leftDownY = a.pos.y - (a.height / 2);
+  double rightUpX = a.pos.x + (a.width / 2);
+  double rightUpY = a.pos.y + (a.height / 2);
+  for (size_t i = 1; i < size; i++)
   {
+    out << leftDownX << " " << leftDownY << " " << rightUpX << " " << rightUpY << " ";
     rectangle_t a = shapes[i]->getFrameRect();
     double leftDownX = a.pos.x - (a.width / 2);
     double leftDownY = a.pos.y - (a.height / 2);
     double rightUpX = a.pos.x + (a.width / 2);
     double rightUpY = a.pos.y + (a.height / 2);
-    out << leftDownX << " " << leftDownY << " " << rightUpX << " " << rightUpY << " ";
   }
+  out << leftDownX << " " << leftDownY << " " << rightUpX << " " << rightUpY;
 }
