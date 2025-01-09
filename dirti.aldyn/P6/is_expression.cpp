4a9cac690942639672f1620b11049f3469de0104
aldyn-d <aldyndirti@gmail.com>
2025-01-09 14:12:42 +0300

    done work but not compiling

---
+++ dirti.aldyn/P6/is_expression.cpp
@@ -0,0 +1,151 @@
+#include "is_expression.hpp"
+
+const char * has_symbol(const char * str, char c)
+{
+  if (!str)
+  {
+    return str;
+  }
+  return (*str == c) ? (str + 1) : nullptr;
+}
+
+const char * has_x(const char * str)
+{
+  return has_symbol(str, 'x');
+}
+const char * has_y(const char * str)
+{
+  return has_symbol(str, 'y');
+}
+const char * has_z(const char * str)
+{
+  return has_symbol(str, 'z');
+}
+const char * has_digit(const char * str)
+{
+  auto next = has_symbol(str, '0');
+  return next ? next : has_symbol(str, '1');
+}
+
+const char * has_id(const char * str)
+{
+  if (!str)
+  {
+    return str;
+  }
+  auto next = has_x(str);
+  if (next)
+  {
+    return next;
+  }
+  next = has_y(str);
+  if (next)
+  {
+    return next;
+  }
+  next = has_z(str);
+  return next;
+}
+const char * has_number(const char * str)
+{
+  if (!str)
+  {
+    return str;
+  }
+  auto next = has_digit(str);
+  if (auto continues = has_number(next))
+  {
+    return continues;
+  }
+  return next;
+}
+const char * has_multiplier(const char * str)
+{
+  if (!str)
+  {
+    return str;
+  }
+  auto next = has_number(str);
+  if (next)
+  {
+    return next;
+  }
+  next = has_id(str);
+  if (next)
+  {
+    return next;
+  }
+  if (next = has_symbol(str, '('))
+  {
+    next = has_expression(next);
+    if (next = has_symbol(next, ')'))
+    {
+      return next;
+    }
+    return nullptr;
+  }
+  return nullptr;
+}
+const char * has_term(const char * str)
+{
+  if (!str)
+  {
+    return str;
+  }
+  if (auto next = has_symbol(str, '('))
+  {
+    next = has_multiplier(next);
+    if (next = has_symbol(next, '+'))
+    {
+      next = has_term(next);
+      if (next)
+      {
+        if (next = has_symbol(next, ')'))
+        {
+          return next;
+        }
+      }
+    }
+    return nullptr;
+  }
+  auto next = has_multiplier(str);
+  if (next = has_symbol(next, '*'))
+  {
+    if (auto continues = has_term(next))
+    {
+      return continues;
+    }
+    return nullptr;
+  }
+  return next;
+}
+const char * has_expression(const char * str)
+{
+  if (!str)
+  {
+    return str;
+  }
+  auto next = has_term(str);
+  if (next = has_symbol(next, '+'))
+  {
+    if (auto continues = has_expression(next))
+    {
+      return continues;
+    }
+    return nullptr;
+  }
+  if (next = has_symbol(next, '-'))
+  {
+    if (auto continues = has_expression(next))
+    {
+      return continues;
+    }
+    return nullptr;
+  }
+  return next;
+}
+bool dirti::is_expression(const char * str)
+{
+  auto next = has_expression(str);
+  return next && (*next == '\0');
+}
--- dirti.aldyn/P6/main.cpp
+++ dirti.aldyn/P6/main.cpp
@@ -1,6 +1,11 @@
 #include <iostream>
+#include <ios>
+#include <str_input.hpp>
+#include "is_expression.hpp"
 
 int main()
 {
-  
+  size_t capacity = 2;
+  const char * str = dirti::str_input(std::cin, capacity);
+  std::cout << std::boolalpha << dirti::is_expression(str);
 }
