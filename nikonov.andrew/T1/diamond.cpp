618aa08a2a3d54b4943f629d13f42b8a0aa127f8
ysffmn <andrewnikonov387@gmail.com>
2025-01-11 04:34:00 +0300

    new diamond realisation done

--- nikonov.andrew/T1/diamond.cpp
+++ nikonov.andrew/T1/diamond.cpp
@@ -3,59 +3,79 @@
 #include "additional-utilities.hpp"
 namespace
 {
-  nikonov::point_t findTop(const nikonov::point_t &p1, const nikonov::point_t &p2, const nikonov::point_t &p3);
-  nikonov::point_t findMid(const nikonov::point_t &p1, const nikonov::point_t &p2, const nikonov::point_t &p3);
-  nikonov::point_t findEdge(const nikonov::point_t &p1, const nikonov::point_t &p2, const nikonov::point_t &p3);
-  bool isEqualPoint(const nikonov::point_t &p1, const nikonov::point_t &p2);
+  using namespace nikonov;
+  point_t findTop(const point_t &p1, const point_t &p2, const point_t &p3);
+  point_t findMid(const point_t &p1, const point_t &p2, const point_t &p3);
+  point_t findRight(const point_t &p1, const point_t &p2, const point_t &p3);
+  bool isEqualPoint(const point_t &p1, const point_t &p2);
+  Triangle findLeftTopTgl(const point_t &p1, const point_t &p2, const point_t &p3);
+  Triangle findLeftBotTgl(const point_t &p1, const point_t &p2, const point_t &p3);
+  Triangle findRightTopTgl(const point_t &p1, const point_t &p2, const point_t &p3);
+  Triangle findRightBotTgl(const point_t &p1, const point_t &p2, const point_t &p3);
+  struct BaseTglPoints
+  {
+    point_t topP;
+    point_t midP;
+    point_t rightP;
+    BaseTglPoints(const point_t &p1, const point_t &p2, const point_t &p3):
+      topP(findTop(p1, p2, p3)),
+      midP(findMid(p1, p2, p3)),
+      rightP(findRight(p1, p2, p3))
+    {}
+  };
 }
 nikonov::Diamond::Diamond(const point_t &p1, const point_t &p2, const point_t &p3):
-  topP_(findTop(p1, p2, p3)),
-  midP_(findMid(p1, p2, p3)),
-  rightP_(findEdge(p1, p2, p3))
+  lt_tgl_(findLeftTopTgl(p1, p2, p3)),
+  lb_tgl_(findLeftBotTgl(p1, p2, p3)),
+  rt_tgl_(findRightTopTgl(p1, p2, p3)),
+  rb_tgl_(findRightBotTgl(p1, p2, p3))
 {
-  if (!(topP_.x == midP_.x && rightP_.y == midP_.y))
+  BaseTglPoints data(p1, p2, p3);
+  if (!(data.topP.x == data.midP.x && data.rightP.y == data.midP.y))
   {
     throw std::logic_error("ERROR:noncorrect diamond parameters");
   }
 }
-double nikonov::Diamond::getArea() const
+double nikonov::Diamond::getArea() const noexcept
 {
-  return (rightP_.x - midP_.x) * (topP_.y - midP_.y) * 2;
+  return lt_tgl_.getArea() + lb_tgl_.getArea() + rt_tgl_.getArea() + rb_tgl_.getArea();
 }
-nikonov::rectangle_t nikonov::Diamond::getFrameRect() const
+nikonov::rectangle_t nikonov::Diamond::getFrameRect() const noexcept
 {
-  point_t rtp({ rightP_.x, topP_.y });
-  point_t lbp({ 2 * midP_.x - rightP_.x, 2 * midP_.y - topP_.y });
-  double width = rtp.x - lbp.x;
-  double height = rtp.y - lbp.y;
-  point_t pos = point_t({ lbp.x + (width / 2), lbp.y + (height / 2) });
+  double width = lt_tgl_.getFrameRect().width * 2;
+  double height = lt_tgl_.getFrameRect().height * 2;
+  point_t tglCenter = lt_tgl_.getFrameRect().pos;
+  point_t pos = point_t({ tglCenter.x + (width / 4), tglCenter.y - (height / 4) });
   return rectangle_t({ width, height, pos });
 }
-void nikonov::Diamond::move(const point_t &newPos)
+void nikonov::Diamond::move(const point_t &newPos) noexcept
 {
   rectangle_t crntRect = getFrameRect();
   double diffX = newPos.x - crntRect.pos.x;
   double diffY = newPos.y - crntRect.pos.y;
   move(diffX, diffY);
 }
-void nikonov::Diamond::move(double x, double y)
+void nikonov::Diamond::move(double x, double y) noexcept
 {
-  movePoint(topP_, x, y);
-  movePoint(midP_, x, y);
-  movePoint(rightP_, x, y);
+  lt_tgl_.move(x, y);
+  lb_tgl_.move(x, y);
+  rt_tgl_.move(x, y);
+  rb_tgl_.move(x, y);
 }
 void nikonov::Diamond::scale(double k) noexcept
 {
   rectangle_t crntRect = getFrameRect();
-  scalePoint(topP_, crntRect.pos, k);
-  scalePoint(rightP_, crntRect.pos, k);
+  ispScale(&lt_tgl_, crntRect.pos.x, crntRect.pos.y, k);
+  ispScale(&lb_tgl_, crntRect.pos.x, crntRect.pos.y, k);
+  ispScale(&rt_tgl_, crntRect.pos.x, crntRect.pos.y, k);
+  ispScale(&rb_tgl_, crntRect.pos.x, crntRect.pos.y, k);
 }
 namespace
 {
-  nikonov::point_t findTop(const nikonov::point_t &p1, const nikonov::point_t &p2, const nikonov::point_t &p3)
+  point_t findTop(const point_t &p1, const point_t &p2, const point_t &p3)
   {
     double maxY = std::max(std::max(p1.y, p2.y), p3.y);
-    nikonov::point_t result({ 0, 0 });
+    point_t result({ 0, 0 });
     size_t maxCnt = (p1.y == maxY) + (p2.y == maxY) + (p3.y == maxY);
     if (maxCnt > 1)
     {
@@ -63,9 +83,9 @@ namespace
     }
     return (p1.y == maxY) ? p1 : (p2.y == maxY) ? p2 : p3;
   }
-  nikonov::point_t findMid(const nikonov::point_t &p1, const nikonov::point_t &p2, const nikonov::point_t &p3)
+  point_t findMid(const point_t &p1, const point_t &p2, const point_t &p3)
   {
-    nikonov::point_t topP = findTop(p1, p2, p3);
+    point_t topP = findTop(p1, p2, p3);
     if (p1.y < topP.y && p1.x == topP.x)
     {
       return p1;
@@ -83,15 +103,15 @@ namespace
       throw std::logic_error("non-correct parameters");
     }
   }
-  bool isEqualPoint(const nikonov::point_t &p1, const nikonov::point_t &p2)
+  bool isEqualPoint(const point_t &p1, const point_t &p2)
   {
     return (p1.x == p2.x) && (p1.y == p2.y);
   }
-  nikonov::point_t findEdge(const nikonov::point_t &p1, const nikonov::point_t &p2, const nikonov::point_t &p3)
+  point_t findRight(const point_t &p1, const point_t &p2, const point_t &p3)
   {
-    nikonov::point_t topP = findTop(p1, p2, p3);
-    nikonov::point_t midP = findMid(p1, p2, p3);
-    nikonov::point_t edgeP({ 0, 0 });
+    point_t topP = findTop(p1, p2, p3);
+    point_t midP = findMid(p1, p2, p3);
+    point_t edgeP({ 0, 0 });
     if (!isEqualPoint(p1, topP) && !isEqualPoint(p1, midP))
     {
       edgeP = p1;
@@ -109,5 +129,29 @@ namespace
       edgeP.x = 2 * midP.x - edgeP.x;
     }
     return edgeP;
+  }
+  Triangle findLeftTopTgl(const point_t &p1, const point_t &p2, const point_t &p3)
+  {
+    BaseTglPoints data(p1, p2, p3);
+    point_t leftP({ 2 * data.midP.x - data.rightP.x, data.midP.y });
+    return Triangle({ data.topP, data.midP, leftP });
+  }
+  Triangle findLeftBotTgl(const point_t &p1, const point_t &p2, const point_t &p3)
+  {
+    BaseTglPoints data(p1, p2, p3);
+    point_t botP({ data.topP.x, 2 * data.midP.y - data.topP.y });
+    point_t leftP({ 2 * data.midP.x - data.rightP.x, data.midP.y });
+    return Triangle({ botP, data.midP, leftP });
+  }
+  Triangle findRightTopTgl(const point_t &p1, const point_t &p2, const point_t &p3)
+  {
+    BaseTglPoints data(p1, p2, p3);
+    return Triangle({ data.topP, data.midP, data.rightP });
+  }
+  Triangle findRightBotTgl(const point_t &p1, const point_t &p2, const point_t &p3)
+  {
+    BaseTglPoints data(p1, p2, p3);
+    point_t botP({ data.topP.x, 2 * data.midP.y - data.topP.y });
+    return Triangle({ botP, data.midP, data.rightP });
   }
 }
--- nikonov.andrew/T1/diamond.hpp
+++ nikonov.andrew/T1/diamond.hpp
@@ -2,21 +2,23 @@
 #define DIAMOND_HPP
 #include "base-types.hpp"
 #include "shape.hpp"
+#include "triangle.hpp"
 namespace nikonov
 {
   class Diamond final: public Shape
   {
   public:
-    Diamond(const point_t &topP, const point_t &midP, const point_t &rightP);
-    double getArea() const override;
-    rectangle_t getFrameRect() const override;
-    void move(const point_t &a) override;
-    void move(double x, double y) override;
+    Diamond(const point_t &p1, const point_t &p2, const point_t &p3);
+    double getArea() const noexcept override;
+    rectangle_t getFrameRect() const noexcept override;
+    void move(const point_t &a) noexcept override;
+    void move(double x, double y) noexcept override;
     void scale(double k) noexcept override;
   private:
-    point_t topP_;
-    point_t midP_;
-    point_t rightP_;
+    Triangle lt_tgl_;
+    Triangle lb_tgl_;
+    Triangle rt_tgl_;
+    Triangle rb_tgl_;
   };
 }
 #endif
