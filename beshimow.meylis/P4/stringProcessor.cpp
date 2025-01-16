6463a78bcc8fc26c1e95541177866816ba280475
Beshimov03 <meylisbesimow44@gmail.com>
2025-01-17 00:52:36 +0300

    for string

---
+++ beshimow.meylis/P4/stringProcessor.cpp
@@ -0,0 +1,32 @@
+#include "stringProcessor.hpp"
+#include <cstddef>
+
+char * beshimow::excludeCharacters(char * str1, const char * str2)
+{
+    char * original = str1;
+    char * result = str1;
+
+    while (*str1)
+    {
+        const char * ptr2 = str2;
+        bool found = false;
+
+        while (*ptr2 && !found)
+        {
+            if (*str1 == *ptr2)
+            {
+                found = true;
+            }
+            ptr2++;
+        }
+
+        if (!found)
+        {
+            *result++ = *str1;
+        }
+        str1++;
+    }
+
+    *result = '\0';
+    return original;
+}
---
+++ beshimow.meylis/P4/stringProcessor.hpp
@@ -0,0 +1,9 @@
+#ifndef STRINGPROCESSOR_HPP
+#define STRINGPROCESSOR_HPP
+
+namespace beshimow
+{
+    char * excludeCharacters(char * str1, const char * str2);
+}
+
+#endif
