ac5ba71c3e478e3be5e92ac23bbedc547dfdd08f
Z-1337 <un228der@gmail.com>
2025-01-18 15:25:58 +0300

    revert concave

--- kushekbaev.danil/T1/concave.hpp
+++ kushekbaev.danil/T1/concave.hpp
@@ -2,11 +2,10 @@
 #define CONCAVE_HPP
 
 #include "shape.hpp"
-#include "parallelogram.hpp"
 
 namespace kushekbaev
 {
-  class Concave : public Shape
+  class Concave final: public Shape
   {
   public:
     Concave(const point_t first, const point_t second, const point_t third, const point_t final);
@@ -21,8 +20,6 @@ namespace kushekbaev
     point_t second_;
     point_t third_;
     point_t final_;
-    Parallelogram parallelogram1_;
-    Parallelogram parallelogram2_;
   };
 }
 
