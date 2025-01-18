79ac2cbf35ed63ef46602cd42fcdbf91156807fa
Z-1337 <un228der@gmail.com>
2025-01-18 19:40:32 +0300

    concave revert

--- kushekbaev.danil/T1/concave.cpp
+++ kushekbaev.danil/T1/concave.cpp
@@ -1,5 +1,7 @@
 #include "concave.hpp"
+#include <cmath>
 #include <stdexcept>
+#include <cctype>
 #include <array>
 #include "base-types.hpp"
 #include "shapeBreeding.hpp"
@@ -27,30 +29,27 @@ namespace kushekbaev
 
   rectangle_t Concave::getFrameRect() const
   {
-    double lowerLeftx = std::min(std::min(std::min(first_.x, second_.x), third_.x), last_.x);
-    double lowerLefty = std::min(std::min(std::min(first_.y, second_.y), third_.y), last_.y);
-    double upperRightx = std::max(std::max(std::max(first_.x, second_.x), third_.x), last_.x);
-    double upperRighty = std::max(std::max(std::max(first_.y, second_.y), third_.y), last_.y);
+    point_t lowerLeft { 0, 0 };
+    lowerLeft.x = std::min(std::min(std::min(first_.x, second_.x), third_.x), last_.x);
+    lowerLeft.y = std::min(std::min(std::min(first_.y, second_.y), third_.y), last_.y);
 
-    point_t lowerLeft { lowerLeftx, lowerLefty };
-    point_t upperRight { upperRightx, upperRighty };
+    point_t upperRight { 0, 0 };
+    upperRight.x = std::max(std::max(std::max(first_.x, second_.x), third_.x), last_.x);
+    upperRight.y = std::max(std::max(std::max(first_.y, second_.y), third_.y), last_.y);
 
-    double frame_rectH = upperRight.y - lowerLeft.y;
-    double frame_rectW = upperRight.x - lowerLeft.x;
-    double frame_rectX = lowerLeft.x + frame_rectW / 2;
-    double frame_rectY = lowerLeft.y + frame_rectH / 2;
-
-    point_t pos { frame_rectX, frame_rectY };
-    rectangle_t frame_rect { frame_rectH, frame_rectW, pos };
-
+    rectangle_t frame_rect;
+    frame_rect.height = upperRight.y - lowerLeft.y;
+    frame_rect.width = upperRight.x - lowerLeft.x;
+    frame_rect.pos.x = lowerLeft.x + frame_rect.width / 2;
+    frame_rect.pos.y = lowerLeft.y + frame_rect.height / 2;
     return frame_rect;
   }
 
-  void Concave::move(point_t scalePoint)
+  void Concave::move(point_t Z)
   {
     point_t middle = getFrameRect().pos;
-    double dx = scalePoint.x - middle.x;
-    double dy = scalePoint.y - middle.y;
+    double dx = Z.x - middle.x;
+    double dy = Z.y - middle.y;
 
     move(dx, dy);
   }
@@ -59,26 +58,26 @@ namespace kushekbaev
   {
     std::array<point_t*, 4> points = { &first_, &second_, &third_, &last_ };
 
-    for (point_t* point: points)
+    for (point_t* point : points)
     {
       point->x += dx;
       point->y += dy;
     }
   }
 
-  void Concave::scale(double scaleCoeff)
+  void Concave::scale(double V)
   {
-    if (scaleCoeff <= 0)
+    if (V <= 0)
     {
       throw std::out_of_range("Scale coefficient should be greater than zero\n");
     }
     point_t middle = getFrameRect().pos;
     std::array<point_t*, 4> points = { &first_, &second_, &third_, &last_ };
 
-    for (point_t* point: points)
+    for (point_t* point : points)
     {
-      point->x = middle.x + (point->x - middle.x) * scaleCoeff;
-      point->y = middle.y + (point->y - middle.y) * scaleCoeff;
+      point->x = middle.x + (point->x - middle.x) * V;
+      point->y = middle.y + (point->y - middle.y) * V;
     }
   }
 }
