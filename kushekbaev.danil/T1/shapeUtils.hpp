fff7e121d75ea8dbd299f15aff7cf5e1dcf6e26f
Z-1337 <un228der@gmail.com>
2025-01-19 16:51:35 +0300

    experiments

--- kushekbaev.danil/T1/shapeUtils.hpp
+++ kushekbaev.danil/T1/shapeUtils.hpp
@@ -7,9 +7,23 @@
 namespace kushekbaev
 {
   template<std::size_t N>
-  void movePoints(std::array<point_t*, N> &points, const double dx, const double dy);
+  void movePoints(std::array<point_t*, N> &points, double dx, double dy)
+  {
+    for (point_t* point : points)
+    {
+      point->x += dx;
+      point->y += dy;
+    }
+  }
   template<std::size_t N>
-  void scalePoints(std::array<point_t*, N> &points, const double scaleCoeff, const point_t middle);
+  void scalePoints(std::array<point_t*, N> &points, const double scaleCoeff, const point_t middle)
+  {
+    for (point_t* point : points)
+    {
+      point->x = middle.x + scaleCoeff * (point->x - middle.x);
+      point->y = middle.y + scaleCoeff * (point->y - middle.y);
+    }
+  }
   double getLineLength(const point_t first, const point_t second);
   double getAreaOfTriangle(const point_t first, const point_t second, const point_t third);
 }
