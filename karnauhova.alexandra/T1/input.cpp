4bfbea10d511f71e04b8b2689917eb5a99e8eee4
“Karnaukhova <masj280906@gmail.com>
2025-01-28 23:31:40 +0300

    otpusty

--- karnauhova.alexandra/T1/input.cpp
+++ karnauhova.alexandra/T1/input.cpp
@@ -116,7 +116,6 @@ bool karnauhova::input_polygon(std::istream & in, Shape** shaps, size_t count_sh
   }
   in.clear();
   shaps[count_shaps] = new Polygon(x_y, count);
-  delete[] x_y;
   return true;
 }
 
