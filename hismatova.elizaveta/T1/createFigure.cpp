560e485bfed57d85f534fa034a78845dad4fe5f7
lee <likachich12@gmail.com>
2025-01-30 18:12:32 +0300

    fix length

--- hismatova.elizaveta/T1/createFigure.cpp
+++ hismatova.elizaveta/T1/createFigure.cpp
@@ -19,20 +19,32 @@ hismatova::Rectangle* hismatova::createRectangle(std::istream& in)
 {
   double coordinates[4];
   readPoints(in, coordinates, 4);
-  Rectangle* rectangle_ = new Rectangle({ coordinates[0], coordinates[1] }, { coordinates[2], coordinates[3] });
+  Rectangle* rectangle_ = new Rectangle(
+    { coordinates[0], coordinates[1] },
+    { coordinates[2], coordinates[3] }
+  );
   return rectangle_;
 }
 hismatova::Triangle* hismatova::createTriangle(std::istream& in)
 {
   double coordinates[6];
   readPoints(in, coordinates, 6);
-  Triangle* triangle_ = new Triangle({ coordinates[0], coordinates[1] }, { coordinates[2], coordinates[3] }, { coordinates[4], coordinates[5] });
+  Triangle* triangle_ = new Triangle(
+    { coordinates[0], coordinates[1] },
+    { coordinates[2], coordinates[3] },
+    { coordinates[4], coordinates[5] }
+  );
   return triangle_;
 }
 hismatova::Concave* hismatova::createConcave(std::istream& in)
 {
   double coordinates[8];
   readPoints(in, coordinates, 8);
-  Concave* concave_ = new Concave({ coordinates[0], coordinates[1] }, { coordinates[2], coordinates[3] }, { coordinates[4], coordinates[5] }, { coordinates[6], coordinates[7] });
+  Concave* concave_ = new Concave(
+    { coordinates[0], coordinates[1] },
+    { coordinates[2], coordinates[3] },
+    { coordinates[4], coordinates[5] },
+    { coordinates[6], coordinates[7] }
+  );
   return concave_;
 }
