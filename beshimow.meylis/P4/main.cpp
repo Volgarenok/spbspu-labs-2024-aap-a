fefcac35069a374b1f33d73602c4b35e10f72ff9
Beshimov03 <meylisbesimow44@gmail.com>
2025-02-01 01:14:39 +0300

    changes P4/main.cpp

--- beshimow.meylis/P4/main.cpp
+++ beshimow.meylis/P4/main.cpp
@@ -4,12 +4,10 @@
 
 namespace beshimow {
     void excludeCharacters(const char* str1, const char* str2, char*& result) {
-        // Определяем длину входной строки
         size_t len1 = std::strlen(str1);
         size_t len2 = std::strlen(str2);
-        
-        // Выделяем память для выходной строки (максимально возможная длина)
-        result = new char[len1 + 1]; // +1 для нуль-терминатора
+
+        result = new char[len1 + 1];
         if (!result) {
             std::cerr << "Ошибка выделения памяти" << std::endl;
             exit(1);
@@ -17,11 +15,9 @@ namespace beshimow {
 
         size_t resultIndex = 0;
 
-        // Проходим по каждому символу входной строки
         for (size_t i = 0; i < len1; ++i) {
             bool shouldExclude = false;
 
-            // Проверяем, есть ли символ в строке исключений
             for (size_t j = 0; j < len2; ++j) {
                 if (str1[i] == str2[j]) {
                     shouldExclude = true;
@@ -29,25 +25,22 @@ namespace beshimow {
                 }
             }
 
-            // Если символ не нужно исключать, добавляем его в выходную строку
             if (!shouldExclude) {
                 result[resultIndex++] = str1[i];
             }
         }
 
-        // Завершаем выходную строку нуль-терминатором
         result[resultIndex] = '\0';
     }
 }
 
 int main() {
-    // Считываем строку из стандартного ввода
+
     char* input = nullptr;
     std::cout << "Введите строку: ";
     std::string temp;
     std::getline(std::cin, temp);
-    
-    // Выделяем память для входной строки
+
     input = new char[temp.length() + 1];
     if (!input) {
         std::cerr << "Ошибка выделения памяти" << std::endl;
@@ -55,19 +48,15 @@ int main() {
     }
     std::strcpy(input, temp.c_str());
 
-    // Вторая строка, символы которой будут исключены
     const char* exclude = "abc";
     char* output = nullptr;
 
-    // Вызываем функцию для исключения символов
     beshimow::excludeCharacters(input, exclude, output);
 
-    // Выводим результат
     std::cout << output << std::endl;
 
-    // Освобождаем выделенную память
     delete[] input;
     delete[] output;
 
-    return 0; // Завершение программы с кодом возврата 0
+    return 0;
 }
