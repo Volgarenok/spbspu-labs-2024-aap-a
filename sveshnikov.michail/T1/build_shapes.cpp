f3e94061ed65b2333e8de9bb76e02f7a7bdf3f7b
svesh3000 <russkij2006@mail.ru>
2025-02-01 18:01:22 +0300

    Fix build error with unused parameter 'in'

--- sveshnikov.michail/T1/build_shapes.cpp
+++ sveshnikov.michail/T1/build_shapes.cpp
@@ -22,7 +22,7 @@ void sveshnikov::build_rectangle(std::istream &in, CompositeShape &comp_shp)
 {
   constexpr size_t num_params = 4;
   double params[num_params] = {};
-  read_params(std::cin, num_params, params);
+  read_params(in, num_params, params);
   comp_shp.push_back(new Rectangle({params[0], params[1]}, {params[2], params[3]}));
 }
 
@@ -30,7 +30,7 @@ void sveshnikov::build_ring(std::istream &in, CompositeShape &comp_shp)
 {
   constexpr size_t num_params = 4;
   double params[num_params] = {};
-  read_params(std::cin, num_params, params);
+  read_params(in, num_params, params);
   comp_shp.push_back(new Ring({params[0], params[1]}, params[2], params[3]));
 }
 
@@ -38,7 +38,7 @@ void sveshnikov::build_ellipse(std::istream &in, CompositeShape &comp_shp)
 {
   constexpr size_t num_params = 4;
   double params[num_params] = {};
-  read_params(std::cin, num_params, params);
+  read_params(in, num_params, params);
   comp_shp.push_back(new Ellipse({params[0], params[1]}, params[2], params[3]));
 }
 
@@ -46,6 +46,6 @@ void sveshnikov::build_square(std::istream &in, CompositeShape &comp_shp)
 {
   constexpr size_t num_params = 3;
   double params[num_params] = {};
-  read_params(std::cin, num_params, params);
+  read_params(in, num_params, params);
   comp_shp.push_back(new Square({params[0], params[1]}, params[2]));
 }
