69893d7882bf78929e9115335c1647ac8e81b5aa
aldyn-d <aldyndirti@gmail.com>
2025-02-03 11:59:11 +0300

    corrections

--- dirti.aldyn/P3/input_matrix.cpp
+++ dirti.aldyn/P3/input_matrix.cpp
@@ -1,7 +1,6 @@
-#include "input_matrix.hpp"
+#include "matrix_functions.hpp"
 
-
-std::istream & dirti::input_matrix(std::istream & in, int * t, size_t m, size_t n, size_t & read)
+void dirti::input_matrix(std::istream & in, int * t, size_t m, size_t n, size_t & read)
 {
   for (size_t i = 0; i < m; ++i)
   {
@@ -13,5 +12,4 @@ std::istream & dirti::input_matrix(std::istream & in, int * t, size_t m, size_t 
       }
     }
   }
-  return in;
 }
--- dirti.aldyn/P3/main.cpp
+++ dirti.aldyn/P3/main.cpp
@@ -2,19 +2,13 @@
 #include <fstream>
 #include <cstring>
 #include <cstdlib>
-#include "input_matrix.hpp"
-#include "matrix_checking.hpp"
-#include "local_min.hpp"
+#include "matrix_functions.hpp"
+
 int main(int argc, char ** argv)
 {
-  if (argc < 4)
+  if (argc != 4)
   {
-    std::cerr << "Not enough arguments" << "\n";
-    return 1;
-  }
-  if (argc > 4)
-  {
-    std::cerr << "Too many arguments" << "\n";
+    std::cerr << "Arguments number is not 4" << "\n";
     return 1;
   }
   if ((argv[1][0] != '1' && argv[1][0] != '2') || argv[1][1] != '\0')
@@ -22,9 +16,8 @@ int main(int argc, char ** argv)
     std::cerr << "First parameter is not suitable" << "\n";
     return 1;
   }
-  size_t m = 0, n = 0;
-  size_t read = 0;
   std::ifstream input(argv[2]);
+  size_t m = 0, n = 0;
   input >> m >> n;
   if (!input)
   {
@@ -33,7 +26,7 @@ int main(int argc, char ** argv)
   }
   if (m == 0 && n == 0)
   {
-    std::cout << 0 << " " << 0 << "\n";
+    std::cout << "0 0\n";
     return 0;
   }
   int fix_matrix[10000] = {};
@@ -50,12 +43,13 @@ int main(int argc, char ** argv)
       dyn_matrix = new int[m * n];
       matrix = dyn_matrix;
     }
-    catch (const std::bad_alloc & e)
+    catch (const std::bad_alloc)
     {
       std::cerr << "Out of memory" << "\n";
       return 2;
     }
   }
+  size_t read = 0;
   dirti::input_matrix(input, matrix, m, n, read);
   if (read != (m * n))
   {
