98a61cedba3620eea703d37664b45e8604aad331
brev1ch <flirktv@gmail.com>
2025-02-06 01:05:21 +0300

    base things

--- brevnov.nikita/T1/base-types.hpp
+++ brevnov.nikita/T1/base-types.hpp
@@ -4,22 +4,11 @@ namespace brevnov
 {
   struct point_t
   {
-    point_t() = default;
-    point_t(double xf, double yf):
-      x(xf),
-      y(yf)
-      {}
     double x;
     double y;
   };
   struct rectangle_t
   {
-    rectangle_t() = default;
-    rectangle_t(double widthf, double heightf, point_t posf):
-      width(widthf),
-      height(heightf),
-      pos(posf)
-      {}
     double width;
     double height;
     point_t pos;
