5f5854bee8fa627e14f691285da5ecfe6723da2c
Z-1337 <un228der@gmail.com>
2025-02-07 00:15:37 +0300

    CG fix: order in shape.hpp

--- kushekbaev.danil/T1/shape.hpp
+++ kushekbaev.danil/T1/shape.hpp
@@ -9,12 +9,12 @@ namespace kushekbaev
   class Shape
   {
   public:
+    virtual ~Shape() = default;
     virtual double getArea() const = 0;
     virtual rectangle_t getFrameRect() const = 0;
     virtual void move(point_t scalePoint) = 0;
     virtual void move(double dx, double dy) = 0;
     virtual void scale(double scaleCoeff) = 0;
-    virtual ~Shape() = default;
     void scaleEverything(point_t scalePoint, double scaleCoeff);
   };
 }
