142619723a639afde62ffbaed1e11abe76b8e202
root <root@kryloff.>
2025-02-07 22:23:20 +0300

    Fixes

--- krylov.matvey/T1/complexquad.cpp
+++ krylov.matvey/T1/complexquad.cpp
@@ -1,65 +1,40 @@
 #include "complexquad.hpp"
-#include "triangle.hpp"
 #include <algorithm>
 #include <stdexcept>
 
 krylov::Complexquad::Complexquad(const point_t& a, const point_t& b, const point_t& c, const point_t& d):
-  a_(a),
-  b_(b),
-  c_(c),
-  d_(d)
+  t1_(a, b, c),
+  t2_(c, d, a),
+  t3_(b, c, d),
+  t4_(d, a, b)
 {
-  bool invalid = (a_.x == b_.x && a_.y == b_.y) || (b_.x == c_.x && b_.y == c_.y) || (c_.x == d_.x && c_.y == d_.y)
-    || (a_.x == c_.x && a_.y == c_.y) || (a_.x == d_.x && a_.y == d_.y) || (b_.x == d_.x && b_.y == d_.y);
-
-  if (invalid)
+  if (getArea() <= 0)
   {
     throw std::invalid_argument("Invalid complexquad coordinates");
   }
 }
 
-bool krylov::Complexquad::isPointOnSegment(const point_t& point, const point_t& segStart, const point_t& segEnd) const
-{
-  return std::min(segStart.x, segEnd.x) <= point.x && point.x <= std::max(segStart.x, segEnd.x)
-    && std::min(segStart.y, segEnd.y) <= point.y && point.y <= std::max(segStart.y, segEnd.y);
-}
-
-krylov::point_t krylov::Complexquad::findIntersection(const point_t& a, const point_t& b, const point_t& c, const point_t& d) const
-{
-  double m1 = (b.y - a.y) / (b.x - a.x);
-  double m2 = (d.y - c.y) / (d.x - c.x);
-  double b1 = a.y - m1 * a.x;
-  double b2 = c.y - m2 * c.x;
-  point_t intersectionPoint = {(b2 - b1) / (m1 - m2), m1 * ((b2 - b1) / (m1 - m2)) + b1};
-
-  if (isPointOnSegment(intersectionPoint, a, b) && isPointOnSegment(intersectionPoint, c, d))
-  {
-    return intersectionPoint;
-  }
-  return {0, 0};
-}
-
 double krylov::Complexquad::getArea() const
 {
-  point_t m1 = {(a_.x + c_.x) / 2, (a_.y + c_.y) / 2};
-  point_t m2 = {(b_.x + d_.x) / 2, (b_.y + d_.y) / 2};
-
-  krylov::Triangle t1(a_, m1, m2);
-  krylov::Triangle t2(m1, c_, m2);
-  krylov::Triangle t3(b_, m1, m2);
-  krylov::Triangle t4(m1, d_, m2);
-
-  return t1.getArea() + t2.getArea() + t3.getArea() + t4.getArea();
+  return t1_.getArea() + t2_.getArea() + t3_.getArea() + t4_.getArea();
 }
 
 krylov::rectangle_t krylov::Complexquad::getFrameRect() const
 {
-  double minX = std::min({a_.x, b_.x, c_.x, d_.x});
-  double maxX = std::max({a_.x, b_.x, c_.x, d_.x});
-  double maxY = std::max({a_.y, b_.y, c_.y, d_.y});
-  double minY = std::min({a_.y, b_.y, c_.y, d_.y});
-  point_t center = {minX + (maxX - minX) / 2, minY + (maxY - minY) / 2};
-  return rectangle_t{maxX - minX, maxY - minY, center};
+  double minX = std::min({t1_.getFrameRect().pos.x, t2_.getFrameRect().pos.x,
+                          t3_.getFrameRect().pos.x, t4_.getFrameRect().pos.x});
+
+  double maxX = std::max({t1_.getFrameRect().pos.x, t2_.getFrameRect().pos.x,
+                          t3_.getFrameRect().pos.x, t4_.getFrameRect().pos.x});
+
+  double minY = std::min({t1_.getFrameRect().pos.y, t2_.getFrameRect().pos.y,
+                          t3_.getFrameRect().pos.y, t4_.getFrameRect().pos.y});
+
+  double maxY = std::max({t1_.getFrameRect().pos.y, t2_.getFrameRect().pos.y,
+                          t3_.getFrameRect().pos.y, t4_.getFrameRect().pos.y});
+
+  point_t center = { (minX + maxX) / 2, (minY + maxY) / 2 };
+  return { maxX - minX, maxY - minY, center };
 }
 
 void krylov::Complexquad::move(const point_t& point)
@@ -72,14 +47,10 @@ void krylov::Complexquad::move(const point_t& point)
 
 void krylov::Complexquad::move(double dx, double dy)
 {
-  a_.x += dx;
-  a_.y += dy;
-  b_.x += dx;
-  b_.y += dy;
-  c_.x += dx;
-  c_.y += dy;
-  d_.x += dx;
-  d_.y += dy;
+  t1_.move(dx, dy);
+  t2_.move(dx, dy);
+  t3_.move(dx, dy);
+  t4_.move(dx, dy);
 }
 
 void krylov::Complexquad::scale(double factor)
@@ -88,13 +59,9 @@ void krylov::Complexquad::scale(double factor)
   {
     throw std::invalid_argument("Scale factor must be positive");
   }
-  point_t center = getFrameRect().pos;
-  a_.x = center.x + factor * (a_.x - center.x);
-  a_.y = center.y + factor * (a_.y - center.y);
-  b_.x = center.x + factor * (b_.x - center.x);
-  b_.y = center.y + factor * (b_.y - center.y);
-  c_.x = center.x + factor * (c_.x - center.x);
-  c_.y = center.y + factor * (c_.y - center.y);
-  d_.x = center.x + factor * (d_.x - center.x);
-  d_.y = center.y + factor * (d_.y - center.y);
+
+  t1_.scale(factor);
+  t2_.scale(factor);
+  t3_.scale(factor);
+  t4_.scale(factor);
 }
--- krylov.matvey/T1/complexquad.hpp
+++ krylov.matvey/T1/complexquad.hpp
@@ -2,7 +2,7 @@
 #define COMPLEXQUAD_HPP
 
 #include "shape.hpp"
-#include "base-types.hpp"
+#include "triangle.hpp"
 
 namespace krylov
 {
@@ -15,13 +15,12 @@ namespace krylov
     void move(const point_t& point) override;
     void move(double dx, double dy) override;
     void scale(double factor) override;
-    bool isPointOnSegment(const point_t& point, const point_t& segStart, const point_t& segEnd) const;
+
   private:
-    point_t a_;
-    point_t b_;
-    point_t c_;
-    point_t d_;
-    point_t findIntersection(const point_t& a, const point_t& b, const point_t& c, const point_t& d) const;
+    Triangle t1_;
+    Triangle t2_;
+    Triangle t3_;
+    Triangle t4_;
   };
 }
 
