382bea27041fdf8f3c7e85d560b4454a4b031cbb
shycoshy <neforscist@gmail.com>
2025-02-08 12:50:36 +0000

    fix no.63

--- asafov.andrew/P4/stringFunctions.cpp
+++ asafov.andrew/P4/stringFunctions.cpp
@@ -3,6 +3,7 @@
 
 void findUniqLetters(const char* string, char* alphabet)
 {
+  alphabet = "abcdefghijklmnopqrstuvwxyz";
   for (size_t i = 0; string[i]!='\0'; i++)
   {
     for (size_t j = 0; j < 26; j++)
@@ -17,7 +18,7 @@ void findUniqLetters(const char* string, char* alphabet)
 
 int asafov::countUniqLetters(const char* string)
 {
-  char alphabet[] = "abcdefghijklmnopqrstuvwxyz";
+  char alphabet[26];
   findUniqLetters(string, alphabet);
   size_t count = 0;
   for (size_t i = 0; i < 26; i++)
@@ -32,24 +33,14 @@ int asafov::countUniqLetters(const char* string)
 
 void asafov::getUnusedLetters(const char* string, char* unusedletters)
 {
-  char alphabet[] = "abcdefghijklmnopqrstuvwxyz";
+  char alphabet[26];
   findUniqLetters(string, alphabet);
   size_t lastletter = 0;
   for (size_t pos = 0; pos <= 26; pos++)
   {
-    if (alphabet[pos] != '\0' && lastletter != pos)
+    if (alphabet[pos] != '\0')
     {
-      alphabet[lastletter] = alphabet[pos];
-      alphabet[pos] = '\0';
-      lastletter++;
+      unusedletters[lastletter] = alphabet[pos];
     }
-    else if (alphabet[pos] != '\0' && lastletter == pos)
-    {
-      lastletter++;
-    }
-  }
-  for (size_t i = 0; unusedletters[i] != 0; i++)
-  {
-    unusedletters[i] = alphabet[i];
   }
 }
