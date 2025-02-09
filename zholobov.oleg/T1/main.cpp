8a490f0c3ae00240767d1697eb2f961491fa5f3c
Zholobov Oleg <zholobov.of@edu.spbstu.ru>
2025-02-09 17:27:46 +0300

    fix possible leak

--- zholobov.oleg/T1/main.cpp
+++ zholobov.oleg/T1/main.cpp
@@ -11,40 +11,42 @@ int main()
   zholobov::CompositeShape composite_shape;
   std::string shape_name;
   bool is_scale_command = false;
-  while (std::cin >> shape_name) {
-    try {
-      zholobov::Shape* shape = zholobov::create_shape(shape_name, std::cin);
-      composite_shape.push_back(shape);
-    } catch (const std::invalid_argument& e) {
-      if (shape_name == "SCALE") {
-        is_scale_command = true;
-        break;
+  try {
+    while (std::cin >> shape_name) {
+      try {
+        zholobov::Shape* shape = zholobov::create_shape(shape_name, std::cin);
+        composite_shape.push_back(shape);
+      } catch (const std::invalid_argument& e) {
+        if (shape_name == "SCALE") {
+          is_scale_command = true;
+          break;
+        }
+        std::cerr << e.what() << "\n";
       }
-      std::cerr << e.what() << "\n";
-    } catch (const std::exception& e) {
-      std::cerr << e.what() << "\n";
-      return 1;
     }
-  }
 
-  if (is_scale_command) {
-    if (composite_shape.empty()) {
-      std::cerr << "There are no shapes to scale\n";
+    if (is_scale_command) {
+      if (composite_shape.empty()) {
+        std::cerr << "There are no shapes to scale\n";
+        return 1;
+      }
+      double params[3];
+      zholobov::read_array(params, 3, std::cin);
+      if (!std::cin) {
+        std::cerr << "Error reading SCALE paramaters\n";
+        return 1;
+      }
+      composite_shape.print(std::cout);
+      std::cout << "\n";
+      composite_shape.scale_relative({params[0], params[1]}, params[2]);
+      composite_shape.print(std::cout);
+      std::cout << "\n";
+    } else {
+      std::cerr << "SCALE command not found\n";
       return 1;
     }
-    double params[3];
-    zholobov::read_array(params, 3, std::cin);
-    if (!std::cin) {
-      std::cerr << "Error reading SCALE paramaters\n";
-      return 1;
-    }
-    composite_shape.print(std::cout);
-    std::cout << "\n";
-    composite_shape.scale_relative({params[0], params[1]}, params[2]);
-    composite_shape.print(std::cout);
-    std::cout << "\n";
-  } else {
-    std::cerr << "SCALE command not found\n";
+  } catch (const std::exception& e) {
+    std::cerr << e.what() << "\n";
     return 1;
   }
 
