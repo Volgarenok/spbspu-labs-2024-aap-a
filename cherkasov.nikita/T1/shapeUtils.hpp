9250721909351d7a1d84c474eab0dcd2f8f74c88
cherkasoov <cherkasov5509@gmail.com>
2025-02-09 16:54:56 +0300

    fix

--- cherkasov.nikita/T1/shapeUtils.hpp
+++ cherkasov.nikita/T1/shapeUtils.hpp
@@ -1,13 +1,9 @@
 #ifndef SHAPE_UTILS
 #define SHAPE_UTILS
+#include "base-types.hpp"
 
 namespace cherkasov
 {
-  struct point_t
-  {
-    double x;
-    double y;
-  };
   void scalePoint(point_t& vertex, const point_t& center, double k);
   void moveVertex(point_t& vertex, double moveX, double moveY);
 }
