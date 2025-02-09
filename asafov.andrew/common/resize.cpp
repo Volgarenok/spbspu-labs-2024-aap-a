4024c3faaccb5334fa037ea05b282222b54bb81b
Asafow Andrew <neforscist@gmail.com>
2025-02-09 17:34:39 +0300

    fix no.76

--- asafov.andrew/common/resize.cpp
+++ asafov.andrew/common/resize.cpp
@@ -7,6 +7,10 @@ char* asafov::resize(const char* string, size_t length)
   for (size_t i = 0; i < length; i++)
   {
     newString[i] = string[i];
+    if (string[i] == '\0')
+    {
+      break;
+    }
   }
   return newString;
 }
