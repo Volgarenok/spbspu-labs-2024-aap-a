7a72d5ab12009eb284132a8cec0d6f4bc8a68aa6
Asafow Andrew <neforscist@gmail.com>
2025-02-09 17:16:28 +0300

    fix no.75

--- asafov.andrew/common/resize.cpp
+++ asafov.andrew/common/resize.cpp
@@ -4,7 +4,7 @@
 char* asafov::resize(const char* string, size_t length)
 {
   char* newString = new char[length + 1];
-  for (size_t i = 0; i < length || string[i] != 0; i++)
+  for (size_t i = 0; i < length; i++)
   {
     newString[i] = string[i];
   }
