2f8041461b84c6beb053874b186950c0fdc4b51d
Beshimov03 <meylisbesimow44@gmail.com>
2025-02-10 01:33:03 +0300

    changed P2 again

--- beshimow.meylis/P2/output.cpp
+++ beshimow.meylis/P2/output.cpp
@@ -1,27 +1,21 @@
 #include <iostream>
 #include <cmath>
-#include <stdexcept>
+#include "output.hpp"
+#include "taylor.hpp"
 
-double taylor(double x, double error) {
-    if (error <= 0) {
-        throw std::invalid_argument("Ошибка: error должен быть положительным.");
+namespace beshimow
+{
+    void rowOutput(double x, size_t k, double error)
+    {
+        try {
+            double result = taylor(x, k, error);
+            std::cout << "x: " << x
+                      << ", Taylor series approximation: " << result
+                      << ", Standard function value: " << std::sin(x)
+                      << std::endl;
+        }
+        catch (const std::exception& e) {
+            std::cerr << "Error calculating Taylor series: " << e.what() << std::endl;
+        }
     }
-    return x;
-}
-
-int main() {
-    double x = 1.0;
-    double error = 0.001;
-
-    try {
-        double result = taylor(x, error);
-        double exact = std::exp(x);
-
-        std::cout << "x: " << x << ", Taylor series approximation: " << result << std::endl;
-        std::cout << "Exact value using standard function: " << exact << std::endl;
-    } catch (const std::exception& e) {
-        std::cerr << "Ошибка: " << e.what() << std::endl;
-    }
-
-    return 0;
 }
