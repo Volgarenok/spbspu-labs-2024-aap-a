6d6af55d3513b1fe2dccf2997190fbf99199ed9f
Asafow Andrew <neforscist@gmail.com>
2025-02-10 15:40:10 +0300

    fix no.83

--- asafov.andrew/common/resize.cpp
+++ asafov.andrew/common/resize.cpp
@@ -2,13 +2,7 @@
 
 static size_t ullMin(size_t a, size_t b)
 {
-  if (a < b){
-    return a;
-  }
-  else
-  {
-    return b;
-  }
+  return (a < b) ? (a) : (b);
 }
 
 char* asafov::resize(const char* string, size_t oldlength, size_t newlength)
