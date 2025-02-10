81f39eda224e53adcd8836005396db009cfdf245
aldyn-d <aldyndirti@gmail.com>
2025-02-10 20:20:51 +0300

    Corrections

--- dirti.aldyn/P4/main.cpp
+++ dirti.aldyn/P4/main.cpp
@@ -1,32 +1,25 @@
 #include <iostream>
 #include <cstddef>
-#include <str_input.hpp>
-#include "str_min.hpp"
-#include "str_min2.hpp"
-#include "str_min_recursed.hpp"
+#include <inputStr.hpp>
+#include "findMin.hpp"
+#include "findTwoMin.hpp"
+#include "findMinRecursed.hpp"
 
 int main()
 {
   size_t capacity = 1;
-  char * str = dirti::str_input(std::cin, capacity);
+  char * str = dirti::inputStr(std::cin, capacity);
   if (str == nullptr)
   {
     std::cerr << "Error" << "\n";
     return 1;
   }
   size_t size = capacity - 1;
-  char str_min = dirti::str_min(str, size);
-  if (str_min == '\n')
-  {
-    std::cout << "Not existing one rare symbol" << "\n";
-  }
-  else
-  {
-    std::cout << str_min << "\n";
-  }
+  char str_min = dirti::findMin(str);
+  std::cout << str_min << "\n";
   char str_min2[2] = {'0', '0'};
-  dirti::str_min2(str, size, str_min2);
-  if (str_min2[1] == '0')
+  dirti::findTwoMin(str, str_min2);
+  if (str_min2[1] == '\0')
   {
     std::cout << "There is only one symbol" << "\n";
   }
@@ -34,20 +27,7 @@ int main()
   {
     std::cout << str_min2[0] << " " << str_min2[1] << "\n";
   }
-  char str_min_recursed = dirti::str_min_recursed(str, size);
-  if (str_min_recursed == '\0')
-  {
-    free(str);
-    std::cerr << "Error: bad alloc" << "\n";
-    return 1;
-  }
-  else if (str_min_recursed == '\n')
-  {
-    std::cout << "Not existing one rare symbol" << "\n";
-  }
-  else
-  {
-    std::cout << str_min_recursed << "\n";
-  }
+  char str_min_recursed = dirti::findMinRecursed(str);
+  std::cout << str_min_recursed << "\n";
   free(str);
 }
