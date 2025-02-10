e3a3874dea18aa2f0cc80137f419cbf20c1a3473
Asafow Andrew <neforscist@gmail.com>
2025-02-10 15:50:03 +0300

    fix no.85

--- asafov.andrew/common/resize.cpp
+++ asafov.andrew/common/resize.cpp
@@ -1,9 +1,11 @@
 #include "resize.hpp"
 
+#define MIN(a, b) (a < b) ? (a) : (b)
+
 char* asafov::resize(const char* string, size_t oldlength, size_t newlength)
 {
   char* newString = new char[newlength + 1];
-  for (size_t i = 0; i < (oldlength < newlength) ? (oldlength) : (newlength); i++)
+  for (size_t i = 0; i < MIN(oldlength, newlength); i++)
   {
     newString[i] = string[i];
   }
