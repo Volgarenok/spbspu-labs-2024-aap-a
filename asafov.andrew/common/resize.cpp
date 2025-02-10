8fa26fd7cdc938bb0023be0f380f51afd8af9750
Asafow Andrew <neforscist@gmail.com>
2025-02-10 15:31:48 +0300

    fix no.82

--- asafov.andrew/common/resize.cpp
+++ asafov.andrew/common/resize.cpp
@@ -1,10 +1,20 @@
 #include "resize.hpp"
-#include <algorithm>
+
+static size_t ullMin(size_t a, size_t b)
+{
+  if (a < b){
+    return a;
+  }
+  else
+  {
+    return b;
+  }
+}
 
 char* asafov::resize(const char* string, size_t oldlength, size_t newlength)
 {
   char* newString = new char[newlength + 1];
-  for (size_t i = 0; i < std::min(oldlength, newlength); i++)
+  for (size_t i = 0; i < ullMin(oldlength, newlength); i++)
   {
     newString[i] = string[i];
   }
