fc03f17ae577a555857344f13470e4f1fb3259f4
Asafow Andrew <neforscist@gmail.com>
2025-02-10 15:56:01 +0300

    fix no.86

--- asafov.andrew/common/resize.cpp
+++ asafov.andrew/common/resize.cpp
@@ -1,11 +1,14 @@
 #include "resize.hpp"
 
-#define MIN(a, b) (a < b) ? (a) : (b)
+size_t asafov::ullMin(size_t a, size_t b)
+{
+  return (a < b) ? (a) : (b);
+}
 
 char* asafov::resize(const char* string, size_t oldlength, size_t newlength)
 {
   char* newString = new char[newlength + 1];
-  for (size_t i = 0; i < MIN(oldlength, newlength); i++)
+  for (size_t i = 0; i < ullMin(oldlength, newlength); i++)
   {
     newString[i] = string[i];
   }
--- asafov.andrew/common/resize.hpp
+++ asafov.andrew/common/resize.hpp
@@ -3,6 +3,7 @@
 #include <cstddef>
 namespace asafov
 {
+  size_t ullMin(size_t a, size_t b);
   char* resize(const char* string, size_t oldlength, size_t newlength);
 }
 #endif
