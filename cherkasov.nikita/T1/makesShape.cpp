8e7bd4eefcf3812cbc7d9d0b766d807b099bf596
cherkasoov <cherkasov5509@gmail.com>
2025-02-11 14:57:46 +0300

    .......

--- cherkasov.nikita/T1/makesShape.cpp
+++ cherkasov.nikita/T1/makesShape.cpp
@@ -9,7 +9,7 @@ cherkasov::Rectangle* cherkasov::getRectangle(std::istream& input)
   double x2 = 0.0;
   double y2 = 0.0;
   input >> x1 >> y1 >> x2 >> y2;
-  if (!input || x1 >= x2 || y1 >= y2)
+  if (!input)
   {
     throw std::invalid_argument("no input coordinate");
   }
@@ -17,9 +17,11 @@ cherkasov::Rectangle* cherkasov::getRectangle(std::istream& input)
 }
 cherkasov::Square* cherkasov::getSquare(std::istream& input)
 {
-  double x = 0.0, y = 0.0, length = 0.0;
+  double x = 0.0;
+  double y = 0.0;
+  double length = 0.0;
   input >> x >> y >> length;
-  if (!input || length <= 0)
+  if (!input)
   {
     throw std::invalid_argument("no input coordinate");
   }
@@ -34,13 +36,9 @@ cherkasov::Parallelogram* cherkasov::getParallelogram(std::istream& input)
   double x3 = 0.0;
   double y3 = 0.0;
   input >> x1 >> y1 >> x2 >> y2 >> x3 >> y3;
-  if (!input || (x1 == x3 && y2 == y3) || (x2 == x3 && y1 == y3))
+  if (!input)
   {
     throw std::invalid_argument("no correct coordinat the parallelogram");
-  }
-  if (!(y1 == y2 || y1 == y3 || y2 == y3))
-  {
-    throw std::invalid_argument("sides of the parallelogram must parallel the x");
   }
   return new Parallelogram(x1, y1, x2, y2, x3, y3);
 }
@@ -53,14 +51,9 @@ cherkasov::Diamond* cherkasov::getDiamond(std::istream& input)
   double x3 = 0.0;
   double y3 = 0.0;
   input >> x1 >> y1 >> x2 >> y2 >> x3 >> y3;
-  if (!input || ((x1 == x2 && y1 == y2) && (x1 == x3 && y1 == y3)))
+  if (!input)
   {
     throw std::invalid_argument("no input coordinat");
   }
-  if ((x1 == x2 && y1 == y2) || (x1 == x3 && y1 == y3) || (x2 == x3 && y2 == y3))
-  {
-    throw std::invalid_argument("the vertices don't have to be equa");
-  }
   return new Diamond(x1, y1, x2, y2, x3, y3);
 }
-
