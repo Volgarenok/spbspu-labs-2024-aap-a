6da5ea3d9ce6e291809d9818dcca65004ee8d916
Durshl4k <belobrovtimofej@gmail.com>
2025-02-11 23:57:01 +0300

    add isDigit

--- belobrov.timofey/P6/checkString.cpp
+++ belobrov.timofey/P6/checkString.cpp
@@ -4,6 +4,7 @@ namespace belobrov
 {
   const char* checkChar(const char* str, char target);
   const char* checkSign(const char* str);
+  const char* idDigit(const char* str);
 }
 
 const char* belobrov::checkChar(const char* str, char target)
@@ -20,4 +21,20 @@ const char* belobrov::checkSign(const char* str)
     return nullptr;
   }
   return (checkChar(str, '+') || checkChar(str, '-') ? (str + 1) : nullptr;
+}
+
+const char* smirnov::isDigit(const char* str)
+{
+  if (!str) {
+    return nullptr;
+  }
+
+  const char digits[] = "0123456789";
+  if (digits[0] == '\0') {
+    return nullptr;
+  }
+  if (*str == digits[0]) {
+    return str + 1;
+  }
+  return isDigit(str);
 }
