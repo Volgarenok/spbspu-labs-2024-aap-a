54e08398181a38791ec46348899a72e586f8dc60
Durshl4k <belobrovtimofej@gmail.com>
2025-02-11 23:48:30 +0300

    add checkSign

--- belobrov.timofey/P6/checkString.cpp
+++ belobrov.timofey/P6/checkString.cpp
@@ -3,13 +3,21 @@
 namespace belobrov
 {
   const char* checkChar(const char* str, char target);
+  const char* checkSign(const char* str);
 }
 
 const char* belobrov::checkChar(const char* str, char target)
 {
-    if (!str)
-    {
-        return nullptr;
-    }
-    return (*str == target) ? (str + 1) : nullptr;
+  if (!str){
+    return nullptr;
+  }
+  return (*str == target) ? (str + 1) : nullptr;
+}
+
+const char* belobrov::checkSign(const char* str)
+{
+  if (!str){
+    return nullptr;
+  }
+  return (checkChar(str, '+') || checkChar(str, '-') ? (str + 1) : nullptr;
 }
