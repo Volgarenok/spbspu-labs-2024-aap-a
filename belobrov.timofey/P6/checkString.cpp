2b5af94278b089c5a999b7eb59c65d2be72ef794
Durshl4k <belobrovtimofej@gmail.com>
2025-02-11 22:42:12 +0300

    add checkChar

--- belobrov.timofey/P6/checkString.cpp
+++ belobrov.timofey/P6/checkString.cpp
@@ -1,1 +1,15 @@
 #include "checkString.hpp"
+
+namespace belobrov
+{
+  const char* checkChar(const char* str, char target);
+}
+
+const char* belobrov::checkChar(const char* str, char target)
+{
+    if (!str)
+    {
+        return nullptr;
+    }
+    return (*str == target) ? (str + 1) : nullptr;
+}
