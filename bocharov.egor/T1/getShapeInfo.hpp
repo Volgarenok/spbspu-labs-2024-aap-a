761e67f8ddcd203d42d939c54c4f94eaf1262873
LuckyBichar <enbocharov2006@gmail.com>
2025-02-12 22:43:49 +0300

    fix

--- bocharov.egor/T1/getShapeInfo.hpp
+++ bocharov.egor/T1/getShapeInfo.hpp
@@ -8,7 +8,7 @@ namespace bocharov
 {
   size_t getShapeInfo(std::istream & input, std::ostream & error, std::ostream & output, Shape ** myShapes);
   void clear(Shape ** myshape, size_t shapeCount);
-  double getAllArea(Shape ** const myShapes, size_t shapeCount);
+  double getAllArea(const Shape * const * myShapes, size_t shapeCount);
   void scaling(Shape ** myShapes, size_t shapeCount, point_t center, double ratio);
 }
 
