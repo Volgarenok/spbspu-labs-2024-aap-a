9d4e8e4e63c96111420058ab04d46a860d015e11
shycoshy <neforscist@gmail.com>
2025-02-12 07:21:58 +0000

    fix no.88

--- asafov.andrew/common/resize.cpp
+++ asafov.andrew/common/resize.cpp
@@ -1,8 +1,9 @@
 #include "resize.hpp"
+#include <algorithm>
 
 char* asafov::resize(const char* string, size_t oldlength, size_t newlength)
 {
-  char* newString = new char[newlength + 1];
+  char* newString = new char[newlength];
   for (size_t i = 0; i < (newlength > oldlength) ? (oldlength) : (newlength); i++)
   {
     newString[i] = string[i];
