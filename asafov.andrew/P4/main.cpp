1e2c9222f5496a25bfdb5c317d7576b265ad6f49
shycoshy <neforscist@gmail.com>
2025-02-14 12:47:21 +0000

    fix no.93

--- asafov.andrew/P4/main.cpp
+++ asafov.andrew/P4/main.cpp
@@ -13,24 +13,11 @@ int main()
 
   std::cout << asafov::countUniqLetters(str) << '\n';
 
-  char* unusedLetters = nullptr;
-  try
-  {
-    unusedLetters = new char[26];
-  }
-  catch (const std::bad_alloc&)
-  {
-    delete[] str;
-    return 1;
-  }
-  for (size_t i = 0; i < 26; i++)
-  {
-    unusedLetters[i] = '\0';
-  }
+  char unusedLetters[26] = {};
+  
   asafov::getUnusedLetters(str, unusedLetters);
   std::cout << unusedLetters << '\n';
 
-  delete[] unusedLetters;
   delete[] str;
   return 0;
 }
