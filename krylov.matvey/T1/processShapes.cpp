75f0e5c95ecbd7aafeefa715758e56ca6f0e09e7
root <root@kryloff.>
2025-02-14 13:43:41 +0300

    Fixes

--- krylov.matvey/T1/processShapes.cpp
+++ krylov.matvey/T1/processShapes.cpp
@@ -7,25 +7,14 @@
 #include "triangle.hpp"
 #include "complexquad.hpp"
 
-
 krylov::Shape* krylov::createRectangle(std::istream& in)
 {
   double x1 = 0.0, y1 = 0.0, x2 = 0.0, y2 = 0.0;
-  Shape* rectangle = nullptr;
   if (!(in >> x1 >> y1 >> x2 >> y2))
   {
     throw std::invalid_argument("Invalid RECTANGLE parameters");
   }
-  try
-  {
-    rectangle = new krylov::Rectangle({x1, y1}, {x2, y2});
-    return rectangle;
-  }
-  catch (const std::exception& e)
-  {
-    if (rectangle) delete rectangle;
-    throw;
-  }
+  return new krylov::Rectangle({x1, y1}, {x2, y2});
 }
 
 krylov::Shape* krylov::createTriangle(std::istream& in)
@@ -33,41 +22,21 @@ krylov::Shape* krylov::createTriangle(std::istream& in)
   double x1 = 0.0, y1 = 0.0;
   double x2 = 0.0, y2 = 0.0;
   double x3 = 0.0, y3 = 0.0;
-  Shape* triangle = nullptr;
   if (!(in >> x1 >> y1 >> x2 >> y2 >> x3 >> y3))
   {
     throw std::invalid_argument("Invalid TRIANGLE parameters");
   }
-  try
-  {
-    triangle = new krylov::Triangle({x1, y1}, {x2, y2}, {x3, y3});
-    return triangle;
-  }
-  catch (const std::exception& e)
-  {
-    if (triangle) delete triangle;
-    throw;
-  }
+  return new krylov::Triangle({x1, y1}, {x2, y2}, {x3, y3});
 }
 
 krylov::Shape* krylov::createRing(std::istream& in)
 {
   double x, y, outerRadius, innerRadius;
-  Shape* ring = nullptr;
   if (!(in >> x >> y >> outerRadius >> innerRadius))
   {
     throw std::invalid_argument("Invalid RING parameters");
   }
-  try
-  {
-    ring = new krylov::Ring({x, y}, outerRadius, innerRadius);
-    return ring;
-  }
-  catch (const std::exception& e)
-  {
-    if (ring) delete ring;
-    throw;
-  }
+  return new krylov::Ring({x, y}, outerRadius, innerRadius);
 }
 
 krylov::Shape* krylov::createComplexquad(std::istream& in)
@@ -76,21 +45,11 @@ krylov::Shape* krylov::createComplexquad(std::istream& in)
   double x2 = 0.0, y2 = 0.0;
   double x3 = 0.0, y3 = 0.0;
   double x4 = 0.0, y4 = 0.0;
-  Shape* complexquad = nullptr;
   if (!(in >> x1 >> y1 >> x2 >> y2 >> x3 >> y3 >> x4 >> y4))
   {
     throw std::invalid_argument("Invalid COMPLEXQUAD parameters");
   }
-  try
-  {
-    complexquad = new krylov::Complexquad({x1, y1}, {x2, y2}, {x3, y3}, {x4, y4});
-    return complexquad;
-  }
-  catch (const std::exception& e)
-  {
-    if (complexquad) delete complexquad;
-    throw;
-  }
+  return new krylov::Complexquad({x1, y1}, {x2, y2}, {x3, y3}, {x4, y4});
 }
 
 void krylov::deleteShapes(krylov::Shape** shapes, size_t shapeCount)
