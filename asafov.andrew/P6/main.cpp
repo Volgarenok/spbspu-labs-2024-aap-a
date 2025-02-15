e7436135928fdcefcfbfc945f72da013a8343c1f
shycoshy <neforscist@gmail.com>
2025-02-15 14:11:11 +0000

    fix no.87

--- asafov.andrew/P6/main.cpp
+++ asafov.andrew/P6/main.cpp
@@ -1,11 +1,11 @@
 #include <iostream>
 #include <cstddef>
-#include "../../asafov.andrew/common/inputString.hpp"
+#include <inputString.hpp>
 #include "isExpression.hpp"
 
 int main()
 {
-  char* str = asafov::inputString();
+  char* str = asafov::inputString(std::cin);
   size_t len = 0;
   for (size_t i = 0; str[i] != '\0'; i++)
   {
