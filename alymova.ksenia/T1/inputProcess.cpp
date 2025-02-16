0fa37b4b55b99762ae7d268975342c818d7d07a5
Ksenia-rgb <ksenialymova@mail.ru>
2025-02-16 22:50:41 +0300

    fix3

--- alymova.ksenia/T1/inputProcess.cpp
+++ alymova.ksenia/T1/inputProcess.cpp
@@ -20,18 +20,18 @@ void alymova::makeShape(std::istream& in, CompositeShape& shapes, bool& wrong_sh
     }
     std::string type;
     in >> type;
-    if (type == "SCALE")
-    {
-      readParameters(in, scale_params, 3);
-      scale_flag = true;
-      continue;
-    }
 
     Shape* shape = nullptr;
     try
     {
       shape = fabric->create(type, in);
       shapes.push_back(shape);
+
+      if (type == "SCALE")
+      {
+        readParameters(in, scale_params, 3);
+        scale_flag = true;
+      }
     }
     catch (const std::logic_error& e)
     {
@@ -41,6 +41,7 @@ void alymova::makeShape(std::istream& in, CompositeShape& shapes, bool& wrong_sh
     catch (const std::bad_alloc& e)
     {
       delete shape;
+      delete fabric;
       throw;
     }
   }
