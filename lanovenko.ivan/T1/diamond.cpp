38f29ec7452b25bdbbdca28c51085e98929e8e4b
Tuc0Sa1amanka <sparowj765@gmail.com>
2025-02-16 13:11:39 +0300

    fixed large duplication in sorting and fixed CG

--- lanovenko.ivan/T1/diamond.cpp
+++ lanovenko.ivan/T1/diamond.cpp
@@ -1,42 +1,81 @@
 #include "diamond.hpp"
-#include "base-types.hpp"
-#include "cmath"
+#include <stdexcept>
+#include <cmath>
 
-lanovenko::Diamond::Diamond(point_t center, point_t py, point_t px) : center_(center), py_(py), px_(px) {}
+lanovenko::Diamond::Diamond(point_t p1, point_t p2, point_t p3): 
+	center_(p1), 
+	vertical_(p2), 
+	horizontal_(p3)
+{
+	bool valid = false;
+	point_t points[3] = { p1, p2, p3 };
+	for (size_t i = 0; i < 3; i++)
+	{
+		point_t candidate = points[i];
+		point_t other1 = points[(i + 1) % 3];
+		point_t other2 = points[(i + 2) % 3];
+		if (candidate.x == other1.x && candidate.y == other2.y)
+		{
+			valid = true;
+			center_ = candidate;
+			vertical_ = other1;
+			horizontal_ = other2;
+			break;
+		}
+		if (candidate.x == other2.x && candidate.y == other1.y)
+		{
+			valid = true;
+			center_ = candidate;
+			vertical_ = other2;
+			horizontal_ = other1;
+			break;
+		}
+	}
+	if (!valid)
+	{
+		throw std::invalid_argument("Wrong Diamond parametrs");
+	}
+}
 
 double lanovenko::Diamond::getArea() const
 {
-  return std::fabs((center_.x - px_.x) * (center_.y - py_.y) * 2);
+	double d1 = std::fabs(center_.x - horizontal_.x) * 2.0;
+	double d2 = std::fabs(center_.y - vertical_.y) * 2.0;
+	return (d1 * d2) / 2.0;
 }
 
 lanovenko::rectangle_t lanovenko::Diamond::getFrameRect() const
 {
-  return { std::fabs((center_.x - px_.x)) * 2, std::fabs((center_.y - py_.y) * 2), center_ };
+	double width = std::fabs(center_.x - horizontal_.x) * 2.0;
+	double heigth = std::fabs(center_.y - vertical_.y) * 2.0;
+	return { width, heigth, center_ };
+}
+
+void lanovenko::Diamond::move(point_t p)
+{
+	double deltaX = p.x - center_.x;
+	double deltaY = p.y - center_.y;
+	move(deltaX, deltaY);
 }
 
 void lanovenko::Diamond::move(double dx, double dy)
 {
-  center_.x += dx;
-  center_.y += dy;
-  py_.x += dx;
-  py_.y += dy;
-  px_.x += dx;
-  px_.y += dy;
+	point_t* points[3] = { &center_, &horizontal_, &vertical_ };
+	for (size_t i = 0; i < 3; i++)
+	{
+		points[i]->x += dx;
+		points[i]->y += dy;
+	}
 }
 
-void lanovenko::Diamond::move(point_t a)
+void lanovenko::Diamond::unsafeScale(double k)
 {
-  double bias_x = a.x - center_.x;
-  double bias_y = a.y - center_.y;
-  center_ = a;
-  py_.x += bias_x;
-  py_.y += bias_y;
-  px_.x += bias_x;
-  px_.y += bias_y;
-}
-
-void lanovenko::Diamond::unsScale(double k)
-{
-  py_.y = center_.y + (py_.y - center_.y) * k;
-  px_.x = center_.x + (px_.x - center_.x) * k;
+	double d1 = std::fabs(center_.x - horizontal_.x) * 2.0;
+	double d2 = std::fabs(center_.y - vertical_.y) * 2.0;
+	double new_d1 = d1 * k;
+	double new_d2 = d2 * k;
+	double deltaX = (new_d1 - d1) / 2.0;
+	double deltaY = (new_d2 - d2) / 2.0;
+	vertical_.y += deltaY;
+	horizontal_.x -= deltaX;
 }
--- lanovenko.ivan/T1/diamond.hpp
+++ lanovenko.ivan/T1/diamond.hpp
@@ -1,26 +1,25 @@
 #ifndef DIAMOND_HPP
 #define DIAMOND_HPP
-#include "shape.hpp"
 
+#include "shape.hpp"
 
 namespace lanovenko
 {
-  class Diamond : public Shape
-  {
-  public:
-    Diamond(point_t center, point_t py, point_t px);
-    virtual double getArea() const;
-    virtual rectangle_t getFrameRect() const;
-    virtual void move(point_t p);
-    virtual void move(double dx, double dy);
-    void unsScale(double k);
-
-  private:
-    point_t center_;
-    point_t py_;
-    point_t px_;
-  };
+	class Diamond final: public Shape
+	{
+	public:
+		~Diamond() = default;
+		Diamond(point_t p1, point_t p2, point_t p3);
+		virtual double getArea() const override;
+		virtual rectangle_t getFrameRect() const override;
+		virtual void move(point_t p) override;
+		virtual void move(double dx, double dy) override;
+		virtual void unsafeScale(double k) override;
+	private:
+		point_t center_;
+		point_t vertical_;
+		point_t horizontal_;
+	};
 }
-
 
 #endif
