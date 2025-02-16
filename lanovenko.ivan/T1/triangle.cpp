3cd60d10f9d20f3bf2f8452ef03c8c6bd651dce5
Tuc0Sa1amanka <sparowj765@gmail.com>
2025-02-16 13:17:06 +0300

    fixed cg triangle

--- lanovenko.ivan/T1/triangle.cpp
+++ lanovenko.ivan/T1/triangle.cpp
@@ -1,83 +1,81 @@
-#include "traingle.hpp"
-#include <cmath>
-#include <algorithm>
+#include "triangle.hpp"
 #include <stdexcept>
+#include <cmath>
 
-lanovenko::Triangle::Triangle(point_t p1, point_t p2, point_t p3): p1_(p1), p2_(p2), p3_(p3)
+lanovenko::Triangle::Triangle(point_t p1, point_t p2, point_t p3): 
+	first_(p1), 
+	second_(p2), 
+	third_(p3)
 {
-  double d = p1_.x * (p2_.y - p3_.y) - p2_.x * (p1_.y - p3_.y) + p3_.x * (p1_.y - p2_.y);
-  if (d == 0)
-  {
-    throw std::invalid_argument("Wrong trangle parametrs!\n");
-  }
+	double orientedArea = first_.x * (second_.y - third_.y) + second_.x * (third_.y - first_.y) + third_.x * (first_.y - second_.y);
+	if (orientedArea == 0)
+	{
+		throw std::logic_error("Wrong Triangle parametrs");
+	}
 }
-
-double lanovenko::Triangle::getDistance(const point_t p1, const point_t p2) const
-{
-  return std::sqrt(std::pow(p2.x - p1.x, 2) + std::pow(p2.y - p1.y, 2));
-}
-
 
 double lanovenko::Triangle::getArea() const
 {
-  double a = Triangle::getDistance(p1_, p2_);
-  double b = Triangle::getDistance(p2_, p3_);
-  double c = Triangle::getDistance(p3_, p1_);
-  double half_per = (a + b + c) / 2.0;
-  return std::sqrt(half_per * (half_per - a) * (half_per - b) * (half_per - c));
+	double a = Triangle::getDistance(first_, second_);
+	double b = Triangle::getDistance(second_, third_);
+	double c = Triangle::getDistance(third_, first_);
+	double semiperimeter = (a + b + c) / 2.0;
+	return std::sqrt(semiperimeter * (semiperimeter - a) * (semiperimeter - b) * (semiperimeter - c));
 }
 
 lanovenko::rectangle_t lanovenko::Triangle::getFrameRect() const
 {
-  double up = std::max(p1_.y, std::max(p2_.y, p3_.y));
-  double down = std::min(p1_.y, std::min(p2_.y, p3_.y));
-  double right = std::max(p1_.x, std::max(p2_.x, p3_.x));
-  double left = std::min(p1_.x, std::min(p2_.x, p3_.x));
-  point_t r_p1 = {left, down};
-  point_t r_p2 = {left, up};
-  point_t r_p3 = {right, up};
-  point_t r_p4 = {right, down};
-  double height = up - down;
-  double width = right - left;
-  double r_center_x = (r_p1.x + r_p2.x + r_p3.x + r_p4.x) / 4;
-  double r_center_y = (r_p1.y + r_p2.y + r_p3.y + r_p4.y) / 4;
-  point_t r_center = {r_center_x, r_center_y};
-  rectangle_t rect = {width, height, r_center};
-  return rect;
-
+	double up = std::max(first_.y, std::max(second_.y, third_.y));
+	double down = std::min(first_.y, std::min(second_.y, third_.y));
+	double right = std::max(first_.x, std::max(second_.x, third_.x));
+	double left = std::min(first_.x, std::min(second_.x, third_.x));
+	point_t rectFirst = { left, down };
+	point_t rectSecond = { left, up };
+	point_t rectThird = { right, up };
+	point_t rectFourth = { right, down };
+	double centerGravityX = (rectFirst.x + rectSecond.x + rectThird.x + rectFourth.x) / 4.0;
+	double centerGravityY = (rectFirst.y + rectSecond.y + rectThird.y + rectFourth.y) / 4.0;
+	point_t gravityCenter = { centerGravityX, centerGravityY };
+	double width = right - left;
+	double height = up - down;
+	return { width, height, gravityCenter };
 }
 
-void lanovenko::Triangle::move(const point_t point)
+void lanovenko::Triangle::move(point_t p)
 {
-  double center_x = (p1_.x + p2_.x + p3_.x) / 3;
-  double center_y = (p1_.y + p2_.y + p3_.y) / 3;
-  point_t center = {center_x, center_y};
-  double dx = point.x - center.x;
-  double dy = point.y - center.y;
-  this->move(dx, dy);
+	double centerX = (first_.x + second_.x + third_.x) / 3.0;
+	double centerY = (first_.y + second_.y + third_.y) / 3.0;
+	point_t center = { centerX, centerY };
+	double deltaX = p.x - center.x;
+	double deltaY = p.y - center.y;
+	move(deltaX, deltaY);
+}
+
+void lanovenko::Triangle::move(double dx, double dy)
+{
+	point_t* points[3] = { &first_, &second_, &third_ };
+	for (size_t i = 0; i < 3; i++)
+	{
+		points[i]->x += dx;
+		points[i]->y += dy;
+	}
+}
+
+void lanovenko::Triangle::unsafeScale(double k)
+{
+	double centerX = (first_.x + second_.x + third_.x) / 3.0;
+	double centerY = (first_.y + second_.y + third_.y) / 3.0;
+	point_t center = { centerX, centerY };
+	point_t* points[3] = { &first_, &second_, &third_ };
+	for (size_t i = 0; i < 3; i++)
+	{
+		points[i]->x = center.x + (points[i]->x - center.x) * k;
+		points[i]->y = center.y + (points[i]->y - center.y) * k;
+	}
 }
 
 
-
-void lanovenko::Triangle::move(double offset_x, double offset_y)
+double lanovenko::Triangle::getDistance(point_t p1, point_t p2) const
 {
-  point_t* points[] = {&p1_, &p2_, &p3_};
-  for (size_t i = 0; i < 3; i++)
-  {
-    points[i]->x += offset_x;
-    points[i]->y += offset_y;
-  }
-}
-
-void lanovenko::Triangle::unsScale(double k)
-{
-  double center_x = (p1_.x + p2_.x + p3_.x) / 3;
-  double center_y = (p1_.y + p2_.y + p3_.y) / 3;
-  point_t center = {center_x, center_y};
-  point_t* points[] = {&p1_, &p2_, &p3_};
-  for (size_t i = 0; i < 3; i++)
-  {
-    points[i]->x = center.x + (points[i]->x - center.x) * k;
-    points[i]->y = center.y + (points[i]->y - center.y) * k;
-  }
+	return std::sqrt(std::pow((p2.x - p1.x), 2.0) + std::pow((p2.y - p1.y), 2.0));
 }
