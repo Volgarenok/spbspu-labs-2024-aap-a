007a65cd0b631f520867f4b8bd7e294d1cfe2fb8
Tuc0Sa1amanka <sparowj765@gmail.com>
2025-02-16 13:20:45 +0300

    new main

--- lanovenko.ivan/T1/main.cpp
+++ lanovenko.ivan/T1/main.cpp
@@ -1,122 +1,74 @@
 #include <iostream>
-#include <iomanip>
-#include <cstring>
-#include "input_string.hpp"
-#include "base-types.hpp"
+#include <string>
 #include "shape.hpp"
-#include "rectangle.hpp"
-#include "traingle.hpp"
-#include "input_shape.hpp"
-#include "string_manipulation.hpp"
-
-using namespace lanovenko;
+#include "string_manipulations.hpp"
+#include "delete_shape.hpp"
+#include "scale_functions.hpp"
 
 int main()
 {
-  constexpr size_t maxcapasity = 1000;
-  char* str = nullptr;
-  size_t shapeNumber = 0;
-  Shape* shapeArray[maxcapasity] = { nullptr };
-  bool errors = false;
-  bool scale_was_met = false;
-
-  while (!scale_was_met)
-  {
-    if (!std::cin.good())
-    {
-      std::cerr << "Wrong input!\n";
-      for (size_t i = 0; i < shapeNumber; i++)
-      {
-        delete shapeArray[i];
-      }
-      return 1;
-    }
-
-    try
-    {
-      str = inputStr(std::cin);
-    }
-    catch (const std::logic_error& e)
-    {
-      std::cerr << e.what() << "\n";
-      delete[] str;
-      for (size_t i = 0; i < shapeNumber; i++)
-      {
-        delete shapeArray[i];
-      }
-      return 2;
-    }
-    constexpr size_t shapes_count = 5;
-    const char* shape_names[shapes_count] = { "RECTANGLE", "TRIANGLE", "DIAMOND", "PARALLELOGRAM", "SCALE"};
-    try
-    {
-      for (size_t i = 0; i < shapes_count; i++)
-      {
-        size_t name_size = std::strlen(shape_names[i]);
-        if (std::strncmp(shape_names[i], str, name_size) == 0)
-        {
-          if (i == 0)
-          {
-            Shape* shape = inputRectangle(str);
-            if (shape != nullptr)
-            {
-              shapeArray[shapeNumber++] = shape;
-            }
-          }
-          if (i == 1)
-          {
-            Shape* shape = inputTriangle(str);
-            if (shape != nullptr)
-            {
-              shapeArray[shapeNumber++] = shape;
-            }
-          }
-          if (i == 2)
-          {
-            Shape* shape = inputDiamond(str);
-            if (shape != nullptr)
-            {
-              shapeArray[shapeNumber++] = shape;
-            }
-          }
-          if (i == 3)
-          {
-            Shape* shape = inputParallelogram(str);
-            if (shape != nullptr)
-            {
-              shapeArray[shapeNumber++] = shape;
-            }
-          }
-          if (i == 4)
-          {
-            scale_was_met = true;
-            scaleCommand(str, shapeArray, shapeNumber, std::cout);
-          }
-        }
-      }
-    }
-    catch (const std::invalid_argument& e)
-    {
-      errors = true;
-    }
-    catch (const std::logic_error& e)
-    {
-      std::cerr << e.what() << "\n";
-      delete[] str;
-      for (size_t i = 0; i < shapeNumber; i++)
-      {
-        delete shapeArray[i];
-      }
-      return 1;
-    }
-    delete[] str;
-  }
-  if (errors)
-  {
-    std::cerr << "Some names are incorrect!\n";
-  }
-  for (size_t i = 0; i < shapeNumber; i++)
-  {
-    delete shapeArray[i];
-  }
+	using namespace lanovenko;
+	bool scale = false;
+	bool errors = false;
+	size_t res = 0;
+	Shape* shapeArray[10000] = { nullptr };
+	while (scale != true)
+	{
+		std::string str = "";
+		Shape* currentFigure = nullptr;
+		try
+		{
+			str = inputStr(std::cin);
+		}
+		catch (const std::runtime_error& e)
+		{
+			deleteShape(shapeArray, res);
+			std::cerr << e.what() << "\n";
+			return 1;
+		}
+		if (str.find("SCALE") == std::string::npos)
+		{
+			try
+			{
+				currentFigure = parseShape(str);
+				if (currentFigure != nullptr)
+				{
+					shapeArray[res++] = currentFigure;
+				}
+				else
+				{
+					errors = true;
+				}
+			}
+			catch (const std::bad_alloc& e)
+			{
+				deleteShape(shapeArray, res);
+				std::cerr << "Out of memmory!\n";
+				return 1;
+			}
+		}
+		if (str.find("SCALE") != std::string::npos)
+		{
+			scale = true;
+			double* scaleParametrs = nullptr;
+			scaleParametrs = paraseScale(str);
+			try
+			{
+				scaleShapes(shapeArray, scaleParametrs, std::cout, res);
+			}
+			catch (const std::invalid_argument& e)
+			{
+				deleteShape(shapeArray, res);
+				delete[] scaleParametrs;
+				std::cerr << e.what() << "\n";
+				return 1;
+			}
+			delete[] scaleParametrs;
+		}
+	}
+	if (errors == true)
+	{
+		std::cerr << "Some figures are incorrectly described!\n";
+	}
+	deleteShape(shapeArray, res);
 }
