ba6a99d1c93b801680193e28cb934dde96a99c7d
Tuc0Sa1amanka <sparowj765@gmail.com>
2025-02-16 12:54:20 +0300

    new scale function

--- lanovenko.ivan/T1/shape.cpp
+++ lanovenko.ivan/T1/shape.cpp
@@ -3,9 +3,5 @@
 
 void lanovenko::Shape::scale(double k)
 {
-  if (k < 0)
-  {
-    throw std::logic_error("Scaling coeficient is negative\n");
-  }
-  unsScale(k);
+	unsafeScale(k);
 }
--- lanovenko.ivan/T1/shape.hpp
+++ lanovenko.ivan/T1/shape.hpp
@@ -1,20 +1,21 @@
 #ifndef SHAPE_HPP
 #define SHAPE_HPP
+
 #include "base-types.hpp"
 
 namespace lanovenko
 {
-  class Shape
-  {
-  public:
-    virtual ~Shape() = default;
-    virtual double getArea() const = 0;
-    virtual rectangle_t getFrameRect() const = 0;
-    virtual void move(point_t p) = 0;
-    virtual void move(double dx, double dy) = 0;
-    virtual void unsScale(double k) = 0;
-    void scale(double k);
-  };
+	class Shape
+	{
+	public:
+		virtual ~Shape() = default;
+		virtual double getArea() const = 0;
+		virtual rectangle_t getFrameRect() const = 0;
+		virtual void move(point_t p) = 0;
+		virtual void move(double dx, double dy) = 0;
+		virtual void unsafeScale(double k) = 0;
+		void scale(double k);
+	};
 }
 
 #endif
