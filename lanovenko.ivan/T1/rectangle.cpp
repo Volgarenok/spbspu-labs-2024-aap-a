8a1f4b8f3e82b558cfcf75a858496d0dfd358584
Tuc0Sa1amanka <sparowj765@gmail.com>
2025-02-16 13:09:06 +0300

    Fixed duplication and CG

--- lanovenko.ivan/T1/rectangle.cpp
+++ lanovenko.ivan/T1/rectangle.cpp
@@ -1,60 +1,60 @@
 #include "rectangle.hpp"
 #include <stdexcept>
 
-lanovenko::Rectangle::Rectangle(point_t p1, point_t p2) : leftlower(p1), rightupper(p2)
+lanovenko::Rectangle::Rectangle(point_t p1, point_t p2): 
+	leftDown_(p1), 
+	rightUpper_(p2)
 {
-  if (leftlower.x >= rightupper.x && leftlower.y >= rightupper.y)
-  {
-    throw std::invalid_argument("Wrong rectangle parametrs!\n");
-  }
+	if (leftDown_.x >= rightUpper_.x || leftDown_.y >= rightUpper_.y)
+	{
+		throw std::invalid_argument("Wrong Rectangle Parametrs");
+	}
 }
 
 double lanovenko::Rectangle::getArea() const
 {
-  double width = std::abs(rightupper.x - leftlower.x);
-  double height = std::abs(rightupper.y - leftlower.y);
-  return width * height;
+	double width = rightUpper_.x - leftDown_.x;
+	double height = rightUpper_.y - leftDown_.y;
+	return width * height;
 }
 
 lanovenko::rectangle_t lanovenko::Rectangle::getFrameRect() const
 {
-  double width = std::abs(rightupper.x - leftlower.x);
-  double height = std::abs(rightupper.y - leftlower.y);
-  point_t center = { (rightupper.x + leftlower.x) / 2, (rightupper.y + leftlower.y) / 2 };
-  return { width, height, center };
+	double width = rightUpper_.x - leftDown_.x;
+	double height = rightUpper_.y - leftDown_.y;
+	point_t center = { (rightUpper_.x + leftDown_.x) / 2.0, (rightUpper_.y + leftDown_.y) / 2.0 };
+	return { width, height, center };
 }
 
 void lanovenko::Rectangle::move(point_t p)
 {
-  point_t center = { (rightupper.x + leftlower.x) / 2, (rightupper.y + leftlower.y) / 2 };
-  double center_bias_x = p.x - center.x;
-  double center_bias_y = p.y - center.y;
-  this->move(center_bias_x, center_bias_y);
+	point_t center = { (rightUpper_.x + leftDown_.x) / 2.0, (rightUpper_.y + leftDown_.y) / 2.0 };
+	double deltaX = p.x - center.x;
+	double deltaY = p.y - center.y;
+	center = p;
+	move(deltaX, deltaY);
 }
 
 void lanovenko::Rectangle::move(double dx, double dy)
 {
-  point_t center = { (rightupper.x + leftlower.x) / 2, (rightupper.y + leftlower.y) / 2 };
-  center.x += dx;
-  center.y += dy;
-  point_t* points[] = { &leftlower, &rightupper };
-  for (size_t i = 0; i < 2; i++)
-  {
-    points[0]->x += dx;
-    points[0]->y += dy;
-  }
+	point_t* points[2] = { &leftDown_, &rightUpper_ };
+	for (size_t i = 0; i < 2; i++)
+	{
+		points[i]->x += dx;
+		points[i]->y += dy;
+	}
 }
 
-void::lanovenko::Rectangle::unsScale(double k)
+void lanovenko::Rectangle::unsafeScale(double k)
 {
-  double width = std::abs(rightupper.x - leftlower.x);
-  double height = std::abs(rightupper.y - leftlower.y);
-  double new_width = width * k;
-  double new_height = height * k;
-  double bias_x = (new_width - width) / 2;
-  double bias_y = (new_height - height) / 2;
-  leftlower.x -= bias_x;
-  leftlower.y -= bias_y;
-  rightupper.x += bias_x;
-  rightupper.y += bias_y;
+	double width = rightUpper_.x - leftDown_.x;
+	double height = rightUpper_.y - leftDown_.y;
+	double new_width = width * k;
+	double new_height = height * k;
+	double deltaX = (new_width - width) / 2.0;
+	double deltaY = (new_height - height) / 2.0;
+	rightUpper_.x += deltaX;
+	rightUpper_.y += deltaY;
+	leftDown_.x -= deltaX;
+	leftDown_.y -= deltaY;
 }
--- lanovenko.ivan/T1/rectangle.hpp
+++ lanovenko.ivan/T1/rectangle.hpp
@@ -1,22 +1,24 @@
 #ifndef RECTANGLE_HPP
 #define RECTANGLE_HPP
+
 #include "shape.hpp"
 
 namespace lanovenko
 {
-  class Rectangle : public Shape
-  {
-    public:
-      Rectangle(point_t p1, point_t p2);
-      virtual double getArea() const;
-      virtual rectangle_t getFrameRect() const;
-      virtual void move(point_t p);
-      virtual void move(double dx, double dy);
-      void unsScale(double k) override;
-    private:
-      point_t leftlower;
-      point_t rightupper;
-  };
+	class Rectangle final: public Shape
+	{
+	public:
+		~Rectangle() = default;
+		Rectangle(point_t p1, point_t p2);
+		virtual double getArea() const override;
+		virtual rectangle_t getFrameRect() const override;
+		virtual void move(point_t p) override;
+		virtual void move(double dx, double dy) override;
+		virtual void unsafeScale(double k) override;
+	private:
+		point_t leftDown_;
+		point_t rightUpper_;
+	};
 }
 
 #endif
