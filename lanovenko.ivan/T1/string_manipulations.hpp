b57f5aa4756dafce8eb7e65e7c903c8077bf4596
Tuc0Sa1amanka <sparowj765@gmail.com>
2025-02-16 13:57:26 +0300

    string manipulations

--- lanovenko.ivan/T1/string_manipulations.hpp
+++ lanovenko.ivan/T1/string_manipulations.hpp
@@ -1,15 +1,21 @@
-#ifndef STRING_MANIPULATION_HPP
-#define STRING_MANIPULATION_HPP
-#include <ostream>
+#ifndef STRING_MANIPULATIONS_HPP
+#define STRING_MANIPULATIONS_HPP
+
+#include <istream>
+#include <string>
 #include "shape.hpp"
-#include "base-types.hpp"
 
 namespace lanovenko
 {
-  void scaleCommand(const char* str, Shape* const* shapeArray, size_t capacity, std::ostream& out);
-  void scaleShapes(Shape* const* shapeArray, size_t shapeNumber, const double* arguments, std::ostream& out);
-  void toScale(Shape* shape, const point_t center, const double k);
-  void outputShapes(const Shape* const* shapeArray, size_t shapeNumbers, std::ostream& out);
+	std::string inputStr(std::istream& in);
+	Shape* parseShape(const std::string& str);
+	Shape* parseRectangle(const std::string& str);
+	Shape* parseDiamond(const std::string& str);
+	Shape* parseTriangle(const std::string& str);
+	lanovenko::Shape* parseParallelogram(const std::string& str);
+	std::size_t split(const std::string& str);
+	double* getCoords(const std::string& coords, size_t capacityCoords);
+	double* paraseScale(const std::string& str);
 }
 
-#endif
+#endif 
