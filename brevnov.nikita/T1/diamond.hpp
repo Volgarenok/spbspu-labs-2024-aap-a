393ca89cf37413f1f204eaf3026d728b6f4598d4
brev1ch <flirktv@gmail.com>
2025-02-19 01:28:22 +0300

    fix diamond

--- brevnov.nikita/T1/diamond.hpp
+++ brevnov.nikita/T1/diamond.hpp
@@ -19,9 +19,5 @@ namespace brevnov
     point_t horizontal_;
     point_t vertical_;
   };
-  point_t getCenter(point_t a, point_t b, point_t c);
-  point_t getHorizontal(point_t center, point_t a, point_t b, point_t c);
-  point_t getVertical(point_t center, point_t horizontal, point_t a, point_t b, point_t c);
-  bool isCorrectDiamond(point_t a, point_t b, point_t c);
 }
 #endif
