90edb6d9338ec98ccd95173b0aa72fe5b8e93438
LuckyBichar <enbocharov2006@gmail.com>
2025-02-18 10:32:03 +0300

    nadeus vse

--- bocharov.egor/T1/base-types.cpp
+++ bocharov.egor/T1/base-types.cpp
@@ -1,19 +1,19 @@
 #include "base-types.hpp"
-bocharov::point_t bocharov::scalePoint(bocharov::point_t p, bocharov::point_t pos, double ratio)
+bocharov::point_t bocharov::scalePoint(point_t p, point_t pos, double ratio)
 {
   p.x = (p.x - pos.x) * ratio + pos.x;
   p.y = (p.y - pos.y) * ratio + pos.y;
   return {p.x, p.y};
 }
 
-bocharov::point_t bocharov::movePoint(bocharov::point_t p, double x, double y)
+bocharov::point_t bocharov::movePoint(point_t p, double x, double y)
 {
   p.x += x;
   p.y += y;
   return {p.x, p.y};
 }
 
-bocharov::point_t bocharov::scalePoint(bocharov::point_t p, double ratio)
+bocharov::point_t bocharov::scalePoint(point_t p, double ratio)
 {
   p.x = p.x * ratio;
   p.y = p.y * ratio;
