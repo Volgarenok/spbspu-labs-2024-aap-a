0889755236a0780c72c17fa02c9ec4ac62475d4a
Beshimov03 <meylisbesimow44@gmail.com>
2025-02-20 01:59:22 +0300

    changed all files P2

--- beshimow.meylis/P2/main.cpp
+++ beshimow.meylis/P2/main.cpp
@@ -1,25 +1,36 @@
 #include <iostream>
+#include <iomanip>
 #include <cmath>
 #include "taylor.hpp"
 #include "output.hpp"
 
-using namespace beshimow;
+int main(int argc, char* argv[]) {
+    if (argc < 4) {
+        std::cerr << "Error: Not enough arguments. Required: start and end of interval, and number of terms.\n";
+        return 1;
+    }
 
-int main() {
-  double start = -0.5, end =
+    double start = atof(argv[1]);
+    double end = atof(argv[2]);
+    if (start >= end) {
+        std::cerr << "Error: Start of interval must be less than end.\n";
+        return 1;
+    }
 
-0.5;
-  size_t max_terms = 10;
-  double step = 0.1;
-  double error_threshold = 0.001;
+    int terms = atoi(argv[3]);
+    if (terms <= 0) {
+        std::cerr << "Error: Number of terms must be positive.\n";
+        return 1;
+    }
 
-  print_header();
-  for (double x = start; x <= end; x += step) {
-    double taylor_val = taylor_sin(x, max_terms, error_threshold);
-    double exact_val = std::sin(x);
-    print_line(x, taylor_val, exact_val, std::abs(taylor_val - exact_val) > error_threshold);
-  }
-  print_footer();
+    beshimow::print_header();
+    double step = (end - start) / terms;
+    for (double x = start; x <= end; x += step) {
+        double taylor_val = beshimow::taylor_sin(x, terms, 0.0001);
+        double exact_val = std::sin(x);
+        beshimow::print_line(x, taylor_val, exact_val, false);
+    }
+    beshimow::print_footer();
 
-  return 0;
+    return 0;
 }
