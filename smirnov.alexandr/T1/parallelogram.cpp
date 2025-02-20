8512c522d96a4847d8f71a4ef081ded547d0d9ba
A1exCRE <alsmmm2065@gmail.com>
2025-02-20 20:19:26 +0300

    fix the check

--- smirnov.alexandr/T1/parallelogram.cpp
+++ smirnov.alexandr/T1/parallelogram.cpp
@@ -9,10 +9,10 @@ smirnov::Parallelogram::Parallelogram(point_t p1, point_t p2, point_t p3):
   vertex3_{p3},
   vertex4_{p1.x + p3.x - p2.x, p1.y + p3.y - p2.y}
 {
-  bool areP1EqualP2 = (p1.x == p2.x && p1.y == p2.y);
-  bool areP1EqualP3 = (p1.x == p3.x && p1.y == p3.y);
-  bool areP2EqualP3 = (p2.x == p3.x && p2.y == p3.y);
-  if (areP1EqualP2 || areP1EqualP3 || areP2EqualP3)
+  bool condition = (p1.x == p2.x && p1.y == p2.y);
+  condition = condition || (p1.x == p3.x && p1.y == p3.y);
+  condition = condition || (p2.x == p3.x && p2.y == p3.y);
+  if (condition)
   {
     throw std::invalid_argument("Points must be unique");
   }
