08ed66077f1a4e447f9253b75601bf2437cfb322
cherkasoov <cherkasov5509@gmail.com>
2025-02-21 04:52:44 +0300

    .

--- cherkasov.nikita/T1/main.cpp
+++ cherkasov.nikita/T1/main.cpp
@@ -72,7 +72,16 @@ int main()
   std::cout << std::fixed << std::setprecision(1);
   std::cout << cherkasov::getSumArea(shapes, shapeCount);
   cherkasov::getCoordinates(shapes, shapeCount);
-  cherkasov::getScaling(shapes, shapeCount, p, scalingFactor);
+  try
+  {
+    cherkasov::getScaling(shapes, shapeCount, p, scalingFactor);
+  }
+  catch(const std::invalid_argument& e)
+  {
+    std::cerr << e.what() << "\n";
+    cherkasov::deleteShapes(shapes, shapeCount);
+    return 1;
+  }
   std::cout << cherkasov::getSumArea(shapes, shapeCount);
   cherkasov::getCoordinates(shapes, shapeCount);
   if (invalidInput)
