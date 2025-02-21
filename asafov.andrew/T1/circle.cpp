fe432674417b3cf7038ebf7292d7f878677497ba
Asafow Andrew <neforscist@gmail.com>
2025-02-21 09:18:29 +0300

    fix no.104

--- asafov.andrew/T1/circle.cpp
+++ asafov.andrew/T1/circle.cpp
@@ -1,5 +1,6 @@
 #include "circle.hpp"
 #include <cmath>
+#include <exceptions>
 #include "supportFunctions.hpp"
 
 #define pi std::atan(1.0) * 4
@@ -39,7 +40,7 @@ void asafov::Circle::scale(double scale)
 {
   if (scale <= 0)
   {
-    throw std::logic_error("incorect scale");
+    throw std::logic_error("incorrect figure");
   }
   rectangle_t rect = getFrameRect();
   scalePoint(center_, rect.pos, scale);
