636dc697365f7a8dfd378b58c7a2a318328d5793
KalmbahMihail <Kalmbahmihail@gmail.com>
2025-02-22 20:06:33 +0300

    Create taylor_functions.h

---
+++ kalmbah.mihail/P2/taylor_functions.h
@@ -0,0 +1,7 @@
+#ifndef TAYLOR_FUNCTIONS_H
+#define TAYLOR_FUNCTIONS_H
+
+double arcsinTaylor(double x, size_t n, double error);
+double arctanTaylor(double x, size_t n, double error);
+
+#endif // TAYLOR_FUNCTIONS_H
