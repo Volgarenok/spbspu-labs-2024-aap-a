f0df2ff03d15584348f18b16338770c0fac5d853
KalmbahMihail <Kalmbahmihail@gmail.com>
2025-02-22 21:36:18 +0300

    Added the connection of the cstddef library for the program to work correctly with the size_t data type.

--- kalmbah.mihail/P2/taylor_functions.h
+++ kalmbah.mihail/P2/taylor_functions.h
@@ -1,5 +1,6 @@
 #ifndef TAYLOR_FUNCTIONS_H
 #define TAYLOR_FUNCTIONS_H
+#include <cstddef>
 
 double arcsinTaylor(double x, size_t n, double error);
 double arctanTaylor(double x, size_t n, double error);
