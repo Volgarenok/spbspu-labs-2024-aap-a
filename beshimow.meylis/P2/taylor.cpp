27b7c8fa92669d566d129eac58cfaf2e1d44fbf0
Beshimov03 <meylisbesimow44@gmail.com>
2025-02-23 01:48:14 +0300

    changed P2 otstup

--- beshimow.meylis/P2/taylor.cpp
+++ beshimow.meylis/P2/taylor.cpp
@@ -4,15 +4,16 @@
 
 namespace beshimow {
 
-double taylor_sin(double x, size_t k, double error) {
+  double taylor_sin(double x, size_t k, double error)
+  {
   double sum = 0.0, term = x;
   for (size_t n = 1; n <= k; ++n) {
-    sum += term;
-    double next_term = -term * x * x / ((2 * n) * (2 * n + 1));
-    if (std::abs(next_term) < error) break;
-    term = next_term;
+  sum += term;
+  double next_term = -term * x * x / ((2 * n) * (2 * n + 1));
+  if (std::abs(next_term) < error) break;
+  term = next_term;
   }
-  return sum;
+ return sum;
 }
 
 }
