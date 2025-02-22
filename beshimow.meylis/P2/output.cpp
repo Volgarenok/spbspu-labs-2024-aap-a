6b33c2d8736b929a52bf54b8b0ad35883fba7e4a
Beshimov03 <meylisbesimow44@gmail.com>
2025-02-22 23:10:11 +0300

    changed P2 otstup

--- beshimow.meylis/P2/output.cpp
+++ beshimow.meylis/P2/output.cpp
@@ -6,21 +6,21 @@ namespace beshimow {
 
 void print_header()
 {
-  std::cout << std::left << std::setw(10) << "X"
-            << std::setw(20) << "Taylor Approximation"
-            << std::setw(20) << "Exact Value" << std::endl;
+ std::cout << std::left << std::setw(10) << "X"
+      << std::setw(20) << "Taylor Approximation"
+      << std::setw(20) << "Exact Value" << std::endl;
 }
 
 void print_footer()
 {
-  std::cout << "End of results." << std::endl;
+ std::cout << "End of results." << std::endl;
 }
 
 void print_line(double x, double taylor, double exact, bool error)
 {
-  std::cout << std::left << std::setw(10) << x
-            << std::setw(20) << (error ? "<MATH ERROR>" : std::to_string(taylor))
-            << std::setw(20) << std::to_string(exact) << std::endl;
+ std::cout << std::left << std::setw(10) << x
+      << std::setw(20) << (error ? "<MATH ERROR>" : std::to_string(taylor))
+      << std::setw(20) << std::to_string(exact) << std::endl;
 }
 
 }
