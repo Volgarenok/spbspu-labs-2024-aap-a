58ce099442dc200e356245b48c1ae68ff0b67dea
KalmbahMihail <Kalmbahmihail@gmail.com>
2025-02-23 14:22:26 +0300

    Fix main.cpp for cg 1.4

--- kalmbah.mihail/P2/main.cpp
+++ kalmbah.mihail/P2/main.cpp
@@ -2,9 +2,7 @@
 #include <cmath>
 #include <iomanip>
 #include <stdexcept>
-
 #include "taylor_functions.h"
-
 int main() {
     try {
         double start, end;
@@ -14,10 +12,8 @@ int main() {
             std::cerr << "Invalid interval!\n";
             return 1;
         }
-
         size_t maxIter = 10;
         double epsilon = 1e-6;
-
         std::cout << std::left << std::setw(10) << "x"
                   << std::setw(15) << "Arcsin(Taylor)"
                   << std::setw(15) << "Arctan(Taylor)"
@@ -25,17 +21,13 @@ int main() {
                   << std::setw(15) << "Arcsin(cmath)"
                   << std::setw(15) << "Arctan(cmath)"
                   << std::setw(15) << "Sum(cmath)\n";
-
         for (double x = start; x <= end; x += 0.1) {
             double arcsinT = arcsinTaylor(x, maxIter, epsilon);
             double arctanT = arctanTaylor(x, maxIter, epsilon);
-
             double arcsinC = std::asin(x);
             double arctanC = std::atan(x);
-
             double sumT = arcsinT + arctanT;
             double sumC = arcsinC + arctanC;
-
             std::cout << std::fixed << std::setprecision(6) << std::left << std::setw(10) << x
                       << std::setw(15) << arcsinT
                       << std::setw(15) << arctanT
@@ -44,7 +36,6 @@ int main() {
                       << std::setw(15) << arctanC
                       << std::setw(15) << sumC << "\n";
         }
-
         return 0;
     } catch (const std::exception &e) {
         std::cerr << "Error: " << e.what() << "\n";
