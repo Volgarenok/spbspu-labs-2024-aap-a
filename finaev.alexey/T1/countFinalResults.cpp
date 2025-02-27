59a51e001493743faa8d5a53c5d707208881be90
AlexeyFinaev02 <finaev0212212@mail.ru>
2025-02-27 20:03:34 +0300

    fix printframeRect

--- finaev.alexey/T1/countFinalResults.cpp
+++ finaev.alexey/T1/countFinalResults.cpp
@@ -3,7 +3,7 @@
 
 namespace
 {
-  void printOneRect(std::ostream& out, finaev::rectangle_t a)
+  void printOneRect(std::ostream& out, const finaev::rectangle_t& a)
   {
     double leftDownX = a.pos.x - (a.width / 2);
     double leftDownY = a.pos.y - (a.height / 2);
@@ -22,10 +22,11 @@ double finaev::getSumArea(const Shape* const* shapes, size_t size)
   }
   return res;
 }
+
 void finaev::printFrameRect(std::ostream& out, const Shape* const* shapes, size_t size)
 {
   printOneRect(out, shapes[0]->getFrameRect());
-  for (size_t i = 0; i < size; ++i)
+  for (size_t i = 1; i < size; ++i)
   {
     out << " ";
     printOneRect(out, shapes[i]->getFrameRect());
