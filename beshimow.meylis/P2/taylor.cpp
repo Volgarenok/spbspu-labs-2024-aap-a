1af3d155a71c3b01023e2eaf4c35454332de7431
Beshimov03 <meylisbesimow44@gmail.com>
2025-02-28 04:34:48 +0300

    changed P2

--- beshimow.meylis/P2/taylor.cpp
+++ beshimow.meylis/P2/taylor.cpp
@@ -2,18 +2,26 @@
 #include <cmath>
 #include <stdexcept>
 
-namespace beshimow {
-
+namespace beshimow
+{
   double taylor_sin(double x, size_t k, double error)
   {
-  double sum = 0.0, term = x;
-  for (size_t n = 1; n <= k; ++n) {
-  sum += term;
-  double next_term = -term * x * x / ((2 * n) * (2 * n + 1));
-  if (std::abs(next_term) < error) break;
-  term = next_term;
-  }
- return sum;
-}
+    double sum = 0.0;
+    double term = x;
 
+    for (size_t n = 1; n <= k; ++n)
+    {
+      sum += term;
+      double nextTerm = -term * x * x / ((2 * n) * (2 * n + 1));
+
+      if (std::abs(nextTerm) < error)
+      {
+        break;
+      }
+
+      term = nextTerm;
+    }
+
+    return sum;
+  }
 }
