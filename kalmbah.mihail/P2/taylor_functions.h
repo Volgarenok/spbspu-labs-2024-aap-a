f97c3ca08fa51240d5f1dd4d8cc4b720dca9c572
KalmbahMihail <Kalmbahmihail@gmail.com>
2025-03-02 14:40:05 +0300

    New function factorial

--- kalmbah.mihail/P2/taylor_functions.h
+++ kalmbah.mihail/P2/taylor_functions.h
@@ -1,6 +1,7 @@
 #ifndef TAYLOR_FUNCTIONS_H
 #define TAYLOR_FUNCTIONS_H
 #include <cstddef>
+unsigned long long factorial(size_t n);
 double arcsinTaylor(double x, size_t n, double error);
 double arctanTaylor(double x, size_t n, double error);
 #endif
