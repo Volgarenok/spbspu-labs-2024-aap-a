e05824d3fc0b1f06e8a10ae3ab5fbf045084e16a
KalmbahMihail <Kalmbahmihail@gmail.com>
2025-03-02 16:41:30 +0300

    CG format

--- kalmbah.mihail/P2/taylor_functions.cpp
+++ kalmbah.mihail/P2/taylor_functions.cpp
@@ -1,16 +1,14 @@
 #include <cmath>
-#include <stdexcept>
 #include "taylor_functions.h"
-double arcsinTaylor(double x, size_t n, double error) {
+double arcsinTaylor(double x, size_t n, double error)
+{
   double sum = 0;
   for (size_t i = 0; i < n; ++i) {
     if (i == 0){
       sum += x;
-    }
-    else if (i == 1){
+    } else if (i == 1) {
       sum += (-2/4)*(pow(x,3)/3);
-    }
-    else{
+    } else {
       double p1 = (pow(-1, i) * factorial(2 * i)) / (pow(2, (2 * i)) * pow(factorial(i), 2));
       double p2 = pow(x, (2 * i + 1)) / (2 * i + 1);
       double element_summirovania = p1 * p2;
@@ -23,7 +21,11 @@ double arcsinTaylor(double x, size_t n, double error) {
   }
   return sum;
 }
-double arctanTaylor(double x, size_t n, double error) {
+double arctanTaylor(double x, size_t n, double error)
+{
+  if (x == 0.0) {
+     return 0.0;
+  }
   double sum = 0;
   double element_summirovania = 0;
   for (size_t i = 0; i < n; ++i) {
