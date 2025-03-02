90f16bf2d70478e9d028af6e7eb05f2437deb906
KalmbahMihail <Kalmbahmihail@gmail.com>
2025-03-02 16:40:36 +0300

    CG format

--- kalmbah.mihail/P2/factorial.cpp
+++ kalmbah.mihail/P2/factorial.cpp
@@ -1,5 +1,6 @@
 #include <cstddef>
-unsigned long long factorial(size_t n) {
+unsigned long long factorial(size_t n)
+{
   unsigned long long result = 1;
   for (size_t i = 1; i <= n; ++i) {
     result *= i;
