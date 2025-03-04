ff76ea453e5b9e125b1dba7a489fbb76084a1817
KalmbahMihail <Kalmbahmihail@gmail.com>
2025-03-05 00:37:11 +0300

    Get namespases for function

--- kalmbah.mihail/P2/taylor_functions.h
+++ kalmbah.mihail/P2/taylor_functions.h
@@ -1,7 +1,12 @@
 #ifndef TAYLOR_FUNCTIONS_H
 #define TAYLOR_FUNCTIONS_H
 #include <cstddef>
-unsigned long long factorial(size_t n);
-double arcsinTaylor(double x, size_t n, double error);
-double arctanTaylor(double x, size_t n, double error);
+
+namespace kalmbah
+{
+  unsigned long long factorial(size_t n);
+  double arcsinTaylor(double x, size_t n, double error);
+  double arctanTaylor(double x, size_t n, double error);
+}
+
 #endif
