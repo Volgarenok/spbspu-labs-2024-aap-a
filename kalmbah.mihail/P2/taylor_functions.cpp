6699eb0d6ad18ac4bb4e5e6528e14cab6cad8a2e
KalmbahMihail <Kalmbahmihail@gmail.com>
2025-03-05 22:53:49 +0300

    CG, std and const fix

--- kalmbah.mihail/P2/taylor_functions.cpp
+++ kalmbah.mihail/P2/taylor_functions.cpp
@@ -2,7 +2,8 @@
 #include <cstddef>
 #include <cmath>
 #include "taylor_functions.h"
-unsigned long long kalmbah::factorial(size_t n)
+
+unsigned long long kalmbah::getFactorial(size_t n)
 {
   unsigned long long result = 1;
   for (size_t i = 1; i <= n; ++i) {
@@ -10,36 +11,38 @@ unsigned long long kalmbah::factorial(size_t n)
   }
   return result;
 }
-double kalmbah::arcsinTaylor(double x, size_t n, double error)
+
+double kalmbah::getArcsinTaylor(double x, size_t n, double error)
 {
   double sum = 0;
   for (size_t i = 0; i < n; ++i) {
     if (i == 0){
       sum += x;
     } else if (i == 1) {
-      sum += (-2/4)*(pow(x,3)/3);
+      sum += (-2/4)*(std::pow(x,3)/3);
     } else {
-      double p1 = (pow(-1, i) * kalmbah::factorial(2 * i)) / (pow(2, (2 * i)) * pow(kalmbah::factorial(i), 2));
-      double p2 = pow(x, (2 * i + 1)) / (2 * i + 1);
-      double element_summirovania = p1 * p2;
-      sum += element_summirovania;
-      if (fabs(element_summirovania) < error) {
-        throw std::runtime_error("<MATH ERROR>");
-      }
+      double multiplire1 = (std::pow(-1, i) * kalmbah::getFactorial(2 * i)) / (std::pow(2, (2 * i)) * std::pow(kalmbah::getFactorial(i), 2));
+      double multiplire2 = std::pow(x, (2 * i + 1)) / (2 * i + 1);
+      double elementSummirovania = multiplire1 * multiplire2;
+      sum += elementSummirovania;
     }
+  }
+  if (std::fabs(sum) < error) {
+    throw std::runtime_error("<MATH ERROR>");
   }
   return sum;
 }
-double kalmbah::arctanTaylor(double x, size_t n, double error)
+
+double kalmbah::getArctanTaylor(double x, size_t n, double error)
 {
   double sum = 0;
-  double element_summirovania = 0;
+  double elementSummirovania;
   for (size_t i = 0; i < n; ++i) {
-    element_summirovania = pow(x, (2 * i + 1)) / (2 * i + 1);
-    sum += element_summirovania;
-    if (fabs(element_summirovania) < error) {
-      throw std::runtime_error("<MATH ERROR>");
-    }
+    elementSummirovania = std::pow(x, (2 * i + 1)) / (2 * i + 1);
+    sum += elementSummirovania;
+  }
+  if (std::fabs(sum) < error) {
+    throw std::runtime_error("<MATH ERROR>");
   }
   return sum;
 }
