47e7d9d32ac92647d037a493d13a2d32295505f7
guseynov.guseyn <ganx.gamer@gmail.com>
2025-03-10 19:42:06 +0300

    add individual variant. Test version

--- guseynov.guseyn/T1/square.cpp
+++ guseynov.guseyn/T1/square.cpp
@@ -1,10 +1,11 @@
 #include "square.hpp"
 
 #include <stdexcept>
+#include <cmath>
 
 guseynov::Square::Square(point_t leftLowP, double length):
-  leftLowP_(leftLowP),
-  length_(length)
+  n_{determinateNum(length)},
+  rectangleArray_{createArrayRectangle(leftLowP, length)}
 {
   if (length <= 0)
   {
@@ -12,38 +13,112 @@ guseynov::Square::Square(point_t leftLowP, double length):
   }
 }
 
+guseynov::Square::~Square()
+{
+  clear(n_);
+}
+
 double guseynov::Square::getArea() const
 {
-  return length_ * length_;
+  double area = 0;
+  for (size_t i = 0; i < n_; i++)
+  {
+    area += rectangleArray_[i]->getArea();
+  }
+  return area;
 }
 
 guseynov::rectangle_t guseynov::Square::getFrameRect() const
 {
-  return {length_, length_, {leftLowP_.x + (length_ / 2), leftLowP_.y + (length_ / 2)}};
+  double length = std::sqrt(guseynov::Square::getArea());
+  point_t leftLowP = rectangleArray_[0]->getFrameRect().pos;
+  return {length, length, {leftLowP.x + (length / 2), leftLowP.y + (length / 2)}};
 }
 
 void guseynov::Square::move(point_t pos)
 {
-  assigment({pos.x - (length_ / 2), pos.y - (length_ / 2)});
+  double length = std::sqrt(guseynov::Square::getArea());
+  assigment({pos.x - (length / 2), pos.y - (length / 2)});
 }
 
 void guseynov::Square::move(double x, double y)
 {
-  assigment({leftLowP_.x + x, leftLowP_.y + y});
+  point_t leftLowP = rectangleArray_[0]->getFrameRect().pos;
+  assigment({leftLowP.x + x, leftLowP.y + y});
 }
 
 void guseynov::Square::scaleWithoutCheck(double k)
 {
-  assigment({leftLowP_.x - (length_ * k - length_) / 2, leftLowP_.y - (length_ * k - length_) / 2});
-  length_ *= k;
+  double length = std::sqrt(guseynov::Square::getArea());
+  point_t leftLowP = rectangleArray_[0]->getFrameRect().pos;
+  assigmentPlusLength({leftLowP.x - (length * k - length) / 2, leftLowP.y - (length * k - length) / 2}, length * k);
 }
 
 void guseynov::Square::assigment(point_t leftLowP)
 {
-  leftLowP_ = leftLowP;
+  double length = std::sqrt(guseynov::Square::getArea());
+  clear(n_);
+  rectangleArray_ = createArrayRectangle(leftLowP, length);
+}
+
+void guseynov::Square::assigmentPlusLength(point_t leftLowP, double length)
+{
+  clear(n_);
+  rectangleArray_ = createArrayRectangle(leftLowP, length);
 }
 
 guseynov::Shape * guseynov::Square::clone() const
 {
-  return new Square(leftLowP_, length_);
+  double length = std::sqrt(guseynov::Square::getArea());
+  point_t leftLowP = rectangleArray_[0]->getFrameRect().pos;
+  return new Square(leftLowP, length);
+}
+
+size_t guseynov::Square::determinateNum(double squareLength)
+{
+  size_t n = 0;
+  while(squareLength * squareLength > 0.01)
+  {
+    n += 4;
+    squareLength = squareLength / 3;
+  }
+  return n;
+}
+
+guseynov::Rectangle ** guseynov::Square::createArrayRectangle(guseynov::point_t lower, double squareLength)
+{
+  static size_t n = 0;
+  static guseynov::Rectangle ** rectangleArray;
+  if (n == 0)
+  {
+    rectangleArray = new guseynov::Rectangle*[n_];
+  }
+  if (n_ != n)
+  {
+    double oneThirdLength = squareLength / 3;
+    double twoThirdLength = squareLength * 2 / 3;
+    try
+    {
+      rectangleArray[n++] = new Rectangle(lower, {lower.x + oneThirdLength, lower.y + twoThirdLength});
+      rectangleArray[n++] = new Rectangle({lower.x, lower.y + twoThirdLength}, {lower.x + twoThirdLength, lower.y + squareLength});
+      rectangleArray[n++] = new Rectangle({lower.x + twoThirdLength, lower.y + oneThirdLength}, {lower.x + squareLength, lower.y + squareLength});
+      rectangleArray[n++] = new Rectangle({lower.x + oneThirdLength, lower.y}, {lower.x + squareLength, lower.y + oneThirdLength});
+    }
+    catch(const std::bad_alloc &)
+    {
+      clear(n);
+      throw;
+    }
+    createArrayRectangle({lower.x + oneThirdLength, lower.y + oneThirdLength}, oneThirdLength);
+  }
+  return rectangleArray;
+}
+
+void guseynov::Square::clear(size_t count)
+{
+  for (size_t i = 0; i < count; i++)
+  {
+    delete rectangleArray_[i];
+  }
+  delete[] rectangleArray_;
 }
--- guseynov.guseyn/T1/square.hpp
+++ guseynov.guseyn/T1/square.hpp
@@ -1,7 +1,10 @@
 #ifndef SQUARE_HPP
 #define SQUARE_HPP
 
+#include <cstddef>
+
 #include "shape.hpp"
+#include "rectangle.hpp"
 
 namespace guseynov
 {
@@ -9,6 +12,7 @@ namespace guseynov
   {
   public:
     Square(point_t leftLowP, double length);
+    ~Square() override;
     double getArea() const override;
     rectangle_t getFrameRect() const override;
     void move(point_t pos) override;
@@ -16,9 +20,13 @@ namespace guseynov
     void scaleWithoutCheck(double k) override;
     Shape * clone() const override;
   private:
-    point_t leftLowP_;
-    double length_;
+    size_t n_;
+    guseynov::Rectangle ** rectangleArray_;
     void assigment(point_t leftLowP);
+    Rectangle ** createArrayRectangle(point_t lower, double squareLength);
+    void assigmentPlusLength(point_t leftLowP, double length);
+    size_t determinateNum(double squareLength);
+    void clear(size_t count);
   };
 }
 
