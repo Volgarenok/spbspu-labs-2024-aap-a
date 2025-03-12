30e116f157e2cdbf5d622d4d604ff8cd262b7b6d
Durshl4k <belobrovtimofej@gmail.com>
2025-03-13 02:30:02 +0300

    .

--- belobrov.timofey/T1/rectangle.hpp
+++ belobrov.timofey/T1/rectangle.hpp
@@ -10,7 +10,7 @@ namespace belobrov
     Rectangle(const point_t& lowerLeft, const point_t& upperRight);
     double getArea() const override;
     rectangle_t getFrameRect() const override;
-    void move(point_t point) override;
+    void move(const point_t&  newPos) override;
     void move(double dx, double dy) override;
     void scale(double k) override;
   private:
