bba95372f8a49d30b6e864032fa798a92d99fe45
ProstoG-byte <asilda2019@gmail.com>
2025-03-12 23:05:06 +0300

    fixed CG

--- lungu.george/P3/processFixedMatrix.cpp
+++ lungu.george/P3/processFixedMatrix.cpp
@@ -2,12 +2,12 @@
 
 namespace lungu
 {
-	void processFixedSizeMatrix(const std::string &inputFilename, const std::string &outputFilename)
-	{
-		const int maxSize = 10000;
-		int matrix[maxSize];
-		auto [rows, cols] = readMatrixFromFile(inputFilename, matrix, maxSize);
-		transformMatrix(matrix, rows, cols);
-		writeMatrixToFile(outputFilename, matrix, rows, cols);
-	}
+  void processFixedSizeMatrix(const std::string &inputFilename, const std::string &outputFilename)
+  {
+    const int maxSize = 10000;
+    int matrix[maxSize];
+    auto [rows, cols] = readMatrixFromFile(inputFilename, matrix, maxSize);
+    transformMatrix(matrix, rows, cols);
+    writeMatrixToFile(outputFilename, matrix, rows, cols);
+  }
 }
