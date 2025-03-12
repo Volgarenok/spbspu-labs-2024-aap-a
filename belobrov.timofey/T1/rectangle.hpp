4242ad0ff957b485a97456515329a0656a840f4c
Durshl4k <belobrovtimofej@gmail.com>
2025-03-13 02:38:06 +0300

    .

--- belobrov.timofey/T1/rectangle.hpp
+++ belobrov.timofey/T1/rectangle.hpp
@@ -1,6 +1,7 @@
 #ifndef RECTANGLE_HPP
 #define RECTANGLE_HPP
 #include "shape.hpp"
+#include "base-types.hpp"
 
 namespace belobrov
 {
@@ -10,9 +11,9 @@ namespace belobrov
     Rectangle(const point_t& lowerLeft, const point_t& upperRight);
     double getArea() const override;
     rectangle_t getFrameRect() const override;
-    void move(const point_t&  newPos) override;
+    void move(const point_t&  point) override;
     void move(double dx, double dy) override;
-    void scale(double k) override;
+    void scale(double coefficient) override;
   private:
     point_t lowerLeft_;
     point_t upperRight_;
--- belobrov.timofey/T1/shape.hpp
+++ belobrov.timofey/T1/shape.hpp
@@ -11,7 +11,7 @@ namespace belobrov
     virtual ~Shape() = default;
     virtual double getArea() const = 0;
     virtual rectangle_t getFrameRect() const = 0;
-    virtual void move(point_t point) = 0;
+    virtual void move(const point_t& point) = 0;
     virtual void move(double dx, double dy) = 0;
     virtual void scale(double k) = 0;
   };
