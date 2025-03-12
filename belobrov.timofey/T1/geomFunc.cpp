1118170048b6447931181a08df6fa3ed2c836ff9
Durshl4k <belobrovtimofej@gmail.com>
2025-03-13 01:39:13 +0300

    add feuncs to geomFunc.cpp and hpp

--- belobrov.timofey/T1/geomFunc.cpp
+++ belobrov.timofey/T1/geomFunc.cpp
@@ -1,2 +1,23 @@
 #include "geomFunc.hpp"
+#include <iostream>
 
+namespace belobrov
+{
+  void deleteShapes(Shape** shapes, size_t count)
+  {
+    for (size_t i = 0; i < count; ++i) {
+      delete shapes[i];
+    }
+  }
+  void outputFrameCoordinates(Shape** shapes, size_t count)
+  {
+    for (size_t i = 0; i < count; ++i) {
+      rectangle_t frame = shapes[i]->getFrameRect();
+      if (i > 0) {
+        std::cout << " ";
+      }
+      std::cout << frame.pos.x - frame.width / 2 << " " << frame.pos.y - frame.height / 2;
+      std::cout << " " << frame.pos.x + frame.width / 2 << " " << frame.pos.y + frame.height / 2;
+    }
+  }
+}
--- belobrov.timofey/T1/geomFunc.hpp
+++ belobrov.timofey/T1/geomFunc.hpp
@@ -2,4 +2,10 @@
 #define GEOM_FUNC_HPP
 #include "shape.hpp"
 
+namespace belobrov
+{
+  void deleteShapes(Shape** shapes, size_t count);
+  void outputFrameCoordinates(Shape** shapes, size_t count);
+}
+
 #endif
