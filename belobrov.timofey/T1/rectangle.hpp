18f413b8e42d053117f7f8dc3f26def13971bce5
Durshl4k <belobrovtimofej@gmail.com>
2025-03-13 16:26:06 +0300

    .

--- belobrov.timofey/T1/rectangle.hpp
+++ belobrov.timofey/T1/rectangle.hpp
@@ -9,15 +9,14 @@ namespace belobrov
   class Rectangle : public Shape
   {
   public:
-    Rectangle(const point_t& lowerLeft, const point_t& upperRight);
+    Rectangle(double x1, double y1, double x2, double y2);
     double getArea() const override;
     rectangle_t getFrameRect() const override;
     void move(const point_t& point) override;
     void move(double dx, double dy) override;
-    void scale(double coefficient) override;
+    void scale(double k) override;
   private:
-    point_t lowerLeft_;
-    point_t upperRight_;
+    double x1_, y1_, x2_, y2_;
   };
 }
 
