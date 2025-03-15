6615207273f834da9e51fc9582ec8c03a4feb158
Beshimov03 <meylisbesimow44@gmail.com>
2025-03-16 01:10:59 +0300

    changed T1

--- beshimow.meylis/T1/main.cpp
+++ beshimow.meylis/T1/main.cpp
@@ -6,18 +6,21 @@ int main()
   beshimow::point_t pos = {0.0, 0.0};
   beshimow::Rectangle rect(4.0, 5.0, pos);
 
-  std::cout << "Rectangle area: " << rect.getArea() << "\n";
+  beshimow::Shape* shape = &rect;
 
-  beshimow::rectangle_t frame = rect.getFrameRect();
+  std::cout << "Rectangle area: " << shape->getArea() << "\n";
+
+  beshimow::rectangle_t frame = shape->getFrameRect();
   std::cout << "Frame rect - Width: " << frame.width
             << ", Height: " << frame.height
             << ", Center: (" << frame.pos.x << ", " << frame.pos.y << ")\n";
 
-  rect.move({2.0, 3.0});
+  shape->move({2.0, 3.0});
+  frame = shape->getFrameRect();
   std::cout << "Moved to (" << frame.pos.x << ", " << frame.pos.y << ")\n";
 
-  rect.scale(2.0);
-  std::cout << "Scaled area: " << rect.getArea() << "\n";
+  shape->scale(2.0);
+  std::cout << "Scaled area: " << shape->getArea() << "\n";
 
   return 0;
 }
