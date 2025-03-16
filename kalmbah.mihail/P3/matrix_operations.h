330006740748d4fe22ea85c894f75f55ad686e9e
KalmbahMihail <Kalmbahmihail@gmail.com>
2025-03-16 15:57:16 +0300

    Rm P3 from P2

--- kalmbah.mihail/P3/matrix_operations.h
+++
@@ -1,8 +0,0 @@
-#ifndef MATRIX_OPERATIONS_H
-#define MATRIX_OPERATIONS_H
-
-#include <vector>
-
-std::vector<std::vector<int>> createSymmetricMatrices(const std::vector<std::vector<int>>& matrix);
-
-#endif
