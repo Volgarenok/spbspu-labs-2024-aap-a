ed6d174da869cee68d745f2c190f64a77c158296
Maksim_Shramko <maxramko@gmail.com>
2025-03-17 10:19:43 +0300

    removing space

--- shramko.maksim/T1/main.cpp
+++ shramko.maksim/T1/main.cpp
@@ -105,6 +105,7 @@ void shramko::printRectangleShapes(Shape** shapes, size_t size)
     auto r = shapes[i]->getFrameRect();
     r.print();
   }
+  std::cout << "\b";
 }
 
 void shramko::scaleShapes(Shape **shapes, size_t size, double k, point_t new_center)
