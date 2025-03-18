80b3313f08412e4d7b91449b088457eb779dd259
evstyunichev.konstantin <korombodis.gmail.com>
2025-03-18 21:17:56 +0300

    typo fix

--- evstyunichev.konstantin/T1/specific_output.cpp
+++ evstyunichev.konstantin/T1/specific_output.cpp
@@ -8,7 +8,7 @@ void evstyunichev::frameOutput(rectangle_t a, std::ostream &out)
 {
   double data[4]{ (a.pos.x  - a.width * 0.5), (a.pos.y - a.height * 0.5), (a.pos.x + a.width * 0.5), (a.pos.y + a.height * 0.5) };
   out << std::fixed << std::setprecision(1) << data[0];
-  for(size_t i = 1; i < 4; i++)
+  for (size_t i = 1; i < 4; i++)
   {
     out << " " << data[i];
   }
@@ -20,7 +20,7 @@ std::ostream & evstyunichev::framesOut(CompositeShape &cmp, std::ostream &out)
   frameOutput(cmp[0]->getFrameRect());
   for (size_t i = 1; i < cmp.size(); i++)
   {
-    out << ' ';
+    out << " ";
     frameOutput(cmp[i]->getFrameRect());
   }
   return out;
