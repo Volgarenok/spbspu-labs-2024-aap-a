e3de078f58d4bf08f0ab34da63578a21c698c6a1
root <root@kryloff.>
2025-03-19 19:19:10 +0300

    While fixed

--- krylov.matvey/T1/main.cpp
+++ krylov.matvey/T1/main.cpp
@@ -34,14 +34,6 @@ int main()
       }
       else if (shapeType == "SCALE")
       {
-        if (shapeCount == 0)
-        {
-          std::cerr << "Error: Nothing to scale\n";
-          return 1;
-        }
-        krylov::printInfoAboutShapes(shapes, shapeCount);
-        krylov::isoScale(std::cin, shapes, shapeCount);
-        krylov::printInfoAboutShapes(shapes, shapeCount);
         scaleCommandProcessed = true;
         break;
       }
@@ -56,7 +48,14 @@ int main()
       std::cin.ignore(std::numeric_limits< std::streamsize >::max(), '\n');
     }
   }
-
+  if (shapeCount == 0)
+  {
+    std::cerr << "Error: Nothing to scale\n";
+    return 1;
+  }
+  krylov::printInfoAboutShapes(shapes, shapeCount);
+  krylov::isoScale(std::cin, shapes, shapeCount);
+  krylov::printInfoAboutShapes(shapes, shapeCount);
   if (!scaleCommandProcessed)
   {
     krylov::deleteShapes(shapes, shapeCount);
