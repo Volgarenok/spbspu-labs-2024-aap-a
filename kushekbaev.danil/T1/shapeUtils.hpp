da96162f26b88519abc438bd5909829d2c997a22
Z-1337 <un228der@gmail.com>
2025-03-19 21:28:21 +0300

    unconsting some things in shapeManipulations and shapeUtils

--- kushekbaev.danil/T1/shapeUtils.hpp
+++ kushekbaev.danil/T1/shapeUtils.hpp
@@ -8,8 +8,8 @@ namespace kushekbaev
 {
   bool isTriangle(point_t first, point_t second, point_t third);
   bool isPointInsideTriangle(point_t first, point_t second, point_t third, point_t last);
-  void movePoints(point_t** points, const size_t size, const double dx, const double dy);
-  void scalePoints(point_t** points, const size_t size, const double scaleCoeff, const point_t mid);
+  void movePoints(point_t** points, size_t size, double dx, double dy);
+  void scalePoints(point_t** points, size_t size, double scaleCoeff, point_t mid);
   double getLineLength(point_t first, point_t second);
   double getAreaOfTriangle(point_t first, point_t second, point_t third);
 }
