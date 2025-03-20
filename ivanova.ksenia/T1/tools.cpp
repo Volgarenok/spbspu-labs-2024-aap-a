514be2788ba38defe1aace7a06f5bd644fef6441
Robotophile <ksyunya.iv@gmail.com>
2025-03-20 15:20:23 +0300

    meh

--- ivanova.ksenia/T1/tools.cpp
+++ ivanova.ksenia/T1/tools.cpp
@@ -67,7 +67,7 @@ void ivanova::scale(Shape** shps, size_t size, point_t scalePoint, double k)
     if (shps[i] != nullptr)
     {
       ivanova::point_t begin = shps[i]->getFrameRect().pos;
-      ivanova::point_t offset = {k * (begin.x - scalePoint.x),k * (begin.y - scalePoint.y)};
+      ivanova::point_t offset = {scalePoint.x + k * (begin.x - scalePoint.x),scalePoint.y + k * (begin.y - scalePoint.y)};
       shps[i]->scale(k);
       shps[i]->move(offset);
     }
