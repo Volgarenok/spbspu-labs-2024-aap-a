401479afcb8211883e3394a158b0e4356288b71e
Robotophile <ksyunya.iv@gmail.com>
2025-03-21 13:49:28 +0300

    nooo

--- ivanova.ksenia/T1/complexquad.cpp
+++ ivanova.ksenia/T1/complexquad.cpp
@@ -1,6 +1,40 @@
 #include "complexquad.hpp"
 #include "tools.hpp"
 #include <cmath>
+namespace
+{
+  double getLengthSide(ivanova::point_t point1, ivanova::point_t point2)
+  {
+    return std::sqrt(std::pow(point2.x - point1.x, 2) + std::pow(point2.y - point1.y, 2));
+  }
+  bool isTriangle(ivanova::point_t point1, ivanova::point_t point2, ivanova::point_t point3)
+  {
+    const double length1 = getLengthSide(point1, point2);
+    const double length2 = getLengthSide(point1, point3);
+    const double length3 = getLengthSide(point3, point2);
+    return length1 < length2 + length3 || length2 < length1 + length3 || length3 < length1 + length2;
+  }
+  ivanova::point_t findP(ivanova::point_t p1, ivanova::point_t p2, ivanova::point_t p3, ivanova::point_t p4,
+    bool& isIntersect)
+  {
+    const double a1 = p2.y - p1.y;
+    const double b1 = p2.x - p1.x;
+    const double c1 = p1.x * a1 + p1.y * b1;
+    const double a2 = p4.y - p3.y;
+    const double b2 = p4.x - p3.x;
+    const double c2 = p3.x * a2 + p3.y * b2;
+    const double determiner = a1 * b2 - b1 * a2;
+    if (determiner == 0)
+    {
+      isIntersect = false;
+      return { 0, 0 };
+    }
+    isIntersect = true;
+    const double x = (c1 * b2 - c2 * b1) / determiner;
+    const double y = (a2 * a1 - a2 * c1) / determiner;
+    return { x, y };
+  }
+}
 
 ivanova::Complexquad::Complexquad(point_t point1, point_t point2, point_t point3, point_t point4):
   point1_(point1),
@@ -8,7 +42,10 @@ ivanova::Complexquad::Complexquad(point_t point1, point_t point2, point_t point3
   point3_(point3),
   point4_(point4)
 {
-  if (!ivanova::areSegmentsIntersecting(point1_, point2_, point3_, point4_))
+  bool isIntersect = true;
+  point_t centre = findP(point1, point2, point3, point4, isIntersect);
+  if (isTriangle(point1, point4, centre) || !isTriangle(point2, point3, centre) || !isIntersect ||
+  (point4.x > point1.x && point3.x > point1.x))
   {
     throw std::invalid_argument("Segments do not intersect");
   }
