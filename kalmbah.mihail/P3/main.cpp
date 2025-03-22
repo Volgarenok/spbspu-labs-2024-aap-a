88cc9bf00cb8be0b0e115d0927209f3ed3c66c63
KalmbahMihail <Kalmbahmihail@gmail.com>
2025-03-22 22:05:07 +0300

    4rd try empty array reaction

--- kalmbah.mihail/P3/main.cpp
+++ kalmbah.mihail/P3/main.cpp
@@ -33,6 +33,12 @@ int main(int argc, char** argv)
 
   input >> cols >> rows;
 
+  if (input.fail())
+  {
+    std::cerr << "Empty file or invalid matrix size\n";
+    return 2;
+  }
+
   if (cols == 0 || rows == 0)
   {
     std::cerr << "Empty file or invalid matrix size\n";
