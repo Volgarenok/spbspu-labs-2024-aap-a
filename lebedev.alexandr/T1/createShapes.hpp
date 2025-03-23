3d7de03de6a57b6a83833c891de8d8fcfb97e896
mokkenz1 <lam1600@mail.ru>
2025-03-23 15:31:41 +0300

    Little fix

--- lebedev.alexandr/T1/createShapes.hpp
+++ lebedev.alexandr/T1/createShapes.hpp
@@ -9,6 +9,7 @@ namespace lebedev
 {
   void sortPoints(point_t& p1, point_t& p2, point_t& p3);
   point_t chooseScalePoint(std::istream& input);
+  point_t readPoint(std::istream& input);
   Rectangle* createRectangle(std::istream& input);
   Diamond* createDiamond(std::istream& input);
   Concave* createConcave(std::istream& input);
