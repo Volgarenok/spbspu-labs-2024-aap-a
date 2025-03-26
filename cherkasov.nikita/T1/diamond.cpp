4f5201894fdff3679f14b8745360b00d5746c1ff
cherkasoov <cherkasov5509@gmail.com>
2025-03-26 21:39:38 +0300

    ..

--- cherkasov.nikita/T1/diamond.cpp
+++ cherkasov.nikita/T1/diamond.cpp
@@ -14,15 +14,13 @@ namespace cherkasov
         vertex1 = {x2, y2};
         vertex2 = {x3, y3};
       }
-      //else if ((x1 == x2 && y2 == y3) || (y1 == y2 && x2 == x3))
-      else if ((x2 == x3 && y2 == y1) || (y2 == y3 && x2 == x1))
+      else if ((x1 == x2 && y2 == y3) || (y1 == y2 && x2 == x3))
       {
         center = {x2, y2};
         vertex1 = {x1, y1};
         vertex2 = {x3, y3};
       }
-      //else if ((x3 == x2 && y1 == y3) || (y3 == y2 && x1 == x3))
-      else if ((x3 == x1 && y3 == y2) || (y3 == y1 && x3 == x2))
+      else if ((x3 == x2 && y1 == y3) || (y3 == y2 && x1 == x3))
       {
         center = {x3, y3};
         vertex1 = {x2, y2};
