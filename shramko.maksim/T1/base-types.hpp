75ea6d46882b48c128e43f89b17a736bd4abea88
Maksim_Shramko <maxramko@gmail.com>
2025-04-03 21:09:10 +0300

    Removing print

--- shramko.maksim/T1/base-types.hpp
+++ shramko.maksim/T1/base-types.hpp
@@ -15,19 +15,7 @@ namespace shramko
     double width;
     double height;
     point_t pos;
-    void print()
-    {
-      std::cout << pos.x - width / 2 << " " << pos.y - height / 2 << " ";
-      std::cout << pos.x + width / 2 << " " << pos.y + height / 2;
-    }
   };
-
-  inline std::ostream& operator<<(std::ostream& out, const rectangle_t& rect)
-  {
-    out << rect.pos.x - rect.width / 2 << " " << rect.pos.y - rect.height / 2 << " ";
-    out << rect.pos.x + rect.width / 2 << " " << rect.pos.y + rect.height / 2;
-    return out;
-  }
 }
 
 #endif
