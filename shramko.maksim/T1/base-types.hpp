d0354781afd975ba1f1fa51d0846c4cb90a3e219
Maksim_Shramko <maxramko@gmail.com>
2025-04-03 21:13:39 +0300

    Forgot about <<

--- shramko.maksim/T1/base-types.hpp
+++ shramko.maksim/T1/base-types.hpp
@@ -10,13 +10,19 @@ namespace shramko
     double x;
     double y;
   };
-  
   struct rectangle_t
   {
     double width;
     double height;
     point_t pos;
   };
+
+  inline std::ostream& operator<<(std::ostream& out, const rectangle_t& rect)
+  {
+    out << rect.pos.x - rect.width / 2 << " " << rect.pos.y - rect.height / 2 << " ";
+    out << rect.pos.x + rect.width / 2 << " " << rect.pos.y + rect.height / 2;
+    return out;
+  }
 }
 
 #endif
